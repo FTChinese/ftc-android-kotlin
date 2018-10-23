56e4412788decdb186dd7d2c2f39ceb3c736ee76
weiguo.ni <neefrankie@gmail.com>
2018-10-23 18:41:18 +0800

    rename fields

--- app/src/main/java/com/ft/ftchinese/models/Membership.kt
+++ app/src/main/java/com/ft/ftchinese/models/Membership.kt
@@ -8,27 +8,27 @@ import org.joda.time.format.ISODateTimeFormat
 data class Membership(
         val tier: String = TIER_FREE,
         val billingCycle: String = BILLING_YEARLY,
-        val startAt: String? = null,
-        // ISO8601 format. Example: 2019-08-05T07:19:41Z
-        val expireAt: String? = null
+        // ISO8601 format. Example: 2019-08-05
+        val expireDate: String? = null
 ) {
     /**
      * Compare expireAt against now.
      */
     val isExpired: Boolean
         get() {
-            val expire = expireAt ?: return true
+            val expire = expireDate ?: return true
 
             return DateTime.parse(expire, ISODateTimeFormat.dateTimeNoMillis()).isBeforeNow
         }
 
     val tierResId: Int
         get() = when(tier) {
-            TIER_STANDARD -> R.string.member_type_standard
-            TIER_PREMIUM -> R.string.member_type_premium
-            else -> R.string.member_type_free
+            TIER_STANDARD -> R.string.member_tier_standard
+            TIER_PREMIUM -> R.string.member_tier_premium
+            else -> R.string.member_tier_free
         }
 
+    // Use after clicked to buy a membership.
     val priceResId: Int
         get() = when(tier) {
             TIER_PREMIUM -> when (billingCycle) {
@@ -41,19 +41,7 @@ data class Membership(
             }
         }
 
-    val price: Int
-        get() = when(tier) {
-            TIER_PREMIUM -> PRICE_PREMIUM
-            else -> PRICE_STANDARD
-        }
 
-    // 2019-08-06
-    val localizedExpireDate: String?
-        get() {
-            val expire = expireAt ?: return null
-            val dateTime = DateTime.parse(expire, ISODateTimeFormat.dateTimeNoMillis())
-            return ISODateTimeFormat.date().print(dateTime)
-        }
 
     companion object {
         const val TIER_FREE = "free"
@@ -62,9 +50,6 @@ data class Membership(
 
         const val BILLING_YEARLY = "year"
         const val BILLING_MONTHLY = "month"
-
-        const val PRICE_STANDARD = 198
-        const val PRICE_PREMIUM = 1998
 
         const val PAYMENT_METHOD_ALI = 1
         const val PAYMENT_METHOD_WX = 2
