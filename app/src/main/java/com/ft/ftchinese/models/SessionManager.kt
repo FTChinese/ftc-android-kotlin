7916db4af10a64a5c4ee84bd8329d04d89c1ccbb
weiguo.ni <neefrankie@gmail.com>
2018-10-23 18:42:18 +0800

    rename fields

--- app/src/main/java/com/ft/ftchinese/models/SessionManager.kt
+++ app/src/main/java/com/ft/ftchinese/models/SessionManager.kt
@@ -6,11 +6,11 @@ import org.joda.time.format.ISODateTimeFormat
 
 private const val SESSION_PREF_NAME = "user_session"
 private const val PREF_ID = "id"
-private const val PREF_NAME = "name"
+private const val PREF_NAME = "user_name"
 private const val PREF_EMAIL = "email"
 private const val PREF_AVATAR = "avatar"
 private const val PREF_IS_VIP = "is_vip"
-private const val PREF_IS_VERIFIED = "verified"
+private const val PREF_IS_VERIFIED = "is_verified"
 private const val PREF_MEMBER_TYPE = "member_type"
 private const val PREF_MEMBER_EXPIRE = "member_expire"
 private const val PREF_IS_LOGGED_IN = "is_logged_in"
@@ -21,13 +21,13 @@ class SessionManager(context: Context) {
 
     fun saveUser(user: User) {
         editor.putString(PREF_ID, user.id)
-        editor.putString(PREF_NAME, user.name)
+        editor.putString(PREF_NAME, user.userName)
         editor.putString(PREF_EMAIL, user.email)
         editor.putString(PREF_AVATAR, user.avatar)
         editor.putBoolean(PREF_IS_VIP, user.isVip)
-        editor.putBoolean(PREF_IS_VERIFIED, user.verified)
+        editor.putBoolean(PREF_IS_VERIFIED, user.isVerified)
         editor.putString(PREF_MEMBER_TYPE, user.membership.tier)
-        editor.putString(PREF_MEMBER_EXPIRE, user.membership.expireAt)
+        editor.putString(PREF_MEMBER_EXPIRE, user.membership.expireDate)
 
         editor.putBoolean(PREF_IS_LOGGED_IN, true)
 
@@ -44,15 +44,15 @@ class SessionManager(context: Context) {
         val memberType = sharedPreferences.getString(PREF_MEMBER_TYPE, Membership.TIER_FREE)
         val expire = sharedPreferences.getString(PREF_MEMBER_EXPIRE, null)
 
-        val membership = Membership(tier = memberType, expireAt = expire)
+        val membership = Membership(tier = memberType, expireDate = expire)
 
         return User(
                 id = id,
-                name = name,
+                userName = name,
                 email = email,
                 avatar = avatar,
                 isVip = isVip,
-                verified = verified,
+                isVerified = verified,
                 membership = membership
         )
     }
