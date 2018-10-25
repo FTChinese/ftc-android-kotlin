e58d3ce5a012bb0794bacb7316718b980743c8ca
weiguo.ni <neefrankie@gmail.com>
2018-10-25 09:50:05 +0800

    rename keys and values

--- app/src/main/java/com/ft/ftchinese/models/SessionManager.kt
+++ app/src/main/java/com/ft/ftchinese/models/SessionManager.kt
@@ -5,13 +5,13 @@ import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
 
 private const val SESSION_PREF_NAME = "user_session"
-private const val PREF_ID = "id"
-private const val PREF_NAME = "user_name"
+private const val PREF_USER_ID = "id"
+private const val PREF_USER_NAME = "user_name"
 private const val PREF_EMAIL = "email"
-private const val PREF_AVATAR = "avatar"
+private const val PREF_AVATAR_URL = "avatar_url"
 private const val PREF_IS_VIP = "is_vip"
 private const val PREF_IS_VERIFIED = "is_verified"
-private const val PREF_MEMBER_TYPE = "member_type"
+private const val PREF_MEMBER_TIER = "member_tier"
 private const val PREF_MEMBER_EXPIRE = "member_expire"
 private const val PREF_IS_LOGGED_IN = "is_logged_in"
 
@@ -19,14 +19,14 @@ class SessionManager(context: Context) {
     private val sharedPreferences = context.getSharedPreferences(SESSION_PREF_NAME, Context.MODE_PRIVATE)
     private val editor = sharedPreferences.edit()
 
-    fun saveUser(user: User) {
-        editor.putString(PREF_ID, user.id)
-        editor.putString(PREF_NAME, user.userName)
+    fun saveUser(user: Account) {
+        editor.putString(PREF_USER_ID, user.id)
+        editor.putString(PREF_USER_NAME, user.userName)
         editor.putString(PREF_EMAIL, user.email)
-        editor.putString(PREF_AVATAR, user.avatar)
+        editor.putString(PREF_AVATAR_URL, user.avatarUrl)
         editor.putBoolean(PREF_IS_VIP, user.isVip)
         editor.putBoolean(PREF_IS_VERIFIED, user.isVerified)
-        editor.putString(PREF_MEMBER_TYPE, user.membership.tier)
+        editor.putString(PREF_MEMBER_TIER, user.membership.tier)
         editor.putString(PREF_MEMBER_EXPIRE, user.membership.expireDate)
 
         editor.putBoolean(PREF_IS_LOGGED_IN, true)
@@ -34,25 +34,25 @@ class SessionManager(context: Context) {
         editor.apply()
     }
 
-    fun loadUser(): User? {
-        val id = sharedPreferences.getString(PREF_ID, null) ?: return null
-        val name = sharedPreferences.getString(PREF_NAME, "")
+    fun loadUser(): Account? {
+        val userId = sharedPreferences.getString(PREF_USER_ID, null) ?: return null
+        val userName = sharedPreferences.getString(PREF_USER_NAME, "")
         val email = sharedPreferences.getString(PREF_EMAIL, "")
-        val avatar = sharedPreferences.getString(PREF_AVATAR, "")
+        val avatarUrl = sharedPreferences.getString(PREF_AVATAR_URL, "")
         val isVip = sharedPreferences.getBoolean(PREF_IS_VIP, false)
-        val verified = sharedPreferences.getBoolean(PREF_IS_VERIFIED, false)
-        val memberType = sharedPreferences.getString(PREF_MEMBER_TYPE, Membership.TIER_FREE)
-        val expire = sharedPreferences.getString(PREF_MEMBER_EXPIRE, null)
+        val isVerified = sharedPreferences.getBoolean(PREF_IS_VERIFIED, false)
+        val memberTier = sharedPreferences.getString(PREF_MEMBER_TIER, Membership.TIER_FREE)
+        val expireDate = sharedPreferences.getString(PREF_MEMBER_EXPIRE, null)
 
-        val membership = Membership(tier = memberType, expireDate = expire)
+        val membership = Membership(tier = memberTier, expireDate = expireDate)
 
-        return User(
-                id = id,
-                userName = name,
+        return Account(
+                id = userId,
+                userName = userName,
                 email = email,
-                avatar = avatar,
+                avatarUrl = avatarUrl,
                 isVip = isVip,
-                isVerified = verified,
+                isVerified = isVerified,
                 membership = membership
         )
     }
@@ -62,7 +62,7 @@ class SessionManager(context: Context) {
     }
 
     fun isPaidMember(): Boolean {
-        val type = sharedPreferences.getString(PREF_MEMBER_TYPE, null) ?: return false
+        val type = sharedPreferences.getString(PREF_MEMBER_TIER, null) ?: return false
 
         return type == Membership.TIER_STANDARD || type == Membership.TIER_PREMIUM
     }
@@ -70,7 +70,7 @@ class SessionManager(context: Context) {
     fun isMembershipExpired(): Boolean {
         val expireAt = sharedPreferences.getString(PREF_MEMBER_EXPIRE, null) ?: return true
 
-        return DateTime.parse(expireAt, ISODateTimeFormat.dateTimeNoMillis()).isBeforeNow
+        return DateTime.parse(expireAt, ISODateTimeFormat.date()).isBeforeNow
     }
 
     fun logout() {
