c045a5c85675f7f59b8ab5b45f07a2d94d0d39a1
weiguo.ni <neefrankie@gmail.com>
2018-10-27 20:28:59 +0800

    rename wx query order path; use serveo for test

--- app/src/main/java/com/ft/ftchinese/util/extensions.kt
+++ app/src/main/java/com/ft/ftchinese/util/extensions.kt
@@ -4,6 +4,10 @@ import android.app.Activity
 import android.content.Context
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
+import com.ft.ftchinese.R
+import com.google.gson.JsonSyntaxException
+import org.jetbrains.anko.toast
+import java.io.IOException
 
 fun Activity.getActiveNetworkInfo(): NetworkInfo {
     val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
@@ -32,5 +36,29 @@ fun Activity.isActiveNetworkMobile(): Boolean {
         ConnectivityManager.TYPE_MOBILE,
         ConnectivityManager.TYPE_MOBILE_DUN -> true
         else -> false
+    }
+}
+
+fun Activity.handleException(e: Exception) {
+    e.printStackTrace()
+    when (e) {
+        is IllegalStateException -> {
+            toast(R.string.api_empty_url)
+        }
+        is NetworkException -> {
+            toast(R.string.api_network_failure)
+        }
+        is EmptyResponseException -> {
+            toast(R.string.api_empty_response)
+        }
+        is IOException -> {
+            toast(R.string.api_io_error)
+        }
+        is JsonSyntaxException -> {
+            toast(R.string.api_json_syntax)
+        }
+        else -> {
+            toast(e.toString())
+        }
     }
 }
