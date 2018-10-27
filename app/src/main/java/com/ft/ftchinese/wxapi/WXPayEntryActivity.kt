6b3d4ec8e8ee7ebfec6c542febb9d002194eb906
weiguo.ni <neefrankie@gmail.com>
2018-10-27 20:29:54 +0800

    handle wxpay result

--- app/src/main/java/com/ft/ftchinese/wxapi/WXPayEntryActivity.kt
+++ app/src/main/java/com/ft/ftchinese/wxapi/WXPayEntryActivity.kt
@@ -3,21 +3,43 @@ package com.ft.ftchinese.wxapi
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.ft.ftchinese.BuildConfig
 import com.ft.ftchinese.R
+import com.ft.ftchinese.models.*
+import com.ft.ftchinese.util.handleException
 import com.tencent.mm.opensdk.constants.ConstantsAPI
 import com.tencent.mm.opensdk.modelbase.BaseReq
 import com.tencent.mm.opensdk.modelbase.BaseResp
 import com.tencent.mm.opensdk.openapi.IWXAPI
 import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler
 import com.tencent.mm.opensdk.openapi.WXAPIFactory
+import kotlinx.android.synthetic.main.activity_wx_pay_result.*
+import kotlinx.android.synthetic.main.progress_bar.*
 import kotlinx.android.synthetic.main.simple_toolbar.*
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.launch
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 
 class WXPayEntryActivity: AppCompatActivity(), IWXAPIEventHandler, AnkoLogger {
     private var api: IWXAPI? = null
+    private var job: Job? = null
+//    private var mAccount: Account? = null
+    private var mSession: SessionManager? = null
+
+    private var isInProgress: Boolean = false
+        set(value) {
+            if (value) {
+                progress_bar.visibility = View.VISIBLE
+                done_button.visibility = View.GONE
+            } else {
+                progress_bar.visibility = View.GONE
+                done_button.visibility = View.VISIBLE
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +50,10 @@ class WXPayEntryActivity: AppCompatActivity(), IWXAPIEventHandler, AnkoLogger {
 
         api = WXAPIFactory.createWXAPI(this, BuildConfig.WECAHT_APP_ID)
 
+//        mAccount = SessionManager.getInstance(this).loadUser()
+
+        mSession = SessionManager.getInstance(this)
+
         api?.handleIntent(intent, this)
     }
 
@@ -37,26 +63,134 @@ class WXPayEntryActivity: AppCompatActivity(), IWXAPIEventHandler, AnkoLogger {
         api?.handleIntent(intent, this)
     }
 
+    // Reference https://pay.weixin.qq.com/wiki/doc/api/app/app.php?chapter=8_5
     override fun onResp(resp: BaseResp?) {
         info("onPayFinish, errCode = ${resp?.errCode}")
 
         if (resp?.type == ConstantsAPI.COMMAND_PAY_BY_WX) {
             when (resp?.errCode) {
+                // 成功
+                // 展示成功页面
                 0 -> {
-                    toast("Payment done")
-                    // Query order
+                    verifyOrder()
                 }
+                // 错误
+                // 可能的原因：签名错误、未注册APPID、项目设置APPID不正确、注册的APPID与设置的不匹配、其他异常等。
                 -1 -> {
-                    toast("Payment error")
+                    result_tv.text = getString(R.string.wxpay_error)
                 }
+                // 用户取消
+                // 无需处理。发生场景：用户不支付了，点击取消，返回APP。
                 -2 -> {
-                    toast("User cancelled")
+                    result_tv.text = getString(R.string.wxpay_cancelled)
                 }
             }
         }
     }
 
+    private fun verifyOrder() {
+        val subs = Subscription.load(this) ?: return
+
+        val user = mSession?.loadUser() ?: return
+
+        isInProgress = true
+        job = launch(UI) {
+
+            try {
+                val payResult = user.wxQueryOrderAsync(subs.orderId).await()
+
+                isInProgress = false
+
+                subs.confirmedAt = payResult.paidAt
+
+                handleQueryResult(payResult, subs)
+
+            } catch (resp: ErrorResponse) {
+                isInProgress = false
+
+                handleApiErr(resp)
+            } catch (e: Exception) {
+                isInProgress = false
+                handleException(e)
+            }
+        }
+    }
+
+    private fun handleQueryResult(queryResult: WxQueryOrder, subs: Subscription) {
+
+        val currentMember = mSession?.loadUser()?.membership ?: return
+
+        var resultText = ""
+
+        when (queryResult.paymentState) {
+            // If payment success, update user sessions's member tier, expire date, billing cycle.
+            "SUCCESS" -> {
+                resultText = getString(R.string.wxpay_done)
+
+
+                val member = subs.updateMembership(currentMember)
+
+                updateSession(member)
+
+            }
+            "REFUND" -> {
+                resultText = getString(R.string.wxpay_refund)
+            }
+            "NOTPAY" -> {
+                resultText = getString(R.string.wxpay_not_paid)
+            }
+            "CLOSED" -> {
+                resultText = getString(R.string.wxpay_closed)
+            }
+            "REVOKED" -> {
+                resultText = getString(R.string.wxpay_revoked)
+            }
+            "USERPAYING" -> {
+                resultText = getString(R.string.wxpay_pending)
+            }
+            "PAYERROR" -> {
+                resultText = getString(R.string.wxpay_failed)
+            }
+        }
+
+        result_tv.text = resultText
+    }
+
+    private fun handleApiErr(resp: ErrorResponse) {
+        when (resp.statusCode) {
+            400 -> {
+                toast(R.string.api_bad_request)
+            }
+            401 -> {
+                toast(R.string.api_unauthorized)
+            }
+            404 -> {
+                toast(R.string.order_not_found)
+            }
+            422 -> {
+                toast(resp.message)
+            }
+            else -> {
+                toast(R.string.api_server_error)
+            }
+        }
+    }
+
+    private fun updateSession(membership: Membership) {
+        mSession?.updateMembership(membership)
+    }
+
     override fun onReq(req: BaseReq?) {
 
+    }
+
+    fun onClickDone(view: View) {
+        finish()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        job?.cancel()
     }
 }
