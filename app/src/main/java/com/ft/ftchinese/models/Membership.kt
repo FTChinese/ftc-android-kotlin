a379353f6da27cba73de16d68768a45b0bfe98f6
weiguo.ni <neefrankie@gmail.com>
2018-10-27 19:42:03 +0800

    rm TIER_FREE free and resource id

--- app/src/main/java/com/ft/ftchinese/models/Membership.kt
+++ app/src/main/java/com/ft/ftchinese/models/Membership.kt
@@ -1,47 +1,74 @@
 package com.ft.ftchinese.models
 
-import com.ft.ftchinese.R
 import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
 
 
 data class Membership(
-        val tier: String = TIER_FREE,
+        val tier: String,
         val billingCycle: String = BILLING_YEARLY,
         // ISO8601 format. Example: 2019-08-05
-        val expireDate: String? = null
+        val expireDate: String
 ) {
     /**
      * Compare expireAt against now.
      */
     val isExpired: Boolean
         get() {
-            val expire = expireDate ?: return true
+            if (expireDate.isBlank()) return true
 
-            return DateTime.parse(expire, ISODateTimeFormat.dateTimeNoMillis()).isBeforeNow
+            // If expire date is before now, it is expired;
+            // otherwise it is still valid.
+            return DateTime.parse(expireDate, ISODateTimeFormat.date()).isBeforeNow
         }
 
-    val tierResId: Int
-        get() = when(tier) {
-            TIER_STANDARD -> R.string.member_tier_standard
-            TIER_PREMIUM -> R.string.member_tier_premium
-            else -> R.string.member_tier_free
-        }
+    // Determine is renewal button is visible.
+    // Only check subscribed user.
+    val isRenewable: Boolean
+        get() {
 
-    // Use after clicked to buy a membership.
-    val priceResId: Int
-        get() = when(tier) {
-            TIER_PREMIUM -> R.string.price_premium_month
-            else -> when (billingCycle) {
-                BILLING_MONTHLY -> R.string.price_standard_month
-                else -> R.string.price_standard_annual
+            if (expireDate.isBlank()) return false
+
+            val expire = DateTime.parse(expireDate, ISODateTimeFormat.date())
+
+            return when (billingCycle) {
+                BILLING_YEARLY -> {
+                    return expire.isBefore(DateTime.now().plusYears(1).plusDays(1))
+                }
+                BILLING_MONTHLY -> {
+                    return expire.isBefore(DateTime.now().plusMonths(1).plusDays(1))
+                }
+                else -> false
             }
+
+        }
+
+    val isPaidMember: Boolean
+        get() {
+            return tier == Membership.TIER_STANDARD || tier == Membership.TIER_PREMIUM
         }
 
 
+    // Use the combination of tier and billing cycle to determine the price to show.
+    val priceKey: String
+        get() = "${tier}_$billingCycle"
+
+    fun extendedExpireDate(cycle: String): String {
+        val inst = DateTime.parse(expireDate, ISODateTimeFormat.date())
+
+        val newInst = when (cycle) {
+            BILLING_YEARLY -> inst.plusYears(1)
+            BILLING_MONTHLY -> inst.plusMonths(1)
+            else -> inst
+        }
+
+
+        return ISODateTimeFormat
+                .date()
+                .print(newInst)
+    }
 
     companion object {
-        const val TIER_FREE = "free"
         const val TIER_STANDARD = "standard"
         const val TIER_PREMIUM = "premium"
 
@@ -52,4 +79,6 @@ data class Membership(
         const val PAYMENT_METHOD_WX = 2
         const val PAYMENT_METHOD_STRIPE = 3
     }
+
 }
+
