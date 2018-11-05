75362ea042ea1b36b1081583f52d129d1515fb1c
weiguo.ni <neefrankie@gmail.com>
2018-11-05 19:23:04 +0800

    ChannelActivity no longer share fragment with ChannelFragment

--- app/src/main/java/com/ft/ftchinese/ChannelActivity.kt
+++ app/src/main/java/com/ft/ftchinese/ChannelActivity.kt
@@ -4,23 +4,77 @@ import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.ft.ftchinese.models.PagerTab
-import com.ft.ftchinese.models.SessionManager
+import android.support.v4.widget.SwipeRefreshLayout
+import android.view.View
+import awaitStringResponse
+import awaitStringResult
+import com.ft.ftchinese.models.*
+import com.ft.ftchinese.util.Store
 import com.ft.ftchinese.util.gson
+import com.ft.ftchinese.util.isNetworkConnected
+import com.github.kittinunf.fuel.Fuel
 import kotlinx.android.synthetic.main.activity_chanel.*
+import kotlinx.android.synthetic.main.progress_bar.*
+import kotlinx.android.synthetic.main.simple_toolbar.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
 
 /**
  * This is used to show a channel page, which consists of a list of article summaries.
  * It is similar to `MainActivity` execpt that it does not wrap a TabLayout.
  */
-class ChannelActivity : AppCompatActivity(), ChannelFragment.OnFragmentInteractionListener, ChannelWebViewClient.OnInAppNavigate, AnkoLogger {
+class ChannelActivity : AppCompatActivity(),
+        SwipeRefreshLayout.OnRefreshListener,
+        JSInterface.OnEventListener,
+        AnkoLogger {
 
+    // Passed from caller
+    private var mPageMeta: PagerTab? = null
+    // Passed from JS
+    private var mChannelItems: Array<ChannelItem>? = null
+    // Passed from JS
+    private var mChannelMeta: ChannelMeta? = null
+
+    private var mLoadJob: Job? = null
+    private var mRefreshJob: Job? = null
     private var mSession: SessionManager? = null
 
-    override fun getSession(): SessionManager? {
-        return mSession
+    // Content in raw/list.html
+    private var mTemplate: String? = null
+
+    private var isInProgress: Boolean = false
+        set(value) {
+            if (value) {
+                progress_bar.visibility = View.VISIBLE
+            } else {
+                progress_bar.visibility = View.GONE
+                swipe_refresh.isRefreshing = false
+            }
+        }
+
+    override fun onPageLoaded(channelDate: ChannelContent) {
+        mChannelItems = channelDate.sections[0].lists[0].items
+        mChannelMeta = channelDate.meta
+    }
+
+    override fun onSelectItem(index: Int) {
+        val channelMeta = mChannelMeta ?: return
+        val channelItem = mChannelItems?.getOrNull(index) ?: return
+
+        channelItem.channelTitle = channelMeta.title
+        channelItem.theme = channelMeta.theme
+        channelItem.adId = channelMeta.adid
+        channelItem.adZone = channelMeta.adZone
+
+        if (!channelItem.isMembershipRequired) {
+
+            return
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,44 +92,140 @@ class ChannelActivity : AppCompatActivity(), ChannelFragment.OnFragmentInteracti
          * Get the metadata for this page of articles
          */
         val data = intent.getStringExtra(EXTRA_LIST_PAGE_META)
-        val pageMeta = gson.fromJson<PagerTab>(data, PagerTab::class.java)
 
-        /**
-         * Set toolbar's title so that reader know where he is now.
-         */
-        toolbar.title = pageMeta.title
+        try {
+            val pageMeta = gson.fromJson<PagerTab>(data, PagerTab::class.java)
 
-        /**
-         * Begin to attach ChannelFragment to this activity
-         */
-        val sectionFragment = ChannelFragment.newInstance(pageMeta)
+            /**
+             * Set toolbar's title so that reader know where he is now.
+             */
+            toolbar.title = pageMeta.title
 
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, sectionFragment)
-                .commit()
+            web_view.apply {
+                addJavascriptInterface(
+                        JSInterface(pageMeta).apply {
+                            setOnEventListener(this@ChannelActivity)
+                        },
+                        JS_INTERFACE_NAME
+                )
+
+                webViewClient = ChannelWebViewClient(this@ChannelActivity)
+                webChromeClient = MyChromeClient()
+            }
+
+            mPageMeta = pageMeta
+            info("Initiating current page with data: $mPageMeta")
+
+            loadHtmlFragment(false)
+        } catch (e: Exception) {
+            info("$e")
+
+            return
+        }
     }
 
-    /**
-     * This is just a placeholder implementation to prevent `cannot cast` error
-     * If user enters this activity, there's no BottomNavigationView
-     */
-    override fun selectBottomNavItem(itemId: Int) {
+    private fun loadHtmlFragment(isRefresh: Boolean) {
+        if (!isNetworkConnected()) {
+            toast(R.string.prompt_no_network)
+            return
+        }
+        mLoadJob = GlobalScope.launch(Dispatchers.Main) {
+            if (mTemplate == null) {
+                mTemplate = Store.readChannelTemplate(resources) ?: return@launch
+            }
 
+            info("Template: $mTemplate")
+
+            if (!isRefresh) {
+                info("Show progress")
+                isInProgress = true
+            }
+
+            val url = mPageMeta?.contentUrl ?: return@launch
+
+            Fuel.get(url)
+                    .awaitStringResult()
+                    .fold(
+                            { data ->
+                                isInProgress = false
+
+                                val fullHtml = mPageMeta?.render(mTemplate, data)
+
+                                web_view.loadDataWithBaseURL(WEBVIEV_BASE_URL, fullHtml, "text/html", null, null)
+                            },
+                            { error ->
+                                isInProgress = false
+
+                                toast("Data not found")
+                            }
+                    )
+
+//            try {
+//
+//                val listFrag = mPageMeta?.crawlWeb()
+//
+//                isInProgress = false
+//
+//                info("List fragment: $listFrag")
+//
+//                if (listFrag == null) {
+//                    toast("No data found")
+//
+//                    return@launch
+//                }
+//
+//                val fullHtml = mPageMeta?.render(mTemplate, listFrag)
+//
+//                web_view.loadDataWithBaseURL(WEBVIEV_BASE_URL, fullHtml, "text/html", null, null)
+//
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//
+//                toast(e.toString())
+//            }
+
+        }
     }
 
-    override fun selectTabLayoutTab(tabIndex: Int) {
+    override fun onRefresh() {
+        toast(R.string.prompt_refreshing)
 
+        if (!isNetworkConnected()) {
+            toast(R.string.prompt_no_network)
+            return
+        }
+
+        if (mLoadJob?.isActive == true) {
+            mLoadJob?.cancel()
+        }
+
+        loadHtmlFragment(true)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mLoadJob?.cancel()
+        mRefreshJob?.cancel()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mLoadJob?.cancel()
+        mRefreshJob?.cancel()
     }
 
     /**
      * Launch this activity with intent
      */
     companion object {
+        private const val WEBVIEV_BASE_URL = "http://www.ftchinese.com"
         private const val EXTRA_LIST_PAGE_META = "extra_list_page_metadata"
 
         fun start(context: Context?, page: PagerTab) {
-            val intent = Intent(context, ChannelActivity::class.java)
-            intent.putExtra(EXTRA_LIST_PAGE_META, gson.toJson(page))
+            val intent = Intent(context, ChannelActivity::class.java).apply {
+                putExtra(EXTRA_LIST_PAGE_META, gson.toJson(page))
+            }
+
             context?.startActivity(intent)
         }
     }
