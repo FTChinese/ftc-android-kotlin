aa16ad4608d186865c7fd5306ff1991b26f3147f
weiguo.ni <neefrankie@gmail.com>
2018-11-13 19:18:08 +0800

    add price field to be recorded by firebase

--- app/src/main/java/com/ft/ftchinese/models/Subscription.kt
+++ app/src/main/java/com/ft/ftchinese/models/Subscription.kt
@@ -1,16 +1,19 @@
 package com.ft.ftchinese.models
 
 import android.content.Context
+import android.content.SharedPreferences
 import org.joda.time.DateTime
 import org.joda.time.format.ISODateTimeFormat
 
 data class AlipayOrder(
         val ftcOrderId: String,
+        val price: Double,
         val param: String
 )
 
 data class WxPrepayOrder(
         val ftcOrderId: String,
+        val price: Double,
         val appid: String,
         val partnerid: String,
         val prepayid: String,
@@ -45,21 +48,11 @@ data class Subscription(
         val orderId: String,
         val tierToBuy: String,
         val billingCycle: String,
-        val paymentMethod: Int,
+        val paymentMethod: String,
+        var apiPrice: Double,
         val createdAt: String = ISODateTimeFormat.dateTimeNoMillis().print(DateTime.now()),
         var confirmedAt: String = ""
 ) {
-    fun save(ctx: Context) {
-        val editor = ctx.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE).edit()
-
-        editor.putString(PREF_ORDER_ID, orderId)
-        editor.putString(PREF_TIER_TO_BUY, tierToBuy)
-        editor.putString(PREF_BILLING_CYCLE, billingCycle)
-        editor.putInt(PREF_PAYMENT_METHOD, paymentMethod)
-        editor.putString(PREF_CREATED_AT, createdAt)
-
-        editor.apply()
-    }
 
     private fun deduceExpireDate(): String {
         val inst = DateTime.parse(confirmedAt, ISODateTimeFormat.dateTimeNoMillis())
@@ -95,32 +88,70 @@ data class Subscription(
     }
 
     companion object {
-        private const val PREF_FILE_NAME = "subscription"
-        private const val PREF_ORDER_ID = "order_id"
-        private const val PREF_TIER_TO_BUY = "tier_to_buy"
-        private const val PREF_BILLING_CYCLE = "billing_cycle"
-        private const val PREF_PAYMENT_METHOD = "payment_method"
-        private const val PREF_CREATED_AT = "create_at"
+        const val PAYMENT_METHOD_ALI = "alipay"
+        const val PAYMENT_METHOD_WX = "tenpay"
+        const val PAYMENT_METHOD_STRIPE = "stripe"
+    }
+}
 
-        const val PAYMENT_METHOD_ALI = 1
-        const val PAYMENT_METHOD_WX = 2
-        const val PAYMENT_METHOD_STRIPE = 3
+private const val PREF_FILE_NAME = "subscription"
+private const val PREF_ORDER_ID = "order_id"
+private const val PREF_TIER_TO_BUY = "tier_to_buy"
+private const val PREF_BILLING_CYCLE = "billing_cycle"
+private const val PREF_PAYMENT_METHOD = "payment_method"
+private const val PREF_PRICE = "api_price"
+private const val PREF_CREATED_AT = "create_at"
 
-        fun load(ctx: Context): Subscription? {
-            val pref = ctx.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
-            val orderId = pref.getString(PREF_ORDER_ID, null) ?: return null
-            val tier = pref.getString(PREF_TIER_TO_BUY, "")
-            val cycle = pref.getString(PREF_BILLING_CYCLE, "")
-            val payMethod = pref.getInt(PREF_PAYMENT_METHOD, 0)
-            val createdAt = pref.getString(PREF_CREATED_AT, "")
+// https://stackoverflow.com/questions/16319237/cant-put-double-sharedpreferences
+fun SharedPreferences.Editor.putDouble(key: String, double: Double) =
+putLong(key, java.lang.Double.doubleToRawLongBits(double))
 
-            return Subscription(
-                    orderId = orderId,
-                    tierToBuy = tier,
-                    billingCycle = cycle,
-                    paymentMethod = payMethod,
-                    createdAt = createdAt
-            )
+fun SharedPreferences.getDouble(key: String, default: Double) =
+        java.lang.Double.longBitsToDouble(getLong(key, java.lang.Double.doubleToRawLongBits(default)))
+
+class OrderManager(context: Context) {
+    private val sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
+    private val editor = sharedPreferences.edit()
+
+    fun save(subs: Subscription) {
+
+        editor.putString(PREF_ORDER_ID, subs.orderId)
+        editor.putString(PREF_TIER_TO_BUY, subs.tierToBuy)
+        editor.putString(PREF_BILLING_CYCLE, subs.billingCycle)
+        editor.putString(PREF_PAYMENT_METHOD, subs.paymentMethod)
+        editor.putDouble(PREF_PRICE, subs.apiPrice)
+        editor.putString(PREF_CREATED_AT, subs.createdAt)
+
+        editor.apply()
+    }
+
+    fun load(): Subscription? {
+        val orderId = sharedPreferences.getString(PREF_ORDER_ID, null) ?: return null
+        val tier = sharedPreferences.getString(PREF_TIER_TO_BUY, "")
+        val cycle = sharedPreferences.getString(PREF_BILLING_CYCLE, "")
+        val payMethod = sharedPreferences.getString(PREF_PAYMENT_METHOD, "")
+        val price = sharedPreferences.getDouble(PREF_PRICE, 0.0)
+        val createdAt = sharedPreferences.getString(PREF_CREATED_AT, "")
+
+        return Subscription(
+                orderId = orderId,
+                tierToBuy = tier,
+                billingCycle = cycle,
+                paymentMethod = payMethod,
+                apiPrice = price,
+                createdAt = createdAt
+        )
+    }
+
+    companion object {
+        private var instance: OrderManager? = null
+
+        @Synchronized fun getInstance(ctx: Context): OrderManager {
+            if (instance == null) {
+                instance = OrderManager(ctx)
+            }
+
+            return instance!!
         }
     }
 }
