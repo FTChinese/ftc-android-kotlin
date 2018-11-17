00659d6e1fe91c7527a82f2f35067804b98f96e1
weiguo.ni <neefrankie@gmail.com>
2018-11-17 17:49:10 +0800

    tried to upgrade network status to api 28 but you cannot do so due to min api issues

--- app/src/main/java/com/ft/ftchinese/util/extensions.kt
+++ app/src/main/java/com/ft/ftchinese/util/extensions.kt
@@ -3,6 +3,8 @@ package com.ft.ftchinese.util
 import android.app.Activity
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
 import android.net.NetworkInfo
 import com.ft.ftchinese.R
 import com.google.gson.JsonSyntaxException
@@ -24,14 +26,20 @@ fun Activity.getActiveNetworkInfo(): NetworkInfo? {
     }
 }
 
+
+
 fun Activity.isNetworkConnected(): Boolean {
 
     return getActiveNetworkInfo()?.isConnected == true
 }
 
+
 fun Activity.isActiveNetworkWifi(): Boolean {
+
     val networkInfo = getActiveNetworkInfo()
 
+    // Ignore Android deprecation warning. It's very stupid since you cannot actually deprecate it
+    // unless you upgrade your min supported sdk, which will in return reduce your supported devices.
     return when (networkInfo?.type) {
         ConnectivityManager.TYPE_WIFI -> true
         else -> false
@@ -47,6 +55,48 @@ fun Activity.isActiveNetworkMobile(): Boolean {
         else -> false
     }
 }
+
+// Using API 28. Unfortunately it also requires that you must increase min supported api.
+//fun Activity.getConnectivityManager(): ConnectivityManager? {
+//    return try {
+//        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE)
+//        if (connectivityManager is ConnectivityManager) {
+//            return connectivityManager
+//        } else {
+//            null
+//        }
+//    } catch (e: Exception) {
+//        null
+//    }
+//}
+//
+//fun Activity.isUsinigWifi(): Boolean {
+//    return try {
+//        val connectivityManager = getConnectivityManager()
+//        // Call to activeNetwork throws error.
+//        // This requires API 23 which we cannot meet.
+//        val network = connectivityManager?.activeNetwork
+//
+//        val networkCapabilities = connectivityManager?.getNetworkCapabilities(network)
+//
+//        networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ?: false
+//
+//    } catch (e: Exception) {
+//        false
+//    }
+//}
+//
+//fun Activity.isUsingCellular(): Boolean {
+//    return try {
+//        val connectivityManager = getConnectivityManager()
+//        val network = connectivityManager?.activeNetwork
+//        val networkCapabilities = connectivityManager?.getNetworkCapabilities(network)
+//
+//        networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ?: false
+//    } catch (e: Exception) {
+//        false
+//    }
+//}
 
 fun Activity.handleException(e: Exception) {
     e.printStackTrace()
