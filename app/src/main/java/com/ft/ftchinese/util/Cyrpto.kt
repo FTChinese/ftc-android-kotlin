4868393f5afb1ae5324eb2a2ef5c1a97afe85172
weiguo.ni <neefrankie@gmail.com>
2018-12-14 18:16:26 +0800

    chores: add docs

--- app/src/main/java/com/ft/ftchinese/util/Cyrpto.kt
+++ app/src/main/java/com/ft/ftchinese/util/Cyrpto.kt
@@ -4,6 +4,11 @@ import java.security.SecureRandom
 
 val secureRandom = SecureRandom()
 
+/**
+ * Generate a secure random number and return it as hexdecimal string.
+ * @param size The number of bytes to use.
+ * @return A string whose length
+ */
 fun generateNonce(size: Int): String {
     val result = ByteArray(size)
     secureRandom.nextBytes(result)
