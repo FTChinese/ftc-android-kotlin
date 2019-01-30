8874d728f8e8d229483827a99ad0d0b1bf97de56
weiguo.ni <neefrankie@gmail.com>
2019-01-30 09:51:53 +0800

    refactor: using Tier and Cycle enum

--- app/src/main/java/com/ft/ftchinese/models/Subscription.kt
+++ app/src/main/java/com/ft/ftchinese/models/Subscription.kt
@@ -2,8 +2,14 @@ package com.ft.ftchinese.models
 
 import android.content.Context
 import android.content.SharedPreferences
-import org.joda.time.DateTime
-import org.joda.time.format.ISODateTimeFormat
+import com.beust.klaxon.Json
+import com.ft.ftchinese.util.formatISODateTime
+import com.ft.ftchinese.util.formatLocalDate
+import com.ft.ftchinese.util.parseISODateTime
+import com.ft.ftchinese.util.parseLocalDate
+import org.threeten.bp.LocalDate
+import org.threeten.bp.ZonedDateTime
+import org.threeten.bp.temporal.ChronoUnit
 
 data class AlipayOrder(
         val ftcOrderId: String,
@@ -44,63 +50,68 @@ data class AliVerifiedOrder(
         val paidAt: String // ISO8601
 )
 
+/**
+ * This is used to store subscription order locally,
+ * and also used to to parse orders retrieved from API.
+ */
 data class Subscription(
         val orderId: String,
-        val tierToBuy: String,
-        val billingCycle: String,
-        val paymentMethod: String,
-        var apiPrice: Double,
-        val createdAt: String = ISODateTimeFormat.dateTimeNoMillis().print(DateTime.now()),
-        var confirmedAt: String = ""
+        @KTier
+        val tier: Tier,
+        @KCycle
+        val cycle: Cycle,
+        var netPrice: Double,
+        @KPayMethod
+        val payMethod: PayMethod,
+        val createdAt: ZonedDateTime = ZonedDateTime.now(),
+        @Json(ignored = true)
+        var confirmedAt: ZonedDateTime? = null,
+        var startDate: LocalDate? = null,
+        var endDate: LocalDate? = null
 ) {
 
-    private fun deduceExpireDate(): String {
-        val inst = DateTime.parse(confirmedAt, ISODateTimeFormat.dateTimeNoMillis())
+    /**
+     * Confirm a subscription and return the new Membership.
+     */
+    fun confirm(m: Membership): Membership {
+        val now = ZonedDateTime.now()
+                .truncatedTo(ChronoUnit.SECONDS)
+        val today = now.toLocalDate()
 
-        val newInst = when (tierToBuy) {
-            Membership.CYCLE_YEAR -> inst.plusYears(1)
-            Membership.CYCLE_MONTH -> inst.plusMonths(1)
-            else -> inst
+        // If current membership does not have expire date,
+        // it means user is not member currently, use
+        // today as startDate;
+        val start = if (m.expireDate == null) {
+            today
+        } else {
+            // If user is already a member, but it is expired, use today
+            if (m.expireDate.isBefore(today)) {
+                today
+            } else {
+                // Membership is not expired yet, use previous membership's expiration date as next membership's start date.
+                m.expireDate
+            }
         }
 
-        return ISODateTimeFormat
-                .date()
-                .print(newInst)
-    }
 
-    // Create new or extended membership based on the current one.
-    fun updateMembership(currentMember: Membership): Membership {
-        // If user is renewing, add billing cycle to the current expire date
-        if (currentMember.isRenewable) {
-            return Membership(
-                    tier = tierToBuy,
-                    billingCycle = billingCycle,
-                    expireDate = currentMember.extendedExpireDate(billingCycle)
-            )
-        }
+        confirmedAt = now
+        startDate = start
+        endDate = cycle.endDate(start)
 
-        // If this is a new subscription, expire date is the payment confirmation time plus billing cycle.
-        return Membership(
-                tier = tierToBuy,
-                billingCycle = billingCycle,
-                expireDate = deduceExpireDate()
-        )
-    }
-
-    companion object {
-        const val PAYMENT_METHOD_ALI = "alipay"
-        const val PAYMENT_METHOD_WX = "tenpay"
-        const val PAYMENT_METHOD_STRIPE = "stripe"
+        return Membership(tier = tier, cycle = cycle, expireDate = endDate)
     }
 }
 
 private const val PREF_FILE_NAME = "subscription"
 private const val PREF_ORDER_ID = "order_id"
-private const val PREF_TIER_TO_BUY = "tier_to_buy"
-private const val PREF_BILLING_CYCLE = "billing_cycle"
-private const val PREF_PAYMENT_METHOD = "payment_method"
-private const val PREF_PRICE = "api_price"
+private const val PREF_TIER_TO_BUY = "tier"
+private const val PREF_BILLING_CYCLE = "cycle"
+private const val PREF_PAYMENT_METHOD = "pay_method"
+private const val PREF_PRICE = "net_price"
 private const val PREF_CREATED_AT = "create_at"
+private const val PREF_CONFIRMED_AT = "confirmed_at"
+private const val PREF_START_DATE = "start_date"
+private const val PREF_END_DATE = "end_date"
 
 // https://stackoverflow.com/questions/16319237/cant-put-double-sharedpreferences
 fun SharedPreferences.Editor.putDouble(key: String, double: Double) =
@@ -109,6 +120,9 @@ putLong(key, java.lang.Double.doubleToRawLongBits(double))
 fun SharedPreferences.getDouble(key: String, default: Double) =
         java.lang.Double.longBitsToDouble(getLong(key, java.lang.Double.doubleToRawLongBits(default)))
 
+/**
+ * Save and load subscription detail to share preferences.
+ */
 class OrderManager(context: Context) {
     private val sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
     private val editor = sharedPreferences.edit()
@@ -116,30 +130,45 @@ class OrderManager(context: Context) {
     fun save(subs: Subscription) {
 
         editor.putString(PREF_ORDER_ID, subs.orderId)
-        editor.putString(PREF_TIER_TO_BUY, subs.tierToBuy)
-        editor.putString(PREF_BILLING_CYCLE, subs.billingCycle)
-        editor.putString(PREF_PAYMENT_METHOD, subs.paymentMethod)
-        editor.putDouble(PREF_PRICE, subs.apiPrice)
-        editor.putString(PREF_CREATED_AT, subs.createdAt)
+        editor.putString(PREF_TIER_TO_BUY, subs.tier.string())
+        editor.putString(PREF_BILLING_CYCLE, subs.cycle.string())
+        editor.putString(PREF_PAYMENT_METHOD, subs.payMethod.string())
+        editor.putDouble(PREF_PRICE, subs.netPrice)
+        editor.putString(PREF_CREATED_AT, formatISODateTime(subs.createdAt))
+        editor.putString(PREF_CONFIRMED_AT, formatISODateTime(subs.confirmedAt))
+        editor.putString(PREF_START_DATE, formatLocalDate(subs.startDate))
+        editor.putString(PREF_END_DATE, formatLocalDate(subs.endDate))
 
         editor.apply()
     }
 
     fun load(): Subscription? {
         val orderId = sharedPreferences.getString(PREF_ORDER_ID, null) ?: return null
-        val tier = sharedPreferences.getString(PREF_TIER_TO_BUY, "")
-        val cycle = sharedPreferences.getString(PREF_BILLING_CYCLE, "")
-        val payMethod = sharedPreferences.getString(PREF_PAYMENT_METHOD, "")
+        val t = sharedPreferences.getString(PREF_TIER_TO_BUY, null)
+        val c = sharedPreferences.getString(PREF_BILLING_CYCLE, null)
+        val pm = sharedPreferences.getString(PREF_PAYMENT_METHOD, null)
         val price = sharedPreferences.getDouble(PREF_PRICE, 0.0)
-        val createdAt = sharedPreferences.getString(PREF_CREATED_AT, "")
+        val created = sharedPreferences.getString(PREF_CREATED_AT, null) ?: return null
+        val confirmed = sharedPreferences.getString(PREF_CONFIRMED_AT, null)
+        val start = sharedPreferences.getString(PREF_START_DATE, null)
+        val end = sharedPreferences.getString(PREF_END_DATE, null)
+
+        val tier = Tier.fromString(t) ?: return null
+        val cycle = Cycle.fromString(c) ?: return  null
+        val payMethod = PayMethod.fromString(pm) ?: return null
+
+        val createdAt = parseISODateTime(created) ?: return null
 
         return Subscription(
                 orderId = orderId,
-                tierToBuy = tier,
-                billingCycle = cycle,
-                paymentMethod = payMethod,
-                apiPrice = price,
-                createdAt = createdAt
+                tier = tier,
+                cycle = cycle,
+                payMethod = payMethod,
+                netPrice = price,
+                createdAt = createdAt,
+                confirmedAt = if (confirmed == null) null else parseISODateTime(confirmed),
+                startDate = if (start == null) null else parseLocalDate(start),
+                endDate = if (end == null) null else parseLocalDate(end)
         )
     }
 
