a02df4111fafd619191b4c78889f11ffa2c64be4
weiguo.ni <neefrankie@gmail.com>
2019-01-30 09:18:41 +0800

    test: parse json

---
+++ app/src/test/java/com/ft/ftchinese/models/JsonTest.kt
@@ -0,0 +1,30 @@
+package com.ft.ftchinese.models
+
+import org.junit.Test
+
+class JsonTest {
+    private val account = """
+    {
+        "id": "86e4acb5-4884-4b7b-94d7-b9475b110996",
+        "userName": "neefrankie@163.com",
+        "email": "neefrankie@163.com",
+        "avatarUrl": "",
+        "isVip": false,
+        "isVerified": false,
+        "wechat": {
+            "nickname": null,
+            "avatarUrl": null
+        },
+        "membership": {
+            "tier": "",
+            "billingCycle": "",
+            "expireDate": ""
+        }
+    }"""
+
+    @Test fun parseAccount() {
+        val acnt = json.parse<Account>(account)
+
+        println(acnt)
+    }
+}
---
+++ app/src/test/java/com/ft/ftchinese/models/TierTest.kt
@@ -0,0 +1,22 @@
+package com.ft.ftchinese.models
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class TierTest {
+
+    @Test
+    fun string() {
+        println(Tier.STANDARD)
+    }
+
+    @Test
+    fun stringCN() {
+    }
+
+    @Test
+    fun fromString() {
+
+    }
+}
