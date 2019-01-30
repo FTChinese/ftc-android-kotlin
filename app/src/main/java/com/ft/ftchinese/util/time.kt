71045ab245a9d643d0f14e06efbbe2b28971d748
weiguo.ni <neefrankie@gmail.com>
2019-01-30 09:54:32 +0800

    feat: date time parser and formatter with java 8 date time

---
+++ app/src/main/java/com/ft/ftchinese/util/time.kt
@@ -0,0 +1,54 @@
+package com.ft.ftchinese.util
+
+import org.threeten.bp.DateTimeException
+import org.threeten.bp.LocalDate
+import org.threeten.bp.ZonedDateTime
+import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.format.DateTimeParseException
+
+/**
+ * Parse ISO8601 data string.
+ */
+fun parseLocalDate(str: String?): LocalDate? {
+    if (str.isNullOrBlank()) {
+        return null
+    }
+
+    return try {
+        LocalDate.parse(str)
+    } catch (e: DateTimeParseException) {
+        null
+    }
+}
+
+fun formatLocalDate(d: LocalDate?): String? {
+    return try {
+        d?.format(DateTimeFormatter.ISO_LOCAL_DATE)
+    } catch (e: DateTimeException) {
+        null
+    }
+}
+
+/**
+ * Parses ISO8601 date time string in UTC.
+ * @throws DateTimeParseException
+ */
+fun parseISODateTime(str: String?): ZonedDateTime? {
+    if (str.isNullOrBlank()) {
+        return null
+    }
+    return try {
+        ZonedDateTime
+                .parse(str, DateTimeFormatter.ISO_DATE_TIME)
+    } catch (e: DateTimeParseException) {
+        null
+    }
+}
+
+fun formatISODateTime(dt: ZonedDateTime?): String? {
+    return try {
+        dt?.format(DateTimeFormatter.ISO_DATE_TIME)
+    } catch (e: DateTimeException) {
+        null
+    }
+}
