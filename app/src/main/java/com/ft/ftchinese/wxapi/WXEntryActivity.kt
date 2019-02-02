0aeb3989adbb86750cc48252254a5834f9e6b268
weiguo.ni <neefrankie@gmail.com>
2019-02-02 15:59:02 +0800

    feat: wechat login

--- app/src/main/java/com/ft/ftchinese/wxapi/WXEntryActivity.kt
+++ app/src/main/java/com/ft/ftchinese/wxapi/WXEntryActivity.kt
@@ -1,15 +1,14 @@
 package com.ft.ftchinese.wxapi
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.ft.ftchinese.BuildConfig
 import com.ft.ftchinese.R
-import com.ft.ftchinese.models.WxManager
-import com.ft.ftchinese.util.Fetch
-import com.ft.ftchinese.util.gson
-import com.google.gson.annotations.SerializedName
+import com.ft.ftchinese.models.*
+import com.ft.ftchinese.util.ClientError
+import com.ft.ftchinese.util.handleException
 import com.tencent.mm.opensdk.constants.ConstantsAPI
 import com.tencent.mm.opensdk.modelbase.BaseReq
 import com.tencent.mm.opensdk.modelbase.BaseResp
@@ -17,6 +16,8 @@ import com.tencent.mm.opensdk.modelmsg.SendAuth
 import com.tencent.mm.opensdk.openapi.IWXAPI
 import com.tencent.mm.opensdk.openapi.IWXAPIEventHandler
 import com.tencent.mm.opensdk.openapi.WXAPIFactory
+import kotlinx.android.synthetic.main.activity_wx_entry.*
+import kotlinx.android.synthetic.main.progress_bar.*
 import kotlinx.android.synthetic.main.simple_toolbar.*
 import kotlinx.coroutines.*
 import org.jetbrains.anko.AnkoLogger
@@ -24,11 +25,10 @@ import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
 import kotlin.Exception
 
-val wxSnsUri = Uri.parse("https://api.weixin.qq.com/sns")
-
 class WXEntryActivity : AppCompatActivity(), IWXAPIEventHandler, AnkoLogger {
     private var api: IWXAPI? = null
-    private val tokenUri = Uri.parse("https://api.weixin.qq.com/sns/oauth2/access_token")
+    private var wxManager: WxManager? = null
+    private var sessionManager: SessionManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +43,8 @@ class WXEntryActivity : AppCompatActivity(), IWXAPIEventHandler, AnkoLogger {
         } catch (e: Exception) {
             e.printStackTrace()
         }
+
+        info("onCreate called")
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -60,12 +62,16 @@ class WXEntryActivity : AppCompatActivity(), IWXAPIEventHandler, AnkoLogger {
         info("Wx login response type: ${resp?.type}, error code: ${resp?.errCode}")
 
         when (resp?.type) {
+            // Wechat Login.
             ConstantsAPI.COMMAND_SENDAUTH -> {
+                heading_tv.text = getString(R.string.progress_logging)
                 info("Wx auth")
                 processLogin(resp)
             }
             // This is used to handle your app sending message to wx and then return back to your app.
+            // Share will return to here.
             ConstantsAPI.COMMAND_SENDMESSAGE_TO_WX -> {
+                heading_tv.text = getString(R.string.wxshare_done)
                 info("Send message to wx")
                 finish()
             }
@@ -88,152 +94,92 @@ class WXEntryActivity : AppCompatActivity(), IWXAPIEventHandler, AnkoLogger {
                 if (resp is SendAuth.Resp) {
                     info("code: ${resp.code}, state: ${resp.state}, lang: ${resp.lang}, country: ${resp.country}")
 
-                    val wxManager = WxManager.getInstance(this)
-                    val state = wxManager.loadState()
+                    wxManager = WxManager.getInstance(this)
+                    sessionManager = SessionManager.getInstance(this)
 
+                    val state = wxManager?.loadState()
+
+                    info("State: $state")
                     if (state == null) {
                         toast("State code not found. Suspicious login attempt")
                         return
                     }
 
                     if (state != resp.state) {
-                        toast("State code not matchi")
+                        toast("State code not match")
                         return
                     }
 
-                    toast("Prepare to get access token")
-//                    val codeResp = WxCodeResp(resp.code, resp.state)
 
-//                    login(codeResp)
+                    login(resp.code)
                 }
             }
 
             BaseResp.ErrCode.ERR_USER_CANCEL -> {
-                info("User canceled")
+                info("OAuth Login Canceled")
             }
             BaseResp.ErrCode.ERR_AUTH_DENIED -> {
-                info("User denied")
+                info("OAuth Login Denied")
             }
         }
     }
 
-    private fun login(resp: WxCodeResp) {
-        val url = wxSnsUri.buildUpon()
-                .appendPath("oauth2")
-                .appendPath("access_token")
-                .appendQueryParameter("appid", BuildConfig.WX_SUBS_APPID)
-                .appendQueryParameter("secret", "")
-                .appendQueryParameter("code", resp.code)
-                .appendQueryParameter("grant_type", "authorization_code")
-                .build()
-                .toString()
+    private fun login(code: String) {
+        showProgress(true)
 
         GlobalScope.launch(Dispatchers.Main) {
             try {
-                // 第二步：通过code获取access_token
-                val wxAccess = async {
-                    resp.getTokenAsync(url)
-                }.await()
+                val sess = withContext(Dispatchers.IO) {
+                    WxLogin(code).send()
+                }
+                if (sess == null) {
+                    showProgress(false)
 
-                info("Get wx access: $wxAccess")
+                    toast("Login failed")
 
-                // 获取用户个人信息
+                    return@launch
+                }
+                wxManager?.saveSession(sess)
 
-                val userInfo = async {
-                    wxAccess?.getUserInfo()
-                }.await()
-                
-                info("Userinfo $userInfo")
+                val acnt = withContext(Dispatchers.IO) {
+                    sess.getAccount()
+                }
 
-            } catch (e: WxAccessException) {
+                if (acnt == null) {
+                    showProgress(false)
+                    toast("Failed to fetch account data")
+                    return@launch
+                }
 
-                toast("Wechat login failed. Error code: ${e.errCode}, error message: ${e.errMsg}")
+                showProgress(false)
 
+                sessionManager?.saveAccount(acnt)
+
+                info("Killing activity")
+                finish()
+            } catch (e: ClientError) {
+                info("API error: $e")
+
+                showProgress(false)
+
+                handleClientError(e)
             } catch (e: Exception) {
-                e.printStackTrace()
+                handleException(e)
             }
         }
-
-    }
-}
-
-data class WxCodeResp(
-        val code: String,
-        val state: String
-) {
-    /**
-     * @return WxAccessResp if request success, or null if failed.
-     * @throws WxAccessException
-     */
-    fun getTokenAsync(url: String): WxAccessResp? {
-        val respStr = Fetch().get(url).string()
-
-        return try {
-            gson.fromJson<WxAccessResp>(respStr, WxAccessResp::class.java)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            return try {
-                val ex =gson.fromJson<WxAccessException>(respStr, WxAccessException::class.java)
-
-                throw ex
-            } catch (e: Exception) {
-                e.printStackTrace()
-                null
-            }
-        }
-    }
-}
-
-data class WxAccessResp(
-        @SerializedName("access_token") val accessToken: String,
-        @SerializedName("expires_in") val expiresIn: Int,
-        @SerializedName("refresh_token") val refreshToken: String,
-        @SerializedName("openid") val openId: String,
-        val scope: String,
-        @SerializedName("unionid") val unionId: String
-
-) {
-//    fun refresh(): Deferred<WxAccessResp?> = async {
 //
-//    }
+    }
 
-    fun getUserInfo(): WxUserInfo? {
-        val url = wxSnsUri.buildUpon()
-                .appendPath("userinfo")
-                .appendQueryParameter("access_token", accessToken)
-                .appendQueryParameter("openid", openId)
-                .build()
-                .toString()
-        val respStr = Fetch().get(url).string()
+    private fun handleClientError(err: ClientError) {
+        toast(err.message)
+    }
 
-        return try {
-            gson.fromJson<WxUserInfo>(respStr, WxUserInfo::class.java)
-        } catch (e: Exception) {
-            return try {
-                val ex = gson.fromJson<WxAccessException>(respStr, WxAccessException::class.java)
-                throw ex
-            } catch (e: Exception) {
-                e.printStackTrace()
-
-                null
-            }
+    private fun showProgress(v: Boolean) {
+        if (v) {
+            progress_bar.visibility = View.VISIBLE
+        } else {
+            progress_bar.visibility = View.GONE
         }
     }
 }
 
-data class WxUserInfo(
-        @SerializedName("openid") val openId: String,
-        @SerializedName("nickname") val nickName: String,
-        @SerializedName("sex") val gender: Int,
-        val province: String,
-        val city: String,
-        val country: String,
-        @SerializedName("headimgurl") val avatar: String,
-        val privilege: Array<String>,
-        @SerializedName("unionid") val unionId: String
-)
-
-data class WxAccessException(
-    @SerializedName("errcode") val errCode: Int,
-    @SerializedName("errmsg") val errMsg: Int
-) : Exception()
