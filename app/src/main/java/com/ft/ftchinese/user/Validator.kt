87966e9171c96d23296996efb419b76d9557e901
weiguo.ni <neefrankie@gmail.com>
2019-02-20 10:17:45 +0800

    feat: a singleton for Validator

--- app/src/main/java/com/ft/ftchinese/user/Validator.kt
+++ app/src/main/java/com/ft/ftchinese/user/Validator.kt
@@ -1,9 +1,29 @@
 package com.ft.ftchinese.user
 
-fun isEmailValid(email: String): Boolean {
-    return email.contains("@")
-}
+import com.ft.ftchinese.R
 
-fun isPasswordValid(password: String): Boolean {
-    return password.length >= 8
+object Validator {
+    fun ensureEmail(email: String): Int? {
+        if (email.isBlank()) {
+            return R.string.error_field_required
+        }
+
+        if (!email.contains("@")) {
+            return R.string.error_invalid_email
+        }
+
+        return null
+    }
+
+    fun ensurePassword(pw: String): Int? {
+        if (pw.isBlank()) {
+            return R.string.error_field_required
+        }
+
+        if (pw.length < 8) {
+            return R.string.error_invalid_password
+        }
+
+        return null
+    }
 }
