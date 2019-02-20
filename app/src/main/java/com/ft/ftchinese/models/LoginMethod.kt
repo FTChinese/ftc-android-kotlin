0a4d6f039cd026d58bb62f83fcb4e3748ec20bde
weiguo.ni <neefrankie@gmail.com>
2019-02-20 10:16:58 +0800

    refactor: create an instance from a string

--- app/src/main/java/com/ft/ftchinese/models/LoginMethod.kt
+++ app/src/main/java/com/ft/ftchinese/models/LoginMethod.kt
@@ -5,11 +5,22 @@ enum class LoginMethod {
     WECHAT,
     MOBILE;
 
-    override fun toString(): String {
+    fun string(): String {
         return when (this) {
             EMAIL -> "email"
             WECHAT -> "wechat"
             MOBILE -> "mobile"
+        }
+    }
+
+    companion object {
+        fun fromString(s: String?): LoginMethod? {
+            return when (s) {
+                "email" -> EMAIL
+                "wechat" -> WECHAT
+                "mobile" -> MOBILE
+                else -> null
+            }
         }
     }
 }
