b5fff203fce795f8e8a289bfdf3e119a03050190
weiguo.ni <neefrankie@gmail.com>
2019-02-20 10:26:14 +0800

    refactor: ForgotPasswordActivity and Fragment

--- app/src/main/java/com/ft/ftchinese/user/ForgotPasswordActivity.kt
+++ app/src/main/java/com/ft/ftchinese/user/ForgotPasswordActivity.kt
@@ -3,170 +3,59 @@ package com.ft.ftchinese.user
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.view.ViewGroup
 import com.ft.ftchinese.R
-import com.ft.ftchinese.models.PasswordReset
-import com.ft.ftchinese.util.ClientError
-import com.ft.ftchinese.util.handleApiError
-import com.ft.ftchinese.util.handleException
-import com.ft.ftchinese.util.isNetworkConnected
-import kotlinx.android.synthetic.main.fragment_forgot_password.*
-import kotlinx.coroutines.*
+import kotlinx.android.synthetic.main.progress_bar.*
+import kotlinx.android.synthetic.main.simple_toolbar.*
 import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.info
-import org.jetbrains.anko.support.v4.toast
 
-class ForgotPasswordActivity : SingleFragmentActivity() {
-    override fun createFragment(): Fragment {
-        return ForgotPasswordFragment.newInstance()
+class ForgotPasswordActivity : AppCompatActivity(),
+        OnCredentialsListener,
+        AnkoLogger {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowTitleEnabled(true)
+        }
+
+        val email = intent.getStringExtra(ARG_EMAIL) ?: return
+
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, ForgotPasswordFragment.newInstance(email))
+                .commit()
+    }
+
+    override fun onProgress(show: Boolean) {
+        if (show) {
+            progress_bar.visibility = View.VISIBLE
+        } else {
+            progress_bar.visibility = View.GONE
+        }
+    }
+
+    override fun onLogIn(email: String) {
+
+    }
+
+    override fun onSignUp(email: String) {
+
     }
 
     companion object {
-        fun start(context: Context?) {
-            val intent = Intent(context, ForgotPasswordActivity::class.java)
+        fun start(context: Context?, email: String) {
+            val intent = Intent(context, ForgotPasswordActivity::class.java).apply {
+                putExtra(ARG_EMAIL, email)
+            }
 
             context?.startActivity(intent)
         }
     }
 }
 
-internal class ForgotPasswordFragment : Fragment(), AnkoLogger {
-
-    private var job: Job? = null
-    private var listener: OnFragmentInteractionListener? = null
-
-    private fun showProgress(value: Boolean) {
-        listener?.onProgress(value)
-
-        // If in progress, hide the message telling what to do next.
-        if (value) {
-            letter_sent_feedback?.visibility = View.GONE
-        }
-    }
-
-    private fun allowInput(value: Boolean) {
-        send_button?.isEnabled = value
-        email?.isEnabled = value
-    }
-
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
-
-        if (context is OnFragmentInteractionListener) {
-            listener = context
-        }
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        super.onCreateView(inflater, container, savedInstanceState)
-
-        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        send_button.setOnClickListener {
-            attemptSendLetter()
-        }
-    }
-
-    private fun attemptSendLetter() {
-        email.error = null
-
-        val emailStr = email.text.toString()
-
-        var cancel = false
-
-        if (emailStr.isBlank()) {
-            email.error = getString(R.string.error_field_required)
-            cancel = true
-        } else if (!isEmailValid(emailStr)) {
-            email.error = getString(R.string.error_invalid_email)
-            cancel = true
-        }
-
-        if (cancel) {
-            email.requestFocus()
-
-            return
-        }
-
-        sendLetter(emailStr)
-    }
-
-    private fun sendLetter(emailStr: String) {
-        showProgress(true)
-        allowInput(false)
-
-        if (activity?.isNetworkConnected() != true) {
-            toast(R.string.prompt_no_network)
-
-            return
-        }
-
-        job = GlobalScope.launch(Dispatchers.Main) {
-            val passwordReset = PasswordReset(emailStr)
-
-            try {
-                val done = withContext(Dispatchers.IO) {
-                    passwordReset.send()
-                }
-
-                if (done) {
-                    toast(R.string.success_letter_sent)
-
-                    showProgress(false)
-                } else {
-                    toast("Sending email failed. Pleans try again")
-                    showProgress(false)
-                    allowInput(true)
-                }
-
-            } catch (e: ClientError) {
-                showProgress(false)
-                allowInput(true)
-
-                info("API error response: ${e.message}")
-                handleClientError(e)
-
-            } catch (e: Exception) {
-
-                showProgress(false)
-                allowInput(true)
-
-                activity?.handleException(e)
-            }
-        }
-    }
-
-    /**
-     * Handle restful api error response
-     */
-    private fun handleClientError(resp: ClientError) {
-        when (resp.statusCode) {
-            // Email is not found
-            404 -> {
-                toast(R.string.api_email_not_found)
-            }
-            else -> {
-                activity?.handleApiError(resp)
-            }
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-
-        job?.cancel()
-    }
-
-    companion object {
-        fun newInstance(): ForgotPasswordFragment {
-            return ForgotPasswordFragment()
-        }
-    }
-}
