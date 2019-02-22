1c56082fae753026a1013296542ee1eb640a7174
weiguo.ni <neefrankie@gmail.com>
2019-02-22 18:21:36 +0800

    refactor: register wechat api

--- app/src/main/java/com/ft/ftchinese/user/EmailFragment.kt
+++ app/src/main/java/com/ft/ftchinese/user/EmailFragment.kt
@@ -7,13 +7,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ft.ftchinese.BuildConfig
 import com.ft.ftchinese.R
 import com.ft.ftchinese.models.WxSessionManager
 import com.ft.ftchinese.util.*
-import com.tencent.mm.opensdk.modelmsg.SendAuth
-import com.tencent.mm.opensdk.openapi.IWXAPI
-import com.tencent.mm.opensdk.openapi.WXAPIFactory
 import kotlinx.android.synthetic.main.fragment_email.*
 import kotlinx.coroutines.*
 import org.jetbrains.anko.AnkoLogger
@@ -26,7 +22,6 @@ class EmailFragment : Fragment(),
     private var listener: OnCredentialsListener? = null
     private var job: Job? = null
     private var wxSessionManager: WxSessionManager? = null
-    private var wxApi: IWXAPI? = null
 
     private fun showProgress(show: Boolean) {
         listener?.onProgress(show)
@@ -48,15 +43,10 @@ class EmailFragment : Fragment(),
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        wxApi = WXAPIFactory.createWXAPI(context, BuildConfig.WX_SUBS_APPID)
-        wxApi?.registerApp(BuildConfig.WX_SUBS_APPID)
-    }
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
+
+        info("onCreateView finished")
 
         return inflater.inflate(R.layout.fragment_email, container, false)
     }
@@ -64,6 +54,9 @@ class EmailFragment : Fragment(),
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        /**
+         * Check whether email exists.
+         */
         next_btn.setOnClickListener {
             val email = email_input.text.toString().trim()
             val isValid = isEmailValid(email)
@@ -72,19 +65,6 @@ class EmailFragment : Fragment(),
             }
 
             emailExists(email)
-        }
-
-        wechat_oauth_btn.setOnClickListener {
-            val nonce = generateNonce(5)
-            info("Wechat oauth state: $nonce")
-
-            wxSessionManager?.saveState(nonce)
-
-            val req = SendAuth.Req()
-            req.scope = "snsapi_userinfo"
-            req.state = nonce
-
-            activity?.finish()
         }
     }
 
@@ -106,6 +86,9 @@ class EmailFragment : Fragment(),
         return true
     }
 
+    /**
+     * Ask API whether the mail exists.
+     */
     private fun emailExists(email: String) {
         if (activity?.isNetworkConnected() != true) {
             toast(R.string.prompt_no_network)
