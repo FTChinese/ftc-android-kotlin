2c7bc7143da7063e4b46a86cdff5179b95dc6dfe
weiguo.ni <neefrankie@gmail.com>
2019-02-28 17:07:24 +0800

    refactor: signup for both email and wechat user

--- app/src/main/java/com/ft/ftchinese/models/Credentials.kt
+++ app/src/main/java/com/ft/ftchinese/models/Credentials.kt
@@ -37,9 +37,23 @@ data class Credentials(
         }
     }
 
-    fun signUp(): String? {
-        val (_, body) = Fetch()
-                .post(NextApi.SIGN_UP)
+    /**
+     * If unionId is null, it indicates this is a regular registration;
+     * If unionId is not null, it indicates a
+     * wechat-logged-in user is trying to create a new
+     * account and this new account should be bound to this
+     * wechat account after creation.
+     */
+    fun signUp(unionId: String? = null): String? {
+        val fetch = if (unionId != null) {
+            Fetch().post(NextApi.WX_SIGNUP)
+                    .setUnionId(unionId)
+        } else {
+            Fetch().post(NextApi.SIGN_UP)
+        }
+
+
+        val (_, body) = fetch
                 .setClient()
                 .noCache()
                 .jsonBody(json.toJsonString(this))
