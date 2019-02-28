befb35f01775bf24a9cf4671fd59a80ea167b70d
weiguo.ni <neefrankie@gmail.com>
2019-02-28 17:19:09 +0800

    refactor: adapter for two col row

---
+++ app/src/main/java/com/ft/ftchinese/user/RowAdapter.kt
@@ -0,0 +1,54 @@
+package com.ft.ftchinese.user
+
+import android.graphics.Typeface
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.ft.ftchinese.R
+
+/**
+ * A two column table-like recycler view.
+ */
+class RowAdapter(private var rows: Array<TableRow>) :
+        RecyclerView.Adapter<RowAdapter.ViewHolder>() {
+
+    class ViewHolder (view: View): RecyclerView.ViewHolder(view) {
+        val labelView: TextView = view.findViewById(R.id.label_tv)
+        val valueView: TextView = view.findViewById(R.id.value_tv)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.list_item_table_row, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = rows.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val row = rows[position]
+        holder.labelView.text = row.header
+        holder.valueView.text = row.data
+        if (row.color != null) {
+            holder.valueView.setTextColor(row.color)
+        }
+
+        if (row.isBold) {
+            holder.valueView.setTypeface(holder.valueView.typeface, Typeface.BOLD)
+        }
+    }
+
+    fun refreshData(rows: Array<TableRow>) {
+        this.rows = rows
+    }
+}
+
+data class TableRow(
+        val header: String,
+        val data: String,
+        val isBold: Boolean = false,
+        val color: Int? = null
+)
