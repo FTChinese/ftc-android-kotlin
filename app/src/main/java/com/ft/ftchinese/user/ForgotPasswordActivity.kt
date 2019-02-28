3e389e8e251ae473ea9021f1a205fd67ec669fff
weiguo.ni <neefrankie@gmail.com>
2019-02-28 17:00:32 +0800

    refactor: no longer using fragment since forgot password is a simple independent activity

--- app/src/main/java/com/ft/ftchinese/user/ForgotPasswordActivity.kt
+++ app/src/main/java/com/ft/ftchinese/user/ForgotPasswordActivity.kt
@@ -4,19 +4,30 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
 import android.view.View
 import com.ft.ftchinese.R
+import com.ft.ftchinese.models.PasswordReset
+import com.ft.ftchinese.util.ClientError
+import com.ft.ftchinese.util.handleApiError
+import com.ft.ftchinese.util.handleException
+import com.ft.ftchinese.util.isNetworkConnected
+import kotlinx.android.synthetic.main.activity_forgot_password.*
 import kotlinx.android.synthetic.main.progress_bar.*
 import kotlinx.android.synthetic.main.simple_toolbar.*
+import kotlinx.coroutines.*
 import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+import org.jetbrains.anko.toast
 
 class ForgotPasswordActivity : AppCompatActivity(),
-        OnCredentialsListener,
         AnkoLogger {
+
+    private var job: Job? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fragment)
+        setContentView(R.layout.activity_forgot_password)
         setSupportActionBar(toolbar)
 
         supportActionBar?.apply {
@@ -26,13 +37,87 @@ class ForgotPasswordActivity : AppCompatActivity(),
 
         val email = intent.getStringExtra(ARG_EMAIL) ?: return
 
-        supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.fragment_container, ForgotPasswordFragment.newInstance(email))
-                .commit()
+        email_input.text = Editable.Factory.getInstance().newEditable(email)
+
+        send_email_btn.setOnClickListener {
+            val inputEmail = email_input.text.toString().trim()
+
+            val isValid = isEmailValid(inputEmail)
+
+            if (!isValid) {
+                return@setOnClickListener
+            }
+
+            sendLetter(inputEmail)
+        }
     }
 
-    override fun onProgress(show: Boolean) {
+    private fun isEmailValid(email: String): Boolean {
+        email_input.error = null
+        val msgId = Validator.ensureEmail(email)
+        if (msgId != null) {
+            email_input.error = getString(msgId)
+            email_input.requestFocus()
+            return false
+        }
+
+        return true
+    }
+
+    private fun sendLetter(email: String) {
+        if (isNetworkConnected()) {
+            toast(R.string.prompt_no_network)
+
+            return
+        }
+
+        showProgress(true)
+        allowInput(false)
+
+        job = GlobalScope.launch(Dispatchers.Main) {
+            val passwordReset = PasswordReset(email)
+
+            try {
+                val done = withContext(Dispatchers.IO) {
+                    passwordReset.send()
+                }
+
+                if (done) {
+                    toast(R.string.prompt_letter_sent)
+
+                    showProgress(false)
+                } else {
+                    toast("Sending email failed. Pleas try again")
+                    showProgress(false)
+                    allowInput(true)
+                }
+
+            } catch (e: ClientError) {
+                showProgress(false)
+                allowInput(true)
+
+                info("API error response: ${e.message}")
+                when (e.statusCode) {
+                    // Email is not found
+                    404 -> {
+                        toast(R.string.api_email_not_found)
+                    }
+                    else -> {
+                        handleApiError(e)
+                    }
+                }
+
+            } catch (e: Exception) {
+
+                showProgress(false)
+                allowInput(true)
+
+                handleException(e)
+            }
+        }
+    }
+
+    private fun showProgress(show: Boolean) {
         if (show) {
             progress_bar.visibility = View.VISIBLE
         } else {
@@ -40,16 +125,16 @@ class ForgotPasswordActivity : AppCompatActivity(),
         }
     }
 
-    override fun onLogIn(email: String) {
 
+    private fun allowInput(value: Boolean) {
+        send_email_btn.isEnabled = value
+        email_input.isEnabled = value
     }
 
-    override fun onSignUp(email: String) {
+    override fun onDestroy() {
+        super.onDestroy()
 
-    }
-
-    override fun onLoadAccount(userId: String) {
-
+        job?.cancel()
     }
 
     companion object {
