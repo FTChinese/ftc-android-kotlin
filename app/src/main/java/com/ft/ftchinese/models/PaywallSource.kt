6d47f352a816e0ecb83a1430b182323d8b43a25e
weiguo.ni <neefrankie@gmail.com>
2019-03-27 17:35:17 +0800

    refactor: tracking what started the paywall and payment

--- app/src/main/java/com/ft/ftchinese/models/PaywallSource.kt
+++ app/src/main/java/com/ft/ftchinese/models/PaywallSource.kt
@@ -1,12 +1,5 @@
 package com.ft.ftchinese.models
 
-data class PaywallSource(
-        val id: String,
-        val category: String,
-        val name: String,
-        val variant: Language? = null
-) {
-    fun getGALable(): String {
-        return "$category/$id"
-    }
+object PaywallTracker {
+    var source: ChannelItem? = null
 }
