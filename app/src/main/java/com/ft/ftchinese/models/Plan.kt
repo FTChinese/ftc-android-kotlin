e5803dd13201062a6bc53ba49bcd6528b5c414d5
weiguo.ni <neefrankie@gmail.com>
2019-05-26 14:23:46 +0800

    refactor: remove Pricing type and add SubsPlans type

--- app/src/main/java/com/ft/ftchinese/models/Plan.kt
+++ app/src/main/java/com/ft/ftchinese/models/Plan.kt
@@ -1,11 +1,11 @@
 package com.ft.ftchinese.models
 
+import android.os.Parcelable
+import com.beust.klaxon.Json
+import com.ft.ftchinese.util.GAAction
 import com.ft.ftchinese.util.KCycle
 import com.ft.ftchinese.util.KTier
-
-const val KEY_STANDARD_YEAR = "standard_year"
-const val KEY_STANDARD_MONTH = "standard_month"
-const val KEY_PREMIUM_YEAR = "premium_year"
+import kotlinx.android.parcel.Parcelize
 
 /**
  * A pricing plan.
@@ -20,50 +20,110 @@ data class Plan(
         val description: String
 )
 
-class Pricing(private val plans: Map<String, Plan>) {
-    fun findPlan(tier: Tier?, cycle: Cycle?): Plan? {
+class SubsPlans(
+        @Json(name = "standard_year")
+        val standardYear: Plan,
 
-        val key = tierCycleKey(tier, cycle) ?: return null
+        @Json(name = "standard_month")
+        val standardMonth: Plan,
 
-        return plans[key]
-    }
-
-    fun findPlan(key: String?): Plan? {
-        return if (key == null) {
-            null
-        } else {
-            plans[key]
+        @Json(name = "premium_year")
+        val premiumYear: Plan
+) {
+    fun of(tier: Tier, cycle: Cycle): Plan {
+        return when (tier) {
+            Tier.STANDARD -> when (cycle) {
+                Cycle.YEAR -> standardYear
+                Cycle.MONTH -> standardMonth
+            }
+            Tier.PREMIUM -> premiumYear
         }
     }
 }
 
-fun tierCycleKey(tier: Tier?, cycle: Cycle?): String? {
-    if (tier == null || cycle == null) {
-        return null
-    }
-    return "${tier.string()}_${cycle.string()}"
-}
-
-val pricingPlans = Pricing(mapOf(
-        KEY_STANDARD_YEAR to Plan(
+val subsPlans = SubsPlans(
+        standardYear = Plan(
                 tier = Tier.STANDARD,
                 cycle = Cycle.YEAR,
                 listPrice = 258.00,
                 netPrice = 258.00,
                 description = "FT中文网 - 年度标准会员"
         ),
-        KEY_STANDARD_MONTH to Plan(
+        standardMonth = Plan(
                 tier = Tier.STANDARD,
                 cycle = Cycle.MONTH,
                 listPrice = 28.00,
                 netPrice = 28.00,
                 description = "FT中文网 - 月度标准会员"
         ),
-        KEY_PREMIUM_YEAR to Plan(
+        premiumYear = Plan(
                 tier = Tier.PREMIUM,
                 cycle = Cycle.YEAR,
                 listPrice = 1998.00,
                 netPrice = 1998.00,
                 description = "FT中文网 - 高端会员"
         )
-))
+)
+
+/**
+ * PlanPayable can be used to build UI for checkout.
+ */
+@Parcelize
+data class PlanPayable(
+        @KTier
+        val tier: Tier,
+        @KCycle
+        val cycle: Cycle,
+        val listPrice: Double,
+        val netPrice: Double,
+
+        val balance: Double,
+        val cycleCount: Int,
+        val extraDays: Int,
+        val payable: Double,
+
+        var isUpgrade: Boolean = false,
+        var isRenew: Boolean = false
+): Parcelable {
+
+    fun isPayRequired(): Boolean {
+        return payable != 0.0
+    }
+
+    // Whether user's account balance is enough to cover
+    // upgrade cost.
+    fun isDirectUpgrade(): Boolean {
+        return isUpgrade && payable > 0.0
+    }
+
+    fun getId(): String {
+        return "${tier.string()}_${cycle.string()}"
+    }
+
+    fun gaAddCartAction(): String {
+        return when (tier) {
+            Tier.STANDARD -> when (cycle) {
+                   Cycle.YEAR -> GAAction.BUY_STANDARD_YEAR
+                   Cycle.MONTH -> GAAction.BUY_STANDARD_MONTH
+            }
+            Tier.PREMIUM -> GAAction.BUY_PREMIUM
+        }
+    }
+
+    companion object {
+        fun fromPlan(p: Plan): PlanPayable {
+            return PlanPayable(
+                    tier = p.tier,
+                    cycle = p.cycle,
+                    listPrice = p.listPrice,
+                    netPrice = p.netPrice,
+                    balance = 0.0,
+                    cycleCount = 1,
+                    extraDays = 0,
+                    payable = p.netPrice,
+                    isUpgrade = false,
+                    isRenew = false
+            )
+        }
+    }
+}
