41523bb38ca40697153c5577c98093ed323408d2
weiguo.ni <neefrankie@gmail.com>
2019-05-26 14:22:53 +0800

    feat: enum for membership status

---
+++ app/src/main/java/com/ft/ftchinese/models/MemberStatus.kt
@@ -0,0 +1,16 @@
+package com.ft.ftchinese.models
+
+/**
+ * The id is used to calculate multiple status.
+ * For example, if the membership is both renewable and upgradable,
+ * we can perform bitwise `or`: RENEWABLE | UPGRADABLE = 1010.
+ * Since each value takes different position in the binary number,
+ * any combination is always unique.
+ */
+enum class MemberStatus(val id: Int) {
+    INVALID(1),
+    EXPIRED(2),
+    RENEWABLE(4),
+    BEYOND_RENEW(8),
+    UPGRADABLE(16)
+}
