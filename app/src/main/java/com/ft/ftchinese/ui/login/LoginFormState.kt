6ba98be1108c4722c19fe9a60f172d1c05af4df7
neefrankie <neefrankie@gmail.com>
2019-06-09 21:47:12 +0800

    refactor: in migration to view model

---
+++ app/src/main/java/com/ft/ftchinese/ui/login/LoginFormState.kt
@@ -0,0 +1,8 @@
+package com.ft.ftchinese.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState (val emailError: Int? = null,
+                           val passwordError: Int? = null,
+                           val isDataValid: Boolean = false)
--- app/src/main/java/com/ft/ftchinese/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/ft/ftchinese/ui/login/LoginViewModel.kt
@@ -2,12 +2,15 @@ package com.ft.ftchinese.ui.login
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ft.ftchinese.model.Credentials
 
 class LoginViewModel : ViewModel() {
 
     val email = MutableLiveData<Pair<String, Boolean>>()
     val inProgress = MutableLiveData<Boolean>()
     val userId = MutableLiveData<String>()
+
+
 
     fun foundEmail(result: Pair<String, Boolean>) {
         this.email.value = result
@@ -19,5 +22,9 @@ class LoginViewModel : ViewModel() {
 
     fun setUserId(id: String) {
         userId.value = id
+    }
+
+    fun login(c: Credentials) {
+
     }
 }
