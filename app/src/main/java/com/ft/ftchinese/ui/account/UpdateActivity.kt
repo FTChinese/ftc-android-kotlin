2295e59ff60d85e525c2b920ba6a7e1a2b6446b8
weiguo.ni <neefrankie@gmail.com>
2019-06-14 12:03:08 +0800

    refactor: update account after submitted data saved

--- app/src/main/java/com/ft/ftchinese/ui/account/UpdateActivity.kt
+++ app/src/main/java/com/ft/ftchinese/ui/account/UpdateActivity.kt
@@ -1,15 +1,16 @@
 package com.ft.ftchinese.ui.account
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.commit
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.ft.ftchinese.R
 import com.ft.ftchinese.base.ScopedAppActivity
 import com.ft.ftchinese.base.handleException
+import com.ft.ftchinese.model.SessionManager
 import kotlinx.android.synthetic.main.progress_bar.*
 import kotlinx.android.synthetic.main.simple_toolbar.*
 import org.jetbrains.anko.AnkoLogger
@@ -19,6 +20,16 @@ import org.jetbrains.anko.toast
 class UpdateActivity : ScopedAppActivity(), AnkoLogger {
 
     private lateinit var viewModel: UpdateViewModel
+    private lateinit var accountViewModel: AccountViewModel
+    private lateinit var sessionManager: SessionManager
+
+    private fun showProgress(show: Boolean) {
+        if (show) {
+            progress_bar.visibility = View.VISIBLE
+        } else {
+            progress_bar.visibility = View.GONE
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +41,14 @@ class UpdateActivity : ScopedAppActivity(), AnkoLogger {
             setDisplayHomeAsUpEnabled(true)
             setDisplayShowTitleEnabled(true)
         }
+
+        viewModel = ViewModelProviders.of(this)
+                .get(UpdateViewModel::class.java)
+
+        accountViewModel = ViewModelProviders.of(this)
+                .get(AccountViewModel::class.java)
+
+        sessionManager = SessionManager.getInstance(this)
 
         val fm = supportFragmentManager
                 .beginTransaction()
@@ -51,9 +70,12 @@ class UpdateActivity : ScopedAppActivity(), AnkoLogger {
 
         fm.commit()
 
-        viewModel = ViewModelProviders.of(this)
-                .get(UpdateViewModel::class.java)
 
+
+        setUp()
+    }
+
+    private fun setUp() {
         viewModel.inProgress.observe(this, Observer<Boolean> {
             showProgress(it)
         })
@@ -61,7 +83,7 @@ class UpdateActivity : ScopedAppActivity(), AnkoLogger {
         viewModel.updateResult.observe(this, Observer {
             val updateResult = it ?: return@Observer
 
-            viewModel.showProgress(false)
+            showProgress(false)
 
             if (updateResult.error != null) {
                 toast(updateResult.error)
@@ -82,15 +104,47 @@ class UpdateActivity : ScopedAppActivity(), AnkoLogger {
             }
 
             toast(R.string.prompt_saved)
-        })
-    }
 
-    private fun showProgress(show: Boolean) {
-        if (show) {
-            progress_bar.visibility = View.VISIBLE
-        } else {
-            progress_bar.visibility = View.GONE
-        }
+            val account = sessionManager.loadAccount()
+            if (account == null) {
+                toast("Account not found")
+                return@Observer
+            }
+
+            accountViewModel.refresh(
+                    account = account
+            )
+        })
+
+        // Observing refreshed account.
+        accountViewModel.accountResult.observe(this, Observer {
+            showProgress(false)
+
+            val accountResult = it ?: return@Observer
+
+            if (accountResult.error != null) {
+                toast(accountResult.error)
+                return@Observer
+            }
+
+            if (accountResult.exception != null) {
+                handleException(accountResult.exception)
+                return@Observer
+            }
+
+            if (accountResult.success == null) {
+                toast("Refreshing account failed. Please refresh manually later.")
+                return@Observer
+            }
+
+            toast(R.string.prompt_updated)
+
+            sessionManager.saveAccount(accountResult.success)
+
+            setResult(Activity.RESULT_OK)
+
+            finish()
+        })
     }
 
     companion object {
