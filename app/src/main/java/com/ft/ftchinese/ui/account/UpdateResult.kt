f667c3451934c395118a0fdb6435d95ec88b4727
neefrankie <neefrankie@gmail.com>
2019-06-14 08:43:52 +0800

    refactor: change success to non nullable default false

--- app/src/main/java/com/ft/ftchinese/ui/account/UpdateResult.kt
+++ app/src/main/java/com/ft/ftchinese/ui/account/UpdateResult.kt
@@ -1,7 +1,7 @@
 package com.ft.ftchinese.ui.account
 
 data class UpdateResult (
-        val success: Boolean? = null,
+        val success: Boolean = false,
         val error: Int? = null,
         val exception: Exception? = null
 )
