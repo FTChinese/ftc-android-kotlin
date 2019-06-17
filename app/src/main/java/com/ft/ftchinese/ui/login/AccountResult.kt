d0de781b9cba5f1c4f6f1ea856f605128aa7061d
neefrankie <neefrankie@gmail.com>
2019-06-17 21:33:21 +0800

    refactor: rename LoginResult to AccountResult

--- app/src/main/java/com/ft/ftchinese/ui/login/AccountResult.kt
+++ app/src/main/java/com/ft/ftchinese/ui/login/AccountResult.kt
@@ -2,7 +2,7 @@ package com.ft.ftchinese.ui.login
 
 import com.ft.ftchinese.model.Account
 
-data class LoginResult(
+data class AccountResult(
     val success: Account? = null,
     val error: Int? = null,
     val exception: Exception? = null // carries information that cannot be determined statically.
