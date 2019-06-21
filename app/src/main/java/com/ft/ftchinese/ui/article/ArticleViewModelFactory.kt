4ae9b8be71af074663c0b89057aa3b08ceec8122
weiguo.ni <neefrankie@gmail.com>
2019-06-21 19:13:21 +0800

    refactor: add followingManager parameter

--- app/src/main/java/com/ft/ftchinese/ui/article/ArticleViewModelFactory.kt
+++ app/src/main/java/com/ft/ftchinese/ui/article/ArticleViewModelFactory.kt
@@ -2,16 +2,20 @@ package com.ft.ftchinese.ui.article
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.ft.ftchinese.ui.channel.ChannelViewModel
+import com.ft.ftchinese.model.FollowingManager
 import com.ft.ftchinese.util.FileCache
 
-class ArticleViewModelFactory(val cache: FileCache) : ViewModelProvider.Factory {
+class ArticleViewModelFactory(
+        private val cache: FileCache,
+        private val followingManager: FollowingManager
+) : ViewModelProvider.Factory {
 
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(ArticleViewModel::class.java)) {
-            return ChannelViewModel(
-                    cache
+            return ArticleViewModel(
+                    cache,
+                    followingManager
             ) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
