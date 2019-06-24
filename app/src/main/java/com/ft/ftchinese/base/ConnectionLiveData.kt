649b1fb1b65ee7a0bc8a4456b96064a1786889c4
weiguo.ni <neefrankie@gmail.com>
2019-06-24 14:15:09 +0800

    refactor: monitor network status

---
+++ app/src/main/java/com/ft/ftchinese/base/ConnectionLiveData.kt
@@ -0,0 +1,83 @@
+package com.ft.ftchinese.base
+
+
+import android.annotation.TargetApi
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkInfo
+import android.net.NetworkRequest
+import android.os.Build
+import androidx.lifecycle.LiveData
+
+class ConnectionLiveData(val context: Context) : LiveData<Boolean>() {
+    private var connectivityManager: ConnectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    private lateinit var connectivityManagerCallback: ConnectivityManager.NetworkCallback
+
+    override fun onActive() {
+        super.onActive()
+        updateConnection()
+
+        when {
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.N -> connectivityManager.registerDefaultNetworkCallback(getConnectivityManagerCallback())
+            Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP -> lollipopNetworkAvailableRequest()
+            else -> {
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                    context.registerReceiver(networkReceiver, IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"))
+                }
+            }
+        }
+    }
+
+    override fun onInactive() {
+        super.onInactive()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            connectivityManager.unregisterNetworkCallback(connectivityManagerCallback)
+        } else {
+            context.unregisterReceiver(networkReceiver)
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private fun lollipopNetworkAvailableRequest() {
+        val builder = NetworkRequest.Builder()
+                .addTransportType(android.net.NetworkCapabilities.TRANSPORT_CELLULAR)
+                .addTransportType(android.net.NetworkCapabilities.TRANSPORT_WIFI)
+
+        connectivityManager.registerNetworkCallback(builder.build(), getConnectivityManagerCallback())
+    }
+
+    private fun getConnectivityManagerCallback(): ConnectivityManager.NetworkCallback {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            connectivityManagerCallback = object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network?) {
+                    postValue(true)
+                }
+
+                override fun onLosing(network: Network?, maxMsToLive: Int) {
+                    postValue(false)
+                }
+            }
+
+            return connectivityManagerCallback
+        } else {
+            throw IllegalAccessError("Should not happened")
+        }
+    }
+
+    private val networkReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            updateConnection()
+        }
+    }
+
+    private fun updateConnection() {
+        val activeNetwork: NetworkInfo? = connectivityManager.activeNetworkInfo
+        postValue(activeNetwork?.isConnected == true)
+    }
+}
