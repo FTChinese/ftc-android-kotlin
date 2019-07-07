6f4c941133f1f532f30c55a68c594df6f631d2ec
weiguo.ni <neefrankie@gmail.com>
2019-07-07 11:08:30 +0800

    feat: stripe ephemeral key provider implementation

---
+++ app/src/main/java/com/ft/ftchinese/service/StripeEphemeralKeyProvider.kt
@@ -0,0 +1,65 @@
+package com.ft.ftchinese.service
+
+import com.ft.ftchinese.model.Account
+import com.ft.ftchinese.util.ClientError
+import com.stripe.android.EphemeralKeyProvider
+import com.stripe.android.EphemeralKeyUpdateListener
+import kotlinx.coroutines.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+
+class StripeEphemeralKeyProvider(
+        private val account: Account
+) : EphemeralKeyProvider, AnkoLogger {
+
+    private var job: Job? = null
+
+    override fun createEphemeralKey(apiVersion: String, keyUpdateListener: EphemeralKeyUpdateListener) {
+
+        info("API version: $apiVersion")
+
+        job = GlobalScope.launch {
+            try {
+                val rawKey = withContext(Dispatchers.IO) {
+                    account.createEphemeralKey(apiVersion)
+                }
+
+
+                // Example
+                // {
+                //  "id": "ephkey_1EsLbEBzTK0hABgJoDVLylpw",
+                //  "object": "ephemeral_key",
+                //  "associated_objects": [
+                //    {
+                //      "type": "customer",
+                //      "id": "cus_FMPLOeckvP32Fo"
+                //    }
+                //  ],
+                //  "created": 1562210828,
+                //  "expires": 1562214428,
+                //  "livemode": false,
+                //  "secret": "ek_test_***aq5O"
+                //}
+                // The key lasts 1 hour.
+                info("rawKey: $rawKey")
+
+                if (rawKey != null) {
+                    keyUpdateListener.onKeyUpdate(rawKey)
+                    return@launch
+                }
+
+                keyUpdateListener.onKeyUpdateFailure(200, "Empty raw key")
+
+            } catch (e: ClientError) {
+                keyUpdateListener.onKeyUpdateFailure(e.statusCode, e.message)
+
+            } catch (e: Exception) {
+                keyUpdateListener.onKeyUpdateFailure(500, e.localizedMessage)
+            }
+        }
+    }
+
+    fun onDestroy() {
+        job?.cancel()
+    }
+}
