4c4dfacf8b63a6f28ab2288e7c6b02eda3eac68e
weiguo.ni <neefrankie@gmail.com>
2019-07-12 20:36:14 +0800

    feat: add tier cycle fields

--- app/src/main/java/com/ft/ftchinese/model/order/StripeSubParams.kt
+++ app/src/main/java/com/ft/ftchinese/model/order/StripeSubParams.kt
@@ -1,6 +1,13 @@
 package com.ft.ftchinese.model.order
 
+import com.ft.ftchinese.util.KCycle
+import com.ft.ftchinese.util.KTier
+
 data class StripeSubParams(
+        @KTier
+        val tier: Tier,
+        @KCycle
+        val cycle: Cycle,
         val customer: String,
         val coupon: String? = null,
         val defaultPaymentMethod: String?
