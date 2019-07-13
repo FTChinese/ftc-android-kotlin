0d7deb5eda0623a34f689133246675d0b526440a
neefrankie <neefrankie@gmail.com>
2019-07-13 23:04:28 +0800

    refactor: include all order info

--- app/src/main/java/com/ft/ftchinese/model/Subscription.kt
+++ app/src/main/java/com/ft/ftchinese/model/Subscription.kt
@@ -2,6 +2,7 @@ package com.ft.ftchinese.model
 
 import com.beust.klaxon.Json
 import com.ft.ftchinese.model.order.Cycle
+import com.ft.ftchinese.model.order.OrderUsage
 import com.ft.ftchinese.model.order.PayMethod
 import com.ft.ftchinese.model.order.Tier
 import com.ft.ftchinese.util.*
@@ -13,39 +14,44 @@ import org.threeten.bp.temporal.ChronoUnit
  * This is used to store subscription order locally,
  * and also used to to parse orders retrieved from API.
  */
-data class Subscription(
-        val orderId: String,
+open class Subscription(
+        open val id: String,
 
         @KTier
-        val tier: Tier,
+        open val tier: Tier,
 
         @KCycle
-        val cycle: Cycle,
+        open val cycle: Cycle,
 
         // After supporting upgrading, the purchased membership
         // duration might not be exactly one cycle.
-        val cycleCount: Long = 1,
+        open val cycleCount: Long = 1,
         // 1 day less than server side so that we could compare
         // locally saved date against server data.
-        val extraDays: Long = 0,
+        open val extraDays: Long = 0,
 
-        var netPrice: Double,
+        open var netPrice: Double,
+
+        @KOrderUsage
+        open val usageType: OrderUsage,
 
         @KPayMethod
-        val payMethod: PayMethod,
+        open val payMethod: PayMethod,
 
         @KDateTime
-        val createdAt: ZonedDateTime = ZonedDateTime.now(),
+        open val createdAt: ZonedDateTime = ZonedDateTime.now(),
 
-        val isUpgrade: Boolean = false,
+//        val isUpgrade: Boolean = false,
 
         @Json(ignored = true)
         var confirmedAt: ZonedDateTime? = null,
 
         @KDate
+        @Json(ignored = true)
         var startDate: LocalDate? = null,
 
         @KDate
+        @Json(ignored = true)
         var endDate: LocalDate? = null
 ) {
 
@@ -72,7 +78,7 @@ data class Subscription(
         // For upgrading the starting date is also today.
         val start = when {
             m.expireDate == null -> today
-            isUpgrade -> today
+            usageType == OrderUsage.UPGRADE -> today
             m.expireDate.isBefore(today) -> today
             else -> m.expireDate
         }
