dc36ba71beb344e505f7143219bc88af20a2a97d
weiguo.ni <neefrankie@gmail.com>
2019-07-16 16:58:34 +0800

    removed

--- app/src/main/java/com/ft/ftchinese/ui/pay/OrderResult.kt
+++
@@ -1,9 +0,0 @@
-package com.ft.ftchinese.ui.pay
-
-import com.ft.ftchinese.model.order.Order
-
-data class OrderResult(
-        val success: Order? = null,
-        val error: Int? = null,
-        val exception: Exception? = null
-)
