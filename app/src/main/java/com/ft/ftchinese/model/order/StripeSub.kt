e38d0d18c169c41c1c8a32c5d8feecc547d238b6
weiguo.ni <neefrankie@gmail.com>
2019-07-17 18:10:44 +0800

    refactor: test payment result

--- app/src/main/java/com/ft/ftchinese/model/order/StripeSub.kt
+++ app/src/main/java/com/ft/ftchinese/model/order/StripeSub.kt
@@ -4,7 +4,12 @@ import com.ft.ftchinese.util.KDateTime
 import com.ft.ftchinese.util.KStripeSubStatus
 import org.threeten.bp.ZonedDateTime
 
-
+/**
+ * Handle initial payment outcome
+ * 1. Payment success
+ * 2. Payment failure
+ * 3. Payment requires customer action.
+ */
 data class StripeSub(
         val cancelAtPeriodEnd: Boolean,
         @KDateTime
@@ -16,6 +21,23 @@ data class StripeSub(
         @KDateTime
         val endedAt: ZonedDateTime? = null,
         val latestInvoice: StripeInvoice,
+
+        // lets us know whether we can provision the good or service associated with the subscription.
         @KStripeSubStatus
         val status: StripeSubStatus?
-)
+) {
+        // The payment is complete, and you should promptly provision access to the good or service.
+        fun isSuccess(): Boolean {
+            return status == StripeSubStatus.Active && latestInvoice.paymentIntent.status == PaymentIntentStatus.Succeeded
+        }
+
+        // Ask user to change payment method.
+        fun isFailure(): Boolean {
+                return status == StripeSubStatus.Incomplete && latestInvoice.paymentIntent.status == PaymentIntentStatus.RequiresPaymentMethod
+        }
+
+        // Some payment methods require additional steps, such as authentication, in order to complete the payment process.
+        fun requiresAction(): Boolean {
+                return status == StripeSubStatus.Incomplete && latestInvoice.paymentIntent.status == PaymentIntentStatus.RequiresAction
+        }
+}
