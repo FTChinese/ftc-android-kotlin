30e59cf31e50efef1bfcda162821b46873d71a24
weiguo.ni <neefrankie@gmail.com>
2019-07-17 18:11:06 +0800

    test

--- app/src/test/java/com/ft/ftchinese/model/order/StripeSubTest.kt
+++ app/src/test/java/com/ft/ftchinese/model/order/StripeSubTest.kt
@@ -1,7 +1,6 @@
 package com.ft.ftchinese.model.order
 
 import com.ft.ftchinese.util.json
-import org.junit.Assert.*
 import org.junit.Test
 
 private val data = """
@@ -18,13 +17,15 @@ private val data = """
 		"invoicePdf": "https://pay.stripe.com/invoice/invst_7w4r7nLr0fylK5nMaG7nVEE6Zs/pdf",
 		"number": "6C9D489E-0007",
 		"paid": true,
-		"paymentIntentStatus": "succeeded",
-		"receiptNumber": "2517-0427",
-		"status": "paid"
+		"paymentIntent": {
+			"clientSecret": "pi_1EwpCsBzTK0hABgJWF2EOnbd_secret_OmJNq8nSigylnFBBnCGMoNjqs",
+			"status": "succeeded"
+		},
+		"receiptNumber": "2517-0427"
 	},
 	"startDate": "2019-07-16T11:52:30Z",
 	"status": "active"
-}    
+} 
 """.trimIndent()
 
 class StripeSubTest {
