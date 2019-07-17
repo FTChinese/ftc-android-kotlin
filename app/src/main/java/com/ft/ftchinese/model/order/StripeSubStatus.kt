c827547193d8469e60a331140f0cea32e91292a8
weiguo.ni <neefrankie@gmail.com>
2019-07-17 18:10:59 +0800

    doc

--- app/src/main/java/com/ft/ftchinese/model/order/StripeSubStatus.kt
+++ app/src/main/java/com/ft/ftchinese/model/order/StripeSubStatus.kt
@@ -1,11 +1,21 @@
 package com.ft.ftchinese.model.order
 
 enum class StripeSubStatus(val symbol: String) {
+    // if the initial payment attempt fails.
     Incomplete("incomplete"),
+
+    // If the first invoice is not paid within 23 hours
+    // This is a terminal state, the open invoice will be voided and no further invoices will be generated
     IncompleteExpired("incomplete_expired"),
+
+
     Trialing("trialing"),
     Active("active"),
+
+    // when payment to renew it fails
     PastDue("past_due"),
+
+    // when Stripe has exhausted all payment retry attempts.
     Canceled("canceled"),
     Unpaid("unpaid");
 
