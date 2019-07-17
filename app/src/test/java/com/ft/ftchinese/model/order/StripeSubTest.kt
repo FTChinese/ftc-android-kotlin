419407288706a09cc77960a5d898641d8890cebb
weiguo.ni <neefrankie@gmail.com>
2019-07-17 15:45:13 +0800

    test

---
+++ app/src/test/java/com/ft/ftchinese/model/order/StripeSubTest.kt
@@ -0,0 +1,36 @@
+package com.ft.ftchinese.model.order
+
+import com.ft.ftchinese.util.json
+import org.junit.Assert.*
+import org.junit.Test
+
+private val data = """
+{
+	"cancelAtPeriodEnd": false,
+	"created": "2019-07-16T11:52:30Z",
+	"currentPeriodEnd": "2020-07-16T11:52:30Z",
+	"currentPeriodStart": "2019-07-16T11:52:30Z",
+	"endedAt": null,
+	"latestInvoice": {
+		"created": "2019-07-16T11:52:30Z",
+		"currency": "gbp",
+		"hostedInvoiceUrl": "https://pay.stripe.com/invoice/invst_7w4r7nLr0fylK5nMaG7nVEE6Zs",
+		"invoicePdf": "https://pay.stripe.com/invoice/invst_7w4r7nLr0fylK5nMaG7nVEE6Zs/pdf",
+		"number": "6C9D489E-0007",
+		"paid": true,
+		"paymentIntentStatus": "succeeded",
+		"receiptNumber": "2517-0427",
+		"status": "paid"
+	},
+	"startDate": "2019-07-16T11:52:30Z",
+	"status": "active"
+}    
+""".trimIndent()
+
+class StripeSubTest {
+    @Test fun parseJson() {
+        val stripeSub = json.parse<StripeSub>(data)
+
+        println(stripeSub)
+    }
+}
