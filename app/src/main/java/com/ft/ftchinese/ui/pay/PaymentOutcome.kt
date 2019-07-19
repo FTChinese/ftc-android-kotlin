9bad7dea572ebfe81594f6d8e78fb1347fcfed3a
weiguo.ni <neefrankie@gmail.com>
2019-07-19 22:00:27 +0800

    feat: user PaymentOutcome to collect text for PaymentResultActivity

---
+++ app/src/main/java/com/ft/ftchinese/ui/pay/PaymentOutcome.kt
@@ -0,0 +1,13 @@
+package com.ft.ftchinese.ui.pay
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class PaymentOutcome(
+        val invoice: String,
+        val plan: String,
+        val period: String,
+        val subStatus: String,
+        val paymentStatus: String
+) : Parcelable
