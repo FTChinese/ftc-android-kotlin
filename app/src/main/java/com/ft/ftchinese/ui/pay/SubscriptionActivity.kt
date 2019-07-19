15e5b09824dc51a0fbfd846e5d5f1b16af31cb5d
weiguo.ni <neefrankie@gmail.com>
2019-07-19 22:04:31 +0800

    feat: handle stripe requires action

--- app/src/main/java/com/ft/ftchinese/ui/pay/SubscriptionActivity.kt
+++ app/src/main/java/com/ft/ftchinese/ui/pay/SubscriptionActivity.kt
@@ -19,8 +19,8 @@ import com.ft.ftchinese.ui.account.AccountViewModel
 import com.ft.ftchinese.ui.login.AccountResult
 import com.ft.ftchinese.util.RequestCode
 import com.stripe.android.*
-import com.stripe.android.model.Customer
-import com.stripe.android.model.PaymentMethod
+import com.stripe.android.exception.InvalidRequestException
+import com.stripe.android.model.*
 import com.stripe.android.view.PaymentMethodsActivity
 import com.stripe.android.view.PaymentMethodsActivityStarter
 import kotlinx.android.synthetic.main.activity_subscription.*
@@ -30,9 +30,9 @@ import kotlinx.android.synthetic.main.simple_toolbar.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.info
-import org.jetbrains.anko.toast
+import org.jetbrains.anko.*
+import org.jetbrains.anko.appcompat.v7.Appcompat
+import org.threeten.bp.format.DateTimeFormatter
 import kotlin.Exception
 
 @kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -42,10 +42,13 @@ class SubscriptionActivity : ScopedAppActivity(),
     private lateinit var sessionManager: SessionManager
     private lateinit var checkOutViewModel: CheckOutViewModel
     private lateinit var accountViewModel: AccountViewModel
-    private lateinit var stripePref: StripePref
+    private lateinit var idempotency: Idempotency
+
     private lateinit var stripe: Stripe
+    private lateinit var planCache: StripePlanCache
 
     private var plan: Plan? = null
+    private var subType: OrderUsage? = null
     private var paymentMethod: PaymentMethod? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -59,6 +62,7 @@ class SubscriptionActivity : ScopedAppActivity(),
         }
 
         PaymentConfiguration.init(BuildConfig.STRIPE_KEY)
+
         checkOutViewModel = ViewModelProviders.of(this)
                 .get(CheckOutViewModel::class.java)
 
@@ -73,7 +77,12 @@ class SubscriptionActivity : ScopedAppActivity(),
             onAccountRefreshed(it)
         })
 
-        stripePref = StripePref.getInstance(this)
+        checkOutViewModel.stripeSubResult.observe(this, Observer {
+            onStripeSubResult(it)
+        })
+
+        idempotency = Idempotency.getInstance(this)
+        planCache = StripePlanCache.getInstance(this)
 
         plan = intent.getParcelableExtra(EXTRA_FTC_PLAN)
         sessionManager = SessionManager.getInstance(this)
@@ -85,44 +94,36 @@ class SubscriptionActivity : ScopedAppActivity(),
                         .publishableKey
         )
 
+        val account = sessionManager.loadAccount()
+
+        subType = account?.membership?.subType(plan)
 
         initUI()
         setupCustomerSession()
+
+        if (isNetworkConnected() && account != null) {
+            checkOutViewModel.getStripePlan(account, plan)
+        }
     }
 
     private fun initUI() {
 
-        buildText(
-                stripePref.getPlan(plan?.getId())
-        )
-
-        val account = sessionManager.loadAccount() ?: return
-
-        if (isNetworkConnected()) {
-
-            checkOutViewModel.getStripePlan(account, plan)
-        }
+        buildText(planCache.load(plan?.getId()))
 
         tv_payment_method.setOnClickListener {
             PaymentMethodsActivityStarter(this)
                     .startForResult(RequestCode.SELECT_SOURCE)
         }
 
-        when (account.membership.subType(plan)) {
-            OrderUsage.CREATE -> {
-                btn_subscribe.setOnClickListener {
-                    onNewSubscription()
-                }
-            }
-            OrderUsage.UPGRADE -> {
-                btn_subscribe.text = getString(R.string.title_upgrade)
-                btn_subscribe.setOnClickListener {
-                    onUpgradeSubscription()
-                }
-            }
+        btn_subscribe.setOnClickListener {
+            startSubscribing()
         }
 
         enableButton(false)
+
+        if (subType == OrderUsage.UPGRADE) {
+            btn_subscribe.text = getString(R.string.title_upgrade)
+        }
     }
 
     private fun setupCustomerSession() {
@@ -169,7 +170,6 @@ class SubscriptionActivity : ScopedAppActivity(),
         }
     }
 
-
     private fun buildText(stripePlan: StripePlan?) {
         if (stripePlan != null) {
 
@@ -203,10 +203,10 @@ class SubscriptionActivity : ScopedAppActivity(),
 
         buildText(result.success)
 
-        stripePref.savePlan(plan?.getId(), result.success)
+        planCache.save(result.success, plan?.getId())
     }
 
-    private fun onNewSubscription() {
+    private fun startSubscribing() {
         val account = sessionManager.loadAccount() ?: return
         val p = plan ?: return
 
@@ -226,153 +226,199 @@ class SubscriptionActivity : ScopedAppActivity(),
             return
         }
 
-        val idempotency = stripePref.getKey(p.tier)
-
-        val subParams = StripeSubParams(
-                tier = p.tier,
-                cycle = p.cycle,
-                customer = account.stripeId,
-                defaultPaymentMethod = pm.id,
-                idempotency = idempotency.key
-        )
-
         showProgress(true)
         enableButton(false)
 
         toast("Creating subscription...")
 
-        launch {
+        when (subType) {
+            OrderUsage.CREATE -> {
+                checkOutViewModel.createStripeSub(account, StripeSubParams(
+                        tier = p.tier,
+                        cycle = p.cycle,
+                        customer = account.stripeId,
+                        defaultPaymentMethod = pm.id,
+                        idempotency = idempotency.retrieveKey()
+                ))
+            }
+            OrderUsage.UPGRADE -> {
+                idempotency.clear()
 
-            try {
-                val sub = withContext(Dispatchers.IO) {
-                    account.createSubscription(subParams)
-                }
-
-                info("Subscription result: $sub")
-
-
-
-                if (sub == null) {
-                    toast("Subscription failed")
-                    showProgress(false)
-                    enableButton(true)
-                    return@launch
-                }
-
-
-                toast("Received response")
-
-                info("Stripe sub: $sub")
-
-                onSubscribed(sub.latestInvoice.paymentIntent.status)
-            } catch (e: Exception) {
-                info(e)
-
-                showProgress(false)
-                enableButton(true)
-                handleException(e)
+                checkOutViewModel.upgradeStripeSub(account, StripeSubParams(
+                        tier = p.tier,
+                        cycle = p.cycle,
+                        customer = account.stripeId,
+                        defaultPaymentMethod = pm.id,
+                        idempotency = idempotency.retrieveKey()
+                ))
             }
         }
     }
 
-    private fun onUpgradeSubscription() {
-        val account = sessionManager.loadAccount() ?: return
-        val p = plan ?: return
+    private fun onStripeSubResult(subResult: StripeSubResult?) {
+        showProgress(false)
 
-        if (account.stripeId == null) {
-            toast("You are not a stripe customer yet")
+
+        if (subResult == null) {
+            idempotency.clear()
+            enableButton(true)
+            toast("Cannot fetch your subscription data from stripe")
             return
         }
 
-        val pm = paymentMethod
-        if (pm == null) {
-            toast(R.string.prompt_pay_method_unknown)
+        if (subResult.error != null) {
+            toast(subResult.error)
+            enableButton(true)
+            idempotency.clear()
             return
         }
 
-        if (!isNetworkConnected()) {
-            toast(R.string.prompt_no_network)
+        if (subResult.exception != null) {
+            handleException(subResult.exception)
+            enableButton(true)
+            idempotency.clear()
             return
         }
 
-        val idempotency = stripePref.getKey(p.tier)
-
-        val subParams = StripeSubParams(
-                tier = p.tier,
-                cycle = p.cycle,
-                customer = account.stripeId,
-                defaultPaymentMethod = pm.id,
-                idempotency = idempotency.key
-        )
-
-        showProgress(true)
-        enableButton(false)
-
-        toast("Creating subscription...")
-
-        launch {
-
-            try {
-                val sub = withContext(Dispatchers.IO) {
-                    account.upgradeStripeSub(subParams)
-                }
-
-                info("Upgrade result: $sub")
-
-
-                if (sub == null) {
-                    toast("Upgrade failed")
-                    showProgress(false)
-                    enableButton(true)
-                    return@launch
-                }
-
-
-                toast("Received response")
-
-                info("Stripe sub: $sub")
-
-                onSubscribed(sub.latestInvoice.paymentIntent.status)
-            } catch (e: Exception) {
-                info(e)
-
-                showProgress(false)
-                enableButton(true)
-                handleException(e)
-            }
+        val sub = subResult.success
+        if (sub == null) {
+            toast("Subscription failed")
+            enableButton(true)
+            idempotency.clear()
+            return
         }
-    }
 
-    private fun onSubscribed(status: PaymentIntentStatus?) {
-        when (status) {
-            PaymentIntentStatus.RequiresPaymentMethod -> {
-                toast("Requires payment method")
-            }
-            PaymentIntentStatus.RequiresConfirmation -> {
-                toast("Requires confirmation")
-            }
-            PaymentIntentStatus.RequiresAction -> {
-                toast("Requires action")
-            }
-            PaymentIntentStatus.Processing -> {
-                toast("Processing")
-            }
-            PaymentIntentStatus.RequiresCapture -> {
-                toast("Requires capture")
-            }
-            PaymentIntentStatus.Canceled -> {
-                toast("Canceled")
-            }
-            PaymentIntentStatus.Succeeded -> {
+        info("Subscription result: $sub")
+
+        when {
+            sub.succeeded() -> {
                 toast("Success")
-                showProgress(false)
-                enableButton(true)
+
                 val account = sessionManager.loadAccount() ?: return
                 toast(R.string.prompt_refreshing)
 
 //                accountViewModel.refresh(account)
+
+                PaymentResultActivity.start(this, buildOutcome(sub))
+//                setResult(Activity.RESULT_OK)
+//                finish()
+            }
+            sub.failure() -> {
+                alert(
+                        Appcompat,
+                        "Subscription failed. Please retry or change you payment card",
+                        "Failed"
+                ).show()
+
+                enableButton(true)
+                idempotency.clear()
+            }
+
+            sub.requiresAction() -> {
+
+                enableButton(true)
+
+                alert(
+                        Appcompat,
+                        "Your payment is incomplete and requires further actions",
+                        "Requires Action"
+                ) {
+                    positiveButton("Go") {
+                        it.dismiss()
+                        createPaymentIntent(sub)
+                    }
+                    negativeButton("Cancel") {
+                        it.dismiss()
+                    }
+                }.show()
             }
         }
+    }
+
+    private fun createPaymentIntent(sub: StripeSub) {
+        info("Creating payment intent params...")
+
+        val paymentIntentParams = PaymentIntentParams
+                .createConfirmPaymentIntentWithPaymentMethodId(
+                        paymentMethod?.id,
+                        sub.latestInvoice.paymentIntent.clientSecret,
+                        "ftc://stripe-post-authentication-return-url"
+                )
+
+        info("confirm payment intent...")
+        launch {
+            try {
+
+                val paymentIntent = withContext(Dispatchers.IO) {
+                    stripe.confirmPaymentIntentSynchronous(
+                            paymentIntentParams,
+                            PaymentConfiguration.getInstance().publishableKey
+                    )
+                } ?: return@launch
+
+                 info("Payment intent: ${paymentIntent.toMap()}")
+
+                info("Payment intent status: ${paymentIntent.status}")
+
+                 if (paymentIntent.status == StripeIntent.Status.RequiresAction) {
+                     val redirectUrl = paymentIntent.redirectUrl ?: return@launch
+
+                     startActivity(Intent(Intent.ACTION_VIEW, redirectUrl))
+                 }
+
+            } catch (e: InvalidRequestException) {
+                info(e)
+
+                alert(Appcompat, "It seems your subscription already succeeded. You can refresh your account or contact our customer service", "Problems")
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+
+        info("Received new intent ...")
+        if (intent?.data != null && intent.data!!.query != null) {
+            val host =intent.data!!.host
+
+            info("Host: $host")
+
+            val clientSecret  = intent.data!!.getQueryParameter("payment_intent_client_secret")
+
+            info("Client secret after redirect: $clientSecret")
+
+            val retrievePaymentIntentParams = PaymentIntentParams
+                    .createRetrievePaymentIntentParams(clientSecret!!)
+
+            launch {
+                val paymentIntent = withContext(Dispatchers.IO) {
+                    stripe.retrievePaymentIntentSynchronous(retrievePaymentIntentParams)
+                }
+
+                info("Retrieve payment intent: ${paymentIntent?.toMap()}")
+
+                if (paymentIntent == null) {
+                    alert("Failed to retrieve payment intent").show()
+
+                    return@launch
+                }
+
+                alert("Payment intent status: ${paymentIntent.status}").show()
+            }
+        }
+    }
+
+    private fun buildOutcome(sub: StripeSub): PaymentOutcome {
+        val start = sub.currentPeriodStart.format(DateTimeFormatter.ISO_LOCAL_DATE)
+        val end = sub.currentPeriodEnd.format(DateTimeFormatter.ISO_LOCAL_DATE)
+
+        return PaymentOutcome(
+                invoice = sub.latestInvoice.number,
+                plan = getTierCycleText(plan?.tier, plan?.cycle) ?: "",
+                period = "$start - $end",
+                subStatus = sub.status.toString(),
+                paymentStatus = sub.latestInvoice.paymentIntent.status.toString()
+        )
     }
 
     private fun onAccountRefreshed(accountResult: AccountResult?) {
