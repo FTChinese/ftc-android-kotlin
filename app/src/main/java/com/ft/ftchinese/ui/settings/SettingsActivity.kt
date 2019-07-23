6753893a9d84378a4c743ec55f229f7fdcdad221
neefrankie <neefrankie@gmail.com>
2019-07-23 23:55:52 +0800

    test download manager

--- app/src/main/java/com/ft/ftchinese/ui/settings/SettingsActivity.kt
+++ app/src/main/java/com/ft/ftchinese/ui/settings/SettingsActivity.kt
@@ -1,10 +1,14 @@
 package com.ft.ftchinese.ui.settings
 
+import android.app.DownloadManager
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import androidx.appcompat.app.AppCompatActivity
 import com.ft.ftchinese.R
+import kotlinx.android.synthetic.main.activity_settings.*
 import kotlinx.android.synthetic.main.simple_toolbar.*
 
 // Reference: https://developer.android.com/guide/topics/ui/settings
@@ -24,6 +28,30 @@ class SettingsActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
                 .replace(R.id.preference_fragment, PreferenceFragment.newInstance())
                 .commit()
+
+        btn_check_version.setOnClickListener {
+            download()
+        }
+    }
+
+    private fun download() {
+        val req = DownloadManager.Request(Uri.parse("http://creatives.ftacademy.cn/minio/android/ftchinese-v2.1.2-ftc-release.apk"))
+        req.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI)
+        req.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+        req.setDestinationInExternalFilesDir(this, Environment.DIRECTORY_DOWNLOADS, "com.ftchinese")
+        req.setTitle("android.apk")
+        req.setDescription("Open it after downloaded")
+        req.setMimeType("application/vnd.android.package-archive")
+
+        val dm: DownloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val downloadId = dm.enqueue(req)
+
+        val query = DownloadManager.Query().setFilterById(downloadId)
+        val c = dm.query(query)
+        if (c != null) {
+
+        }
+
     }
 
     companion object {
