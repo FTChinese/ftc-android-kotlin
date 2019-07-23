ab7838947b959aafb4cf5a1e21ea7c6ea2835e11
neefrankie <neefrankie@gmail.com>
2019-07-23 08:33:11 +0800

    experiment with recycler view to replace ftc account items

--- app/src/main/java/com/ft/ftchinese/ui/account/FtcFragment.kt
+++ app/src/main/java/com/ft/ftchinese/ui/account/FtcFragment.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
