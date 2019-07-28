05e5696b2e768e412383334ba0060d355e13126f
weiguo.ni <neefrankie@gmail.com>
2019-07-28 10:03:57 +0800

    feat: add a test activity

--- app/src/main/java/com/ft/ftchinese/ui/TestActivity.kt
+++ app/src/main/java/com/ft/ftchinese/ui/TestActivity.kt
@@ -11,16 +11,26 @@ import android.os.Bundle
 import android.os.Environment
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AlertDialog
+import android.view.View
+import androidx.core.content.edit
+import androidx.fragment.app.commit
 import com.ft.ftchinese.R
 import com.ft.ftchinese.service.PollService
+import com.ft.ftchinese.ui.account.UnlinkActivity
+import com.ft.ftchinese.ui.account.UnlinkAnchorFragment
+import com.ft.ftchinese.ui.login.WxExpireDialogFragment
+import com.ft.ftchinese.util.RequestCode
 import kotlinx.android.synthetic.main.activity_test.*
+import kotlinx.android.synthetic.main.progress_bar.*
 import kotlinx.android.synthetic.main.simple_toolbar.*
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.appcompat.v7.Appcompat
+import org.jetbrains.anko.info
 import org.jetbrains.anko.toast
+import java.io.File
 
-class TestActivity : AppCompatActivity() {
-
-    private var downloadId: Long? = null
+class TestActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,19 +42,14 @@ class TestActivity : AppCompatActivity() {
             setDisplayShowTitleEnabled(true)
         }
 
-        btn_test_alert.setOnClickListener {
-            AlertDialog.Builder(this)
-                    .setTitle("Title")
-                    .setMessage("Test alert dialog")
-                    .setPositiveButton("OK") { dialog, which ->
 
-                        dialog.dismiss()
-                    }
-                    .setNegativeButton("Cancel") { dialog, which ->
-                        dialog.dismiss()
-                    }
-                    .create().show()
-        }
+        info("Internal directory of this app: $filesDir")
+        info("Internal directory for this app's temporary cache files: $cacheDir")
+        info("External files dir: ${getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)}")
+
+        info("isExternalStorageWritable: ${isExternalStorageWritable()}")
+        info("External storage directory: ${Environment.getExternalStorageDirectory()}")
+        info("External Download directory: ${Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)}")
 
 //        val i = PollService.newIntent(this)
 //        startService(i)
@@ -52,33 +57,180 @@ class TestActivity : AppCompatActivity() {
 //        PollService.setServiceAlarm(this, true)
 
         registerReceiver(onDownloadComplete, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
-        btn_check_version.setOnClickListener {
+
+        registerReceiver(onNotificationClicked, IntentFilter(DownloadManager.ACTION_NOTIFICATION_CLICKED))
+
+        btn_start_download.setOnClickListener {
+            it.isEnabled = false
+            toast("Start downloading")
+            showProgress(true)
             download()
         }
+
+        btn_find_download.setOnClickListener {
+            findDownload()
+        }
+
+        btn_download_status.setOnClickListener {
+            getDownloadStatus()
+        }
+
     }
 
-    private fun download() {
-        val req = DownloadManager.Request(Uri.parse("https://creatives.ftacademy.cn/minio/android/ftchinese-v2.1.3-ftc-release.apk"))
-            .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI)
-            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
-            .setDestinationInExternalFilesDir(this, Environment.DIRECTORY_DOWNLOADS, "com.ftchinese")
-            .setTitle("FT中文网最新版安卓App")
-            .setDescription("Open it after downloaded")
-            .setMimeType("application/vnd.android.package-archive")
-
-        val dm: DownloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
-        downloadId = dm.enqueue(req)
-
+    private val onNotificationClicked = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            info("intent: $intent")
+        }
     }
 
     private val onDownloadComplete = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
+            val downloadId = loadDownloadId()
+            if (downloadId < 0) {
+                return
+            }
+
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
 
             if (downloadId == id) {
+                btn_start_download.isEnabled = true
+                showProgress(false)
+
+                alertInstall()
+
                 toast("Download complete")
             }
         }
+    }
+
+    private fun alertInstall() {
+        alert(Appcompat,"Download complete. Install now?", "Install") {
+            positiveButton("Install") {
+                it.dismiss()
+
+                val fileUri = findDownload()
+                if (fileUri == null) {
+                    toast("Downloded file not found")
+                }
+
+                val promptInstall = Intent(Intent.ACTION_VIEW).
+                        setDataAndType(Uri.parse(fileUri), "application/vnd.android.package-archive")
+
+                startActivity(promptInstall)
+            }
+            negativeButton("Later") {
+                it.dismiss()
+            }
+        }.show()
+    }
+
+    private fun download() {
+        val req = DownloadManager.Request(Uri.parse("https://creatives.ftacademy.cn/minio/android/ftchinese-v2.1.3-ftc-release.apk"))
+                .setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI)
+                .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "ftchinese-v2.1.3-ftc-release.apk")
+                .setTitle("FT中文网App更新")
+                .setMimeType("application/vnd.android.package-archive")
+
+        val dm: DownloadManager = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        val id = dm.enqueue(req)
+
+        info("Download id: $id")
+
+        saveDownloadId(id)
+    }
+
+    private fun findDownload(): String? {
+        val id = loadDownloadId()
+        if (id < 0) {
+            toast("Download id $id not found")
+            return null
+        }
+
+        val dm = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+
+        val query = DownloadManager.Query().setFilterById(id)
+        val c = dm.query(query)
+        if (c != null) {
+            if (c.moveToFirst()) {
+                val fileUri = c.getString(c.getColumnIndexOrThrow(DownloadManager.COLUMN_LOCAL_URI))
+
+                c.close()
+
+                toast("Download file $fileUri")
+
+                return fileUri
+            }
+        }
+
+        c.close()
+
+        return null
+    }
+
+    private fun getDownloadStatus() {
+
+        val id = loadDownloadId()
+        if (id < 0) {
+            return
+        }
+
+        val dm = getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+
+        val query = DownloadManager.Query().setFilterById(id)
+
+        val c = dm.query(query)
+
+        if (c != null && c.moveToFirst()) {
+            val status = c.getInt(c.getColumnIndexOrThrow(DownloadManager.COLUMN_STATUS))
+
+            when (status) {
+                DownloadManager.STATUS_PENDING -> {
+                    toast("Pending")
+                }
+                DownloadManager.STATUS_PAUSED -> {
+                    toast("Paused")
+                }
+                DownloadManager.STATUS_RUNNING -> {
+                    toast("Running")
+                }
+                DownloadManager.STATUS_SUCCESSFUL -> {
+                    toast("Successful")
+                }
+                DownloadManager.STATUS_FAILED -> {
+                    toast("Failed")
+                }
+            }
+        }
+
+        c.close()
+    }
+
+    private fun saveDownloadId(id: Long) {
+        getSharedPreferences(PREF_FILE_DOWNLOAD, Context.MODE_PRIVATE).edit {
+            putLong(PREF_DOWNLOAD_ID, id)
+        }
+    }
+
+    private fun loadDownloadId(): Long {
+        return getSharedPreferences(PREF_FILE_DOWNLOAD, Context.MODE_PRIVATE).getLong(PREF_DOWNLOAD_ID, -1)
+    }
+
+    private fun showProgress(show: Boolean) {
+        progress_bar.visibility = if (show) View.VISIBLE else View.GONE
+    }
+
+    private fun isExternalStorageWritable(): Boolean {
+        return Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED
+    }
+
+    private fun getPublicAlbumStorageDir(albumName: String): File? {
+        val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), albumName)
+        if (!file.mkdir()) {
+            info("Directory not created")
+        }
+
+        return file
     }
 
     override fun onStart() {
@@ -95,6 +247,7 @@ class TestActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         unregisterReceiver(onDownloadComplete)
+        unregisterReceiver(onNotificationClicked)
     }
 
     private val onShowNotification = object : BroadcastReceiver() {
@@ -122,11 +275,41 @@ class TestActivity : AppCompatActivity() {
 
                 true
             }
+            R.id.menu_test_wechat_expired -> {
+                WxExpireDialogFragment().show(supportFragmentManager, "WxExpiredDialog")
+                true
+            }
+            R.id.menu_test_anko_alert -> {
+                alert(Appcompat,"Anko alert", "Test") {
+                    positiveButton("OK") {
+
+                    }
+                    negativeButton("Cancel") {
+
+                    }
+                }.show()
+
+                true
+            }
+            R.id.menu_show_unlink_activity -> {
+                UnlinkActivity.startForResult(this, RequestCode.UNLINK)
+                true
+            }
+            R.id.menu_show_unlink_anchor -> {
+                supportFragmentManager.commit {
+                    replace(R.id.test_container, UnlinkAnchorFragment.newInstance())
+                }
+                
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
     companion object {
+        private const val PREF_FILE_DOWNLOAD = "app_download"
+        private const val PREF_DOWNLOAD_ID = "download_id"
+
         @JvmStatic
         fun start(context: Context) {
             context.startActivity(Intent(context, TestActivity::class.java))
