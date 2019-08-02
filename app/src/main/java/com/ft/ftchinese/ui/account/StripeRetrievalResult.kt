ee8faaa1002a9a8481cbde761f076033caab9c30
weiguo.ni <neefrankie@gmail.com>
2019-08-02 18:47:09 +0800

    feat: move refreshing stripe subscription

--- app/src/main/java/com/ft/ftchinese/ui/account/StripeRetrievalResult.kt
+++ app/src/main/java/com/ft/ftchinese/ui/account/StripeRetrievalResult.kt
@@ -1,8 +1,8 @@
-package com.ft.ftchinese.ui.pay
+package com.ft.ftchinese.ui.account
 
 import com.ft.ftchinese.model.order.StripeSub
 
-data class StripeSubResult(
+data class StripeRetrievalResult(
         val success: StripeSub? = null,
         val error: Int? = null,
         val exception: Exception? = null
