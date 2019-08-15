2669810a1ee74ef3bedb840e3b69943dc9ad57dd
weiguo.ni <neefrankie@gmail.com>
2019-08-15 17:25:56 +0800

    feat: add MemberStatus and NextStep

--- app/src/main/java/com/ft/ftchinese/model/Permission.kt
+++ app/src/main/java/com/ft/ftchinese/model/Permission.kt
@@ -11,3 +11,23 @@ enum class Permission(val id: Int) {
     STANDARD(2), // 0010
     PREMIUM(4) // 0100
 }
+
+enum class MemberStatus {
+    Vip, // MemberActivity do not show any buttons
+    Empty, // Show Subscribe button only
+    Expired, // Show Subscribe button only
+    InactiveStripe, // if Member.status.shouldResubscribe, show Subscribe button only
+    ActiveStandard, // Always show Upgrade button.
+    ActivePremium
+    // For ActiveStandard and ActivePremium, if autoRenew, do not show Renew button; else test whether expireDate is within 3 year, and show Renew button if within allowed range.
+}
+
+/**
+ * Determines which buttons are visible on membership info activity.
+ */
+enum class NextStep(val id: Int) {
+    None(0),
+    Resubscribe(1),
+    Renew(2),
+    Upgrade(4)
+}
