27927997048ccc4e7d516fd664af14da1c02d9dd
weiguo.ni <neefrankie@gmail.com>
2019-08-15 18:04:40 +0800

    test

--- app/src/test/java/com/ft/ftchinese/model/reader/MembershipTest.kt
+++ app/src/test/java/com/ft/ftchinese/model/reader/MembershipTest.kt
@@ -1,49 +1,113 @@
 package com.ft.ftchinese.model.reader
 
+import com.ft.ftchinese.model.NextStep
 import com.ft.ftchinese.model.Permission
+import com.ft.ftchinese.model.order.Cycle
 import com.ft.ftchinese.model.order.PayMethod
 import com.ft.ftchinese.model.order.StripeSubStatus
 import com.ft.ftchinese.model.order.Tier
 import org.junit.Assert.*
 import org.junit.Test
 import org.threeten.bp.LocalDate
+import org.threeten.bp.Month
+import org.threeten.bp.temporal.ChronoUnit
 
 class MembershipTest {
+
+    private val mVip =  Membership(
+            tier = null,
+            cycle = null,
+            expireDate = null,
+            payMethod = null,
+            autoRenew = null,
+            status = null,
+            vip = true
+    )
+
+    private val mStandard = Membership(
+            tier = Tier.STANDARD,
+            cycle = Cycle.YEAR,
+            expireDate = LocalDate.now().plusMonths(1),
+            payMethod = PayMethod.ALIPAY,
+            autoRenew = false,
+            status = null,
+            vip = false
+    )
+
+    private val mPremium = Membership(
+            tier = Tier.PREMIUM,
+            cycle = Cycle.YEAR,
+            expireDate = LocalDate.now().plusMonths(1),
+            payMethod = PayMethod.WXPAY,
+            autoRenew = false,
+            status = null,
+            vip = false
+    )
+
+    @Test fun remainingDays() {
+        val diff = LocalDate.now().until(LocalDate.of(2020, Month.JULY, 4), ChronoUnit.DAYS)
+
+        println(diff)
+    }
+
     @Test fun vip() {
-        val m = Membership(
-                vip = true
-        )
-        val (readerPermBits, status) = m.getPermission()
+
+        val (readerPermBits, status) = mVip.getPermission()
+        val nextAction = mVip.nextAction()
 
         println("Member status $status")
 
-        assertTrue((readerPermBits and Permission.FREE.id) > 0)
-        assertTrue((readerPermBits and Permission.STANDARD.id) > 0)
-        assertTrue((readerPermBits and Permission.PREMIUM.id) > 0)
+        assertTrue(
+                "VIP can read free content",
+                (readerPermBits and Permission.FREE.id) > 0
+        )
+        assertTrue(
+                "VIP can read standard content",
+                (readerPermBits and Permission.STANDARD.id) > 0
+        )
+        assertTrue(
+                "VIP can read premium content",
+                (readerPermBits and Permission.PREMIUM.id) > 0
+        )
+
+        assertTrue("VIP has no next action", nextAction == NextStep.None.id)
     }
 
     @Test fun standard() {
-        val m = Membership(
-                expireDate = LocalDate.now().plusMonths(1),
-                tier = Tier.STANDARD
-        )
 
-        val (readerPermBits, status) = m.getPermission()
+        val (readerPermBits, status) = mStandard.getPermission()
+        val nextAction = mStandard.nextAction()
 
         println("Member status $status")
 
-        assertTrue((readerPermBits and Permission.FREE.id) > 0)
-        assertTrue((readerPermBits and Permission.STANDARD.id) > 0)
-        assertTrue((readerPermBits and Permission.PREMIUM.id) == 0)
+        assertTrue(
+                "Standard member can read free content",
+                (readerPermBits and Permission.FREE.id) > 0
+        )
+        assertTrue(
+                "Standard member can read standard content",
+                (readerPermBits and Permission.STANDARD.id) > 0
+        )
+        assertFalse(
+                "Standard member cannot reader premium content",
+                (readerPermBits and Permission.PREMIUM.id) > 0
+        )
+
+        assertTrue(
+                "Standard member can renew",
+                (nextAction and NextStep.Renew.id) > 0
+        )
+
+        assertTrue(
+                "Standard member can upgrade",
+                (nextAction and NextStep.Upgrade.id) > 0
+        )
     }
 
     @Test fun premium() {
-        val m = Membership(
-                expireDate = LocalDate.now().plusMonths(1),
-                tier = Tier.PREMIUM
-        )
 
-        val (readerPermBits, status) = m.getPermission()
+
+        val (readerPermBits, status) = mPremium.getPermission()
 
         println("Member status $status")
 
@@ -52,7 +116,7 @@ class MembershipTest {
         assertTrue((readerPermBits and Permission.PREMIUM.id) > 0)
     }
 
-    @Test fun empty() {
+    @Test fun emptyPerm() {
         val m = Membership()
 
         val (readerPermBits, status) = m.getPermission()
