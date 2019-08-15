2fd3af1590793dcbf8065f4115f2f65fdb9603e1
weiguo.ni <neefrankie@gmail.com>
2019-08-15 17:26:16 +0800

    refactor: grantPermission process

--- app/src/main/java/com/ft/ftchinese/ui/pay/grantPermission.kt
+++ app/src/main/java/com/ft/ftchinese/ui/pay/grantPermission.kt
@@ -2,12 +2,12 @@ package com.ft.ftchinese.ui.pay
 
 import android.app.Activity
 import com.ft.ftchinese.R
+import com.ft.ftchinese.model.MemberStatus
 import com.ft.ftchinese.model.reader.Account
 import com.ft.ftchinese.model.Permission
-import com.ft.ftchinese.model.order.PayMethod
-import com.ft.ftchinese.model.order.StripeSubStatus
 import com.ft.ftchinese.ui.login.LoginActivity
-import com.ft.ftchinese.util.RequestCode
+import org.jetbrains.anko.alert
+import org.jetbrains.anko.appcompat.v7.Appcompat
 import org.jetbrains.anko.toast
 
 /**
@@ -24,73 +24,57 @@ fun Activity.grantPermission(account: Account?, contentPerm: Permission): Boolea
         return true
     }
 
-    // Content requires membership.
-    if (account == null) {
+    // Content is not free.
+    val permResult = account?.membership?.getPermission()
+
+
+    // Indicates reader is not logged in.
+    if (permResult == null) {
         toast(R.string.prompt_login_to_read)
         LoginActivity.startForResult(this)
         return false
     }
 
-    if (account.isVip) {
+
+    val (readerPermBits, status) = permResult
+
+    // Free content is excluded when reaching here.
+    if ((readerPermBits and contentPerm.id) > 0) {
         return true
     }
 
-    // If user is not or was not a member.
-    // Take into account the content's permission type.
-    // If content is premium-only, tell user to buy premium.
-    if (!account.isMember) {
-        // If use is not a member yet while content requires
-        // premium, show premium product on top.
-        if (contentPerm == Permission.PREMIUM) {
-            toast(R.string.prompt_premium_only)
-            PaywallActivity.start(this, true)
-        } else {
-            toast(R.string.prompt_subscribe_to_read)
-            PaywallActivity.start(this)
-        }
-
+    if (status == MemberStatus.InactiveStripe) {
+        toast(R.string.stripe_not_active)
+        MemberActivity.start(this)
         return false
     }
 
-    // If stripe subscription status is not active.
-    if (account.membership.payMethod == PayMethod.STRIPE) {
-        if (account.membership.status != StripeSubStatus.Active) {
-            toast(R.string.stripe_not_active)
-            MemberActivity.start(this)
-            return false
-        }
-    }
-
-    // User is a member, but the membership might be expired.
-    // If expired, show user paywall.
-    // If the content requires premium membership, paywall should
-    // display premium card on top.
-    if (account.membership.expired() && account.membership.autoRenew == false) {
-        // If membership is expired, show paywall again,
-        // and put premium product on top.
-        if (contentPerm == Permission.PREMIUM) {
-            toast(R.string.prompt_premium_only)
-            PaywallActivity.start(this, true)
-        } else {
-            // If the article does not require PREMIUM,
-            // go to MemberActivity and ask user to resubscribe.
-            toast(R.string.prompt_membership_expired)
-            MemberActivity.start(this)
-        }
-
-
+    if (status == MemberStatus.Empty) {
+        toast(R.string.prompt_member_only)
+        PaywallActivity.start(this, contentPerm == Permission.PREMIUM)
         return false
     }
 
-
-    // Grant access
-    if ((account.permission and contentPerm.id) <= 0) {
+    if (status == MemberStatus.Expired) {
         toast(R.string.prompt_premium_only)
-        UpgradeActivity.startForResult(this, RequestCode.PAYMENT, true)
+        PaywallActivity.start(this, contentPerm == Permission.PREMIUM)
+
         return false
     }
 
-    // If code reaches here, user must be a valid member.
-    // Access denied could only be caused by premium content.
-    return true
+    if (status == MemberStatus.ActiveStandard) {
+        toast(R.string.prompt_upgrade_premium)
+        MemberActivity.start(this)
+
+        return false
+    }
+
+
+    alert(Appcompat, "There might be problems with your permission. Please check your membership or contact our customer service", "Error") {
+        positiveButton("OK") {
+            it.dismiss()
+        }
+    }
+
+    return false
 }
