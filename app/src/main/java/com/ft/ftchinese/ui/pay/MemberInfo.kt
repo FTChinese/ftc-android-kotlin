f46bf30fef16a27c0848889d6a18515bb5d90b16
weiguo.ni <neefrankie@gmail.com>
2019-08-16 12:19:20 +0800

    refactor: remove show buttons fields

--- app/src/main/java/com/ft/ftchinese/ui/pay/MemberInfo.kt
+++ app/src/main/java/com/ft/ftchinese/ui/pay/MemberInfo.kt
@@ -6,8 +6,5 @@ data class MemberInfo(
         val autoRenewal: Boolean,
         val stripeStatus: String?, // Show stripe status or hide it if null
         val stripeInactive: Boolean, // Show stripe status warning
-        val remains: String?, // Show remaining days that will expire or expired.
-        val showSubscribe: Boolean,
-        val showRenew: Boolean,
-        val showUpgrade: Boolean
+        val remains: String? // Show remaining days that will expire or expired.
 )
