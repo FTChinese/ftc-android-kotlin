e4ae2e19d6b861ad6f04d3737faf58373d91305e
weiguo.ni <neefrankie@gmail.com>
2019-08-16 16:55:38 +0800

    refactor: AvatarResult uses InputStream to handle both locally read image and remote fetch image

--- app/src/main/java/com/ft/ftchinese/ui/account/ImageResult.kt
+++
@@ -1,6 +0,0 @@
-package com.ft.ftchinese.ui.account
-
-data class ImageResult (
-        val success: ByteArray? = null,
-        val exception: Exception? = null
-)
---
+++ app/src/main/java/com/ft/ftchinese/ui/launch/AvatarResult.kt
@@ -0,0 +1,8 @@
+package com.ft.ftchinese.ui.launch
+
+import java.io.InputStream
+
+data class AvatarResult(
+        val success: InputStream? = null,
+        val exception: Exception? = null
+)
