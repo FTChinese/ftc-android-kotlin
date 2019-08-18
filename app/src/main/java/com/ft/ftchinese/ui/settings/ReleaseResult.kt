cd53b34827ddcc04f8a85c2d7907b5fd4996ed0d
weiguo.ni <neefrankie@gmail.com>
2019-08-18 16:07:23 +0800

    refactor: renamed from LatestReleaseResult

--- app/src/main/java/com/ft/ftchinese/ui/settings/ReleaseResult.kt
+++ app/src/main/java/com/ft/ftchinese/ui/settings/ReleaseResult.kt
@@ -2,7 +2,7 @@ package com.ft.ftchinese.ui.settings
 
 import com.ft.ftchinese.model.AppRelease
 
-data class LatestReleaseResult(
+data class ReleaseResult(
         val success: AppRelease? = null,
         val error: Int? = null,
         val exception: Exception? = null
