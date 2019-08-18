07e1fa857f71b394ae7a9b90d5df1eff9a963e29
weiguo.ni <neefrankie@gmail.com>
2019-08-18 16:06:37 +0800

    refactor: move recyclerview

--- app/src/main/java/com/ft/ftchinese/ui/channel/ReadArticleFragment.kt
+++ app/src/main/java/com/ft/ftchinese/ui/channel/ReadArticleFragment.kt
@@ -4,16 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.ft.ftchinese.ui.article.ArticleActivity
 import com.ft.ftchinese.R
 import com.ft.ftchinese.ui.base.ScopedFragment
-import com.ft.ftchinese.database.ReadArticle
 import com.ft.ftchinese.ui.article.ReadArticleViewModel
+import com.ft.ftchinese.ui.base.ReadArticleAdapter
 import kotlinx.android.synthetic.main.fragment_recycler.*
 import org.jetbrains.anko.AnkoLogger
 
@@ -27,7 +24,7 @@ class ReadArticleFragment : ScopedFragment(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        model = ViewModelProviders.of(this).get(ReadArticleViewModel::class.java)
+        model = ViewModelProvider(this).get(ReadArticleViewModel::class.java)
 
         viewAdapter = ReadArticleAdapter(listOf())
 
@@ -53,45 +50,4 @@ class ReadArticleFragment : ScopedFragment(),
     }
 }
 
-class ReadArticleAdapter(private var articles: List<ReadArticle>) :
-        RecyclerView.Adapter<ReadArticleAdapter.ViewHolder>() {
 
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val primaryTextView: TextView = itemView.findViewById(R.id.primary_text_view)
-        val secondaryTextView: TextView = itemView.findViewById(R.id.secondary_text_view)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-                .inflate(
-                        R.layout.card_primary_secondary,
-                        parent,
-                        false)
-
-        return ViewHolder(view)
-    }
-
-    override fun getItemCount(): Int {
-        return articles.size
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val article = articles[position]
-
-        holder.primaryTextView.text = article.title
-        holder.secondaryTextView.text = article.standfirst
-
-        holder.itemView.setOnClickListener {
-            if (article.isWebpage) {
-                ArticleActivity.startWeb(holder.itemView.context, article.toChannelItem())
-            } else {
-                ArticleActivity.start(holder.itemView.context, article.toChannelItem())
-            }
-        }
-    }
-
-    fun setData(newData: List<ReadArticle>) {
-        this.articles = newData
-        notifyDataSetChanged()
-    }
-}
--- app/src/main/java/com/ft/ftchinese/ui/channel/StarredArticleFragment.kt
+++ app/src/main/java/com/ft/ftchinese/ui/channel/StarredArticleFragment.kt
@@ -1,23 +1,16 @@
 package com.ft.ftchinese.ui.channel
 
-import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.ft.ftchinese.ui.article.ArticleActivity
 import com.ft.ftchinese.R
 import com.ft.ftchinese.ui.base.ScopedFragment
-import com.ft.ftchinese.database.StarredArticle
 import com.ft.ftchinese.ui.article.StarArticleViewModel
-import kotlinx.android.synthetic.main.card_primary_secondary.view.*
+import com.ft.ftchinese.ui.base.StarredArticleAdapter
 import kotlinx.android.synthetic.main.fragment_recycler.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
@@ -33,7 +26,7 @@ class StarredArticleFragment : ScopedFragment(),
         super.onCreate(savedInstanceState)
         info("onCreate")
 
-        starViewModel = ViewModelProviders.of(this).get(StarArticleViewModel::class.java)
+        starViewModel = ViewModelProvider(this).get(StarArticleViewModel::class.java)
 
         viewAdapter = StarredArticleAdapter(context)
 
@@ -67,69 +60,4 @@ class StarredArticleFragment : ScopedFragment(),
     }
 }
 
-@kotlinx.coroutines.ExperimentalCoroutinesApi
-class StarredArticleAdapter(private val context: Context?) :
-        RecyclerView.Adapter<StarredArticleAdapter.ViewHolder>() {
 
-    private var articles = mutableListOf(StarredArticle(
-            id = "banner",
-            type = "notice",
-            title = "关于我的收藏",
-            standfirst = """用户您好。目前版本的"收藏文章"数据保存在您的本机，暂时无法与服务器同步，我们会在后续版本中开发远程同步功能。如果你需要查看在FT中文网网站或者旧版app中收藏的文章，可以点击此处打开网页，在FT中文网用户中心"收藏的文章"一栏下查看。"""
-    ))
-
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val primaryTextView: TextView = itemView.primary_text_view
-        val secondaryTextView: TextView = itemView.secondary_text_view
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context)
-                .inflate(
-                        R.layout.card_primary_secondary,
-                        parent,
-                        false)
-
-        return ViewHolder(view)
-    }
-
-    override fun getItemCount(): Int {
-        return articles.size
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val article = articles[position]
-
-        holder.primaryTextView.text = article.title
-        holder.secondaryTextView.text = article.standfirst
-
-        holder.itemView.setOnClickListener {
-            when {
-                article.id == "banner" -> {
-                    val webpage: Uri = Uri.parse("http://users.ftchinese.com/starred")
-                    val intent = Intent(Intent.ACTION_VIEW, webpage)
-
-                    if (context != null) {
-                        if (intent.resolveActivity(context.packageManager) != null) {
-                            context.startActivity(intent)
-                        }
-                    }
-
-//                    val customTabsInt = CustomTabsIntent.Builder().build()
-//                    customTabsInt.launchUrl(context, Uri.parse("http://next.ftchinese.com/user/starred"))
-                }
-                article.isWebpage -> {
-                    ArticleActivity.startWeb(holder.itemView.context, article.toChannelItem())
-                }
-                else -> {
-                    ArticleActivity.start(holder.itemView.context, article.toChannelItem())
-                }
-            }
-        }
-    }
-
-    fun setData(newData: List<StarredArticle>) {
-        this.articles.addAll(newData)
-        notifyDataSetChanged()
-    }
-}
