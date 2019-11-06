62979f6345a15ba325b93aa70752d833ed6efb4d
weiguo.ni <neefrankie@gmail.com>
2019-11-06 10:06:17 +0800

    fix: enclose Fetch int try catch

--- app/src/main/java/com/ft/ftchinese/repository/ReaderRepo.kt
+++ app/src/main/java/com/ft/ftchinese/repository/ReaderRepo.kt
@@ -3,17 +3,23 @@ package com.ft.ftchinese.repository
 import com.beust.klaxon.Klaxon
 import com.ft.ftchinese.model.reader.ReadingDuration
 import com.ft.ftchinese.util.Fetch
+import com.ft.ftchinese.util.currentFlavor
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import kotlin.Exception
 
 class ReaderRepo : AnkoLogger {
 
     fun engaged(dur: ReadingDuration) {
         info("Engagement length of ${dur.userId}: ${dur.startUnix} - ${dur.endUnix}")
 
-        Fetch().post("http://www.ftchinese.com/engagment.php")
-                .jsonBody(Klaxon().toJsonString(dur))
-                .responseString()
+        try {
+            Fetch().post("${currentFlavor.baseUrl}/engagment.php")
+                    .jsonBody(Klaxon().toJsonString(dur))
+                    .responseString()
+        } catch (e: Exception) {
+            info("Error when tracking reading duration $e")
+        }
     }
 
     companion object {
