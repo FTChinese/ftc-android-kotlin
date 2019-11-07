5c39648749ff51ad467905e3a7f862c2468da4b2
weiguo.ni <neefrankie@gmail.com>
2019-11-07 11:24:44 +0800

    fix: tracking url

--- app/src/main/java/com/ft/ftchinese/repository/ReaderRepo.kt
+++ app/src/main/java/com/ft/ftchinese/repository/ReaderRepo.kt
@@ -1,24 +1,84 @@
 package com.ft.ftchinese.repository
 
 import com.beust.klaxon.Klaxon
+import com.ft.ftchinese.model.reader.Credentials
 import com.ft.ftchinese.model.reader.ReadingDuration
 import com.ft.ftchinese.util.Fetch
+import com.ft.ftchinese.util.NextApi
 import com.ft.ftchinese.util.currentFlavor
+import com.ft.ftchinese.util.json
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
+import org.json.JSONException
+import org.json.JSONObject
 import kotlin.Exception
 
 class ReaderRepo : AnkoLogger {
 
-    fun engaged(dur: ReadingDuration) {
+    fun login(c: Credentials): String? {
+        val (_, body) = Fetch()
+                .post(NextApi.LOGIN)
+                .setClient()
+                .noCache()
+                .jsonBody(json.toJsonString(c))
+                .responseApi()
+
+        return if (body == null) {
+            null
+        } else {
+            decodeUserId(body)
+        }
+    }
+
+    private fun decodeUserId(json: String): String? {
+        return try {
+            JSONObject(json).getString("id")
+        } catch (e: JSONException) {
+            info(e)
+            null
+        }
+    }
+
+    /**
+     * If unionId is null, it indicates this is a regular registration;
+     * If unionId is not null, it indicates a
+     * wechat-logged-in user is trying to create a new
+     * account and this new account should be bound to this
+     * wechat account after creation.
+     */
+    fun signUp(c: Credentials, unionId: String? = null): String? {
+        val fetch = if (unionId != null) {
+            Fetch().post(NextApi.WX_SIGNUP)
+                    .setUnionId(unionId)
+        } else {
+            Fetch().post(NextApi.SIGN_UP)
+        }
+
+
+        val (_, body) = fetch
+                .setClient()
+                .noCache()
+                .jsonBody(json.toJsonString(c))
+                .responseApi()
+
+        return if (body == null) {
+            null
+        } else {
+            decodeUserId(body)
+        }
+    }
+
+    fun engaged(dur: ReadingDuration): String? {
         info("Engagement length of ${dur.userId}: ${dur.startUnix} - ${dur.endUnix}")
 
-        try {
-            Fetch().post("${currentFlavor.baseUrl}/engagment.php")
+        return try {
+            Fetch().post("${currentFlavor.baseUrl}/engagement.php")
                     .jsonBody(Klaxon().toJsonString(dur))
                     .responseString()
+
         } catch (e: Exception) {
             info("Error when tracking reading duration $e")
+            null
         }
     }
 
---
+++ app/src/test/java/com/ft/ftchinese/repository/ReaderRepoTest.kt
@@ -0,0 +1,22 @@
+package com.ft.ftchinese.repository
+
+import com.ft.ftchinese.model.reader.ReadingDuration
+import org.junit.Test
+import java.util.*
+
+class ReaderRepoTest {
+    @Test fun readingDuration() {
+        val dur = ReadingDuration(
+                url = "http://www.ftchinese.com/",
+                refer = "http://www.ftchinese.com/",
+                startUnix = Date().time / 1000,
+                endUnix = Date().time / 1000,
+                userId = "00f8fb6b-ec7b-45b7-91f7-cb379df6a3a1",
+                functionName = "onLoad"
+        )
+
+        val result = ReaderRepo.getInstance().engaged(dur)
+
+        println("Result: $result")
+    }
+}
