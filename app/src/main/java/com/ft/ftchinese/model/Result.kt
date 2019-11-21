df0bf22e7ea61628a9d4ef4787fd69202e9fe5ea
weiguo.ni <neefrankie@gmail.com>
2019-11-21 19:43:43 +0800

    refactor: add a instance to hold string resources

--- app/src/main/java/com/ft/ftchinese/model/Result.kt
+++ app/src/main/java/com/ft/ftchinese/model/Result.kt
@@ -7,11 +7,13 @@ package com.ft.ftchinese.model
 sealed class Result<out T : Any> {
 
     data class Success<out T : Any>(val data: T) : Result<T>()
+    data class LocalizedError(val msgId: Int) : Result<Nothing>()
     data class Error(val exception: Exception) : Result<Nothing>()
 
     override fun toString(): String {
         return when (this) {
             is Success<*> -> "Success[data=$data]"
+            is LocalizedError -> "LocalizedError[msgId=$msgId"
             is Error -> "Error[exception=$exception]"
         }
     }
