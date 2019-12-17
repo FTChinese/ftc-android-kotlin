c3f691b7b19c7b15ce30b3917cad83c792f929ea
weiguo.ni <neefrankie@gmail.com>
2019-12-17 16:04:21 +0800

    refactor: add enableButton field

--- app/src/main/java/com/ft/ftchinese/wxapi/UIWxOAuth.kt
+++ app/src/main/java/com/ft/ftchinese/wxapi/UIWxOAuth.kt
@@ -2,7 +2,6 @@ package com.ft.ftchinese.wxapi
 
 data class UIWxOAuth (
         val heading: String,
-        var body: String? = null,
-        val done: Boolean = false,
-        val restartLogin: Boolean = false
+        var body: String = "",
+        val enableButton: Boolean = false
 )
