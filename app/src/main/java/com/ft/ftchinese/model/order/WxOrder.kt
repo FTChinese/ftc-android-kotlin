0e0fa17b733c4240bfab28569e72197ee447b795
weiguo.ni <neefrankie@gmail.com>
2019-12-17 16:03:34 +0800

    refactor: keep backward compatibility

--- app/src/main/java/com/ft/ftchinese/model/order/WxOrder.kt
+++ app/src/main/java/com/ft/ftchinese/model/order/WxOrder.kt
@@ -5,21 +5,7 @@ import com.ft.ftchinese.model.subscription.Tier
 import com.ft.ftchinese.util.*
 import org.threeten.bp.ZonedDateTime
 
-data class WxOrder(
-        override val id: String,
-        @KTier
-        override val tier: Tier,
-        @KCycle
-        override val cycle: Cycle,
-        override val cycleCount: Long,
-        override val extraDays: Long,
-        override var amount: Double,
-        @KOrderUsage
-        override val usageType: OrderUsage,
-        @KPayMethod
-        override val payMethod: PayMethod,
-        @KDateTime
-        override val createdAt: ZonedDateTime,
+data class WxPayParams(
         val appId: String,
         val partnerId: String,
         val prepayId: String,
@@ -27,6 +13,31 @@ data class WxOrder(
         val nonce: String,
         val pkg: String,
         val signature: String
+)
+
+data class WxOrder(
+        override val id: String,
+        @KTier
+        override val tier: Tier,
+        @KCycle
+        override val cycle: Cycle,
+        override var amount: Double,
+        override val cycleCount: Long,
+        override val extraDays: Long,
+        @KOrderUsage
+        override val usageType: OrderUsage,
+        @KPayMethod
+        override val payMethod: PayMethod,
+        @KDateTime
+        override val createdAt: ZonedDateTime,
+        val appId: String, // Deprecate
+        val partnerId: String, // Deprecate
+        val prepayId: String, // Deprecate
+        val timestamp: String, // Deprecate
+        val nonce: String, // Deprecate
+        val pkg: String, // Deprecate
+        val signature: String, // Deprecate
+        val params: WxPayParams
 ) : Subscription(
         id = id,
         tier = tier,
