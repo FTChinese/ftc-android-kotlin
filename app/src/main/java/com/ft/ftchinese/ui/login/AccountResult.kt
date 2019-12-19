c615558b4d61706e4be2ae531a6de355d6ba1527
weiguo.ni <neefrankie@gmail.com>
2019-12-19 11:28:46 +0800

    refactor: dropped

--- app/src/main/java/com/ft/ftchinese/ui/login/AccountResult.kt
+++
@@ -1,9 +0,0 @@
-package com.ft.ftchinese.ui.login
-
-import com.ft.ftchinese.model.reader.Account
-
-data class AccountResult(
-        val success: Account? = null,
-        val error: Int? = null,
-        val exception: Exception? = null // carries information that cannot be determined statically.
-)
--- app/src/main/java/com/ft/ftchinese/ui/login/FindEmailResult.kt
+++
@@ -1,7 +0,0 @@
-package com.ft.ftchinese.ui.login
-
-data class FindEmailResult(
-    val success: Pair<String, Boolean>? = null,
-    val error: Int? = null,
-    val exception: Exception? = null
-)
