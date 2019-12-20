3ad922420ed8a2e77692e2355dd1da7140be9f18
neefrankie <neefrankie@gmail.com>
2019-12-20 12:38:07 +0800

    refactor: using data binding

--- app/src/main/java/com/ft/ftchinese/ui/login/EmailFragment.kt
+++ app/src/main/java/com/ft/ftchinese/ui/login/EmailFragment.kt
@@ -4,15 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.ft.ftchinese.R
+import com.ft.ftchinese.databinding.FragmentEmailBinding
 import com.ft.ftchinese.ui.base.ScopedFragment
 import com.ft.ftchinese.ui.base.afterTextChanged
 import com.ft.ftchinese.ui.base.isNetworkConnected
 import com.ft.ftchinese.viewmodel.LoginViewModel
 import com.ft.ftchinese.viewmodel.Result
-import kotlinx.android.synthetic.main.fragment_email.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.support.v4.toast
 
@@ -21,18 +22,17 @@ class EmailFragment : ScopedFragment(),
         AnkoLogger {
 
     private lateinit var viewModel: LoginViewModel
+    private lateinit var binding: FragmentEmailBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         super.onCreateView(inflater, container, savedInstanceState)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_email, container, false)
+        // Initially we want the button disabled and input
+        // enabled so that user cannot submit empty email.
+        binding.emailInput.isEnabled = true
+        binding.emailInput.requestFocus()
 
-        return inflater.inflate(R.layout.fragment_email, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        emailInput.requestFocus()
-        next_btn.isEnabled = false
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -45,49 +45,45 @@ class EmailFragment : ScopedFragment(),
 
 
         // Validate email upon changed.
-        emailInput.afterTextChanged {
-            viewModel.emailDataChanged(emailInput.text.toString().trim())
+        binding.emailInput.afterTextChanged {
+            viewModel.emailDataChanged(binding.emailInput.text.toString().trim())
         }
 
         viewModel.loginFormState.observe(viewLifecycleOwner, Observer {
             val loginState = it ?: return@Observer
 
-            next_btn.isEnabled = loginState.isEmailValid
+            binding.nextBtn.isEnabled = loginState.isEmailValid
 
             if (loginState.error != null) {
-                emailInput.error = getString(loginState.error)
-                emailInput.requestFocus()
+                binding.emailInput.error = getString(loginState.error)
+                binding.emailInput.requestFocus()
             }
         })
 
-        next_btn.setOnClickListener {
+        binding.nextBtn.setOnClickListener {
             if (activity?.isNetworkConnected() != true) {
                 toast(R.string.prompt_no_network)
                 return@setOnClickListener
             }
 
-            enableInput(false)
+            // Upon submitting data, disable input and button.
+            // Shwo progress.
+            binding.enableInput = false
             viewModel.inProgress.value = true
 
-            viewModel.checkEmail(emailInput.text.toString().trim())
+            viewModel.checkEmail(binding.emailInput.text.toString().trim())
         }
 
         // Enable or disable input depending on network result.
         // Only re-enable button if there's any error.
         viewModel.emailResult.observe(viewLifecycleOwner, Observer {
-//            if (it.error != null || it.exception != null) {
-//                enableInput(true)
-//            }
 
+            // In case any error occurred, allow user to
+            // re-enter and re-submit data.
             if (it !is Result.Success) {
-                enableInput(true)
+                binding.enableInput = true
             }
         })
-    }
-
-    private fun enableInput(enable: Boolean) {
-        emailInput.isEnabled = enable
-        next_btn.isEnabled = enable
     }
 
     companion object {
