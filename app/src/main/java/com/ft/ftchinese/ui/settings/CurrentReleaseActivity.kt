84dbc2898b8257fb31533c8c3f470bffedf96e84
neefrankie <neefrankie@gmail.com>
2019-12-23 10:45:51 +0800

    refactor: migrate to data binding

--- app/src/main/java/com/ft/ftchinese/ui/settings/CurrentReleaseActivity.kt
+++ app/src/main/java/com/ft/ftchinese/ui/settings/CurrentReleaseActivity.kt
@@ -3,17 +3,17 @@ package com.ft.ftchinese.ui.settings
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.commit
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.ft.ftchinese.BuildConfig
 import com.ft.ftchinese.R
+import com.ft.ftchinese.databinding.ActivityCurrentReleaseBinding
 import com.ft.ftchinese.ui.base.ScopedAppActivity
 import com.ft.ftchinese.ui.base.isNetworkConnected
 import com.ft.ftchinese.util.FileCache
 import com.ft.ftchinese.viewmodel.SettingsViewModel
-import kotlinx.android.synthetic.main.progress_bar.*
 import kotlinx.android.synthetic.main.simple_toolbar.*
 import org.jetbrains.anko.toast
 
@@ -24,12 +24,12 @@ import org.jetbrains.anko.toast
 class CurrentReleaseActivity : ScopedAppActivity() {
 
     private lateinit var settingsViewModel: SettingsViewModel
+    private lateinit var binding: ActivityCurrentReleaseBinding
     private lateinit var cache: FileCache
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_current_release)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_current_release)
         setSupportActionBar(toolbar)
 
         supportActionBar?.apply {
@@ -60,21 +60,17 @@ class CurrentReleaseActivity : ScopedAppActivity() {
                 return@Observer
             }
 
-            showProgress(true)
+            binding.inProgress = true
 
             settingsViewModel.fetchRelease(cache, BuildConfig.VERSION_NAME)
         })
 
         settingsViewModel.releaseResult.observe(this, Observer {
-            showProgress(false)
+            binding.inProgress = false
         })
 
-        showProgress(true)
+        binding.inProgress = true
         settingsViewModel.loadCachedRelease(cache)
-    }
-
-    private fun showProgress(show: Boolean) {
-        progress_bar.visibility = if (show) View.VISIBLE else View.GONE
     }
 
     companion object {
