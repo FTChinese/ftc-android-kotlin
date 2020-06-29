43de058eb4ee0ecc955587622c1bdf29b9c30bb4
neefrankie <neefrankie@gmail.com>
2020-06-29 10:24:15 +0800

    refactor: deprecate Flavor

--- app/src/main/java/com/ft/ftchinese/repository/flavors.kt
+++ app/src/main/java/com/ft/ftchinese/repository/flavors.kt
@@ -5,55 +5,17 @@ import com.ft.ftchinese.BuildConfig
 const val HOST_FTC = "www.ftchinese.com"
 const val HOST_FTA = "www.ftacademy.cn"
 
-private const val OFFICIAL_URL = "http://$HOST_FTC"
-
-val defaultFlavor = Flavor(
-    query = "an_ftc",
-    baseUrl = OFFICIAL_URL
-)
+const val defaultFlavor = "an_ftc"
 
 val flavors = mapOf(
-    "play" to Flavor(
-        query = "play_store",
-        baseUrl = OFFICIAL_URL
-    ),
-    "xiaomi" to Flavor(
-        query = "an_xiaomi",
-        baseUrl = OFFICIAL_URL
-    ),
-    "huawei" to Flavor(
-        query = "an_huawei",
-        baseUrl = OFFICIAL_URL
-    ),
-    "baidu" to Flavor(
-        query = "an_baidu",
-        baseUrl = OFFICIAL_URL
-    ),
-    "sanliuling" to Flavor(
-        query = "an_360shouji",
-        baseUrl = OFFICIAL_URL
-    ),
+    "play" to "play_store",
+    "xiaomi" to "an_xiaomi",
+    "huawei" to "an_huawei",
+    "baidu" to "an_baidu",
+    "sanliuling" to "an_360shouji",
     "ftc" to defaultFlavor,
-    "tencent" to Flavor(
-        query = "an_tencent",
-        baseUrl = OFFICIAL_URL
-    ),
-    "samsung" to Flavor(
-        query = "an_samsung",
-        baseUrl = OFFICIAL_URL
-    ),
-    "standard" to Flavor(
-        query = "standard",
-        baseUrl = BuildConfig.BASE_URL_STANDARD
-    ),
-    "premium" to Flavor(
-        query = "premium",
-        baseUrl = BuildConfig.BASE_URL_PREMIUM
-    ),
-    "b2b" to Flavor(
-        query = "b2b",
-        baseUrl = BuildConfig.BASE_URL_B2B
-    )
+    "tencent" to "an_tencent",
+    "samsung" to "an_samsung"
 )
 
 val currentFlavor = flavors[BuildConfig.FLAVOR] ?: defaultFlavor
