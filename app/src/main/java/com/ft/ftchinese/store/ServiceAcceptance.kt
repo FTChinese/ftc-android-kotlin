dfb97eb8ba17dc363f32113bb500f790598b19de
neefrankie <neefrankie@gmail.com>
2020-07-07 20:31:51 +0800

    feat: store service acceptance status

---
+++ app/src/main/java/com/ft/ftchinese/store/ServiceAcceptance.kt
@@ -0,0 +1,40 @@
+package com.ft.ftchinese.store
+
+import android.content.Context
+import androidx.core.content.edit
+import org.jetbrains.anko.AnkoLogger
+
+private const val SESSION_PREF_NAME = "ftc_service_acceptance"
+private const val PREF_IS_ACCEPTED = "is_accepted"
+
+class ServiceAcceptance private constructor(context: Context) : AnkoLogger {
+    private val sharedPreferences = context.getSharedPreferences(SESSION_PREF_NAME, Context.MODE_PRIVATE)
+
+    fun isAccepted(): Boolean {
+        return sharedPreferences.getBoolean(PREF_IS_ACCEPTED, false)
+    }
+
+    fun accepted() {
+        sharedPreferences.edit {
+            putBoolean(PREF_IS_ACCEPTED, true)
+        }
+    }
+
+    fun clear() {
+        sharedPreferences.edit {
+            clear()
+        }
+    }
+
+    companion object {
+        private var instance: ServiceAcceptance? = null
+
+        @Synchronized fun getInstance(ctx: Context): ServiceAcceptance {
+            if (instance == null) {
+                instance = ServiceAcceptance(ctx.applicationContext)
+            }
+
+            return instance!!
+        }
+    }
+}
