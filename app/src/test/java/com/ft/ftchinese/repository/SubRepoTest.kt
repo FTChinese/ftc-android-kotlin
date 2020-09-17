116098d352720b92308aa22a8be194b451607de0
weiguo.ni <neefrankie@gmail.com>
2020-09-17 11:32:26 +0800

    refactor: test

---
+++ app/src/test/java/com/ft/ftchinese/repository/SubRepoTest.kt
@@ -0,0 +1,34 @@
+package com.ft.ftchinese.repository
+
+import com.ft.ftchinese.model.reader.Account
+import com.ft.ftchinese.model.reader.LoginMethod
+import com.ft.ftchinese.model.reader.Membership
+import com.ft.ftchinese.model.reader.Wechat
+import com.ft.ftchinese.model.subscription.Cycle
+import com.ft.ftchinese.model.subscription.PlanStore
+import com.ft.ftchinese.model.subscription.Tier
+import org.junit.Assert.*
+import org.junit.Test
+
+class SubRepoTest {
+
+    private val account = Account(
+        id = "c07f79dc-664b-44ca-87ea-42958e7991b0",
+        unionId = null,
+        stripeId = null,
+        userName = null,
+        email = "ali-wx.test@ftchinese.com",
+        isVerified = false,
+        avatarUrl = null,
+        loginMethod = LoginMethod.EMAIL,
+        wechat = Wechat(),
+        membership = Membership()
+    )
+
+    @Test
+    fun createAliOrder() {
+        val order = SubRepo.aliPlaceOrder(account, PlanStore.find(Tier.STANDARD, Cycle.YEAR)!!)
+
+        println(order)
+    }
+}
