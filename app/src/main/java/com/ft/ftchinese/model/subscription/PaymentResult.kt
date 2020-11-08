60ddd274cd2823a4080479e2998723f7a6d11894
neefrankie <neefrankie@gmail.com>
2020-11-08 16:19:35 +0800

    feat: add isOrderPaid

--- app/src/main/java/com/ft/ftchinese/model/subscription/PaymentResult.kt
+++ app/src/main/java/com/ft/ftchinese/model/subscription/PaymentResult.kt
@@ -1,14 +1,21 @@
 package com.ft.ftchinese.model.subscription
 
+val paymentSuccessStates = arrayOf("TRADE_SUCCESS", "SUCCESS")
+
 /**
  * After WXPayEntryActivity received result 0, check against our server for the payment result.
  * Server will in turn check payment result from Wechat server.
  */
 data class PaymentResult(
-        val paymentState: String,
-        val paymentStateDesc: String,
-        val totalFee: Int,
-        val transactionId: String,
-        val ftcOrderId: String,
-        val paidAt: String // ISO8601
-)
+    val paymentState: String,
+    val paymentStateDesc: String,
+    val totalFee: Int,
+    val transactionId: String,
+    val ftcOrderId: String,
+    val paidAt: String, // ISO8601
+    val payMethod: PayMethod
+) {
+    fun isOrderPaid(): Boolean {
+        return paymentSuccessStates.contains(paymentState)
+    }
+}
