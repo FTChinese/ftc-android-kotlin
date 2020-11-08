b1c9eacc1fcaec8f4e3ed31c5864d212306c608a
neefrankie <neefrankie@gmail.com>
2020-11-08 16:18:23 +0800

    refactor: order fields

--- app/src/main/java/com/ft/ftchinese/store/OrderManager.kt
+++ app/src/main/java/com/ft/ftchinese/store/OrderManager.kt
@@ -15,14 +15,20 @@ import com.ft.ftchinese.util.parseLocalDate
 
 private const val PREF_FILE_NAME = "subscription"
 private const val PREF_ORDER_ID = "order_id"
+private const val PREF_FTC_ID = "ftc_id"
+private const val PREF_UNION_ID = "union_id"
+private const val PREF_PLAN_ID = "plan_id"
+private const val PREF_DISCOUNT_ID = "discount_id"
+private const val PREF_PRICE = "price"
 private const val PREF_TIER = "tier"
 private const val PREF_CYCLE = "cycle"
+private const val PREF_AMOUNT = "amount"
 private const val PREF_CYCLE_COUNT = "cycle_count"
 private const val PREF_EXTRA_DAYS = "extra_days"
-private const val PREF_PAYMENT_METHOD = "pay_method"
-private const val PREF_AMOUNT = "amount"
-private const val PREF_CREATED_AT = "create_at"
 private const val PREF_USAGE = "usage_type"
+private const val PREF_PAYMENT_METHOD = "pay_method"
+private const val PREF_BALANCE = "total_balance"
+private const val PREF_CREATED_AT = "create_at"
 private const val PREF_CONFIRMED_AT = "confirmed_at"
 private const val PREF_START_DATE = "start_date"
 private const val PREF_END_DATE = "end_date"
@@ -47,14 +53,22 @@ class OrderManager private constructor(context: Context) {
 
         sharedPreferences.edit {
             putString(PREF_ORDER_ID, order.id)
+            putString(PREF_FTC_ID, order.ftcId)
+            putString(PREF_UNION_ID, order.unionId)
+            putString(PREF_PLAN_ID, order.planId)
+            putString(PREF_DISCOUNT_ID, order.discountId)
+            if( order.price != null) {
+                putDouble(PREF_PRICE, order.price)
+            }
             putString(PREF_TIER, order.tier.toString())
             putString(PREF_CYCLE, order.cycle.toString())
+            putDouble(PREF_AMOUNT, order.amount)
             putLong(PREF_CYCLE_COUNT, order.cycleCount)
             putLong(PREF_EXTRA_DAYS, order.extraDays)
-            putString(PREF_PAYMENT_METHOD, order.payMethod.toString())
-            putDouble(PREF_AMOUNT, order.amount)
-            putString(PREF_CREATED_AT, formatISODateTime(order.createdAt))
             putString(PREF_USAGE, order.usageType.toString())
+            putString(PREF_PAYMENT_METHOD, order.payMethod.toString())
+            putDouble(PREF_BALANCE, order.totalBalance)
+            putString(PREF_CREATED_AT, formatISODateTime(order.createdAt))
             putString(PREF_CONFIRMED_AT, formatISODateTime(order.confirmedAt))
             putString(PREF_START_DATE, formatLocalDate(order.startDate))
             putString(PREF_END_DATE, formatLocalDate(order.endDate))
@@ -63,13 +77,19 @@ class OrderManager private constructor(context: Context) {
 
     fun load(): Order? {
         val orderId = sharedPreferences.getString(PREF_ORDER_ID, null) ?: return null
+        val ftcId = sharedPreferences.getString(PREF_FTC_ID, null)
+        val unionId = sharedPreferences.getString(PREF_UNION_ID, null)
+        val planId = sharedPreferences.getString(PREF_PLAN_ID, null)
+        val discountId = sharedPreferences.getString(PREF_DISCOUNT_ID, null)
+        val price = sharedPreferences.getDouble(PREF_PRICE, 0.0)
         val tier = Tier.fromString(sharedPreferences.getString(PREF_TIER, null)) ?: return null
+        val amount = sharedPreferences.getDouble(PREF_AMOUNT, 0.0)
         val cycle = Cycle.fromString(sharedPreferences.getString(PREF_CYCLE, null)) ?: return null
-        val payMethod = PayMethod.fromString(sharedPreferences.getString(PREF_PAYMENT_METHOD, null))
-                ?: return null
-        val netPrice = sharedPreferences.getDouble(PREF_AMOUNT, 0.0)
         val usageType = OrderUsage.fromString(sharedPreferences.getString(PREF_USAGE, null))
                 ?: return null
+        val payMethod = PayMethod.fromString(sharedPreferences.getString(PREF_PAYMENT_METHOD, null))
+            ?: return null
+        val balance = sharedPreferences.getDouble(PREF_BALANCE, 0.0)
         val createdAt = parseISODateTime(sharedPreferences.getString(PREF_CREATED_AT, null)) ?: return null
         val confirmed = parseISODateTime(sharedPreferences.getString(PREF_CONFIRMED_AT, null))
         val start = parseLocalDate(sharedPreferences.getString(PREF_START_DATE, null))
@@ -77,18 +97,24 @@ class OrderManager private constructor(context: Context) {
 
 
         return Order(
-                id = orderId,
-                tier = tier,
-                cycle = cycle,
-                cycleCount = sharedPreferences.getLong(PREF_CYCLE_COUNT, 1),
-                extraDays = sharedPreferences.getLong(PREF_EXTRA_DAYS, 0),
-                payMethod = payMethod,
-                amount = netPrice,
-                createdAt = createdAt,
-                usageType = usageType,
-                confirmedAt = confirmed,
-                startDate = start,
-                endDate = end
+            id = orderId,
+            ftcId= ftcId,
+            unionId = unionId,
+            planId = planId,
+            discountId = discountId,
+            price = price,
+            tier = tier,
+            cycle = cycle,
+            amount = amount,
+            cycleCount = sharedPreferences.getLong(PREF_CYCLE_COUNT, 1),
+            extraDays = sharedPreferences.getLong(PREF_EXTRA_DAYS, 0),
+            payMethod = payMethod,
+            totalBalance = balance,
+            createdAt = createdAt,
+            usageType = usageType,
+            confirmedAt = confirmed,
+            startDate = start,
+            endDate = end
         )
     }
 
@@ -103,4 +129,19 @@ class OrderManager private constructor(context: Context) {
             return instance!!
         }
     }
+}
+
+private const val PREF_FILE_LAST_ORDER = "com.ft.ftchinese.last_paid_order"
+
+fun lastPaidOrderId(ctx: Context): String? {
+    return ctx
+        .getSharedPreferences(PREF_FILE_LAST_ORDER, Context.MODE_PRIVATE)
+        .getString(PREF_ORDER_ID, null)
+}
+
+fun saveLastPaidOrderId(ctx: Context, id: String) {
+    ctx.getSharedPreferences(PREF_FILE_LAST_ORDER, Context.MODE_PRIVATE)
+        .edit {
+            putString(PREF_ORDER_ID, id)
+        }
 }
