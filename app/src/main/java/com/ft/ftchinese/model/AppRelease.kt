e9eb46a3516d15866a2dcdf8f1dad940e31f27aa
weiguo.ni <neefrankie@gmail.com>
2020-11-19 17:45:58 +0800

    feat: cacheFileName

--- app/src/main/java/com/ft/ftchinese/model/AppRelease.kt
+++ app/src/main/java/com/ft/ftchinese/model/AppRelease.kt
@@ -6,35 +6,37 @@ import org.threeten.bp.ZonedDateTime
 import org.threeten.bp.format.DateTimeFormatter
 
 data class AppRelease(
-        val versionName: String = "",
-        val versionCode: Int = 0,
-        val body: String = "",
-        val apkUrl: String = "",
-        @KDateTime
-        val createdAt: ZonedDateTime? = null
+    val versionName: String = "",
+    val versionCode: Int = 0,
+    val body: String = "",
+    val apkUrl: String = "",
+    @KDateTime
+    val createdAt: ZonedDateTime? = null
 ) {
-        val isNew: Boolean
-                get() = versionCode > BuildConfig.VERSION_CODE
+    val isNew: Boolean
+            get() = versionCode > BuildConfig.VERSION_CODE
 
-        val isCurrent: Boolean
-                get() = versionCode == BuildConfig.VERSION_CODE
+    val isCurrent: Boolean
+            get() = versionCode == BuildConfig.VERSION_CODE
 
-        val creationTime: String
-                get() = createdAt?.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) ?: ""
+    val creationTime: String
+            get() = createdAt?.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) ?: ""
 
-        fun splitBody(): List<String> {
-                return body.split("\n")
-                        .map {
-                                it
-                                        .removePrefix("*")
-                                        .trim()
-                        }
-        }
+    fun splitBody(): List<String> {
+            return body.split("\n")
+                    .map {
+                            it
+                                    .removePrefix("*")
+                                    .trim()
+                    }
+    }
 
-        companion object {
+    fun cacheFileName() = "release_log_$versionCode.json"
 
-                @JvmStatic
-                fun currentCacheFile() = "release_log_${BuildConfig.VERSION_CODE}.json"
-        }
+    companion object {
+
+            @JvmStatic
+            fun currentCacheFile() = "release_log_${BuildConfig.VERSION_CODE}.json"
+    }
 
 }
