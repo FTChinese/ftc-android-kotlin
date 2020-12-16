542d6d16523562f86591ec8908bb8895d6deea39
weiguo.ni <neefrankie@gmail.com>
2020-12-16 17:42:49 +0800

    refactor: deprecated field

--- app/src/main/java/com/ft/ftchinese/model/order/StripeSubParams.kt
+++ app/src/main/java/com/ft/ftchinese/model/order/StripeSubParams.kt
@@ -10,7 +10,7 @@ data class StripeSubParams(
         val tier: Tier,
         @KCycle
         val cycle: Cycle,
-        val customer: String,
+        val customer: String, // Deprecated
         val coupon: String? = null,
         val defaultPaymentMethod: String?,
         val idempotency: String? = null
