dcea9ac846b55b4ea0edb70d7ecffc75c2ed6ca4
weiguo.ni <neefrankie@gmail.com>
2020-12-16 17:37:23 +0800

    test

---
+++ app/src/test/java/com/ft/ftchinese/model/subscription/DiscountTest.kt
@@ -0,0 +1,23 @@
+package com.ft.ftchinese.model.subscription
+
+import com.ft.ftchinese.util.json
+import org.junit.Test
+
+
+class DiscountTest {
+    private val data = """
+    {
+        "id": "dsc_ytFW0fkWYipx",
+        "priceOff": 100,
+        "percent": null,
+        "startUtc": "2020-10-19T16:00:00Z",
+        "endUtc": "2020-10-30T16:00:00Z",
+        "description": null
+    }
+    """.trimIndent()
+    @Test
+    fun parseJSON() {
+        val d = json.parse<Discount>(data)
+        println(d)
+    }
+}
