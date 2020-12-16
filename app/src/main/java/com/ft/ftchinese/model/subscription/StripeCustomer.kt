c603c9dafd6fd2a8b9acf8c2f27f1450d1eda401
weiguo.ni <neefrankie@gmail.com>
2020-12-16 17:40:41 +0800

    refactor: default value

--- app/src/main/java/com/ft/ftchinese/model/subscription/StripeCustomer.kt
+++ app/src/main/java/com/ft/ftchinese/model/subscription/StripeCustomer.kt
@@ -1,10 +1,20 @@
 package com.ft.ftchinese.model.subscription
 
+// Format for version prior to v3.5.4.
+//data class StripeCustomer(
+//        val id: String = "", // Deprecated. Stripe id for backward compatible.
+//        val ftcId: String,
+//        val unionId: String?,
+//        val stripeId: String,
+//        val userName: String?,
+//        val email: String
+//)
+
 data class StripeCustomer(
-        val id: String = "",
-        val ftcId: String,
-        val unionId: String?,
-        val stripeId: String,
-        val userName: String?,
-        val email: String
+    val id: String,
+    val ftcId: String,
+    val defaultSource: String? = null,
+    val defaultPaymentMethod: String? = null,
+    val email: String,
+    val liveMode: Boolean = true
 )
