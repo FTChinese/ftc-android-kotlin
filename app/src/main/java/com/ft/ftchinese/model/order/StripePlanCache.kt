dce9c8064ebea6bc8568523ccdc2a70c01c149f7
weiguo.ni <neefrankie@gmail.com>
2020-12-16 17:40:53 +0800

    refactor: rename

--- app/src/main/java/com/ft/ftchinese/model/order/StripePlanCache.kt
+++ app/src/main/java/com/ft/ftchinese/model/order/StripePlanCache.kt
@@ -2,7 +2,7 @@ package com.ft.ftchinese.model.order
 
 import android.content.Context
 import androidx.core.content.edit
-import com.ft.ftchinese.model.subscription.StripePlan
+import com.ft.ftchinese.model.subscription.StripePrice
 import com.ft.ftchinese.util.json
 
 private const val PREF_FILE_NAME = "stripe_plans"
@@ -10,19 +10,19 @@ private const val PREF_FILE_NAME = "stripe_plans"
 class StripePlanCache private constructor(context: Context) {
     private val sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
 
-    fun save(plan: StripePlan, key: String?) {
+    fun save(price: StripePrice, key: String?) {
         if (key == null) {
             return
         }
 
-        val data = json.toJsonString(plan)
+        val data = json.toJsonString(price)
 
         sharedPreferences.edit {
             putString(key, data)
         }
     }
 
-    fun load(key: String?): StripePlan? {
+    fun load(key: String?): StripePrice? {
         if (key == null) {
             return null
         }
@@ -30,7 +30,7 @@ class StripePlanCache private constructor(context: Context) {
         val data = sharedPreferences.getString(key, null) ?: return null
 
         return try {
-            json.parse<StripePlan>(data)
+            json.parse<StripePrice>(data)
         } catch (e: Exception) {
             null
         }
