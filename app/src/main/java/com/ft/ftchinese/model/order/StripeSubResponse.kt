ff89e15b5f86d965d6ba454a42cbb9a51e39cc94
weiguo.ni <neefrankie@gmail.com>
2020-12-16 17:35:40 +0800

    feat: StripeSubResult

--- app/src/main/java/com/ft/ftchinese/model/order/StripeSubResponse.kt
+++
@@ -1,6 +0,0 @@
-package com.ft.ftchinese.model.order
-
-data class StripeSubResponse(
-        val requiresAction: Boolean,
-        val paymentIntentClientSecret: String?
-)
---
+++ app/src/main/java/com/ft/ftchinese/model/order/StripeSubResult.kt
@@ -0,0 +1,55 @@
+package com.ft.ftchinese.model.order
+
+import com.ft.ftchinese.model.reader.Membership
+import com.ft.ftchinese.model.subscription.Cycle
+import com.ft.ftchinese.model.subscription.Tier
+import com.ft.ftchinese.util.KCycle
+import com.ft.ftchinese.util.KDateTime
+import com.ft.ftchinese.util.KStripeSubStatus
+import com.ft.ftchinese.util.KTier
+import org.threeten.bp.ZonedDateTime
+
+data class StripePaymentResult(
+    val requiresAction: Boolean,
+    val paymentIntentClientSecret: String? = null
+)
+
+data class StripeSubs(
+    val id: String,
+    @KTier
+    val tier: Tier,
+    @KCycle
+    val cycle: Cycle,
+    @KDateTime
+    val cancelAtUtc: ZonedDateTime? = null,
+    val cancelAtPeriodEnd: Boolean,
+    @KDateTime
+    val canceledUtc: ZonedDateTime? = null,
+    @KDateTime
+    val currentPeriodEnd: ZonedDateTime,
+    @KDateTime
+    val currentPeriodStart: ZonedDateTime,
+    val customerId: String,
+    val defaultPaymentMethod: String? = null,
+    val subsItemId: String,
+    val priceId: String,
+    val latestInvoiceId: String,
+    val liveMode: Boolean,
+    @KDateTime
+    val startDateUtc: ZonedDateTime? = null,
+    @KDateTime
+    val endedUtc: ZonedDateTime? = null,
+    @KDateTime
+    val createdUtc: ZonedDateTime? = null,
+    @KDateTime
+    val updatedUtc: ZonedDateTime? = null,
+    @KStripeSubStatus
+    val status: StripeSubStatus? = null,
+    val ftcUserId: String? = null
+)
+
+data class StripeSubResult(
+    val payment:  StripePaymentResult,
+    val subs: StripeSubs,
+    val membership: Membership
+)
