c35b8b02a8c3b6cd42da6756d8f379843b15b4d9
weiguo.ni <neefrankie@gmail.com>
2020-12-16 17:39:15 +0800

    refactor: drip withStripePlan

--- app/src/main/java/com/ft/ftchinese/model/subscription/PaymentIntent.kt
+++ app/src/main/java/com/ft/ftchinese/model/subscription/PaymentIntent.kt
@@ -2,33 +2,21 @@ package com.ft.ftchinese.model.subscription
 
 import android.os.Parcelable
 import com.ft.ftchinese.util.KOrderUsage
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class PaymentIntent(
-        val amount: Double,
-        val currency: String,
-        val cycleCount: Int = 1,
-        val extraDays: Int = 1,
-        @KOrderUsage
+    val amount: Double,
+    val currency: String,
+    val cycleCount: Int = 1,
+    val extraDays: Int = 1,
+    @KOrderUsage
     val kind: OrderKind? = null,
-        val wallet: Wallet = Wallet(),
-        val plan: Plan
+    val wallet: Wallet = Wallet(),
+    val plan: Plan
 ) : Parcelable {
 
     fun isPayRequired(): Boolean {
         return amount != 0.0
-    }
-
-    fun withStripePlan(stripePlan: StripePlan?): PaymentIntent {
-        return PaymentIntent(
-                amount = stripePlan?.price() ?: 0.0,
-                currency = stripePlan?.currency ?: "",
-                cycleCount = 1,
-                extraDays = 0,
-                kind = kind,
-                wallet = wallet,
-                plan = plan
-        )
     }
 }
