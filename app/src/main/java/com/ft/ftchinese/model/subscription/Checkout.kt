493b2855c05b7c2f023698b621f8fa2578220082
neefrankie <neefrankie@gmail.com>
2020-12-17 22:14:47 +0800

    feat: Checkout

---
+++ app/src/main/java/com/ft/ftchinese/model/subscription/Checkout.kt
@@ -0,0 +1,39 @@
+package com.ft.ftchinese.model.subscription
+
+import com.ft.ftchinese.util.KOrderUsage
+
+data class CheckedItem(
+    val plan: Plan,
+    val discount: Discount,
+)
+
+data class Duration(
+    val cycleCount: Int = 1,
+    val extraDays: Int = 0,
+)
+
+data class Charge(
+    val amount: Double,
+    val currency: String = "cny"
+)
+data class Checkout(
+    @KOrderUsage
+    val kind: OrderKind,
+    val item: CheckedItem,
+    val wallet: Wallet,
+    val duration: Duration,
+    val payable: Charge,
+    val isFree: Boolean,
+    val live: Boolean,
+) {
+    fun upgradePlan(): Plan {
+        return Plan(
+            id = item.plan.id,
+            productId = item.plan.productId,
+            price = payable.amount,
+            tier = item.plan.tier,
+            cycle = item.plan.cycle,
+            discount = Discount()
+        )
+    }
+}
