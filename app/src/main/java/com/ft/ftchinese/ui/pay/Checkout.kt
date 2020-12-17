ebb468b78ccd65ba6063d39e4b351b84c2cf9002
neefrankie <neefrankie@gmail.com>
2020-12-17 22:19:27 +0800

    feat: UpgradePreview

--- app/src/main/java/com/ft/ftchinese/ui/pay/Checkout.kt
+++ app/src/main/java/com/ft/ftchinese/ui/pay/Checkout.kt
@@ -2,6 +2,8 @@ package com.ft.ftchinese.ui.pay
 
 import android.content.Context
 import android.os.Parcelable
+import com.ft.ftchinese.R
+import com.ft.ftchinese.model.subscription.Checkout
 import com.ft.ftchinese.model.subscription.OrderKind
 import com.ft.ftchinese.model.subscription.Plan
 import com.ft.ftchinese.model.subscription.StripePrice
@@ -57,5 +59,53 @@ fun ftcCartItem(ctx: Context, plan: Plan): CartItem {
             tier = plan.tier,
             cycle = plan.cycle
         )
+    )
+}
+
+data class UpgradePreview(
+    val amount: String = "",
+    val price: String = "",
+    val balance: String = "",
+    val conversion: String = "",
+    val confirmUpgrade: String = ""
+)
+
+fun upgradePreviewUI(ctx: Context, co: Checkout): UpgradePreview {
+    return UpgradePreview(
+        amount = formatPrice(
+            ctx = ctx,
+            currency = co.payable.currency,
+            price = co.payable.amount
+        ),
+        price = ctx.getString(
+            R.string.premium_price,
+            formatPrice(
+                ctx = ctx,
+                currency = co.item.plan.currency,
+                price = co.item.plan.price,
+            )
+        ),
+        balance = ctx.getString(
+            R.string.account_balance,
+            formatPrice(
+                ctx = ctx,
+                currency = co.payable.currency,
+                price = co.wallet.balance
+            )
+        ),
+        conversion = if (!co.isFree) {
+            "升级首先使用当前余额抵扣，不足部分需要另行支付"
+        } else {
+            ctx.getString(
+                R.string.balance_conversion,
+                co.duration.cycleCount,
+                co.duration.extraDays
+            )
+        },
+        confirmUpgrade = if (co.isFree) {
+            ctx.getString(R.string.confirm_upgrade)
+        } else {
+            ctx.getString(R.string.direct_upgrade)
+        }
     )
 }
