367cb8831c1dc6a058cad26212b0dfd9e2773423
neefrankie <neefrankie@gmail.com>
2020-12-20 20:37:01 +0800

    feat: in-memory store for account

---
+++ app/src/main/java/com/ft/ftchinese/store/AccountStore.kt
@@ -0,0 +1,44 @@
+package com.ft.ftchinese.store
+
+import android.content.Context
+import androidx.core.content.edit
+import com.ft.ftchinese.model.subscription.StripeCustomer
+import com.ft.ftchinese.util.json
+
+private const val PREF_FILE_NAME = "com.ft.ftchinese.account"
+private const val KEY_STRIPE_CUSTOMER = "stripe_customer"
+
+object AccountStore {
+    var customer: StripeCustomer? = null
+
+    fun saveStripeCustomer(ctx: Context, c: StripeCustomer) {
+        customer = c
+
+        val data = try {
+            json.toJsonString(c)
+        } catch (e: Exception) {
+            return
+        }
+
+        ctx.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
+            .edit {
+                putString(KEY_STRIPE_CUSTOMER, data)
+            }
+    }
+
+    fun loadStripeCustomer(ctx: Context): StripeCustomer? {
+        val data = ctx
+            .getSharedPreferences(
+                PREF_FILE_NAME,
+                Context.MODE_PRIVATE
+            )
+            .getString(KEY_STRIPE_CUSTOMER, null)
+            ?: return null
+
+        return try {
+            json.parse<StripeCustomer>(data)
+        } catch (e: Exception) {
+            null
+        }
+    }
+}
