38dbb341f8302290382bf9d6612bcfecf608481d
neefrankie <neefrankie@gmail.com>
2021-01-24 16:13:09 +0800

    refactor: import path

--- app/src/main/java/com/ft/ftchinese/ui/account/FtcAccountFragment.kt
+++ app/src/main/java/com/ft/ftchinese/ui/account/FtcAccountFragment.kt
@@ -15,6 +15,9 @@ import com.ft.ftchinese.model.reader.Account
 import com.ft.ftchinese.model.reader.LoginMethod
 import com.ft.ftchinese.store.SessionManager
 import com.ft.ftchinese.ui.base.*
+import com.ft.ftchinese.ui.lists.AccountAdapter
+import com.ft.ftchinese.ui.lists.AccountRow
+import com.ft.ftchinese.ui.lists.AccountRowType
 import com.ft.ftchinese.viewmodel.AccountViewModel
 import com.ft.ftchinese.viewmodel.Result
 import org.jetbrains.anko.AnkoLogger
