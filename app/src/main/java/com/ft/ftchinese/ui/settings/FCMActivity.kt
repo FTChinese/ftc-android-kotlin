b41a0b054a7df7d1d90b7ca146a64788534da190
neefrankie <neefrankie@gmail.com>
2021-01-24 16:12:07 +0800

    refactor: list

--- app/src/main/java/com/ft/ftchinese/ui/settings/FCMActivity.kt
+++ app/src/main/java/com/ft/ftchinese/ui/settings/FCMActivity.kt
@@ -7,14 +7,17 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.view.View
+import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ft.ftchinese.BuildConfig
 import com.ft.ftchinese.R
 import com.ft.ftchinese.databinding.ActivityFcmBinding
-import com.ft.ftchinese.ui.base.ListAdapter
-import com.ft.ftchinese.ui.base.ListItem
+import com.ft.ftchinese.ui.lists.SingleLineItem
+import com.ft.ftchinese.ui.lists.SingleLineItemViewHolder
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.tasks.OnCompleteListener
@@ -60,40 +63,43 @@ class FCMActivity : AppCompatActivity(), AnkoLogger {
         binding.btnCheckFcm.setOnClickListener {
             binding.inProgress = true
 
+            // Clean data every time user clicked the check button.
             listAdapter?.clear()
 
+            // Check if Google Play Service is avaiable on device.
             val item = buildPlayServiceText(checkPlayServices())
 
+            // Show the checking result.
             listAdapter?.add(item)
 
             retrieveRegistrationToken()
         }
     }
 
-    private fun buildPlayServiceText(available: Boolean): ListItem {
+    private fun buildPlayServiceText(available: Boolean): SingleLineItem {
         return if (available) {
-            ListItem(
-                    primaryText = getString(R.string.play_service_available),
-                    startIconRes = R.drawable.ic_done_claret_24dp
+            SingleLineItem(
+                    text = getString(R.string.play_service_available),
+                    icon = R.drawable.ic_done_claret_24dp
             )
         } else {
-            ListItem(
-                    primaryText = getString(R.string.play_service_not_available),
-                    startIconRes = R.drawable.ic_error_outline_claret_24dp
+            SingleLineItem(
+                    text = getString(R.string.play_service_not_available),
+                    icon = R.drawable.ic_error_outline_claret_24dp
             )
         }
     }
 
-    private fun buildTokenText(retrieved: Boolean): ListItem {
+    private fun buildTokenText(retrieved: Boolean): SingleLineItem {
         return if (retrieved) {
-            ListItem(
-                    primaryText = getString(R.string.fcm_accessible),
-                    startIconRes = R.drawable.ic_done_claret_24dp
+            SingleLineItem(
+                    text = getString(R.string.fcm_accessible),
+                    icon = R.drawable.ic_done_claret_24dp
             )
         } else {
-            ListItem(
-                    primaryText = getString(R.string.fcm_inaccessible),
-                    startIconRes = R.drawable.ic_error_outline_claret_24dp
+            SingleLineItem(
+                    text = getString(R.string.fcm_inaccessible),
+                    icon = R.drawable.ic_error_outline_claret_24dp
             )
         }
     }
@@ -145,6 +151,35 @@ class FCMActivity : AppCompatActivity(), AnkoLogger {
         @JvmStatic
         fun start(context: Context?) {
             context?.startActivity(Intent(context, FCMActivity::class.java))
+        }
+    }
+
+    inner class ListAdapter(rows: List<SingleLineItem>) : RecyclerView.Adapter<SingleLineItemViewHolder>() {
+
+        private var items: MutableList<SingleLineItem> = rows.toMutableList()
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SingleLineItemViewHolder {
+            return SingleLineItemViewHolder.create(parent)
+        }
+
+        override fun onBindViewHolder(holder: SingleLineItemViewHolder, position: Int) {
+            val item = items[position]
+            holder.disclosure.visibility = View.GONE
+            if (item.icon != null) {
+                holder.icon.setImageResource(item.icon)
+            }
+            holder.text.text = item.text
+        }
+
+        override fun getItemCount() = items.size
+
+        fun add(item: SingleLineItem) {
+            items.add(item)
+            notifyDataSetChanged()
+        }
+
+        fun clear() {
+            items.clear()
         }
     }
 }
