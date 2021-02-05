a454ca258b8f1a662228bf212e363bd32ca7b9e6
weiguo.ni <neefrankie@gmail.com>
2021-02-05 18:20:39 +0800

    refactor: removed Parcelable

--- app/src/main/java/com/ft/ftchinese/model/subscription/Edition.kt
+++ app/src/main/java/com/ft/ftchinese/model/subscription/Edition.kt
@@ -1,14 +1,11 @@
 package com.ft.ftchinese.model.subscription
 
-import android.os.Parcelable
 import com.ft.ftchinese.model.fetch.KCycle
 import com.ft.ftchinese.model.fetch.KTier
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class Edition(
     @KTier
     val tier: Tier,
     @KCycle
     val cycle: Cycle
-) : Parcelable
+)
