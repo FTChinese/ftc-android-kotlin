4f09dfcc1a7921ef7acc7a2cdafe9281ab32fb6d
weiguo.ni <neefrankie@gmail.com>
2021-02-07 14:29:06 +0800

    refactor: margin and padding or recycler view

--- app/src/main/java/com/ft/ftchinese/ui/about/AboutListFragment.kt
+++ app/src/main/java/com/ft/ftchinese/ui/about/AboutListFragment.kt
@@ -1,49 +1,31 @@
 package com.ft.ftchinese.ui.about
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ft.ftchinese.R
 import com.ft.ftchinese.model.legal.legalDocs
 import com.ft.ftchinese.ui.lists.SingleLineItemViewHolder
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AboutListFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AboutListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_about_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_recycler, container, false)
 
-        view.findViewById<RecyclerView>(R.id.list_rv).apply {
+        view.findViewById<RecyclerView>(R.id.recycler_view).apply {
             setHasFixedSize(true)
             layoutManager = LinearLayoutManager(context).apply {
                 orientation = LinearLayoutManager.VERTICAL
             }
-
+            addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
             adapter = ListAdapter()
         }
         return view
@@ -52,7 +34,11 @@ class AboutListFragment : Fragment() {
     inner class ListAdapter : RecyclerView.Adapter<SingleLineItemViewHolder>() {
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SingleLineItemViewHolder {
-            return SingleLineItemViewHolder.create(parent)
+            return SingleLineItemViewHolder.create(parent).apply {
+                setPadding(
+                    resources.getDimension(R.dimen.space_medium).toInt(),
+                )
+            }
         }
 
         override fun onBindViewHolder(holder: SingleLineItemViewHolder, position: Int) {
@@ -67,16 +53,5 @@ class AboutListFragment : Fragment() {
         }
 
         override fun getItemCount() = legalDocs.size
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AboutListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
