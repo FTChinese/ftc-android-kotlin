babebf83239314257424bcf01c071f7677f424a8
weiguo.ni <neefrankie@gmail.com>
2021-02-07 14:28:23 +0800

    refactor: rename Price to PriceParams

--- app/src/main/java/com/ft/ftchinese/model/ui/PriceParams.kt
+++ app/src/main/java/com/ft/ftchinese/model/ui/PriceParams.kt
@@ -1,12 +1,14 @@
 package com.ft.ftchinese.model.ui
 
 import com.ft.ftchinese.model.enums.Cycle
+import com.ft.ftchinese.model.enums.Tier
 
 /**
  * Describes the params to build a human-readable price string
  */
-data class Price(
+data class PriceParams(
     val currency: String,
     val amount: Double,
     val cycle: Cycle,
+    val tier: Tier,
 )
