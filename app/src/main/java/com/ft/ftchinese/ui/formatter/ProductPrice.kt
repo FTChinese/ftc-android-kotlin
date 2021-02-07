81edd0e53fb15ad5f40d5037beec78c64cd12e32
weiguo.ni <neefrankie@gmail.com>
2021-02-07 14:30:19 +0800

    refactor: buildFtcPrice

--- app/src/main/java/com/ft/ftchinese/ui/formatter/ProductPrice.kt
+++ app/src/main/java/com/ft/ftchinese/ui/formatter/ProductPrice.kt
@@ -1,28 +1,51 @@
 package com.ft.ftchinese.ui.formatter
 
 import android.content.Context
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.StrikethroughSpan
 import com.ft.ftchinese.R
-import com.ft.ftchinese.model.subscription.Plan
+import com.ft.ftchinese.model.subscription.CheckoutItem
+import com.ft.ftchinese.model.ui.PriceParams
 
 data class ProductPrice(
     val payable: String,         // The actually charged amount
-    val original: String?, // The original price if discount exists.
+    val original: Spannable?, // The original price if discount exists.
 )
 
-fun buildFtcPrice(ctx: Context, plan: Plan): ProductPrice {
-    val item = plan.checkoutItem()
+/**
+ * Use spannable to style text.
+ * See https://developer.android.com/reference/android/text/style/StrikethroughSpan
+ */
+fun buildFtcPrice(ctx: Context, item: CheckoutItem): ProductPrice {
 
     return ProductPrice(
         payable = formatPriceCycle(
             ctx = ctx,
-            price = item.originalPriceParams),
+            price = item.payablePriceParams),
 
         original = if (item.discount != null) {
-            ctx.getString(R.string.original_price) + formatPriceCycle(
+            SpannableString(
+                ctx.getString(R.string.original_price) + formatPriceCycle(
                 ctx = ctx,
-                price = item.payablePriceParams)
+                price = item.originalPriceParams)
+            )
+                .apply {
+                    setSpan(StrikethroughSpan(), 0, length-1, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+                }
         } else {
             null
         },
+    )
+}
+
+fun buildStripePrice(ctx: Context, params: PriceParams): ProductPrice {
+    return ProductPrice(
+        payable = formatPriceCycle(
+            ctx = ctx,
+            price = params
+        ),
+        original = null,
     )
 }
