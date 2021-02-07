dd1bc9f8653805d5c0a4a60b700a7615a211fd1f
neefrankie <neefrankie@gmail.com>
2021-02-08 00:54:20 +0800

    feat: recycler item for two line text with leading and trailing icons

---
+++ app/src/main/java/com/ft/ftchinese/ui/lists/TwoLineItemViewHolder.kt
@@ -0,0 +1,58 @@
+package com.ft.ftchinese.ui.lists
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ft.ftchinese.R
+
+class TwoLineItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val leadingIcon: ImageView = view.findViewById(R.id.list_item_leading_icon)
+    val primaryText: TextView = view.findViewById(R.id.list_item_primary_text)
+    val secondaryText: TextView = view.findViewById(R.id.list_item_secondary_text)
+    val trailingIcon: ImageView = view.findViewById(R.id.list_item_trailing_icon)
+
+    fun setLeadingIcon(resId: Int?) {
+        if (resId != null) {
+            leadingIcon.setImageResource(resId)
+        } else {
+            leadingIcon.visibility = View.GONE
+        }
+    }
+
+    fun setTrailingIcon(resId: Int?) {
+        if (resId != null) {
+            trailingIcon.setImageResource(resId)
+        } else {
+            leadingIcon.visibility = View.GONE
+        }
+    }
+
+    fun setPrimaryText(text: CharSequence) {
+        primaryText.text = text
+    }
+
+    fun setSecondaryText(text: CharSequence?) {
+        if (text != null) {
+            secondaryText.text = text
+        } else {
+            secondaryText.visibility = View.GONE
+        }
+    }
+
+    fun setPadding(size: Int) {
+        itemView.setPadding(size, size, size, size)
+    }
+
+    companion object {
+        fun create(parent: ViewGroup): TwoLineItemViewHolder {
+            return TwoLineItemViewHolder(
+                LayoutInflater
+                    .from(parent.context)
+                    .inflate(R.layout.list_item_two_line, parent, false)
+            )
+        }
+    }
+}
