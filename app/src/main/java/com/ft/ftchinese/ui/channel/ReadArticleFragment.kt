54758df692edf80f6097fc2cae5317a792a4f086
weiguo.ni <neefrankie@gmail.com>
2021-02-08 16:26:19 +0800

    refactor: cleanup

--- app/src/main/java/com/ft/ftchinese/ui/channel/ReadArticleFragment.kt
+++ app/src/main/java/com/ft/ftchinese/ui/channel/ReadArticleFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
