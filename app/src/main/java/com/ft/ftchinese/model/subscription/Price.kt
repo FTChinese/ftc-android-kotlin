495075ab865a9e18e56a037ac04c57d334429f5c
neefrankie <neefrankie@gmail.com>
2021-02-12 13:51:43 +0800

    refactor: add edition

--- app/src/main/java/com/ft/ftchinese/model/subscription/Price.kt
+++ app/src/main/java/com/ft/ftchinese/model/subscription/Price.kt
@@ -1,12 +1,12 @@
 package com.ft.ftchinese.model.subscription
 
 import com.ft.ftchinese.model.enums.Cycle
+import com.ft.ftchinese.model.enums.Edition
 import com.ft.ftchinese.model.enums.PriceSource
 import com.ft.ftchinese.model.enums.Tier
 import com.ft.ftchinese.model.fetch.KCycle
 import com.ft.ftchinese.model.fetch.KPriceSource
 import com.ft.ftchinese.model.fetch.KTier
-import com.ft.ftchinese.model.ui.PriceParams
 
 data class Price(
     val id: String,
@@ -24,19 +24,10 @@ data class Price(
     val unitAmount: Double,
     val promotionOffer: Discount = Discount(),
 ) {
-    val originalPriceParams: PriceParams
-        get() = PriceParams(
-            currency = currency,
-            amount = unitAmount,
+
+    val edition: Edition
+        get() = Edition(
             tier = tier,
             cycle = cycle,
-        )
-
-    val payablePriceParams: PriceParams
-        get() = PriceParams(
-            currency = currency,
-            amount = unitAmount - (promotionOffer.priceOff ?: 0.0),
-            tier = tier,
-            cycle = cycle
         )
 }
