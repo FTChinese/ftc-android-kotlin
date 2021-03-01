fa1f2bea401e0a060125d6af9d11657a0d4a098f
weiguo.ni <neefrankie@gmail.com>
2021-03-01 16:09:55 +0800

    refactor: productPriceParams to checkoutItem

--- app/src/main/java/com/ft/ftchinese/ui/checkout/CheckoutCounter.kt
+++ app/src/main/java/com/ft/ftchinese/ui/checkout/CheckoutCounter.kt
@@ -2,35 +2,46 @@ package com.ft.ftchinese.ui.checkout
 
 import android.content.Context
 import com.ft.ftchinese.model.enums.PayMethod
+import com.ft.ftchinese.model.price.CheckoutItem
 import com.ft.ftchinese.model.reader.Membership
 import com.ft.ftchinese.model.price.Discount
 import com.ft.ftchinese.model.price.Price
 import org.jetbrains.anko.AnkoLogger
 
-class Cart(
+/**
+ * CheckoutCounter is used to build the CheckOutActivity based on the
+ * price user chosen and current subscription status.
+ */
+class CheckoutCounter(
     val price: Price,
     val member: Membership,
 ) : AnkoLogger {
+    // Use might enjoy multiple discount at the same moment:
+    // promotion offered by FTC, or reward for timely renewal.
     private var discounts = mutableListOf<Discount>()
     private var discountIndex: Int = -1
     private var intents: CheckoutIntents
 
     init {
+        // Determine if promotion discount is available for now.
         if (price.promotionOffer.isValid()) {
             discounts.add(price.promotionOffer)
         }
 
         discountIndex = findMaxDiscount()
 
+        // Possible checkout intents prior to selecting a payment method.
         intents = CheckoutIntents.newInstance(member, price.edition)
     }
 
-    val productPriceParams: ProductPriceParams
-        get() = ProductPriceParams(
+    // Get the selected price and optional discount.
+    val checkoutItem: CheckoutItem
+        get() = CheckoutItem(
             price = price,
             discount = selectedDiscount
         )
 
+    // Show a spinner UI so that user choose a valid discount.
     val discountSpinnerParams: DiscountSpinnerParams
         get() = DiscountSpinnerParams(
             items = discounts,
@@ -50,6 +61,7 @@ class Cart(
     private val payableAmount: Double
         get() = price.unitAmount - (selectedDiscount?.priceOff ?: 0.0)
 
+    // By default we pre-select the discount with highest rate.
     private fun findMaxDiscount(): Int {
         if (discounts.size == 0) {
             return -1
@@ -71,6 +83,7 @@ class Cart(
         return maxIndex
     }
 
+    // Use the discount specified by user in case user manually changed the dropdown spinner.
     fun useDiscount(pos: Int) {
         if (pos > 0 && pos < discounts.size) {
             discountIndex = pos
