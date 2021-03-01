4735b3a00372724bb7eb200373ff2f5f4f77d789
weiguo.ni <neefrankie@gmail.com>
2021-03-01 16:04:50 +0800

    refactor: use Price to represent both ftc and stripe price

--- app/src/main/java/com/ft/ftchinese/model/price/Price.kt
+++ app/src/main/java/com/ft/ftchinese/model/price/Price.kt
@@ -6,7 +6,12 @@ import com.ft.ftchinese.model.enums.Tier
 import com.ft.ftchinese.model.fetch.KCycle
 import com.ft.ftchinese.model.fetch.KPriceSource
 import com.ft.ftchinese.model.fetch.KTier
+import com.ft.ftchinese.tracking.GAAction
 
+/**
+ * Price contains data of a product's price.
+ * It unifies both ftc and Stripe product.
+ */
 data class Price(
     val id: String,
     @KTier
@@ -29,4 +34,33 @@ data class Price(
             tier = tier,
             cycle = cycle,
         )
+
+    val namedKey: String
+        get() = "${tier}_${cycle}"
+
+    val gaAction: String
+        get() = when (tier) {
+            Tier.STANDARD -> when (cycle) {
+                Cycle.YEAR -> GAAction.BUY_STANDARD_YEAR
+                Cycle.MONTH -> GAAction.BUY_STANDARD_MONTH
+            }
+            Tier.PREMIUM -> GAAction.BUY_PREMIUM
+        }
+
+    // Turn to CheckoutItem used only by WxPayEntryActivity.
+    // Usually you should use CheckoutCounter to calculate it.
+    // The WxPayEntryActivity is isolated from the rest of teh app.
+    // When user enters that page, we've lost track of what user
+    // has selected.
+    val checkoutItem: CheckoutItem
+        get() = if (promotionOffer.isValid()) {
+            CheckoutItem(
+                price = this,
+                discount = promotionOffer,
+            )
+        } else {
+            CheckoutItem(
+                price = this,
+            )
+        }
 }
