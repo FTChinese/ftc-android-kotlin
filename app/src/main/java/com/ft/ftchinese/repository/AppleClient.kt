be2b2d4cead5d47876973bece2f2522c8e694d76
weiguo.ni <neefrankie@gmail.com>
2021-03-03 15:34:14 +0800

    feat: AppleClient to handle apple-only data fetching

---
+++ app/src/main/java/com/ft/ftchinese/repository/AppleClient.kt
@@ -0,0 +1,24 @@
+package com.ft.ftchinese.repository
+
+import com.ft.ftchinese.model.fetch.Fetch
+import com.ft.ftchinese.model.fetch.json
+import com.ft.ftchinese.model.iapsubs.IAPSubsResult
+import com.ft.ftchinese.model.reader.Account
+
+object AppleClient {
+    fun refreshIAP(account: Account): IAPSubsResult? {
+
+        val origTxId = account.membership.appleSubsId ?: throw Exception("Not an Apple subscription")
+
+        val (_, body) = Fetch()
+            .patch(Endpoint.subsBase(account.isTest) + "/apple/subs/$origTxId")
+            .noCache()
+            .endJsonText()
+
+        return if (body == null) {
+            null
+        } else {
+            json.parse<IAPSubsResult>(body)
+        }
+    }
+}
