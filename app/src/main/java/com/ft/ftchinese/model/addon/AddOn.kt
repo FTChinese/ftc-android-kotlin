fa406d97745190ca822036e8c4651b543a071abf
weiguo.ni <neefrankie@gmail.com>
2021-03-10 11:14:20 +0800

    refactor: removed AddOn; renamed ReservedDays to AddOn

--- app/src/main/java/com/ft/ftchinese/model/addon/AddOn.kt
+++ app/src/main/java/com/ft/ftchinese/model/addon/AddOn.kt
@@ -1,31 +1,28 @@
 package com.ft.ftchinese.model.addon
 
-import com.ft.ftchinese.model.enums.CarryOverSource
-import com.ft.ftchinese.model.enums.Cycle
-import com.ft.ftchinese.model.enums.PayMethod
 import com.ft.ftchinese.model.enums.Tier
-import com.ft.ftchinese.model.fetch.KCarryOverSource
-import com.ft.ftchinese.model.fetch.KCycle
-import com.ft.ftchinese.model.fetch.KDateTime
-import com.ft.ftchinese.model.fetch.KTier
-import org.threeten.bp.ZonedDateTime
 
 data class AddOn(
-    val id: String,
-    @KTier
-    val tier: Tier,
-    @KCycle
-    val cycle: Cycle,
-    val cycleCount: Int,
-    val daysRemained: Int,
-    @KCarryOverSource
-    val carryOverSource: CarryOverSource,
-    val payMethod: PayMethod,
-    val compoundId: String,
-    val orderId: String?,
-    val priceId: String?,
-    @KDateTime
-    val createdUtc: ZonedDateTime?,
-    @KDateTime
-    val consumedUtc: ZonedDateTime?,
-)
+    val standardAddOn: Int,
+    val premiumAddOn: Int,
+) {
+    fun plus(other: AddOn): AddOn {
+        return AddOn(
+            standardAddOn = standardAddOn + other.standardAddOn,
+            premiumAddOn = premiumAddOn + other.premiumAddOn,
+        )
+    }
+
+    fun clear(tier: Tier): AddOn {
+        return when (tier) {
+            Tier.STANDARD -> AddOn(
+                standardAddOn = 0,
+                premiumAddOn = premiumAddOn,
+            )
+            Tier.PREMIUM -> AddOn(
+                standardAddOn = standardAddOn,
+                premiumAddOn = 0,
+            )
+        }
+    }
+}
