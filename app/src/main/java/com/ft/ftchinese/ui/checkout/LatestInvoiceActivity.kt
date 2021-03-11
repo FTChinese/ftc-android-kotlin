44d241d1ef443cfeb0a2884089a5c5bdccf70a9e
weiguo.ni <neefrankie@gmail.com>
2021-03-11 17:26:20 +0800

    feat: show latest invoice

---
+++ app/src/main/java/com/ft/ftchinese/ui/checkout/LatestInvoiceActivity.kt
@@ -0,0 +1,130 @@
+package com.ft.ftchinese.ui.checkout
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.ft.ftchinese.R
+import com.ft.ftchinese.databinding.ActivityLatestInvoiceBinding
+import com.ft.ftchinese.model.enums.OrderKind
+import com.ft.ftchinese.model.invoice.Invoice
+import com.ft.ftchinese.store.InvoiceStore
+import com.ft.ftchinese.ui.base.ScopedAppActivity
+import com.ft.ftchinese.ui.formatter.formatEdition
+import com.ft.ftchinese.ui.formatter.getCurrencySymbol
+import com.ft.ftchinese.ui.lists.TwoColItemViewHolder
+import com.ft.ftchinese.ui.member.MemberActivity
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
+
+/**
+ * [LatestInvoiceActivity] shows the payment result of alipay of wxpay.
+ */
+@kotlinx.coroutines.ExperimentalCoroutinesApi
+class LatestInvoiceActivity : ScopedAppActivity(), AnkoLogger {
+
+    private lateinit var binding: ActivityLatestInvoiceBinding
+    private lateinit var listAdapter: ListAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_latest_invoice)
+
+        setSupportActionBar(binding.toolbar.toolbar)
+        supportActionBar?.apply {
+            setDisplayShowTitleEnabled(true)
+        }
+
+        binding.btnSubscriptionDone.setOnClickListener {
+            MemberActivity.start(this)
+            finish()
+        }
+
+        listAdapter = ListAdapter()
+
+        binding.rvLastInvoice.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this@LatestInvoiceActivity)
+            adapter = listAdapter
+        }
+
+        InvoiceStore.getInstance(this).loadInvoices()?.let {
+            listAdapter.setData(purchasedInvoiceRows(it.purchased))
+
+            if (it.carriedOver != null) {
+                binding.carriedOver = "购买前会员剩余 ${it.carriedOver.totalDays} 天，将在当前会员到期后继续使用"
+            }
+        }
+    }
+
+    private fun purchasedInvoiceRows(inv: Invoice): List<Pair<String, String>> {
+        return listOf(
+            Pair(
+                "订单号",
+                inv.orderId ?: "",
+            ),
+            Pair(
+                "订阅方案",
+                formatEdition(this, inv.edition),
+            ),
+            Pair(
+                "支付金额",
+                getString(
+                    R.string.formatter_price,
+                    getCurrencySymbol(inv.currency),
+                    inv.paidAmount,
+                ),
+            ),
+            Pair(
+                "支付方式",
+                inv.payMethod?.stringRes?.let {
+                      getString(it)
+                } ?: inv.payMethod.toString(),
+            ),
+            Pair(
+                "订阅期限",
+                if (inv.orderKind == OrderKind.AddOn) {
+                    when {
+                        inv.years > 0 -> "${inv.years}${getString(inv.cycle.stringRes)}"
+                        inv.months > 0 -> "${inv.months}${getString(inv.cycle.stringRes)}"
+                        else -> "1${getString(inv.cycle.stringRes)}"
+                    } + "(当前订阅过期后启用)"
+                } else {
+                    "${inv.startUtc?.toLocalDate()} 至 ${inv.endUtc?.toLocalDate()}"
+                }
+            )
+        )
+    }
+
+    inner class ListAdapter : RecyclerView.Adapter<TwoColItemViewHolder>() {
+        private var rows: List<Pair<String, String>> = listOf()
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TwoColItemViewHolder {
+            return TwoColItemViewHolder.create(parent)
+        }
+
+        override fun onBindViewHolder(holder: TwoColItemViewHolder, position: Int) {
+            val pair = rows[position]
+            holder.setLeadingText(pair.first)
+            holder.setTrailingText(pair.second)
+        }
+
+        override fun getItemCount() = rows.size
+
+        fun setData(pairs: List<Pair<String, String>>) {
+            info("Recycle view data $pairs")
+            this.rows = pairs
+            notifyDataSetChanged()
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun start(context: Context) {
+            context.startActivity(Intent(context, LatestInvoiceActivity::class.java))
+        }
+    }
+}
