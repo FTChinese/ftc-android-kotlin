a2de813b9ca9c082270d48f29eaa14b7d219bb5a
weiguo.ni <neefrankie@gmail.com>
2021-03-11 17:22:16 +0800

    feat: invoices field

--- app/src/main/java/com/ft/ftchinese/model/ftcsubs/ConfirmationResult.kt
+++ app/src/main/java/com/ft/ftchinese/model/ftcsubs/ConfirmationResult.kt
@@ -1,8 +1,11 @@
 package com.ft.ftchinese.model.ftcsubs
 
+import com.beust.klaxon.Json
 import com.ft.ftchinese.model.reader.Membership
 
 data class ConfirmationResult (
-        val order: Order,
-        val membership: Membership
+    val order: Order,
+    val membership: Membership,
+    @Json(ignored = true)
+    val invoices: Invoices
 )
