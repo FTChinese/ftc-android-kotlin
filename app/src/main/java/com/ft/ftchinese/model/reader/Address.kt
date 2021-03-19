a455906f9b706124f7fc7cab2fdcc87e9dbd89e8
weiguo.ni <neefrankie@gmail.com>
2021-03-19 14:56:19 +0800

    feat: methods to update Address

--- app/src/main/java/com/ft/ftchinese/model/reader/Address.kt
+++ app/src/main/java/com/ft/ftchinese/model/reader/Address.kt
@@ -7,4 +7,60 @@ data class Address(
     val district: String? = null,
     val street: String? = null,
     val postcode: String? = null,
-)
+) {
+
+    fun withProvince(p: String): Address {
+        return Address(
+            country = country,
+            province = p,
+            city = city,
+            district = district,
+            street = street,
+            postcode = postcode
+        )
+    }
+
+    fun withCity(c: String): Address {
+        return Address(
+            country = country,
+            province = province,
+            city = c,
+            district = district,
+            street = street,
+            postcode = postcode
+        )
+    }
+
+    fun withDistrict(d: String): Address {
+        return Address(
+            country = country,
+            province = province,
+            city = city,
+            district = d,
+            street = street,
+            postcode = postcode
+        )
+    }
+
+    fun withStreet(s: String): Address {
+        return Address(
+            country = country,
+            province = province,
+            city = city,
+            district = district,
+            street = s,
+            postcode = postcode
+        )
+    }
+
+    fun withPostcode(code: String): Address {
+        return Address(
+            country = country,
+            province = province,
+            city = city,
+            district = district,
+            street = street,
+            postcode = code,
+        )
+    }
+}
