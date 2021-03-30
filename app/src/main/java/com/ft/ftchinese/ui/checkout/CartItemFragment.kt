a251735d21555a84ecaafd92d0b71f02d3acbec6
weiguo.ni <neefrankie@gmail.com>
2021-03-30 15:14:19 +0800

    refactor: removed discounts list

--- app/src/main/java/com/ft/ftchinese/ui/checkout/CartItemFragment.kt
+++ app/src/main/java/com/ft/ftchinese/ui/checkout/CartItemFragment.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.ft.ftchinese.R
 import com.ft.ftchinese.databinding.FragmentCartItemBinding
 import com.ft.ftchinese.ui.base.ScopedFragment
-import com.ft.ftchinese.ui.formatter.getCurrencySymbol
 import com.ft.ftchinese.viewmodel.Result
 import org.jetbrains.anko.support.v4.toast
 
@@ -44,8 +41,12 @@ class CartItemFragment : ScopedFragment() {
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupViewModel()
+    }
+
+    private fun setupViewModel() {
 
         checkoutViewModel = activity?.run {
             ViewModelProvider(this).get(CheckOutViewModel::class.java)
@@ -58,46 +59,10 @@ class CartItemFragment : ScopedFragment() {
                 is Result.Success -> {
 
                     val counter = result.data
-                    binding.cartItem = CartItem.from(requireContext(), counter.checkoutItem)
-
-                    initSpinner(counter.discountOptions)
+                    binding.cartItem = CartItem.from(requireContext(), counter.item)
                 }
             }
         }
-
-        checkoutViewModel.checkoutItem.observe(viewLifecycleOwner) {
-            binding.cartItem = CartItem.from(requireContext(), it)
-        }
-
-        binding.discountSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, pos: Int, id: Long) {
-                checkoutViewModel.changeDiscount(pos)
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {}
-        }
-    }
-
-    private fun initSpinner(opts: DiscountOptions) {
-        if (!opts.hasDiscount) {
-            return
-        }
-
-        binding.hasDiscount = true
-
-        val options = opts.discounts.map { discount ->
-            "限时促销 -${context?.getString(R.string.formatter_price, getCurrencySymbol(discount.currency), discount.priceOff)}"
-        }
-
-        ArrayAdapter(
-            requireContext(),
-            android.R.layout.simple_spinner_item,
-            options
-        ).also { adapter ->
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-            binding.discountSpinner.adapter = adapter
-        }
-        binding.discountSpinner.setSelection(opts.spinnerIndex)
     }
 
     companion object {
