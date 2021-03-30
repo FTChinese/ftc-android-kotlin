0e1c4b2f6b47d663ab8ad42fa3d84aaa4baa050d
weiguo.ni <neefrankie@gmail.com>
2021-03-30 10:59:27 +0800

    feat: format price button on paywall

---
+++ app/src/main/java/com/ft/ftchinese/ui/product/formatPriceButton.kt
@@ -0,0 +1,34 @@
+package com.ft.ftchinese.ui.product
+
+import android.content.Context
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.StrikethroughSpan
+import com.ft.ftchinese.R
+import com.ft.ftchinese.model.ftcsubs.CheckoutItem
+import com.ft.ftchinese.ui.formatter.getCurrencySymbol
+
+fun formatPriceButton(ctx: Context, item: CheckoutItem): Spannable {
+    val payableStr = ctx.getString(
+        R.string.formatter_price_cycle,
+        getCurrencySymbol(item.price.currency),
+        item.payableAmount,
+        ctx.getString(item.price.cycle.stringRes)
+    )
+
+    // Show original price.
+    if (item.discount == null) {
+        return SpannableString(payableStr)
+    }
+
+    val crossed = ctx.getString(
+        R.string.formatter_price,
+        getCurrencySymbol(item.price.currency),
+        item.price.unitAmount
+    )
+
+    return SpannableString("$crossed $payableStr").apply {
+        setSpan(StrikethroughSpan(), 0, crossed.length, Spanned.SPAN_INCLUSIVE_INCLUSIVE)
+    }
+}
