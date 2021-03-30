2a9d1b1ec3802c6789c034024d37860ac5455537
weiguo.ni <neefrankie@gmail.com>
2021-03-30 11:10:58 +0800

    feat: add enum type OfferKind

--- app/src/main/java/com/ft/ftchinese/model/price/Discount.kt
+++ app/src/main/java/com/ft/ftchinese/model/price/Discount.kt
@@ -1,7 +1,9 @@
 package com.ft.ftchinese.model.price
 
 import android.os.Parcelable
+import com.ft.ftchinese.model.enums.OfferKind
 import com.ft.ftchinese.model.fetch.KDateTime
+import com.ft.ftchinese.model.fetch.KOfferKind
 import kotlinx.parcelize.Parcelize
 import org.threeten.bp.ZonedDateTime
 
@@ -15,13 +17,19 @@ data class Discount(
     val startUtc: ZonedDateTime? = null,
     @KDateTime
     val endUtc: ZonedDateTime? = null,
-    val description: String? = null
+    val description: String? = null,
+    @KOfferKind
+    val kind: OfferKind? = null,
 ) : Parcelable {
 
     // isValid checks whether a discount exists and whether it is in valid period.
     fun isValid(): Boolean {
         if (priceOff == null || priceOff <= 0) {
             return false
+        }
+
+        if (startUtc == null || endUtc == null) {
+            return true
         }
 
         val now = ZonedDateTime.now()
