1417e565d7437c180dd81d5a9e6eff414df82eac
weiguo.ni <neefrankie@gmail.com>
2021-04-09 11:22:19 +0800

    feat: BaseAccount

---
+++ app/src/main/java/com/ft/ftchinese/model/reader/BaseAccount.kt
@@ -0,0 +1,12 @@
+package com.ft.ftchinese.model.reader
+
+open class BaseAccount(
+    open val id: String,
+    open val unionId: String? = null,
+    open val stripeId: String? = null,
+    open val email: String,
+    open val mobile: String? = null,
+    open val userName: String? = null,
+    open val avatarUrl: String? = null,
+    open val isVerified: Boolean = false,
+)
