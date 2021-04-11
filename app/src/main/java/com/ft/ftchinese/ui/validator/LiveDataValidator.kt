0a0c8c3e0d7bf3c469c2f11a0bffd69943d5529a
neefrankie <neefrankie@gmail.com>
2021-04-11 15:09:02 +0800

    feat: logging

--- app/src/main/java/com/ft/ftchinese/ui/validator/LiveDataValidator.kt
+++ app/src/main/java/com/ft/ftchinese/ui/validator/LiveDataValidator.kt
@@ -2,14 +2,17 @@ package com.ft.ftchinese.ui.validator
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class LiveDataValidator(private val liveData: LiveData<String>) {
+class LiveDataValidator(private val liveData: LiveData<String>) : AnkoLogger {
     private val validationRules = mutableListOf<Predicate>()
     private val errorMessages = mutableListOf<String>()
 
     var error = MutableLiveData<String?>()
 
     fun isValid(): Boolean {
+        info("Validating live data ${liveData.value}")
         for (i in 0 until validationRules.size) {
             if (validationRules[i](liveData.value)) {
                 emitErrorMessage(errorMessages[i])
