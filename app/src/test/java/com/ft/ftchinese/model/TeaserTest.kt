57db58e73d257a69d5836502ee3ab18ca41f6176
weiguo.ni <neefrankie@gmail.com>
2021-04-22 16:33:55 +0800

    refactor: test

--- app/src/test/java/com/ft/ftchinese/model/TeaserTest.kt
+++ app/src/test/java/com/ft/ftchinese/model/TeaserTest.kt
@@ -1,22 +1,8 @@
 package com.ft.ftchinese.model
 
-import com.ft.ftchinese.model.content.ArticleType
-import com.ft.ftchinese.model.content.Teaser
 import org.junit.Test
 
 class TeaserTest {
-
-    private val item = Teaser(
-            id = "13139",
-            type = ArticleType.Interactive,
-            subType = null,
-            title = "2019 年 1 月中国市场汽车销量继续下降",
-            audioUrl = null,
-            radioUrl = null,
-            publishedAt = null,
-            tag = "FT研究院,报告,置顶,去广告,会员专享",
-            webUrl = "",
-            isWebpage = true)
 
     @Test
     fun renderStory() {
