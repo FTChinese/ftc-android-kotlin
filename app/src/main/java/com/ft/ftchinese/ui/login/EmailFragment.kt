ba56e01c1ccdfe2e19563cbeb3c078ec1f433f44
weiguo.ni <neefrankie@gmail.com>
2021-04-30 17:44:18 +0800

    refactor: use view model with data binding

--- app/src/main/java/com/ft/ftchinese/ui/login/EmailFragment.kt
+++ app/src/main/java/com/ft/ftchinese/ui/login/EmailFragment.kt
@@ -5,88 +5,57 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.ft.ftchinese.R
 import com.ft.ftchinese.databinding.FragmentEmailBinding
 import com.ft.ftchinese.ui.base.ScopedFragment
-import com.ft.ftchinese.ui.base.afterTextChanged
-import com.ft.ftchinese.ui.base.isConnected
-import com.ft.ftchinese.viewmodel.LoginViewModel
-import com.ft.ftchinese.viewmodel.Result
 import org.jetbrains.anko.AnkoLogger
-import org.jetbrains.anko.support.v4.toast
 
 @kotlinx.coroutines.ExperimentalCoroutinesApi
 class EmailFragment : ScopedFragment(),
         AnkoLogger {
 
     private lateinit var viewModel: LoginViewModel
+    private lateinit var emailViewModel: EmailExistsViewModel
     private lateinit var binding: FragmentEmailBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_email, container, false)
-        // Initially we want the button disabled and input
-        // enabled so that user cannot submit empty email.
-        binding.emailInput.isEnabled = true
+
         binding.emailInput.requestFocus()
 
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         viewModel = activity?.run {
             ViewModelProvider(this)
-                    .get(LoginViewModel::class.java)
+                .get(LoginViewModel::class.java)
         } ?: throw Exception("Invalid Activity")
 
+        emailViewModel = activity?.run {
+            ViewModelProvider(this)
+                .get(EmailExistsViewModel::class.java)
+        } ?: throw Exception("Invalid activity")
 
-        // Validate email upon changed.
-        binding.emailInput.afterTextChanged {
-            viewModel.emailDataChanged(binding.emailInput.text.toString().trim())
+        binding.viewModel = emailViewModel
+        binding.handler = this
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        // It's very weired if we assign the view model's
+        // isFormEnabled directly in xml, it only works on initial
+        // state. When progress changes, the button state does
+        // not change.
+        emailViewModel.isFormEnabled.observe(viewLifecycleOwner) {
+            binding.isFormEnabled = it
         }
-
-        viewModel.loginFormState.observe(viewLifecycleOwner, Observer {
-            val loginState = it ?: return@Observer
-
-            binding.nextBtn.isEnabled = loginState.isEmailValid
-
-            if (loginState.error != null) {
-                binding.emailInput.error = getString(loginState.error)
-                binding.emailInput.requestFocus()
-            }
-        })
-
-        binding.nextBtn.setOnClickListener {
-            if (context?.isConnected != true) {
-                toast(R.string.prompt_no_network)
-                return@setOnClickListener
-            }
-
-            // Upon submitting data, disable input and button.
-            // Shwo progress.
-            binding.enableInput = false
-            viewModel.inProgress.value = true
-
-            viewModel.checkEmail(binding.emailInput.text.toString().trim())
-        }
-
-        // Enable or disable input depending on network result.
-        // Only re-enable button if there's any error.
-        viewModel.emailResult.observe(viewLifecycleOwner, {
-
-            // In case any error occurred, allow user to
-            // re-enter and re-submit data.
-            binding.enableInput = it !is Result.Success
-        })
     }
 
-    override fun onResume() {
-        super.onResume()
-        binding.enableInput = true
+    fun onSubmit(view: View) {
+        emailViewModel.startChecking()
     }
 
     companion object {
