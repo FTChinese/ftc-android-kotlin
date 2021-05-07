999c2d611b0f921c5230abc16a18c916ada8cc54
weiguo.ni <neefrankie@gmail.com>
2021-05-07 20:01:46 +0800

    refactor: Credentials import path

--- app/src/main/java/com/ft/ftchinese/repository/LinkRepo.kt
+++ app/src/main/java/com/ft/ftchinese/repository/LinkRepo.kt
@@ -3,7 +3,7 @@ package com.ft.ftchinese.repository
 import com.beust.klaxon.Klaxon
 import com.ft.ftchinese.model.fetch.Fetch
 import com.ft.ftchinese.model.reader.Account
-import com.ft.ftchinese.model.reader.Credentials
+import com.ft.ftchinese.model.request.Credentials
 import com.ft.ftchinese.model.reader.UnlinkAnchor
 import com.ft.ftchinese.model.fetch.json
 
