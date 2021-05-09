97ecf55383f63bc9c5d4a860de1b6ffa56a911b0
neefrankie <neefrankie@gmail.com>
2021-05-09 09:04:04 +0800

    refactor: instance name

--- app/src/main/java/com/ft/ftchinese/ui/login/AuthKind.kt
+++ app/src/main/java/com/ft/ftchinese/ui/login/AuthKind.kt
@@ -2,7 +2,7 @@ package com.ft.ftchinese.ui.login
 
 // AuthKind is used to determine how the LogInFragment and SignInFragment will be used.
 enum class AuthKind {
-    EmailOnly,
-    LinkMobile,
-    LinkWechat;
+    EmailLogin, // User email + password to login
+    MobileLink, // Mobile login for the first time and link to an existing email account
+    WechatLink; // Wechat-logged-in user is trying to link to an existing email account.
 }
