f0f1390326a01c7157377aea3245a8d892b7450c
neefrankie <neefrankie@gmail.com>
2021-05-09 09:09:32 +0800

    refactor: removed login view mode land signup view model

--- app/src/main/java/com/ft/ftchinese/ui/wxlink/LinkFtcActivity.kt
+++ app/src/main/java/com/ft/ftchinese/ui/wxlink/LinkFtcActivity.kt
@@ -25,8 +25,6 @@ import org.jetbrains.anko.toast
 class LinkFtcActivity : ScopedAppActivity(), AnkoLogger {
 
     private lateinit var sessionManager: SessionManager
-    private lateinit var loginViewModel: SignInViewModel
-    private lateinit var signUpViewModel: SignUpViewModel
     private lateinit var emailViewModel: EmailExistsViewModel
     private lateinit var binding: ActivityFragmentDoubleBinding
 
@@ -46,25 +44,15 @@ class LinkFtcActivity : ScopedAppActivity(), AnkoLogger {
 
         sessionManager = SessionManager.getInstance(this)
 
-        loginViewModel = ViewModelProvider(this)
-            .get(SignInViewModel::class.java)
-
         emailViewModel = ViewModelProvider(this)
             .get(EmailExistsViewModel::class.java)
 
-        signUpViewModel = ViewModelProvider(this)
-            .get(SignUpViewModel::class.java)
-
         connectionLiveData.observe(this) {
-            loginViewModel.isNetworkAvailable.value = it
             emailViewModel.isNetworkAvailable.value = it
-            signUpViewModel.isNetworkAvailable.value = it
         }
 
         isConnected.let {
             emailViewModel.isNetworkAvailable.value = it
-            loginViewModel.isNetworkAvailable.value = it
-            signUpViewModel.isNetworkAvailable.value = it
         }
 
         setupViewModel()
@@ -80,14 +68,6 @@ class LinkFtcActivity : ScopedAppActivity(), AnkoLogger {
             binding.inProgress = it
         }
 
-        loginViewModel.progressLiveData.observe(this) {
-            binding.inProgress = it
-        }
-
-        signUpViewModel.progressLiveData.observe(this) {
-            binding.inProgress = it
-        }
-
         emailViewModel.existsResult.observe(this) { result ->
             when (result) {
                 is FetchResult.LocalizedError -> toast(result.msgId)
@@ -95,46 +75,17 @@ class LinkFtcActivity : ScopedAppActivity(), AnkoLogger {
                 is FetchResult.Success -> {
                     // If email exists, show sign in.
                     if (result.data) {
-                        SignInFragment()
-                            .show(supportFragmentManager, "WxLinkEmailSignIn")
+                        SignInFragment
+                            .forWechatLink()
+                            .show(supportFragmentManager, "WechatLink")
                     } else {
                         // If email does not exist, show sing up.
-                        SignUpFragment()
-                            .show(supportFragmentManager, "WxLinkEmailSignUp")
+                        SignUpFragment
+                            .forWechatLink()
+                            .show(supportFragmentManager, "WechatLink")
                     }
                 }
             }
-        }
-
-        // Account acquired from email login.
-        loginViewModel.accountResult.observe(this) {
-            when (it) {
-                is FetchResult.LocalizedError -> toast(it.msgId)
-                is FetchResult.Error -> it.exception.message?.let { msg -> toast(msg) }
-                is FetchResult.Success -> {
-                    // Show preview UI.
-                    LinkPreviewFragment()
-                        .show(supportFragmentManager, "WxLinkEmailPreview")
-                }
-            }
-        }
-
-        signUpViewModel.accountResult.observe(this) {
-           when (it) {
-               is FetchResult.LocalizedError -> toast(it.msgId)
-               is FetchResult.Error -> it.exception.message?.let { msg -> toast(msg) }
-               is FetchResult.Success -> {
-                   toast(R.string.prompt_linked)
-                   sessionManager.saveAccount(it.data)
-                   /**
-                    * If user created a new ftc account,
-                    * it is automatically linked upon creation.
-                    * back to [AccountActivity]
-                    */
-                   setResult(Activity.RESULT_OK)
-                   finish()
-               }
-           }
         }
     }
 
@@ -168,8 +119,8 @@ class LinkFtcActivity : ScopedAppActivity(), AnkoLogger {
         @JvmStatic
         fun startForResult(activity: Activity?) {
             activity?.startActivityForResult(
-                    Intent(activity, LinkFtcActivity::class.java),
-                    RequestCode.LINK
+                Intent(activity, LinkFtcActivity::class.java),
+                RequestCode.LINK
             )
 
         }
