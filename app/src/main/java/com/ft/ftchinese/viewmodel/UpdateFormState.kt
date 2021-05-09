0f44c6c8b3d446183998fbe845f8c8564c2f03ba
neefrankie <neefrankie@gmail.com>
2021-05-09 22:03:52 +0800

    refactor: removed

--- app/src/main/java/com/ft/ftchinese/viewmodel/UpdateFormState.kt
+++
@@ -1,8 +0,0 @@
-package com.ft.ftchinese.viewmodel
-
-data class UpdateFormState (
-        val emailError: Int? = null,
-        val nameError: Int? = null,
-        val passwordError: Int? = null,
-        val isDataValid: Boolean = false
-)
