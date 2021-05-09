8d4e13c5eaf3d2cd545de603dd81391bb9fba67c
neefrankie <neefrankie@gmail.com>
2021-05-09 21:59:37 +0800

    rename and delete Passwords class

--- app/src/main/java/com/ft/ftchinese/model/reader/PwResetBearer.kt
+++ app/src/main/java/com/ft/ftchinese/model/reader/PwResetBearer.kt
@@ -1,11 +1,5 @@
 package com.ft.ftchinese.model.reader
 
-// Parameter to update password.
-data class Passwords(
-    val oldPassword: String,
-    val password: String
-)
-
 // After the password reset code is verified to be valid,
 // server send back a token. Submit this token together with
 // new password.
