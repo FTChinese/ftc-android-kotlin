bf22f599a226f480a674dabe5488bd7b4f159128
neefrankie <neefrankie@gmail.com>
2021-05-09 21:56:22 +0800

    feat: pkg for user name updating

---
+++ app/src/main/java/com/ft/ftchinese/ui/account/name/UpdateNameFragment.kt
@@ -0,0 +1,91 @@
+package com.ft.ftchinese.ui.account.name
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.ft.ftchinese.R
+import com.ft.ftchinese.databinding.FragmentUpdateUsernameBinding
+import com.ft.ftchinese.store.SessionManager
+import com.ft.ftchinese.ui.base.ScopedFragment
+import com.ft.ftchinese.ui.base.isNetworkConnected
+import com.ft.ftchinese.ui.data.FetchResult
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.support.v4.toast
+
+@kotlinx.coroutines.ExperimentalCoroutinesApi
+class UpdateNameFragment : ScopedFragment(), AnkoLogger {
+    private lateinit var sessionManager: SessionManager
+    private lateinit var binding: FragmentUpdateUsernameBinding
+    private lateinit var viewModel: NameViewModel
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        sessionManager = SessionManager.getInstance(context)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_update_username,
+            container,
+            false,
+        )
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel = activity?.run {
+            ViewModelProvider(this)
+                .get(NameViewModel::class.java)
+        } ?: throw Exception("Invalid activity")
+
+        connectionLiveData.observe(viewLifecycleOwner) {
+            viewModel.isNetworkAvailable.value = it
+        }
+        activity?.isNetworkConnected()?.let {
+            viewModel.isNetworkAvailable.value = it
+        }
+
+        setupViewModel()
+
+        binding.userName = sessionManager.loadAccount()?.userName
+        binding.userNameInput.requestFocus()
+    }
+
+    private fun setupViewModel() {
+        viewModel.nameUpdated.observe(viewLifecycleOwner) {
+            when (it) {
+                is FetchResult.LocalizedError -> toast(it.msgId)
+                is FetchResult.Error -> it.exception.message?.let { msg -> toast(msg) }
+                is FetchResult.Success -> {
+                    toast(R.string.prompt_updated)
+                    sessionManager
+                        .loadAccount()
+                        ?.withBaseAccount(it.data)
+                        ?.let { account ->
+                            sessionManager.saveAccount(account)
+                        }
+                }
+            }
+        }
+    }
+
+    fun onSubmit(view: View) {
+        sessionManager.loadAccount()?.let {
+            viewModel.update(it.id)
+        }
+    }
+    companion object {
+
+        @JvmStatic
+        fun newInstance() = UpdateNameFragment()
+    }
+}
