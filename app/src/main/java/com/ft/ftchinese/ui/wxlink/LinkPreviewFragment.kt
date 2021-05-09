ae46c2b70fba1e93b898130e8176112f30f9064f
neefrankie <neefrankie@gmail.com>
2021-05-09 09:10:03 +0800

    refactor: pass parameter rather than using view model to get account data

--- app/src/main/java/com/ft/ftchinese/ui/wxlink/LinkPreviewFragment.kt
+++ app/src/main/java/com/ft/ftchinese/ui/wxlink/LinkPreviewFragment.kt
@@ -14,13 +14,11 @@ import androidx.lifecycle.ViewModelProvider
 import com.ft.ftchinese.R
 import com.ft.ftchinese.databinding.FragmentLinkPreviewBinding
 import com.ft.ftchinese.model.reader.Account
-import com.ft.ftchinese.model.reader.LoginMethod
 import com.ft.ftchinese.store.SessionManager
 import com.ft.ftchinese.ui.base.ScopedBottomSheetDialogFragment
+import com.ft.ftchinese.ui.base.isConnected
 import com.ft.ftchinese.ui.data.FetchResult
-import com.ft.ftchinese.ui.login.SignInViewModel
 import com.ft.ftchinese.util.RequestCode
-import com.ft.ftchinese.wxapi.WxOAuthViewModel
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.sdk27.coroutines.onClick
 import org.jetbrains.anko.support.v4.toast
@@ -33,18 +31,15 @@ import org.jetbrains.anko.support.v4.toast
  * 2. Email user wants to link to wechat.
  */
 @kotlinx.coroutines.ExperimentalCoroutinesApi
-class LinkPreviewFragment : ScopedBottomSheetDialogFragment(), AnkoLogger {
+class LinkPreviewFragment(
+    private val params: LinkParams
+) : ScopedBottomSheetDialogFragment(), AnkoLogger {
 
     private lateinit var sessionManager: SessionManager
     private lateinit var binding: FragmentLinkPreviewBinding
 
-    // Used to retrieve login account
-    private lateinit var loginViewModel: SignInViewModel
     // Perform link and refresh account
     private lateinit var linkViewModel: LinkViewModel
-    // Used to retrieve wechat account after authorization.
-    // TODO: add WxOAuthViewModel
-    private lateinit var wxAuthViewModel: WxOAuthViewModel
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -75,25 +70,24 @@ class LinkPreviewFragment : ScopedBottomSheetDialogFragment(), AnkoLogger {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        loginViewModel = activity?.run {
-            ViewModelProvider(this)
-                .get(SignInViewModel::class.java)
-        } ?: throw Exception("Invalid activity")
-
-        wxAuthViewModel = activity?.run {
-            ViewModelProvider(this)
-                .get(WxOAuthViewModel::class.java)
-        } ?: throw Exception("Invalid activity")
-
         // Link view model does not rely on hosting activity.
         linkViewModel = ViewModelProvider(this)
             .get(LinkViewModel::class.java)
+
+        connectionLiveData.observe(this) {
+            linkViewModel.isNetworkAvailable.value = it
+        }
+
+        activity?.isConnected?.let {
+            linkViewModel.isNetworkAvailable.value
+        }
 
         binding.viewModel = linkViewModel
         binding.lifecycleOwner = this
         binding.handler = this
 
         setupViewModel()
+        initUI()
     }
 
     private fun setupViewModel() {
@@ -101,41 +95,6 @@ class LinkPreviewFragment : ScopedBottomSheetDialogFragment(), AnkoLogger {
             binding.inProgress = it
         }
 
-        // After email account authenticated.
-        // Current account is wechat.
-        loginViewModel.accountResult.observe(viewLifecycleOwner) {
-            if (it !is FetchResult.Success) {
-                toast("Email account not found in view model")
-                return@observe
-            }
-
-            sessionManager.loadAccount()?.let { current ->
-                initUI(LinkParams(
-                    ftc = it.data,
-                    wx = current,
-                    loginMethod = current.loginMethod ?: LoginMethod.WECHAT
-                ))
-            }
-        }
-
-        // After wechat account authorized.
-        // Current account is ftc.
-        wxAuthViewModel.accountResult.observe(viewLifecycleOwner) {
-            if (it !is FetchResult.Success) {
-                toast("Email account not found in view model")
-                return@observe
-            }
-
-            sessionManager.loadAccount()?.let { current ->
-                initUI(LinkParams(
-                    ftc = current,
-                    wx = it.data,
-                    loginMethod = current.loginMethod ?: LoginMethod.EMAIL
-                ))
-            }
-        }
-
-        // TODO: email link wechat.
         linkViewModel.accountLinked.observe(this) {
             when (it) {
                 is FetchResult.LocalizedError -> toast(it.msgId)
@@ -157,7 +116,7 @@ class LinkPreviewFragment : ScopedBottomSheetDialogFragment(), AnkoLogger {
         }
     }
 
-    private fun initUI(params: LinkParams) {
+    private fun initUI() {
 
         binding.toolbar.bottomSheetToolbar.onClick {
             dismiss()
@@ -194,6 +153,7 @@ class LinkPreviewFragment : ScopedBottomSheetDialogFragment(), AnkoLogger {
     companion object {
         private const val EXTRA_ACCOUNT = "extra_account"
 
+        @Deprecated("")
         fun startForResult(activity: Activity?, account: Account) {
             val intent = Intent(activity, LinkPreviewFragment::class.java).apply {
                 putExtra(EXTRA_ACCOUNT, account)
