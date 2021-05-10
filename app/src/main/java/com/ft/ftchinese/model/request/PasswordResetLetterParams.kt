5e0192ba3b2bb8d154b7c959bbe6a89fddabb005
weiguo.ni <neefrankie@gmail.com>
2021-05-10 17:48:38 +0800

    fix: add useCode field and set default value to true

--- app/src/main/java/com/ft/ftchinese/model/request/PasswordResetLetterParams.kt
+++ app/src/main/java/com/ft/ftchinese/model/request/PasswordResetLetterParams.kt
@@ -2,4 +2,5 @@ package com.ft.ftchinese.model.request
 
 data class PasswordResetLetterParams(
     val email: String,
+    val useCode: Boolean = true, // On mobile apps use a code rather than link.
 )
