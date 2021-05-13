e5d2134cc59730fde411845d3d3b2deca619e5da
weiguo.ni <neefrankie@gmail.com>
2021-05-13 17:19:11 +0800

    feat: loadAvatar

--- app/src/main/java/com/ft/ftchinese/ui/account/WxInfoViewModel.kt
+++ app/src/main/java/com/ft/ftchinese/ui/account/WxInfoViewModel.kt
@@ -1,40 +1,76 @@
 package com.ft.ftchinese.ui.account
 
-import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.ft.ftchinese.R
 import com.ft.ftchinese.model.fetch.FetchResult
 import com.ft.ftchinese.model.fetch.ServerError
 import com.ft.ftchinese.model.reader.Account
+import com.ft.ftchinese.model.reader.Wechat
 import com.ft.ftchinese.model.reader.WxSession
 import com.ft.ftchinese.repository.AccountRepo
+import com.ft.ftchinese.store.CacheFileNames
+import com.ft.ftchinese.store.FileCache
 import com.ft.ftchinese.ui.base.BaseViewModel
 import com.ft.ftchinese.ui.data.ApiRequest
 import com.ft.ftchinese.viewmodel.WxRefreshState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import java.io.ByteArrayInputStream
+import java.io.InputStream
 
 class WxInfoViewModel : BaseViewModel() {
-    val swipingLiveData = MutableLiveData(false)
-
-    val isButtonEnabled = MediatorLiveData<Boolean>().apply {
-        value = true
-        addSource(progressLiveData) {
-            value = !it && (swipingLiveData.value == false)
-        }
-        addSource(swipingLiveData) {
-            value == !it && (progressLiveData.value == false)
-        }
-    }
 
     val accountLoaded: MutableLiveData<FetchResult<Account>> by lazy {
         MutableLiveData<FetchResult<Account>>()
     }
 
+    val avatarLoaded: MutableLiveData<FetchResult<InputStream>> by lazy {
+        MutableLiveData<FetchResult<InputStream>>()
+    }
+
+    // Should we show reauth dialog?
     val sessionState: MutableLiveData<WxRefreshState> by lazy {
         MutableLiveData<WxRefreshState>()
+    }
+
+    // Load avatar, upon initial start, from cache, and fallback to network.
+    fun loadAvatar(wechat: Wechat, cache: FileCache) {
+        wechat.avatarUrl ?: return
+
+        viewModelScope.launch {
+            try {
+                val fis = withContext(Dispatchers.IO) {
+                    cache.readBinaryFile(CacheFileNames.wxAvatar)
+                }
+
+                if (fis != null) {
+                    avatarLoaded.value = FetchResult.Success(fis)
+                    return@launch
+                }
+
+                if (isNetworkAvailable.value == false) {
+                    return@launch
+                }
+
+                val bytes = withContext(Dispatchers.IO) {
+                    AccountRepo.loadWxAvatar(wechat.avatarUrl)
+                } ?: return@launch
+
+                avatarLoaded.value = FetchResult.Success(ByteArrayInputStream(bytes))
+
+                withContext(Dispatchers.IO) {
+                    cache.writeBinaryFile(
+                        CacheFileNames.wxAvatar,
+                        bytes
+                    )
+                }
+            } catch (e: Exception) {
+                avatarLoaded.value = FetchResult.fromException(e)
+            }
+
+        }
     }
 
     // Refresh wechat info using current session.
@@ -46,7 +82,7 @@ class WxInfoViewModel : BaseViewModel() {
             return
         }
 
-        swipingLiveData.value = true
+        progressLiveData.value = true
         viewModelScope.launch {
             try {
                 val done = withContext(Dispatchers.IO) {
@@ -56,22 +92,55 @@ class WxInfoViewModel : BaseViewModel() {
                 if (done) {
                     sessionState.value = WxRefreshState.SUCCESS
                     accountLoaded.value = ApiRequest.asyncRefreshAccount(account)
-                    // TODO: also refresh avatar.
                 } else {
                     sessionState.value = WxRefreshState.ReAuth
                 }
 
-                swipingLiveData.value = false
+                progressLiveData.value = false
             } catch (e: ServerError) {
-                swipingLiveData.value = false
+                progressLiveData.value = false
                 accountLoaded.value = when (e.statusCode) {
                     404 -> FetchResult.LocalizedError(R.string.account_not_found)
                     // TODO: handle 422?
                     else -> FetchResult.fromServerError(e)
                 }
             } catch (e: Exception) {
-                swipingLiveData.value = false
+                progressLiveData.value = false
                 accountLoaded.value = FetchResult.fromException(e)
+            }
+        }
+    }
+
+    // Retrieve avatar from network.
+    fun refreshAvatar(wechat: Wechat, cache: FileCache) {
+
+        if (isNetworkAvailable.value == false) {
+            avatarLoaded.value = FetchResult.LocalizedError(R.string.prompt_no_network)
+            return
+        }
+
+        wechat.avatarUrl ?: return
+
+        progressLiveData.value = true
+        // No progress indicator for avatar
+        viewModelScope.launch {
+            try {
+                val bytes = withContext(Dispatchers.IO) {
+                    AccountRepo.loadWxAvatar(wechat.avatarUrl)
+                } ?: return@launch
+
+                progressLiveData.value = false
+                avatarLoaded.value = FetchResult.Success(ByteArrayInputStream(bytes))
+
+                withContext(Dispatchers.IO) {
+                    cache.writeBinaryFile(
+                        CacheFileNames.wxAvatar,
+                        bytes
+                    )
+                }
+            } catch (e: Exception) {
+                progressLiveData.value = false
+                avatarLoaded.value = FetchResult.fromException(e)
             }
         }
     }
