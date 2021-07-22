a2cc4c071266fd37a54bae3b2542d7e36eb091d2
weiguo.ni <neefrankie@gmail.com>
2021-07-22 11:17:04 +0800

    refactor: doc

--- app/src/main/java/com/ft/ftchinese/ui/base/WVViewModel.kt
+++ app/src/main/java/com/ft/ftchinese/ui/base/WVViewModel.kt
@@ -7,16 +7,18 @@ import com.ft.ftchinese.model.content.ChannelSource
 class WVViewModel : ViewModel() {
     // When a url in webview is clicked and the url point to
     // another channel page.
+    // Use inside channel fragment
     val urlChannelSelected: MutableLiveData<ChannelSource> by lazy {
         MutableLiveData<ChannelSource>()
     }
 
-    val pageFinished: MutableLiveData<Boolean> by lazy {
-        MutableLiveData<Boolean>()
-    }
-
     val pagingBtnClicked: MutableLiveData<Paging> by lazy {
         MutableLiveData<Paging>()
+    }
+
+    // Used inside article activity.
+    val pageFinished: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
     }
 
     val openGraphEvaluated: MutableLiveData<String> by lazy {
