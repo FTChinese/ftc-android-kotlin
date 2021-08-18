d6340457dc7ad7b6e10f52ba12c50f741c4d213f
weiguo.ni <neefrankie@gmail.com>
2021-08-18 14:14:42 +0800

    refactor: log url

--- app/src/main/java/com/ft/ftchinese/repository/ArticleClient.kt
+++ app/src/main/java/com/ft/ftchinese/repository/ArticleClient.kt
@@ -1,21 +1,27 @@
 package com.ft.ftchinese.repository
 
+import android.util.Log
 import com.ft.ftchinese.model.content.Story
 import com.ft.ftchinese.model.content.Teaser
 import com.ft.ftchinese.model.fetch.Fetch
 import com.ft.ftchinese.model.fetch.JSONResult
 import com.ft.ftchinese.model.fetch.json
-import org.jetbrains.anko.AnkoLogger
 
-object ArticleClient : AnkoLogger {
+object ArticleClient {
+    private const val TAG = "ArticleClient"
+
     /**
      * Load JSON documents from server. The raw json string
      * is returned together with the parsed doc so that
      * we don't need to re-serialize the parsed JSON.
      */
     fun fetchStory(teaser: Teaser, baseUrl: String): JSONResult<Story>? {
+        val url = teaser.articleUrl(baseUrl)
+
+        Log.i(TAG, "Loading article data from $url")
+
         val body = Fetch()
-            .get("${baseUrl}${teaser.apiPathSegment()}")
+            .get(url)
             .endPlainText()
 
         if (body.isNullOrBlank()) {
@@ -26,6 +32,7 @@ object ArticleClient : AnkoLogger {
         return if (s == null) {
             null
         } else {
+            s.teaser = teaser
             JSONResult(s, body)
         }
     }
