29d767113054469dd69c00110748d58fb72655f9
weiguo.ni <neefrankie@gmail.com>
2021-08-25 15:31:30 +0800

    feat: TAG

--- app/src/main/java/com/ft/ftchinese/ui/share/ScreenshotViewModel.kt
+++ app/src/main/java/com/ft/ftchinese/ui/share/ScreenshotViewModel.kt
@@ -3,6 +3,7 @@ package com.ft.ftchinese.ui.share
 import android.app.Application
 import android.os.Build
 import android.provider.MediaStore
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
@@ -11,25 +12,29 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
+private const val TAG = "ScreenShot"
+
 // TODO: can we merge this into SocialShareViewModel?
 class ScreenshotViewModel(application: Application): AndroidViewModel(application) {
 
     val progressLiveData = MutableLiveData(false)
 
+    // After a row is created in db for the
+    // image saving location
     val imageRowCreated: MutableLiveData<ArticleScreenshot> by lazy {
         MutableLiveData<ArticleScreenshot>()
     }
 
+    // After used clicked an app to share.
     val shareSelected: MutableLiveData<SocialAppId> by lazy {
         MutableLiveData<SocialAppId>()
     }
 
     /**
      * Insert a row for the image-to-be-created in MediaStore
-     * from [ArticleActivity].
-     * The child fragment [WebViewFragment] only need to
+     * The child fragment only need to
      * observe the uri of the image and save screenshot to it.
-     * @param teaser Used to build teh ContentValue of this record.
+     * @param article Used to build the ContentValue of this record.
      */
     fun createUri(article: ReadArticle) {
         progressLiveData.value = true
@@ -48,6 +53,8 @@ class ScreenshotViewModel(application: Application): AndroidViewModel(applicatio
                     .insert(imageCollection, ShareUtils.screenshotDetails(article))
             } ?: return@launch
 
+            Log.i(TAG, "Screenshot will be saved to $imageUri")
+
             imageRowCreated.value = ArticleScreenshot(
                 imageUri = imageUri,
                 content = article
@@ -56,6 +63,7 @@ class ScreenshotViewModel(application: Application): AndroidViewModel(applicatio
         }
     }
 
+    // Share the screenshot to the specified app.
     fun shareTo(app: SocialApp) {
         shareSelected.value = app.id
     }
