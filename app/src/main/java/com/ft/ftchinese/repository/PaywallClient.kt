b14088170a1500607ecfa354d2e0d398027cfcbc
weiguo.ni <neefrankie@gmail.com>
2021-09-16 18:45:54 +0800

    refactor: paywall for livemode

--- app/src/main/java/com/ft/ftchinese/repository/PaywallClient.kt
+++ app/src/main/java/com/ft/ftchinese/repository/PaywallClient.kt
@@ -4,15 +4,12 @@ import com.ft.ftchinese.model.fetch.Fetch
 import com.ft.ftchinese.model.fetch.JSONResult
 import com.ft.ftchinese.model.fetch.json
 import com.ft.ftchinese.model.paywall.Paywall
-import com.ft.ftchinese.model.price.Price
 
 object PaywallClient {
-    // Paywall data does not distinguish test or live account.
-    private val baseUrl = "${Endpoint.subsBase()}/paywall"
 
-    fun retrieve(): JSONResult<Paywall>? {
+    fun retrieve(isTest: Boolean): JSONResult<Paywall>? {
         val (_, body) = Fetch()
-            .get(baseUrl)
+            .get(Endpoint.paywall(isTest))
             .endJsonText()
 
         if (body.isNullOrBlank()) {
@@ -20,27 +17,16 @@ object PaywallClient {
         }
 
         val pw = json.parse<Paywall>(body)
+
         return if (pw == null) {
             null
         } else {
+            // If liveMode is true and isTest is true, or liveMode is false and isTes is false,
+            // environment mismatches.
+            if (pw.liveMode == isTest) {
+                throw Exception("Data environment mismatch: liveMode: ${pw.liveMode}, isTest: $isTest")
+            }
             JSONResult(pw, body)
-        }
-    }
-
-    fun listPrices(): JSONResult<List<Price>>? {
-        val (_, body) = Fetch()
-            .get("$baseUrl/plans")
-            .endJsonText()
-
-        if (body.isNullOrBlank()) {
-            return null
-        }
-
-        val plans = json.parseArray<Price>(body)
-        return if (plans == null) {
-            null
-        } else {
-            JSONResult(plans, body)
         }
     }
 }
