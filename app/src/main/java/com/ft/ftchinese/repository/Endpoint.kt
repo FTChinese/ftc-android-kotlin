c7ce4f7eac946d5d5c2d8f1e61f841d3cefb235c
weiguo.ni <neefrankie@gmail.com>
2021-09-16 18:46:06 +0800

    refactor:

--- app/src/main/java/com/ft/ftchinese/repository/Endpoint.kt
+++ app/src/main/java/com/ft/ftchinese/repository/Endpoint.kt
@@ -24,38 +24,15 @@ object ContentApi {
 }
 
 //private const val devIP = "http://192.168.10.115"
-private const val devIP = "http://10.0.0.2"
+private const val devIP = "http://192.168.1.52"
 object Endpoint {
 
-    private val authEmailBase = "${subsBase()}/auth/email"
-    val emailExists = "${authEmailBase}/exists"
-    val emailLogin = "${authEmailBase}/login"
-    val emailSignUp = "${authEmailBase}/signup"
-
-    private val authMobileBase = "${subsBase()}/auth/mobile"
-    val mobileVerificationCode = "${authMobileBase}/verification"
-    val mobileInitialLink = "${authMobileBase}/link"
-    val mobileSignUp = "${authMobileBase}/signup"
-
-    val passwordReset = "${subsBase()}/auth/password-reset"
-    val passwordResetLetter = "${passwordReset}/letter"
-    val passwordResetCodes = "${passwordReset}/codes"
-
-    private val authWxBase = "${subsBase()}/auth"
-    val wxLogin = "${authWxBase}/wx/login"
-    val wxRefresh = "${authWxBase}/wx/refresh"
-
-    val ftcAccount = "${subsBase()}/account"
-    val email = "${ftcAccount}/email"
-    val emailVrfLetter = "${ftcAccount}/email/request-verification"
-
-    val userName = "${ftcAccount}/name"
-    val passwordUpdate = "${ftcAccount}/password"
-    val address = "${ftcAccount}/address"
-    val wxAccount = "${ftcAccount}/wx"
-    val wxSignUp = "${wxAccount}/signup"
-    val wxLink = "${wxAccount}/link"
-    val wxUnlink = "${wxAccount}/unlink"
+    val accessToken = if (BuildConfig.DEBUG) {
+        BuildConfig.ACCESS_TOKEN_TEST
+//        BuildConfig.ACCESS_TOKEN_LIVE
+    } else {
+        BuildConfig.ACCESS_TOKEN_LIVE
+    }
 
     val contentBase = if (BuildConfig.DEBUG) {
         "$devIP:8100"
@@ -68,29 +45,95 @@ object Endpoint {
      * @isTest indicates whether the current user is a test account.
      * For endpoints that do not requires user being logged in,
      * use the default value.
+     *
+     * Live/test modes is determined by user account settings.
+     *
+     * For testing account, DEBUG reaches local server or sandbox server;
+     * otherwise reaches local server or live server.
+     *
+     * For Alipay/Wechat, use test account to get test data; otherwise live data.
+     * You cannot use wechat pay in debug mode, so to test them correctly, do it
+     * only in production app.
+     *
+     * For Apple, we also use test account to distinguish Product/Sandbox data.
      */
     fun subsBase(isTest: Boolean = false) = if (isTest) {
+        // When account is test, always use sandbox url, which might be local or online
         if (BuildConfig.DEBUG) {
-//            "$devIP:8202"
-            BuildConfig.API_SUBS_SANDBOX
+            "$devIP:8203"
+//        BuildConfig.API_SUBS_SANDBOX
         } else {
             BuildConfig.API_SUBS_SANDBOX
         }
     } else {
+        // When account is not test, always use live url, which might be local or online
         if (BuildConfig.DEBUG) {
-//            "$devIP:8202"
-            BuildConfig.API_SUBS_LIVE
+            "$devIP:8203"
+//        BuildConfig.API_SUBS_LIVE
         } else {
             BuildConfig.API_SUBS_LIVE
         }
     }
 
-    val accessToken = if (BuildConfig.DEBUG) {
-//        BuildConfig.ACCESS_TOKEN_TEST
-        BuildConfig.ACCESS_TOKEN_LIVE
+    /**
+     * For Stripe, however, things are quite different since a live/test key is involved
+     * in client app. For DEBUG app we are using testing key and production key otherwise.
+     * This means the account type does not matter. The mode is always determined by app's
+     * DEBUG configuration. Test account is meaningless here.
+     * In such case simply pass DEBUG as the isTest
+     * parameter.
+     */
+    private val apiBase = if (BuildConfig.DEBUG) {
+        "$devIP:8203"
+//        BuildConfig.API_SUBS_SANDBOX
     } else {
-        BuildConfig.ACCESS_TOKEN_LIVE
+        BuildConfig.API_SUBS_LIVE
     }
+
+    private val stripeBase = "${apiBase}/stripe"
+    val stripePrices = "${stripeBase}/prices"
+    val stripeCustomers = "${stripeBase}/customers"
+    val stripeSubs = "${stripeBase}/subs"
+
+    private val paywallBase = "${subsBase()}/paywall"
+
+    fun paywall(isTest: Boolean): String {
+        return "$paywallBase?live=${!isTest}"
+    }
+
+    fun refreshIAP(isTest: Boolean, origTxID: String): String {
+        return "${subsBase(isTest)}/apple/subs/$origTxID"
+    }
+
+    private val authEmailBase = "$apiBase/auth/email"
+    val emailExists = "${authEmailBase}/exists"
+    val emailLogin = "${authEmailBase}/login"
+    val emailSignUp = "${authEmailBase}/signup"
+
+    private val authMobileBase = "$apiBase/auth/mobile"
+    val mobileVerificationCode = "${authMobileBase}/verification"
+    val mobileInitialLink = "${authMobileBase}/link"
+    val mobileSignUp = "${authMobileBase}/signup"
+
+    val passwordReset = "$apiBase/auth/password-reset"
+    val passwordResetLetter = "${passwordReset}/letter"
+    val passwordResetCodes = "${passwordReset}/codes"
+
+    private val authWxBase = "$apiBase/auth"
+    val wxLogin = "${authWxBase}/wx/login"
+    val wxRefresh = "${authWxBase}/wx/refresh"
+
+    val ftcAccount = "$apiBase/account"
+    val email = "${ftcAccount}/email"
+    val emailVrfLetter = "${ftcAccount}/email/request-verification"
+
+    val userName = "${ftcAccount}/name"
+    val passwordUpdate = "${ftcAccount}/password"
+    val address = "${ftcAccount}/address"
+    val wxAccount = "${ftcAccount}/wx"
+    val wxSignUp = "${wxAccount}/signup"
+    val wxLink = "${wxAccount}/link"
+    val wxUnlink = "${wxAccount}/unlink"
 
     const val splashSchedule = "/index.php/jsapi/applaunchschedule"
 }
