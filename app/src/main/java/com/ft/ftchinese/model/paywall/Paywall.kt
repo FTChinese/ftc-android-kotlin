da870c627ca84442b7c189bc62f81b17326ec3c6
weiguo.ni <neefrankie@gmail.com>
2021-09-16 18:44:20 +0800

    feat: add activePrices, findPrice methods

--- app/src/main/java/com/ft/ftchinese/model/paywall/Paywall.kt
+++ app/src/main/java/com/ft/ftchinese/model/paywall/Paywall.kt
@@ -1,9 +1,27 @@
 package com.ft.ftchinese.model.paywall
 
+import com.ft.ftchinese.model.price.Edition
+import com.ft.ftchinese.model.price.Price
+
 data class Paywall(
     val banner: Banner,
     val promo: Promo,
-    val products: List<Product>
-)
+    val products: List<Product>,
+    val liveMode: Boolean = true,
+) {
+    val activePrices: List<Price>
+        get() = products.flatMap { it.prices }
+
+    fun findPrice(e: Edition): Price? {
+        return products
+            .find { it.tier == e.tier }
+            ?.prices
+            ?.find { it.cycle == e.cycle }
+    }
+
+    fun findPrice(id: String): Price? {
+        return activePrices.find { it.id == id }
+    }
+}
 
 
