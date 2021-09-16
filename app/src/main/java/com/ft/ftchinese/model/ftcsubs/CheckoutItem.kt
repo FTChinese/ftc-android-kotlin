f6e9fd8681b92b92c4726114f027c3b81708165c
weiguo.ni <neefrankie@gmail.com>
2021-09-16 18:44:51 +0800

    feat: CheckoutItem

--- app/src/main/java/com/ft/ftchinese/model/ftcsubs/CheckoutItem.kt
+++ app/src/main/java/com/ft/ftchinese/model/ftcsubs/CheckoutItem.kt
@@ -1,8 +1,11 @@
 package com.ft.ftchinese.model.ftcsubs
 
+import android.os.Parcelable
 import com.ft.ftchinese.model.price.Discount
 import com.ft.ftchinese.model.price.Price
 import com.ft.ftchinese.model.reader.Membership
+import com.ft.ftchinese.model.request.OrderParams
+import kotlinx.parcelize.Parcelize
 
 /**
  * CartItem is the item user put in shopping cart.
@@ -10,13 +13,27 @@ import com.ft.ftchinese.model.reader.Membership
  * and optional discount available for current moment.
  * NOTE the discount might be different from the one nested in price since the actually valid discount might comes from other sources.
  */
+@Parcelize
 data class CheckoutItem(
     val price: Price,
     val discount: Discount? = null,
-) {
+): Parcelable {
+    /**
+     * Calculates the amount that should actually paid.
+     */
     val payableAmount: Double
         get() = price.unitAmount - (discount?.priceOff ?: 0.0)
 
+    val reqParams: OrderParams
+        get() = OrderParams(
+            priceId = price.id,
+            discountId = discount?.id,
+        )
+
+    fun verify(isTest: Boolean): Boolean {
+        return price.liveMode != isTest
+    }
+    
     companion object {
         @JvmStatic
         fun newInstance(price: Price, m: Membership): CheckoutItem {
