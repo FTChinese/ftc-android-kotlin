579728e2796f649c90a21842423015402635ed35
weiguo.ni <neefrankie@gmail.com>
2021-09-22 11:48:00 +0800

    refactor: parcelable

--- app/src/main/java/com/ft/ftchinese/model/reader/Access.kt
+++ app/src/main/java/com/ft/ftchinese/model/reader/Access.kt
@@ -1,13 +1,20 @@
 package com.ft.ftchinese.model.reader
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Access(
     val status: MemberStatus,
     val rights: Int, // User owned permissions. Sum of multiple binary.
     val content: Permission, // Content allowed permission.
-) {
+) : Parcelable {
     // Check if content's permission is contained by user rights.
     val granted: Boolean
         get() = content.grant(rights)
+
+    val loggedIn: Boolean
+        get() = status != MemberStatus.NotLoggedIn
 
     companion object {
         // Deduce user's access permissions against
