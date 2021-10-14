a8e25d8795c96261e28434a4817bdf7a05ea7ed1
weiguo.ni <neefrankie@gmail.com>
2021-10-14 15:59:45 +0800

    feat: make AuthKind parcelable

--- app/src/main/java/com/ft/ftchinese/ui/login/AuthKind.kt
+++ app/src/main/java/com/ft/ftchinese/ui/login/AuthKind.kt
@@ -1,7 +1,11 @@
 package com.ft.ftchinese.ui.login
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 // AuthKind is used to determine how the LogInFragment and SignInFragment will be used.
-enum class AuthKind {
+@Parcelize
+enum class AuthKind : Parcelable {
     EmailLogin, // User email + password to login
     MobileLink, // Mobile login for the first time and link to an existing email account
     WechatLink; // Wechat-logged-in user is trying to link to an existing email account.
