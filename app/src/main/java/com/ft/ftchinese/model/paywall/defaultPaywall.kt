2bdd7f27bbc8d460c1a54bd1ccceb10490e32b0d
weiguo.ni <neefrankie@gmail.com>
2021-11-18 10:33:01 +0800

    update

--- app/src/main/java/com/ft/ftchinese/model/paywall/defaultPaywall.kt
+++ app/src/main/java/com/ft/ftchinese/model/paywall/defaultPaywall.kt
@@ -1,9 +1,11 @@
 package com.ft.ftchinese.model.paywall
 
 import com.ft.ftchinese.model.enums.Cycle
-import com.ft.ftchinese.model.enums.PriceSource
+import com.ft.ftchinese.model.enums.OfferKind
 import com.ft.ftchinese.model.enums.Tier
-import com.ft.ftchinese.model.price.Price
+import com.ft.ftchinese.model.ftcsubs.Discount
+import com.ft.ftchinese.model.ftcsubs.DiscountStatus
+import com.ft.ftchinese.model.ftcsubs.Price
 
 /**
  * A hard-coded paywall data used when server is not available.
@@ -31,33 +33,64 @@ val defaultPaywall = Paywall(
             id = "prod_9xrJdHFq0wmq",
             tier = Tier.STANDARD,
             heading = "标准会员",
-            description = "专享订阅内容每日仅需0.72元(或按月订阅每日0.93元)\n精选深度分析\n中英双语内容\n金融英语速读训练\n英语原声电台\n无限浏览2日前所有历史文章（近9万篇）",
+            description = "专享订阅内容每日仅需{{dailyAverageOfYear}}元(或按月订阅每日{{dailyAverageOfMonth}}元)\n精选深度分析\n中英双语内容\n金融英语速读训练\n英语电台\n阅读1日前历史文章（近9万篇）",
             smallPrint = null,
             prices = listOf(
                 Price(
-                    id = "plan_CvvaJr9jyGYt",
+                    id = "plan_RKy1IuKSXyua",
                     tier = Tier.STANDARD,
                     cycle = Cycle.YEAR,
                     active = true,
                     currency = "cny",
                     liveMode = true,
-                    nickname = "Standard Yearly Plan",
+                    nickname = "Standard Yearly Edition",
                     productId = "prod_9xrJdHFq0wmq",
-                    source = PriceSource.Ftc,
                     unitAmount = 298.0,
-                    offers = listOf(),
+                    stripePriceId = "",
+                    offers = listOf(
+                        Discount(
+                            id = "dsc_fAFaUX9VreYX",
+                            currency = "cny",
+                            description =  "现在续订享75折优惠",
+                            kind = OfferKind.Retention,
+                            startUtc = null,
+                            endUtc = null,
+                            priceOff = 80.0,
+                            percent = null,
+                            priceId = "plan_RKy1IuKSXyua",
+                            recurring = true,
+                            liveMode = true,
+                            status = DiscountStatus.Active,
+                        ),
+                        Discount(
+                            id = "dsc_2XOFwX42bAvI",
+                            currency = "cny",
+                            description = "重新购买会员享85折优惠",
+                            kind = OfferKind.WinBack,
+                            startUtc = null,
+                            endUtc = null,
+                            priceOff = 40.0,
+                            percent = null,
+                            priceId = "plan_RKy1IuKSXyua",
+                            recurring = true,
+                            liveMode = true,
+                            status = DiscountStatus.Active,
+
+                        )
+                    ),
                 ),
                 Price(
-                    id = "plan_8laAatSmDeGd",
+                    id = "plan_ohky3lyEMPSf",
                     tier = Tier.STANDARD,
                     cycle = Cycle.MONTH,
                     active = true,
                     currency = "cny",
                     liveMode = true,
-                    nickname = "Standard Monthly Plan",
+                    nickname = "Standard Monthly Edition",
                     productId = "prod_9xrJdHFq0wmq",
-                    source = PriceSource.Ftc,
                     unitAmount = 35.0,
+                    stripePriceId = "",
+                    offers = listOf()
                 ),
             )
         ),
@@ -65,7 +98,7 @@ val defaultPaywall = Paywall(
             id = "prod_zSgOTS6DWLmu",
             tier = Tier.PREMIUM,
             heading = "高端会员",
-            description = "专享订阅内容每日仅需5.5元\n享受“标准会员”所有权益\n编辑精选，总编/各版块主编每周五为您推荐本周必读资讯，分享他们的思考与观点\nFT商学院高端专享\nFT中文网2021年度论坛门票2张",
+            description = "专享订阅内容每日仅需{{dailyAverageOfYear}}元\n享受“标准会员”所有权益\n编辑精选，总编/各版块主编每周五为您推荐本周必读资讯，分享他们的思考与观点\nFT商学院高端专享\nFT中文网2022年度论坛门票2张",
             smallPrint = "注：所有活动门票不可折算现金、不能转让、不含差旅与食宿",
             prices = listOf(
                 Price(
@@ -75,11 +108,39 @@ val defaultPaywall = Paywall(
                     active = true,
                     currency = "cny",
                     liveMode = true,
-                    nickname = "Premium Yearly Plan",
+                    nickname = "Premium Yearly Edition",
                     productId = "prod_zSgOTS6DWLmu",
-                    source = PriceSource.Ftc,
                     unitAmount =  1998.0,
-                    offers = listOf(),
+                    stripePriceId = "",
+                    offers = listOf(
+                        Discount(
+                            id = "dsc_4jvMFdhEYXWk",
+                            currency = "cny",
+                            description = "现在续订享75折优惠",
+                            kind = OfferKind.Retention,
+                            startUtc = null,
+                            endUtc = null,
+                            priceOff = 500.0,
+                            percent = null,
+                            priceId = "plan_rLIy6LJYW8LV",
+                            recurring = true,
+                            liveMode = true,
+                            status = DiscountStatus.Active,
+                        ),
+                        Discount(
+                            id = "dsc_GzsiHfNQuKyn",
+                            currency = "cny",
+                            description = "重新购买会员享85折优惠",
+                            kind = OfferKind.WinBack,
+                            startUtc = null,
+                            endUtc = null,
+                            priceOff = 300.0,
+                            percent = null,
+                            recurring = true,
+                            liveMode = true,
+                            status = DiscountStatus.Active
+                        )
+                    ),
                 ),
             ),
         ),
