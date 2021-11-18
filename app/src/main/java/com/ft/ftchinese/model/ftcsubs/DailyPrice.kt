9fc5f5d3b1cfda3f79b83938870d73d752739ad9
weiguo.ni <neefrankie@gmail.com>
2021-11-18 10:32:16 +0800

    feat: daily cost placeholder

---
+++ app/src/main/java/com/ft/ftchinese/model/ftcsubs/DailyPrice.kt
@@ -0,0 +1,9 @@
+package com.ft.ftchinese.model.ftcsubs
+
+/**
+ * DailyPrice is used to replace placeholder in product description.
+ */
+data class DailyPrice(
+    val holder: String,
+    val replacer: String,
+)
