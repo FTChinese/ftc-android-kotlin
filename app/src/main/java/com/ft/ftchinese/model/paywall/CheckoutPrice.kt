cab387935db2aeb48ae87797b0419db36fb70737
weiguo.ni <neefrankie@gmail.com>
2021-12-07 17:42:11 +0800

    feat: add introductory field

--- app/src/main/java/com/ft/ftchinese/model/paywall/CheckoutPrice.kt
+++ app/src/main/java/com/ft/ftchinese/model/paywall/CheckoutPrice.kt
@@ -8,6 +8,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class CheckoutPrice(
+    val introductory: Introductory, // Use this to find stripe introductory offer only when favour is an introductory price.
     // Always the price you should usually charge user.
     val regular: UnifiedPrice,
     // A discounted price for ftc, or introductory price for stripe.
