9c6772b5485280a76ebfbf1440d2567ed8e803f6
weiguo.ni <neefrankie@gmail.com>
2021-12-07 17:40:13 +0800

    feat: parcelize

--- app/src/main/java/com/ft/ftchinese/model/paywall/Introductory.kt
+++ app/src/main/java/com/ft/ftchinese/model/paywall/Introductory.kt
@@ -1,5 +1,9 @@
 package com.ft.ftchinese.model.paywall
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Introductory(
     val stripePriceId: String?,
-)
+): Parcelable
