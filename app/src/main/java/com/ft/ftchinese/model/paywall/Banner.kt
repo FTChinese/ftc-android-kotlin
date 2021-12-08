19ef9ab231681fdec0d010fb702906a32660e642
weiguo.ni <neefrankie@gmail.com>
2021-12-08 16:30:09 +0800

    refactor: add startUtc and endUtc fields

--- app/src/main/java/com/ft/ftchinese/model/paywall/Banner.kt
+++ app/src/main/java/com/ft/ftchinese/model/paywall/Banner.kt
@@ -1,9 +1,17 @@
 package com.ft.ftchinese.model.paywall
 
+import com.ft.ftchinese.model.fetch.KDateTime
+import org.threeten.bp.ZonedDateTime
+
 data class Banner(
     val id: String,
     val heading: String,
     val subHeading: String? = null,
     val coverUrl: String? = null,
-    val content: String? = null
+    val content: String? = null,
+    val terms: String? = null,
+    @KDateTime
+    val startUtc: ZonedDateTime?,
+    @KDateTime
+    val endUtc: ZonedDateTime?
 )
