e03960a2b0948a55e1ca05816035b4adddc30622
weiguo.ni <neefrankie@gmail.com>
2021-12-22 12:08:53 +0800

    refactor

--- app/src/main/java/com/ft/ftchinese/model/paywall/defaultPaywall.kt
+++ app/src/main/java/com/ft/ftchinese/model/paywall/defaultPaywall.kt
@@ -2,10 +2,12 @@ package com.ft.ftchinese.model.paywall
 
 import com.ft.ftchinese.model.enums.Cycle
 import com.ft.ftchinese.model.enums.OfferKind
+import com.ft.ftchinese.model.enums.PriceKind
 import com.ft.ftchinese.model.enums.Tier
 import com.ft.ftchinese.model.ftcsubs.Discount
 import com.ft.ftchinese.model.ftcsubs.DiscountStatus
 import com.ft.ftchinese.model.ftcsubs.Price
+import com.ft.ftchinese.model.ftcsubs.YearMonthDay
 
 /**
  * A hard-coded paywall data used when server is not available.
@@ -38,9 +40,7 @@ val defaultPaywall = Paywall(
             heading = "标准会员",
             description = "专享订阅内容每日仅需{{dailyAverageOfYear}}元(或按月订阅每日{{dailyAverageOfMonth}}元)\n精选深度分析\n中英双语内容\n金融英语速读训练\n英语电台\n阅读1日前历史文章（近9万篇）",
             smallPrint = null,
-            introductory = Introductory(
-                stripePriceId = null,
-            ),
+            introductory = null,
             prices = listOf(
                 Price(
                     id = "plan_RKy1IuKSXyua",
@@ -48,11 +48,19 @@ val defaultPaywall = Paywall(
                     cycle = Cycle.YEAR,
                     active = true,
                     currency = "cny",
+                    kind = PriceKind.Recurring,
                     liveMode = true,
                     nickname = "Standard Yearly Edition",
+                    periodCount = YearMonthDay(
+                        years = 1,
+                        months = 0,
+                        days = 0,
+                    ),
                     productId = "prod_9xrJdHFq0wmq",
-                    unitAmount = 298.0,
                     stripePriceId = "",
+                    unitAmount = 298.0,
+                    startUtc = null,
+                    endUtc = null,
                     offers = listOf(
                         Discount(
                             id = "dsc_fAFaUX9VreYX",
@@ -81,7 +89,6 @@ val defaultPaywall = Paywall(
                             recurring = true,
                             liveMode = true,
                             status = DiscountStatus.Active,
-
                         )
                     ),
                 ),
@@ -91,11 +98,19 @@ val defaultPaywall = Paywall(
                     cycle = Cycle.MONTH,
                     active = true,
                     currency = "cny",
+                    kind = PriceKind.Recurring,
                     liveMode = true,
                     nickname = "Standard Monthly Edition",
+                    periodCount = YearMonthDay(
+                        years = 0,
+                        months = 1,
+                        days = 0,
+                    ),
                     productId = "prod_9xrJdHFq0wmq",
-                    unitAmount = 35.0,
                     stripePriceId = "",
+                    unitAmount = 35.0,
+                    startUtc = null,
+                    endUtc = null,
                     offers = listOf()
                 ),
             )
@@ -106,9 +121,7 @@ val defaultPaywall = Paywall(
             heading = "高端会员",
             description = "专享订阅内容每日仅需{{dailyAverageOfYear}}元\n享受“标准会员”所有权益\n编辑精选，总编/各版块主编每周五为您推荐本周必读资讯，分享他们的思考与观点\nFT商学院高端专享\nFT中文网2022年度论坛门票2张",
             smallPrint = "注：所有活动门票不可折算现金、不能转让、不含差旅与食宿",
-            introductory = Introductory(
-                stripePriceId = null,
-            ),
+            introductory = null,
             prices = listOf(
                 Price(
                     id = "plan_rLIy6LJYW8LV",
@@ -116,11 +129,19 @@ val defaultPaywall = Paywall(
                     cycle = Cycle.YEAR,
                     active = true,
                     currency = "cny",
+                    kind = PriceKind.Recurring,
                     liveMode = true,
                     nickname = "Premium Yearly Edition",
+                    periodCount = YearMonthDay(
+                        years = 1,
+                        months = 0,
+                        days = 0,
+                    ),
                     productId = "prod_zSgOTS6DWLmu",
-                    unitAmount =  1998.0,
                     stripePriceId = "",
+                    unitAmount =  1998.0,
+                    startUtc = null,
+                    endUtc = null,
                     offers = listOf(
                         Discount(
                             id = "dsc_4jvMFdhEYXWk",
