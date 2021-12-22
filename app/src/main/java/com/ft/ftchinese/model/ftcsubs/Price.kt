ef55a652519d102ac1975c0c59d8763077138db2
weiguo.ni <neefrankie@gmail.com>
2021-12-22 11:47:16 +0800

    refactor: cycle field turned to nullable; added kind, periodCount, title, startUtc and endUtc fields

--- app/src/main/java/com/ft/ftchinese/model/ftcsubs/Price.kt
+++ app/src/main/java/com/ft/ftchinese/model/ftcsubs/Price.kt
@@ -1,46 +1,96 @@
 package com.ft.ftchinese.model.ftcsubs
 
-import com.ft.ftchinese.model.enums.Cycle
-import com.ft.ftchinese.model.enums.OfferKind
-import com.ft.ftchinese.model.enums.Tier
+import android.os.Parcelable
+import com.ft.ftchinese.model.enums.*
 import com.ft.ftchinese.model.fetch.KCycle
+import com.ft.ftchinese.model.fetch.KDateTime
+import com.ft.ftchinese.model.fetch.KPriceKind
 import com.ft.ftchinese.model.fetch.KTier
-import com.ft.ftchinese.model.enums.Edition
+import kotlinx.parcelize.Parcelize
+import org.threeten.bp.ZonedDateTime
 
 /**
  * Price contains data of a product's price.
  * It unifies both ftc and Stripe product.
  */
+@Parcelize
 data class Price(
     val id: String,
     @KTier
-    override val tier: Tier,
+    val tier: Tier,
     @KCycle
-    override val cycle: Cycle,
+    val cycle: Cycle?,
     val active: Boolean = true,
     val currency: String = "cny",
-    val description: String? = null,
+    @KPriceKind
+    val kind: PriceKind? = null,
     val liveMode: Boolean,
     val nickname: String? = null,
+    val periodCount: YearMonthDay = YearMonthDay(),
     val productId: String,
-    val unitAmount: Double,
     val stripePriceId: String,
-    val offers: List<Discount> = listOf(),
-) : Edition(
-    tier = tier,
-    cycle = cycle,
-) {
+    val title: String? = null,
+    val unitAmount: Double,
+    @KDateTime
+    val startUtc: ZonedDateTime? = null,
+    @KDateTime
+    val endUtc: ZonedDateTime? = null,
+    val offers: List<Discount> = listOf(), // Does not exist when used as introductory price.
+) : Parcelable {
+
+    fun isAnnual(): Boolean {
+        return periodCount.years > 0
+    }
+
+    fun isMonthly(): Boolean {
+        return periodCount.months > 0
+    }
+
+    val edition: Edition
+        get() = Edition(
+            tier = tier,
+            cycle = cycle ?: periodCount.toCycle()
+        )
+
+    fun normalizeYMD(): YearMonthDay {
+        if (!periodCount.isZero()) {
+            return periodCount
+        }
+
+        return cycle?.let {
+            YearMonthDay.of(it)
+        }
+            ?: YearMonthDay.zero()
+    }
+
+    fun isValid(): Boolean {
+        if (startUtc == null || endUtc == null) {
+            return true
+        }
+
+        val now = ZonedDateTime.now()
+
+        if (now.isBefore(startUtc) || now.isAfter(endUtc)) {
+            return false
+        }
+
+        return true
+    }
 
     fun dailyPrice(): DailyPrice {
-        return when (cycle) {
-            Cycle.YEAR -> DailyPrice(
-                holder = "{{dailyAverageOfYear}}",
-                replacer = "%.2f".format(unitAmount.div(365))
-            )
-            Cycle.MONTH -> DailyPrice(
-                holder = "{{dailyAverageOfMonth}}",
-                replacer = "%.2f".format(unitAmount.div(30))
-            )
+        val ymd = normalizeYMD()
+        val c = ymd.toCycle()
+        val days = if (ymd.isZero()) {
+            1
+        } else {
+            ymd.totalDays()
+        }
+
+        val avg = unitAmount.div(days)
+
+        return when (c) {
+            Cycle.YEAR -> DailyPrice.ofYear(avg)
+            Cycle.MONTH -> DailyPrice.ofMonth(avg)
         }
     }
 
@@ -56,8 +106,9 @@ data class Price(
         // Filter the offers that are in the filters
         // list. If there are multiple ones, use
         // the one with the biggest price off.
+        // Ignore introductory discount which is no longer used.
         val filtered = offers.filter {
-                it.isValid() && filters.contains(it.kind)
+                it.kind != OfferKind.Introductory && it.isValid() && filters.contains(it.kind)
             }
             .sortedByDescending {
                 it.priceOff
