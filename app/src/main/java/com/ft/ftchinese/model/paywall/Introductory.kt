156804c83bcb0025911e2e25809804fb514f3c82
weiguo.ni <neefrankie@gmail.com>
2021-12-22 11:58:47 +0800

    delete files

--- app/src/main/java/com/ft/ftchinese/model/paywall/Introductory.kt
+++
@@ -1,9 +0,0 @@
-package com.ft.ftchinese.model.paywall
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class Introductory(
-    val stripePriceId: String?,
-): Parcelable
