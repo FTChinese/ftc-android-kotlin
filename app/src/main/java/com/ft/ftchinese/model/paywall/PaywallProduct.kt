fbb9834e7cba1a2fb9a7583a2bb0aa7c7286940f
weiguo.ni <neefrankie@gmail.com>
2021-12-22 11:59:51 +0800

    refactor: introductory changed to Price

--- app/src/main/java/com/ft/ftchinese/model/paywall/PaywallProduct.kt
+++ app/src/main/java/com/ft/ftchinese/model/paywall/PaywallProduct.kt
@@ -1,9 +1,7 @@
 package com.ft.ftchinese.model.paywall
 
-import com.ft.ftchinese.model.enums.OfferKind
 import com.ft.ftchinese.model.enums.Tier
 import com.ft.ftchinese.model.fetch.KTier
-import com.ft.ftchinese.model.ftcsubs.Discount
 import com.ft.ftchinese.model.ftcsubs.Price
 import com.ft.ftchinese.model.reader.Membership
 
@@ -18,7 +16,7 @@ data class PaywallProduct(
     val heading: String,
     val description: String,
     val smallPrint: String?,
-    val introductory: Introductory,
+    val introductory: Price? = null,
     val prices: List<Price>,
 ) {
     fun descWithDailyCost(): String {
@@ -35,30 +33,30 @@ data class PaywallProduct(
         return desc
     }
 
-    private fun findIntro(): Discount? {
-        return prices.flatMap {
-            it.offers
-        }
-            .find {
-                it.kind == OfferKind.Introductory && it.isValid()
-            }
+    fun introPrice(m: Membership): FtcCheckout? {
+        return if ((introductory?.isValid() == true) && m.isZero) {
+            FtcCheckout(
+                price = introductory,
+                discount = null,
+                isIntroductory = true,
+                stripeTrialParents = prices.map { it.stripePriceId }
+            )
+        } else null
     }
 
     /**
      * Compose information required by price buttons on ui.
      */
-    fun checkoutPrices(m: Membership): List<CheckoutPrice> {
-        val introDiscount = findIntro()
+    fun recurringPrices(m: Membership): List<FtcCheckout> {
+
+        val offerKinds = m.offerKinds
 
         return prices.map {
-            CheckoutPrice(
-                introductory = Introductory(
-                    stripePriceId = if (introDiscount == null) null else introductory.stripePriceId
-                ),
-                regular = UnifiedPrice.fromFtc(it, null),
-                favour = it.applicableOffer(m.offerKinds)?.let { discount ->
-                    UnifiedPrice.fromFtc(it, discount)
-                }
+            FtcCheckout(
+                price = it,
+                discount = it.applicableOffer(offerKinds),
+                isIntroductory = false,
+                stripeTrialParents = listOf(),
             )
         }
     }
