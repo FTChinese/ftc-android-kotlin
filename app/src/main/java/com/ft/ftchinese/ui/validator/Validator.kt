4f32492345efe8c5d32f6d2bcb7606f4cfca790c
Oliver Zhang <oli.zhang@gmail.comm>
2022-01-05 14:23:13 +0800

    mainland phone number

--- app/src/main/java/com/ft/ftchinese/ui/validator/Validator.kt
+++ app/src/main/java/com/ft/ftchinese/ui/validator/Validator.kt
@@ -42,13 +42,7 @@ object Validator {
     }
 
     /**
-     * 大陆手机号码11位数，匹配格式：前三位固定格式+后8位任意数
-     * 此方法中前三位格式有：
-     * 13+任意数
-     * 15+除4的任意数
-     * 18+除1和4的任意数
-     * 17+除9的任意数
-     * 147
+     * 大陆手机号码11位数字，未来可能会增加
      */
     fun isMainlandPhone(str: String?): Boolean {
         if (str == null) {
@@ -59,7 +53,7 @@ object Validator {
             return true
         }
 
-        val regExp = "^((13[0-9])|(15[^4])|(18[0,2,3,5-9])|(17[0-8])|(147))\\d{8}$"
+        val regExp = "^\\d{11}$"
         val p: Pattern = Pattern.compile(regExp)
         return p.matcher(str).matches()
     }
