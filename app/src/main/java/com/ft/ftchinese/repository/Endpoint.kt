7bbe94f7fc421275788364cb52bf8be3a411bac4
weiguo.ni <neefrankie@gmail.com>
2022-01-05 10:37:41 +0800

    ip

--- app/src/main/java/com/ft/ftchinese/repository/Endpoint.kt
+++ app/src/main/java/com/ft/ftchinese/repository/Endpoint.kt
@@ -2,7 +2,7 @@ package com.ft.ftchinese.repository
 
 import com.ft.ftchinese.BuildConfig
 
-private const val devIP = "http://192.168.1.36"
+private const val devIP = "http://192.168.1.65"
 private const val devPort = "8206"
 
 object NextApi {
