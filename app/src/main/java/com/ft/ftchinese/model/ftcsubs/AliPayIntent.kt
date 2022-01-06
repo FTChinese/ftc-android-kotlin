30183e49bbf7a0ae2724b17d34d1f1965abfa573
weiguo.ni <neefrankie@gmail.com>
2022-01-06 17:11:43 +0800

    refactor: adapt for api changes

--- app/src/main/java/com/ft/ftchinese/model/ftcsubs/AliPayIntent.kt
+++ app/src/main/java/com/ft/ftchinese/model/ftcsubs/AliPayIntent.kt
@@ -1,6 +1,14 @@
 package com.ft.ftchinese.model.ftcsubs
 
 data class AliPayIntent(
-    val order: Order,
-    val param: String,
+    override val price: Price,
+    override val order: Order,
+    val param: AliPaySDKParams,
+) : PayIntent(
+    price = price,
+    order = order,
+)
+
+data class AliPaySDKParams(
+    val appSdk: String?
 )
--- app/src/main/java/com/ft/ftchinese/model/ftcsubs/WxPayIntent.kt
+++ app/src/main/java/com/ft/ftchinese/model/ftcsubs/WxPayIntent.kt
@@ -2,6 +2,24 @@ package com.ft.ftchinese.model.ftcsubs
 
 // This is user's payment intent.
 data class WxPayIntent(
-    val order: Order,
-    val params: WxPayParams,
+    override val price: Price,
+    override val order: Order,
+    val params: WxPaySDKParams,
+) : PayIntent(
+    price = price,
+    order = order
+)
+
+data class WxPaySDKParams(
+    val appSdk: WxAppPayParams?
+)
+
+data class WxAppPayParams(
+    val appId: String,
+    val partnerId: String,
+    val prepayId: String,
+    val timestamp: String,
+    val nonce: String,
+    val pkg: String,
+    val signature: String,
 )
