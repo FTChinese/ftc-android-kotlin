51421d81f4f57194da4968afff51ab42bcbdeb84
weiguo.ni <neefrankie@gmail.com>
2022-01-10 17:26:16 +0800

    refactor: removed findPrice

--- app/src/main/java/com/ft/ftchinese/model/paywall/Paywall.kt
+++ app/src/main/java/com/ft/ftchinese/model/paywall/Paywall.kt
@@ -1,7 +1,5 @@
 package com.ft.ftchinese.model.paywall
 
-import com.ft.ftchinese.model.enums.Edition
-import com.ft.ftchinese.model.ftcsubs.Price
 import org.threeten.bp.ZonedDateTime
 
 data class Paywall(
@@ -11,13 +9,6 @@ data class Paywall(
     val products: List<PaywallProduct>,
     val liveMode: Boolean = true,
 ) {
-
-    fun findPrice(e: Edition): Price? {
-        return products
-            .find { it.tier == e.tier }
-            ?.prices
-            ?.find { it.cycle == e.cycle }
-    }
 
     fun isPromoValid(): Boolean {
         if (promo.id.isEmpty()) {
