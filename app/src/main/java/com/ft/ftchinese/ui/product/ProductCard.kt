0a6b3d3ecdfd34e00212a201922c571557406df4
weiguo.ni <neefrankie@gmail.com>
2022-03-16 17:38:23 +0800

    try compose ui

---
+++ app/src/main/java/com/ft/ftchinese/ui/product/ProductCard.kt
@@ -0,0 +1,65 @@
+package com.ft.ftchinese.ui.product
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ft.ftchinese.R
+import com.ft.ftchinese.model.paywall.PaywallProduct
+import com.ft.ftchinese.model.paywall.defaultPaywall
+
+@Composable
+fun ProductCard(prod: PaywallProduct) {
+    Column {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = prod.heading,
+                style = MaterialTheme.typography.subtitle1,
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column() {
+            prod.descWithDailyCost().split("\n").forEach { line ->
+                ProductDescRow(txt = line)
+            }
+
+            prod.smallPrint?.let {
+                Text(text = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun ProductDescRow(txt: String) {
+    Row {
+       Image(
+           painter = painterResource(id = R.drawable.ic_done_gray_24dp),
+           contentDescription = null,
+       )
+
+        Spacer(modifier = Modifier.width(4.dp))
+
+        Text(
+            text = txt,
+            style = MaterialTheme.typography.body2,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewProductCard() {
+    ProductCard(prod = defaultPaywall.products[0])
+}
