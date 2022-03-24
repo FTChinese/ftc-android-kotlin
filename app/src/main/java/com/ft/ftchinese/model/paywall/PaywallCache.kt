0b010fe630992fb609af4c1196c0b6d9a8786fd2
weiguo.ni <neefrankie@gmail.com>
2022-03-24 17:20:39 +0800

    refactor

--- app/src/main/java/com/ft/ftchinese/model/paywall/PaywallCache.kt
+++ app/src/main/java/com/ft/ftchinese/model/paywall/PaywallCache.kt
@@ -4,16 +4,14 @@ import com.ft.ftchinese.model.stripesubs.StripePrice
 
 object PaywallCache {
     private var ftc = defaultPaywall
-    private var stripe: MutableMap<String, StripePrice> = mutableMapOf()
+    private var stripe: Map<String, StripePrice> = mapOf()
 
     fun setFtc(p: Paywall) {
         ftc = p
     }
 
     fun setStripe(prices: List<StripePrice>) {
-        prices.forEach {
-            stripe[it.id] = it
-        }
+        stripe = prices.associateBy { it.id }
     }
 
     fun getFtc(): Paywall {
@@ -22,6 +20,6 @@ object PaywallCache {
 
     fun clear() {
         ftc = defaultPaywall
-        stripe.clear()
+        stripe = mapOf()
     }
 }
