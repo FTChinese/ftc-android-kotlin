15b243dab3205c92b735049688e4ef6c6a1bf438
weiguo.ni <neefrankie@gmail.com>
2022-03-25 09:56:03 +0800

    feat: PriceList

--- app/src/main/java/com/ft/ftchinese/ui/paywall/PriceList.kt
+++ app/src/main/java/com/ft/ftchinese/ui/paywall/PriceList.kt
@@ -1,10 +1,12 @@
-package com.ft.ftchinese.ui.product
+package com.ft.ftchinese.ui.paywall
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import com.ft.ftchinese.model.paywall.CartItemFtcV2
 import com.ft.ftchinese.model.paywall.CartItemStripeV2
+import com.ft.ftchinese.ui.product.PriceCard
+import com.ft.ftchinese.ui.product.PriceCardParams
 
 @Composable
 fun PriceList(
@@ -16,16 +18,14 @@ fun PriceList(
     Column {
         ftcCartItems.forEach { cartItem ->
             PriceCard(
-                params = PriceCardParams
-                    .ofFtc(LocalContext.current, cartItem),
+                params = PriceCardParams.ofFtc(LocalContext.current, cartItem),
                 onClick = { onFtcPay(cartItem) }
             )
         }
 
         stripeCartItems.forEach { cartItem ->
             PriceCard(
-                params = PriceCardParams
-                    .ofStripe(LocalContext.current, cartItem),
+                params = PriceCardParams.ofStripe(LocalContext.current, cartItem),
                 onClick = { onStripePay(cartItem) },
             )
         }
