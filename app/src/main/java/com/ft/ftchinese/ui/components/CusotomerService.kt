17dff7ce003e95e72cee5332ee46fdc623274fb3
weiguo.ni <neefrankie@gmail.com>
2022-03-25 17:10:35 +0800

    feat: CustomerService

---
+++ app/src/main/java/com/ft/ftchinese/ui/components/CusotomerService.kt
@@ -0,0 +1,47 @@
+package com.ft.ftchinese.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.ft.ftchinese.R
+import com.ft.ftchinese.ui.base.IntentsUtil
+import com.ft.ftchinese.ui.theme.OColor
+
+@Composable
+fun CustomerService(
+    onError: (String) -> Unit,
+) {
+    val context = LocalContext.current
+
+    Column {
+        Text(
+            text = stringResource(id = R.string.title_customer_service),
+            style = MaterialTheme.typography.h6,
+        )
+        Divider(color = OColor.teal)
+        TextButton(onClick = {
+            val intent = IntentsUtil.emailCustomerService("FT中文网会员订阅")
+            if (intent.resolveActivity(context.packageManager) != null) {
+                context.startActivity(intent)
+            } else {
+                onError(context.getString(R.string.prompt_no_email_app))
+            }
+        }) {
+            Text(text = stringResource(id = R.string.customer_service_email))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCustomerService() {
+    CustomerService(
+        onError = { }
+    )
+}
