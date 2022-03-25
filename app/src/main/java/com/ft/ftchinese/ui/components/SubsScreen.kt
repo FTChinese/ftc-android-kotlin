fae3f59ccdc931bde83a16076f1ff6c53f658eab
weiguo.ni <neefrankie@gmail.com>
2022-03-25 14:29:12 +0800

    feat: enum for subscription screens

---
+++ app/src/main/java/com/ft/ftchinese/ui/components/SubsScreen.kt
@@ -0,0 +1,28 @@
+package com.ft.ftchinese.ui.components
+
+import com.ft.ftchinese.R
+
+enum class SubsScreen(
+    val titleId: Int,
+) {
+    Paywall(
+        titleId = R.string.title_subscription
+    ),
+    FtcPay(
+        titleId = R.string.check_out
+    ),
+    StripePay(
+        titleId = R.string.pay_method_stripe
+    );
+
+    companion object {
+        fun fromRoute(route: String?): SubsScreen =
+            when (route?.substringBefore("/")) {
+                Paywall.name -> Paywall
+                FtcPay.name -> FtcPay
+                StripePay.name -> StripePay
+                null -> Paywall
+                else -> throw IllegalArgumentException("Route $route is not recognized")
+            }
+    }
+}
