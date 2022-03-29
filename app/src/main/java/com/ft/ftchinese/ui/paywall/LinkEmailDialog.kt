1a74c2ddee1ef4d8dffca81b466b620f717da929
weiguo.ni <neefrankie@gmail.com>
2022-03-29 17:17:31 +0800

    feat: LinkEmailDialog for wechat-only user

---
+++ app/src/main/java/com/ft/ftchinese/ui/paywall/LinkEmailDialog.kt
@@ -0,0 +1,48 @@
+package com.ft.ftchinese.ui.paywall
+
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.ft.ftchinese.R
+
+@Composable
+fun LinkEmailDialog(
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit
+){
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = {
+            Text(text = stringResource(id = R.string.title_link_email))
+        },
+        text = {
+            Text(text = "您当前使用了微信登录。Stripe支付要求提供邮箱，是否绑定邮箱？")
+        },
+        confirmButton = {
+            TextButton(
+                onClick = onConfirm
+            ) {
+                Text(text = stringResource(id = R.string.yes))
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = onDismiss
+            ) {
+                Text(text = stringResource(id = R.string.no))
+            }
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewLinkEmailDialog() {
+    LinkEmailDialog(
+        onConfirm = {},
+        onDismiss = {},
+    )
+}
