b581d9ac52dd1b4a9a848f70dc18d6653625b638
weiguo.ni <neefrankie@gmail.com>
2022-03-31 17:48:53 +0800

    simply paywall cache

--- app/src/main/java/com/ft/ftchinese/store/PaywallCache.kt
+++ app/src/main/java/com/ft/ftchinese/store/PaywallCache.kt
@@ -1,6 +1,5 @@
 package com.ft.ftchinese.store
 
-import com.ft.ftchinese.model.ftcsubs.Price
 import com.ft.ftchinese.model.paywall.Paywall
 import com.ft.ftchinese.model.paywall.defaultPaywall
 import com.ft.ftchinese.model.stripesubs.StripePrice
@@ -13,20 +12,8 @@ object PaywallCache {
         ftc = p
     }
 
-    fun setStripe(prices: List<StripePrice>) {
-        stripe = prices.associateBy { it.id }
-    }
-
-    fun findFtcPrice(id: String): Price? {
-        return ftc.products.flatMap {
-            it.prices
-        }.find {
-            it.id == id
-        }
-    }
-
-    fun findStripePrice(id: String): StripePrice? {
-        return stripe[id]
+    fun setStripe(p: Map<String, StripePrice>) {
+        stripe = p
     }
 
     fun clear() {
