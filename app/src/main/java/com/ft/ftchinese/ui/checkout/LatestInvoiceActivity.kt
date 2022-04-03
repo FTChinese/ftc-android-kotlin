78176a08c7b3b1ef9e3a627042778cb73c0d2e27
weiguo.ni <neefrankie@gmail.com>
2022-04-03 19:50:24 +0800

    refactor: change to compose ui

--- app/src/main/java/com/ft/ftchinese/ui/checkout/LatestInvoiceActivity.kt
+++ app/src/main/java/com/ft/ftchinese/ui/checkout/LatestInvoiceActivity.kt
@@ -3,127 +3,200 @@ package com.ft.ftchinese.ui.checkout
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.ft.ftchinese.R
-import com.ft.ftchinese.databinding.ActivityLatestInvoiceBinding
 import com.ft.ftchinese.model.enums.Cycle
 import com.ft.ftchinese.model.enums.OrderKind
+import com.ft.ftchinese.model.enums.PayMethod
+import com.ft.ftchinese.model.enums.Tier
+import com.ft.ftchinese.model.ftcsubs.Invoices
 import com.ft.ftchinese.model.invoice.Invoice
 import com.ft.ftchinese.store.InvoiceStore
-import com.ft.ftchinese.ui.base.ScopedAppActivity
+import com.ft.ftchinese.ui.components.PrimaryButton
+import com.ft.ftchinese.ui.components.Toolbar
 import com.ft.ftchinese.ui.formatter.FormatHelper
-import com.ft.ftchinese.ui.lists.TwoColItemViewHolder
-import org.jetbrains.anko.sdk27.coroutines.onClick
+import com.ft.ftchinese.ui.theme.Dimens
+import com.ft.ftchinese.ui.theme.OTheme
+import org.threeten.bp.ZonedDateTime
 
 /**
  * [LatestInvoiceActivity] shows the payment result of alipay of wxpay.
  */
-class LatestInvoiceActivity : ScopedAppActivity() {
-
-    private lateinit var binding: ActivityLatestInvoiceBinding
-    private lateinit var listAdapter: ListAdapter
+class LatestInvoiceActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(
-            this,
-            R.layout.activity_latest_invoice,
-        )
 
-        setSupportActionBar(binding.toolbar.toolbar)
-        supportActionBar?.apply {
-            setDisplayShowTitleEnabled(true)
-        }
-
-        binding.btnSubscriptionDone.onClick {
-            BuyerInfoActivity.start(this@LatestInvoiceActivity)
-            finish()
-        }
-
-        listAdapter = ListAdapter()
-
-        binding.rvLastInvoice.apply {
-            setHasFixedSize(true)
-            layoutManager = LinearLayoutManager(this@LatestInvoiceActivity)
-            adapter = listAdapter
-        }
-
-        InvoiceStore.getInstance(this).loadInvoices()?.let {
-            listAdapter.setData(purchasedInvoiceRows(it.purchased))
-
-            if (it.carriedOver != null) {
-                binding.carriedOver = "购买前会员剩余 ${it.carriedOver.totalDays} 天，将在当前会员到期后继续使用"
+        val invoices = InvoiceStore.getInstance(this).loadInvoices()
+        setContent {
+            OTheme {
+                Scaffold(
+                    topBar = {
+                        Toolbar(
+                            heading = stringResource(id = R.string.title_latest_invoice),
+                            onBack = { finish() }
+                        )
+                    },
+                    scaffoldState = rememberScaffoldState()
+                ) {
+                    invoices?.let {
+                        LatestInvoiceScreen(
+                            invoice = it,
+                            onClickNext =  {
+                                BuyerInfoActivity.start(this@LatestInvoiceActivity)
+                                finish()
+                            }
+                        )
+                    }
+                }
             }
         }
     }
 
-    private fun purchasedInvoiceRows(inv: Invoice): List<Pair<String, String>> {
-        return listOf(
-            Pair(
-                "订单号",
-                inv.orderId ?: "",
-            ),
-            Pair(
-                "订阅方案",
-                FormatHelper.getTier(this, inv.tier),
-            ),
-            Pair(
-                "支付金额",
-                FormatHelper.formatPrice(this, inv.currency, inv.paidAmount),
-            ),
-            Pair(
-                "支付方式",
-                inv.payMethod?.let {
-                    FormatHelper.getPayMethod(this, it)
-                } ?: "",
-            ),
-            Pair(
-                "订阅期限",
-                if (inv.orderKind == OrderKind.AddOn) {
-                    when {
-                        inv.years > 0 -> FormatHelper.getCycleN(this, Cycle.YEAR, inv.years)
-                        inv.months > 0 -> FormatHelper.getCycleN(this, Cycle.MONTH, inv.months)
-                        else -> FormatHelper.getCycleN(this, inv.cycle, 1)
-                    } + "(当前订阅过期后启用)"
-                } else {
-                    "${inv.startUtc?.toLocalDate()} 至 ${inv.endUtc?.toLocalDate()}"
-                }
-            )
-        )
-    }
-
-    inner class ListAdapter : RecyclerView.Adapter<TwoColItemViewHolder>() {
-        private var rows: List<Pair<String, String>> = listOf()
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TwoColItemViewHolder {
-            return TwoColItemViewHolder.create(parent)
-        }
-
-        override fun onBindViewHolder(holder: TwoColItemViewHolder, position: Int) {
-            val pair = rows[position]
-            holder.setLeadingText(pair.first)
-            holder.setTrailingText(pair.second)
-        }
-
-        override fun getItemCount() = rows.size
-
-        fun setData(pairs: List<Pair<String, String>>) {
-            Log.i(TAG, "Recycle view data $pairs")
-            this.rows = pairs
-            notifyDataSetChanged()
-        }
-    }
-
     companion object {
-        private const val TAG = "LatestInvoiceActivity"
-
         @JvmStatic
         fun start(context: Context) {
             context.startActivity(Intent(context, LatestInvoiceActivity::class.java))
         }
     }
+}
+
+
+
+@Composable
+private fun LatestInvoiceScreen(
+    invoice: Invoices,
+    onClickNext: () -> Unit,
+) {
+    val context = LocalContext.current
+
+    Column {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(Dimens.dp8)
+        ) {
+            InvoiceTable(data = invoiceRow(context, invoice.purchased))
+
+            Spacer(modifier = Modifier.height(Dimens.dp16))
+
+            invoice.carriedOver?.let {
+                Text(
+                    text = "购买前会员剩余 ${it.totalDays} 天，将在当前会员到期后继续使用",
+                    modifier = Modifier.padding(Dimens.dp8)
+                )
+            }
+        }
+
+        PrimaryButton(
+            onClick = onClickNext,
+            modifier = Modifier
+                .padding(Dimens.dp16)
+                .fillMaxWidth()
+        ) {
+            Text(text = "下一步，确认或完善个人信息")
+        }
+    }
+}
+
+@Composable
+private fun InvoiceTable(
+    data: List<Pair<String, String>>
+) {
+    Column {
+        data.forEach { pair ->
+            Row(
+                modifier = Modifier.padding(Dimens.dp8)
+            ) {
+                Text(
+                    text = pair.first,
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding()
+                )
+                Text(
+                    text = pair.second,
+                    modifier = Modifier.weight(2f)
+                )
+            }
+        }
+    }
+}
+
+private fun invoiceRow(ctx: Context, inv: Invoice): List<Pair<String, String>> {
+    return listOf(
+        Pair(
+            "订单号",
+            inv.orderId ?: "",
+        ),
+        Pair(
+            "订阅方案",
+            FormatHelper.getTier(ctx, inv.tier),
+        ),
+        Pair(
+            "支付金额",
+            FormatHelper.currencySymbol(inv.currency) + FormatHelper.formatMoney(ctx, inv.paidAmount),
+        ),
+        Pair(
+            "支付方式",
+            inv.payMethod?.let {
+                FormatHelper.getPayMethod(ctx, it)
+            } ?: "",
+        ),
+        Pair(
+            "订阅期限",
+            if (inv.orderKind == OrderKind.AddOn) {
+                when {
+                    inv.years > 0 -> FormatHelper.getCycleN(ctx, Cycle.YEAR, inv.years)
+                    inv.months > 0 -> FormatHelper.getCycleN(ctx, Cycle.MONTH, inv.months)
+                    else -> FormatHelper.getCycleN(ctx, inv.period.toCycle(), 1)
+                } + "(当前订阅过期后启用)"
+            } else {
+                "${inv.startUtc?.toLocalDate()} 至 ${inv.endUtc?.toLocalDate()}"
+            }
+        )
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewLatestInvoiceScreen() {
+    LatestInvoiceScreen(
+        invoice = Invoices(
+            purchased = Invoice(
+                id = "FT1234567890",
+                compoundId = "",
+                tier = Tier.PREMIUM,
+                cycle = Cycle.YEAR,
+                years = 1,
+                months = 0,
+                days = 0,
+                paidAmount = 1998.0,
+                payMethod = PayMethod.ALIPAY,
+                startUtc = ZonedDateTime.now(),
+                endUtc = ZonedDateTime.now().plusYears(1)
+            ),
+            carriedOver = Invoice(
+                id = "",
+                compoundId = "",
+                tier = Tier.STANDARD,
+                cycle = Cycle.YEAR,
+                years = 0,
+                months = 0,
+                days = 99,
+                paidAmount = 0.0,
+            )
+        ),
+        onClickNext = {}
+    )
 }
