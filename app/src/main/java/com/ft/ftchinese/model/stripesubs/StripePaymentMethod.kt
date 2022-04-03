5c460da35e7ed958274a8b403873f5cc5b70a395
weiguo.ni <neefrankie@gmail.com>
2022-04-03 11:49:41 +0800

    feat: StripePaymentMethod.newInstance

--- app/src/main/java/com/ft/ftchinese/model/stripesubs/StripePaymentMethod.kt
+++ app/src/main/java/com/ft/ftchinese/model/stripesubs/StripePaymentMethod.kt
@@ -1,10 +1,29 @@
 package com.ft.ftchinese.model.stripesubs
 
+import com.stripe.android.model.PaymentMethod
+
 data class StripePaymentMethod(
     val id: String,
     val customerId: String,
     val card: StripePaymentCard,
-)
+) {
+    companion object {
+        @JvmStatic
+        fun newInstance(pm: PaymentMethod): StripePaymentMethod {
+            return StripePaymentMethod(
+                id = pm.id ?: "",
+                customerId = pm.customerId ?: "",
+                card = StripePaymentCard(
+                    brand = pm.card?.brand?.displayName ?: "",
+                    country = pm.card?.country ?: "",
+                    expMonth = pm.card?.expiryMonth ?: 0,
+                    expYear = pm.card?.expiryYear ?: 0,
+                    last4 = pm.card?.last4 ?: "",
+                )
+            )
+        }
+    }
+}
 
 data class StripePaymentCard(
     val brand: String,
