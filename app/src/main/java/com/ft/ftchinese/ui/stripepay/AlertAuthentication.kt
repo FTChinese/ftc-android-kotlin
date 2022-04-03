d49fa6f6b5a2b8bbd7fb765993a0c5199ba2876e
weiguo.ni <neefrankie@gmail.com>
2022-04-03 12:10:08 +0800

    feat: AlertAuthentication

---
+++ app/src/main/java/com/ft/ftchinese/ui/stripepay/AlertAuthentication.kt
@@ -0,0 +1,38 @@
+package com.ft.ftchinese.ui.stripepay
+
+import androidx.compose.material.AlertDialog
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
+import com.ft.ftchinese.R
+
+@Composable
+fun AlertAuthentication(
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit,
+) {
+    AlertDialog(
+        onDismissRequest = {},
+        title = {
+            Text(text = stringResource(id = R.string.title_requires_action))
+        },
+        text = {
+            Text(text = stringResource(id = R.string.stripe_requires_action))
+        },
+        confirmButton = {
+            TextButton(
+                onClick = onConfirm
+            ) {
+                Text(text = stringResource(id = R.string.action_ok))
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = onDismiss
+            ) {
+                Text(text = stringResource(id = R.string.action_cancel))
+            }
+        }
+    )
+}
