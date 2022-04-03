b1b65419577def4fa7c0e56708b65ce3685a9361
weiguo.ni <neefrankie@gmail.com>
2022-04-03 12:09:41 +0800

    refactor:

--- app/src/main/java/com/ft/ftchinese/ui/stripepay/StripePayViewModel.kt
+++ app/src/main/java/com/ft/ftchinese/ui/stripepay/StripePayViewModel.kt
@@ -1,17 +1,96 @@
 package com.ft.ftchinese.ui.stripepay
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import com.ft.ftchinese.model.fetch.FetchUi
+import androidx.lifecycle.viewModelScope
+import com.ft.ftchinese.R
+import com.ft.ftchinese.model.fetch.APIError
 import com.ft.ftchinese.model.paywall.CartItemStripeV2
 import com.ft.ftchinese.model.reader.Account
+import com.ft.ftchinese.model.stripesubs.PaymentSheetParams
+import com.ft.ftchinese.repository.StripeClient
+import com.ft.ftchinese.ui.base.BaseViewModel
+import com.stripe.android.ApiResultCallback
+import com.stripe.android.Stripe
+import com.stripe.android.model.SetupIntent
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 private const val TAG = "StripePayViewModel"
 
-class StripePayViewModel : ViewModel() {
+class StripePayViewModel : BaseViewModel() {
 
-    private var _progress = MutableLiveData<FetchUi>(FetchUi.Progress(false))
-    val progressLiveData = _progress
+    val inProgress: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
+    }
+
+    val status: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    val stringRes: MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>()
+    }
+
+    val setupLiveData: MutableLiveData<PaymentSheetParams> by lazy {
+        MutableLiveData<PaymentSheetParams>()
+    }
+
+    fun createSetupIntent(account: Account) {
+        Log.i(TAG, "Creating setup intent")
+
+        val customerId = account.stripeId ?: return
+
+        inProgress.value = true
+        viewModelScope.launch {
+
+            try {
+                val resp = withContext(Dispatchers.IO) {
+                    StripeClient.setupWithEphemeral(customerId = customerId)
+                }
+                Log.i(TAG, "Http request result ${resp.body}")
+                if (resp.body == null) {
+                    stringRes.value = R.string.error_unknown
+                    return@launch
+                }
+
+                inProgress.value = false
+                setupLiveData.value = resp.body
+            } catch (e: APIError) {
+                inProgress.value = false
+                status.value = e.message
+            } catch (e: Exception) {
+                inProgress.value = false
+                status.value = e.message
+            }
+        }
+    }
+
+    private val onSetupIntent = object : ApiResultCallback<SetupIntent> {
+        override fun onError(e: Exception) {
+            inProgress.value = false
+            // TODO: alert dialog.
+            Log.i(TAG, e.message ?: "")
+        }
+
+        override fun onSuccess(result: SetupIntent) {
+            inProgress.value = false
+            Log.i(TAG, "${result.paymentMethod}")
+            Log.i(TAG, "${result.paymentMethodId}")
+        }
+    }
+
+    fun retrieveSetupIntent(stripe: Stripe) {
+        Log.i(TAG, "Retrieving setup intent")
+
+        inProgress.value = true
+
+        setupLiveData.value?.let {
+            stripe.retrieveSetupIntent(it.clientSecret, null, onSetupIntent)
+        }
+
+    }
 
     fun subscribe(account: Account, item: CartItemStripeV2) {
 
