acf48dc02d20d2e6c5c952bb0ee68ba610c4b54f
weiguo.ni <neefrankie@gmail.com>
2022-04-04 18:15:42 +0800

    refactor: change to compose ui

--- app/src/main/java/com/ft/ftchinese/ui/settings/FCMActivity.kt
+++ app/src/main/java/com/ft/ftchinese/ui/settings/FCMActivity.kt
@@ -6,99 +6,79 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.view.ViewGroup
+import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.ft.ftchinese.BuildConfig
 import com.ft.ftchinese.R
-import com.ft.ftchinese.databinding.ActivityFcmBinding
-import com.ft.ftchinese.ui.lists.SingleLineItemViewHolder
+import com.ft.ftchinese.ui.components.*
+import com.ft.ftchinese.ui.theme.Dimens
+import com.ft.ftchinese.ui.theme.OColor
+import com.ft.ftchinese.ui.theme.OTheme
 import com.google.android.gms.common.ConnectionResult
 import com.google.android.gms.common.GoogleApiAvailability
 import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
 
 class FCMActivity : AppCompatActivity() {
 
     private var errorDialog: Dialog? = null
-    private var listAdapter: ListAdapter? = null
-    private lateinit var binding: ActivityFcmBinding
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_fcm)
-        setSupportActionBar(binding.toolbar.toolbar)
 
-        supportActionBar?.apply {
-            setDisplayShowTitleEnabled(true)
-            setDisplayHomeAsUpEnabled(true)
-        }
+        setContent {
+            OTheme {
 
-        listAdapter = ListAdapter(listOf())
+                val state = remember {
+                    FcmState()
+                }
 
-        binding.rvFcmProgress.apply {
-            layoutManager = LinearLayoutManager(this@FCMActivity)
-            adapter = listAdapter
-        }
+                Scaffold(
+                    topBar = {
+                        Toolbar(
+                            heading = stringResource(id = R.string.fcm_setting_title),
+                            onBack = { finish() }
+                        )
+                    }
+                ) {
+                    FcmScreen(
+                        state = state,
+                        onCheckPlayService = {
+                            state.inProgress = true
+                            state.clear()
 
-        // See https://developer.android.com/training/notify-user/channels#UpdateChannel
-        // Open settings for a specific channel
-        binding.tvNotificationSetting.setOnClickListener {
-            val intent = Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS).apply {
-                putExtra(Settings.EXTRA_APP_PACKAGE, BuildConfig.APPLICATION_ID)
-                putExtra(Settings.EXTRA_CHANNEL_ID, getString(R.string.news_notification_channel_id))
+                            val available = checkPlayServices()
+                            state.playAvailable = available
+
+                            retrieveRegistrationToken {
+                                state.inProgress = false
+                                state.tokenRetrievable = it.isSuccessful
+
+                                // val token = it.result?.token
+                            }
+                        }
+                    )
+                }
             }
-
-            startActivity(intent)
-        }
-
-        binding.btnCheckFcm.setOnClickListener {
-            binding.inProgress = true
-
-            // Clean data every time user clicked the check button.
-            listAdapter?.clear()
-
-            // Check if Google Play Service is avaiable on device.
-            val item = buildPlayServiceText(checkPlayServices())
-
-            // Show the checking result.
-            listAdapter?.add(item)
-
-            retrieveRegistrationToken()
         }
     }
 
-    private fun buildPlayServiceText(available: Boolean): SingleLineItem {
-        return if (available) {
-            SingleLineItem(
-                    text = getString(R.string.play_service_available),
-                    icon = R.drawable.ic_done_claret_24dp
-            )
-        } else {
-            SingleLineItem(
-                    text = getString(R.string.play_service_not_available),
-                    icon = R.drawable.ic_error_outline_claret_24dp
-            )
-        }
-    }
 
-    private fun buildTokenText(retrieved: Boolean): SingleLineItem {
-        return if (retrieved) {
-            SingleLineItem(
-                    text = getString(R.string.fcm_accessible),
-                    icon = R.drawable.ic_done_claret_24dp
-            )
-        } else {
-            SingleLineItem(
-                    text = getString(R.string.fcm_inaccessible),
-                    icon = R.drawable.ic_error_outline_claret_24dp
-            )
-        }
-    }
 
     private fun checkPlayServices(): Boolean {
         val googleApiAvailability = GoogleApiAvailability.getInstance()
@@ -120,24 +100,11 @@ class FCMActivity : AppCompatActivity() {
         return resultCode == ConnectionResult.SUCCESS
     }
 
-    private fun retrieveRegistrationToken() {
-        FirebaseInstanceId
-                .getInstance()
-                .instanceId
-                .addOnCompleteListener(OnCompleteListener {
-                    if (!it.isSuccessful) {
-
-                        listAdapter?.add(buildTokenText(false))
-
-                        binding.inProgress = false
-                        return@OnCompleteListener
-                    }
-
-                    val token = it.result?.token
-
-                    listAdapter?.add(buildTokenText(true))
-                    binding.inProgress = false
-                })
+    private fun retrieveRegistrationToken(listener: OnCompleteListener<String>) {
+        FirebaseMessaging
+            .getInstance()
+            .token
+            .addOnCompleteListener(listener)
     }
 
     companion object {
@@ -146,31 +113,162 @@ class FCMActivity : AppCompatActivity() {
             context?.startActivity(Intent(context, FCMActivity::class.java))
         }
     }
+}
 
-    inner class ListAdapter(rows: List<SingleLineItem>) : RecyclerView.Adapter<SingleLineItemViewHolder>() {
+private fun launchNotificationSetting(context: Context) {
+    val intent = Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS).apply {
+        putExtra(Settings.EXTRA_APP_PACKAGE, BuildConfig.APPLICATION_ID)
+        putExtra(Settings.EXTRA_CHANNEL_ID, context.getString(R.string.news_notification_channel_id))
+    }
 
-        private var items: MutableList<SingleLineItem> = rows.toMutableList()
+    context.startActivity(intent)
+}
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SingleLineItemViewHolder {
-            return SingleLineItemViewHolder.create(parent)
-        }
+class FcmState {
+    var inProgress by mutableStateOf(false)
 
-        override fun onBindViewHolder(holder: SingleLineItemViewHolder, position: Int) {
-            val item = items[position]
-            holder.setLeadingIcon(item.icon)
-            holder.setTrailingIcon(null)
-            holder.text.text = item.text
-        }
+    var playAvailable by mutableStateOf<Boolean?>(null)
 
-        override fun getItemCount() = items.size
+    var tokenRetrievable by mutableStateOf<Boolean?>(null)
 
-        fun add(item: SingleLineItem) {
-            items.add(item)
-            notifyDataSetChanged()
-        }
+    fun clear() {
+        playAvailable = null
+        tokenRetrievable = null
+    }
+}
 
-        fun clear() {
-            items.clear()
+@Composable
+private fun FcmScreen(
+    state: FcmState,
+    onCheckPlayService: () -> Unit
+) {
+    val context = LocalContext.current
+
+    FcmBody(
+        loading = state.inProgress,
+        isPlayAvailable = state.playAvailable,
+        isTokenRetrievable = state.tokenRetrievable,
+        onSetting = {
+            launchNotificationSetting(context)
+        },
+        onCheck = onCheckPlayService,
+    )
+}
+
+@Composable
+private fun FcmBody(
+    loading: Boolean,
+    isPlayAvailable: Boolean? = null,
+    isTokenRetrievable: Boolean? = null,
+    onSetting: () -> Unit,
+    onCheck: () -> Unit,
+) {
+
+    ProgressLayout(
+        loading = loading
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .verticalScroll(rememberScrollState())
+            ) {
+                Text(
+                    text = stringResource(id = R.string.fcm_check_guide),
+                    modifier = Modifier.padding(Dimens.dp16),
+                    style = MaterialTheme.typography.body2,
+                    color = OColor.black60,
+                )
+
+                ClickableRow(
+                    onClick = onSetting,
+                    modifier = Modifier
+                        .background(OColor.black5)
+                        .padding(Dimens.dp16)
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.channel_setting_news),
+                        style = MaterialTheme.typography.h6,
+                        modifier = Modifier.weight(1f)
+                    )
+                }
+
+                Column(
+                    modifier = Modifier.padding(Dimens.dp16)
+                ) {
+                    isPlayAvailable?.let {
+                        PlayServiceStatus(available = it)
+                    }
+
+                    Spacer(modifier = Modifier.height(Dimens.dp16))
+
+                    isTokenRetrievable?.let {
+                        TokenStatus(retrievable = it)
+                    }
+                }
+            }
+
+            PrimaryButton(
+                onClick = onCheck,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(Dimens.dp16),
+                enabled = !loading,
+            ) {
+                Text(text = stringResource(id = R.string.fcm_start_checking))
+            }
         }
     }
+}
+
+@Composable
+fun PlayServiceStatus(
+    available: Boolean
+) {
+    if (available) {
+        IconTextItem(
+            icon = painterResource(id = R.drawable.ic_done_claret_24dp),
+            text = stringResource(id = R.string.play_service_available),
+            iconTint = OColor.claret
+        )
+    } else {
+        IconTextItem(
+            icon = painterResource(id = R.drawable.ic_error_outline_claret_24dp),
+            text = stringResource(id = R.string.play_service_not_available),
+            iconTint = OColor.claret
+        )
+    }
+}
+
+@Composable
+private fun TokenStatus(
+    retrievable: Boolean,
+) {
+    if (retrievable) {
+        IconTextItem(
+            icon = painterResource(id = R.drawable.ic_done_claret_24dp),
+            text = stringResource(R.string.fcm_accessible),
+            iconTint = OColor.claret,
+        )
+    } else {
+        IconTextItem(
+            icon = painterResource(id = R.drawable.ic_error_outline_claret_24dp),
+            text = stringResource(R.string.fcm_inaccessible),
+            iconTint = OColor.claret
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewFcmBody() {
+    FcmBody(
+        loading = true,
+        isPlayAvailable = true,
+        isTokenRetrievable = true,
+        onSetting = {},
+        onCheck = {}
+    )
 }
--- app/src/main/java/com/ft/ftchinese/ui/settings/SingleLineItem.kt
+++
@@ -1,6 +0,0 @@
-package com.ft.ftchinese.ui.settings
-
-data class SingleLineItem(
-    val icon: Int? = null,
-    val text: String,
-)
