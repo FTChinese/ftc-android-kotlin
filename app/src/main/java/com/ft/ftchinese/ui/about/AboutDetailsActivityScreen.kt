eb1a3163dc746f02cecf1ac7cda19d177cab1ce2
neefrankie <neefrankie@gmail.com>
2022-04-24 06:26:36 +0800

    refactor: web view

--- app/src/main/java/com/ft/ftchinese/ui/about/AboutDetailsActivityScreen.kt
+++ app/src/main/java/com/ft/ftchinese/ui/about/AboutDetailsActivityScreen.kt
@@ -1,23 +1,35 @@
 package com.ft.ftchinese.ui.about
 
+import android.webkit.WebViewClient
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import com.ft.ftchinese.ui.components.WebClientViewModel
-import com.ft.ftchinese.ui.components.WebPage
-import com.ft.ftchinese.ui.webpage.WVClient
+import androidx.compose.ui.Modifier
+import com.ft.ftchinese.ui.components.ProgressLayout
+import com.ft.ftchinese.ui.webpage.ChromeClient
+import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewState
 
 @Composable
 fun AboutDetailsActivityScreen(
-    url: String,
-    webViewModel: WebClientViewModel
+    url: String
 ) {
     val wvState = rememberWebViewState(url = url)
-    val wvClient = WVClient()
 
-    WebPage(
-        loading = false,
-        state = wvState,
-        wvClient = wvClient,
-        jsInterface = webViewModel,
-    )
+    ProgressLayout(
+        loading = false
+    ) {
+        WebView(
+            state = wvState,
+            modifier = Modifier.fillMaxSize(),
+            captureBackPresses = false,
+            onCreated = {
+                it.settings.javaScriptEnabled = true
+                it.settings.loadsImagesAutomatically = true
+                it.settings.domStorageEnabled = true
+                it.settings.databaseEnabled = true
+                it.webChromeClient = ChromeClient()
+                it.webViewClient = WebViewClient()
+            }
+        )
+    }
 }
