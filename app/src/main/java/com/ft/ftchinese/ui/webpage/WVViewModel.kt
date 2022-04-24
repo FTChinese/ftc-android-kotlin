61dca9e3f51abd1de5c858fc16e2647fa0255dfc
neefrankie <neefrankie@gmail.com>
2022-04-24 11:02:06 +0800

    refactor: add Deprecated

--- app/src/main/java/com/ft/ftchinese/ui/webpage/WVViewModel.kt
+++ app/src/main/java/com/ft/ftchinese/ui/webpage/WVViewModel.kt
@@ -9,19 +9,23 @@ class WVViewModel : ViewModel() {
     // When a url in webview is clicked and the url point to
     // another channel page.
     // Use inside channel fragment
+    @Deprecated("")
     val urlChannelSelected: MutableLiveData<ChannelSource> by lazy {
         MutableLiveData<ChannelSource>()
     }
 
+    @Deprecated("")
     val pagingBtnClicked: MutableLiveData<Paging> by lazy {
         MutableLiveData<Paging>()
     }
 
     // Used inside article activity.
+    @Deprecated("")
     val pageFinished: MutableLiveData<Boolean> by lazy {
         MutableLiveData<Boolean>()
     }
 
+    @Deprecated("")
     val openGraphEvaluated: MutableLiveData<String> by lazy {
         MutableLiveData<String>()
     }
