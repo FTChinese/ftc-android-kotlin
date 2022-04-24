93c8fb1017bf55e526d6fef2a09012fde1c97c04
neefrankie <neefrankie@gmail.com>
2022-04-24 18:37:12 +0800

    refactor: use ClickableRow

--- app/src/main/java/com/ft/ftchinese/ui/about/AbountActivityScreen.kt
+++ app/src/main/java/com/ft/ftchinese/ui/about/AbountActivityScreen.kt
@@ -1,18 +1,13 @@
 package com.ft.ftchinese.ui.about
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Divider
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.ft.ftchinese.R
 import com.ft.ftchinese.model.legal.WebpageMeta
 import com.ft.ftchinese.model.legal.legalPages
+import com.ft.ftchinese.ui.components.ClickableRow
 import com.ft.ftchinese.ui.theme.Dimens
 
 @Composable
@@ -21,21 +16,12 @@ fun AboutActivityScreen(
 ) {
     Column {
         legalPages.forEach { pageMeta ->
-            Row(
-                modifier = Modifier
-                    .clickable {
-                        onNavigate(pageMeta)
-                    }
-                    .fillMaxWidth()
-                    .padding(Dimens.dp16),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
+            ClickableRow(
+                onClick = {
+                  onNavigate(pageMeta)
+                },
             ) {
                 Text(text = pageMeta.title)
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_keyboard_arrow_right_gray_24dp),
-                    contentDescription = "Open"
-                )
             }
 
             Divider(startIndent = Dimens.dp16)
