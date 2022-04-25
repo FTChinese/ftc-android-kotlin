34ac9427f1fa092f299d4a56ee00a56937698064
weiguo.ni <neefrankie@gmail.com>
2022-04-25 11:45:27 +0800

    refactor: removed PriceList

--- app/src/main/java/com/ft/ftchinese/ui/product/ProductCard.kt
+++ app/src/main/java/com/ft/ftchinese/ui/product/ProductCard.kt
@@ -8,6 +8,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -15,6 +16,9 @@ import com.ft.ftchinese.R
 import com.ft.ftchinese.model.enums.Cycle
 import com.ft.ftchinese.model.enums.PayMethod
 import com.ft.ftchinese.model.enums.Tier
+import com.ft.ftchinese.model.paywall.CartItemFtc
+import com.ft.ftchinese.model.paywall.CartItemStripe
+import com.ft.ftchinese.model.paywall.ProductItem
 import com.ft.ftchinese.model.paywall.defaultPaywall
 import com.ft.ftchinese.model.reader.Membership
 import com.ft.ftchinese.ui.theme.Dimens
@@ -24,10 +28,9 @@ import org.threeten.bp.LocalDate
 @OptIn(ExperimentalFoundationApi::class, androidx.compose.material.ExperimentalMaterialApi::class)
 @Composable
 fun ProductCard(
-    heading: String,
-    description: String,
-    smallPrint: String?,
-    priceContent: @Composable () -> Unit,
+    item: ProductItem,
+    onFtcPay: (item: CartItemFtc) -> Unit,
+    onStripePay: (item: CartItemStripe) -> Unit,
 ) {
 
     Card {
@@ -35,9 +38,35 @@ fun ProductCard(
             modifier = Modifier
                 .padding(Dimens.dp16),
         ) {
-            ProductHeading(text = heading)
+            ProductHeading(text = item.content.heading)
 
-            priceContent()
+            item.ftcItems.forEach { cartItem ->
+                Card(
+                    onClick = { onFtcPay(cartItem) },
+                    backgroundColor = OColor.wheat,
+                    elevation = Dimens.dp4
+                ) {
+                    PriceCard(
+                        params = PriceCardParams.ofFtc(LocalContext.current, cartItem),
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(Dimens.dp16))
+            }
+
+            item.stripeItems.forEach { cartItem ->
+                Card(
+                    onClick = { onStripePay(cartItem) },
+                    backgroundColor = OColor.wheat,
+                    elevation = Dimens.dp4,
+                ) {
+                    PriceCard(
+                        params = PriceCardParams.ofStripe(LocalContext.current, cartItem),
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(Dimens.dp16))
+            }
 
             Text(
                 text = stringResource(id = R.string.stripe_requirement),
@@ -47,7 +76,7 @@ fun ProductCard(
 
             Spacer(modifier = Modifier.height(Dimens.dp16))
 
-            description
+            item.content.description
                 .split("\n")
                 .forEach { line ->
                     ProductDescRow(text = line)
@@ -55,7 +84,7 @@ fun ProductCard(
 
             Spacer(modifier = Modifier.height(Dimens.dp8))
 
-            smallPrint?.let {
+            item.content.smallPrint?.let {
                 Text(
                     text = it,
                     style = MaterialTheme.typography.body2,
@@ -89,25 +118,15 @@ private fun ProductDescRow(text: String) {
 @Composable
 fun PreviewProductCard() {
     val product = defaultPaywall.products[0]
-    val ftcItems = product
-        .listShoppingItems(Membership(
-            tier = Tier.STANDARD,
-            cycle = Cycle.YEAR,
-            expireDate = LocalDate.now().plusMonths(6),
-            payMethod = PayMethod.ALIPAY,
-        ))
+    val uiItem = product.buildUiItem(Membership(
+        tier = Tier.STANDARD,
+        cycle = Cycle.YEAR,
+        expireDate = LocalDate.now().plusMonths(6),
+        payMethod = PayMethod.ALIPAY,
+    ), mapOf())
 
     ProductCard(
-        heading = product.heading,
-        description = product.description,
-        smallPrint = product.smallPrint,
-        priceContent = {
-            PriceList(
-                ftcCartItems = ftcItems,
-                stripeCartItems = listOf(),
-                onFtcPay = { },
-                onStripePay = { }
-            )
-        }
-    )
+        item = uiItem,
+        onFtcPay = {}
+    ) {}
 }
