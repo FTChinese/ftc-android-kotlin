188acd71256f2bd386ff873a9faefcde231f1594
weiguo.ni <neefrankie@gmail.com>
2022-04-25 11:47:03 +0800

    refactor: buildCartItems signature

--- app/src/main/java/com/ft/ftchinese/model/paywall/StripePriceIDs.kt
+++ app/src/main/java/com/ft/ftchinese/model/paywall/StripePriceIDs.kt
@@ -1,7 +1,6 @@
 package com.ft.ftchinese.model.paywall
 
 import com.ft.ftchinese.model.reader.Membership
-import com.ft.ftchinese.model.stripesubs.StripePrice
 
 /**
  * Stripe price ids attached to a specific ftc product.
@@ -10,46 +9,32 @@ data class StripePriceIDs(
     val recurring: List<String>,
     val trial: String?,
 ) {
-    fun listShoppingItems(prices: Map<String, StripePrice>, m: Membership): List<CartItemStripe> {
+    fun buildCartItems(
+        prices: Map<String, StripePaywallItem>,
+        m: Membership
+    ): List<CartItemStripe> {
         if (prices.isEmpty()) {
             return listOf()
         }
 
-        val trialPrice = trial?.let { prices[it] }
+        val trialItem = trial?.let { prices[it] }
 
         val items = mutableListOf<CartItemStripe>()
 
         recurring.forEach { id ->
-            val price = prices[id]
-            if (price != null) {
-                items.add(CartItemStripe(
-                    intent = price.checkoutIntent(m),
-                    recurring = price,
-                    trial = trialPrice
-                ))
+            val pwItem = prices[id]
+            if (pwItem != null) {
+                items.add(
+                    CartItemStripe(
+                        intent = pwItem.price.checkoutIntent(m),
+                        recurring = pwItem.price,
+                        trial = trialItem?.price,
+                        coupon = pwItem.getCoupon()
+                    )
+                )
             }
         }
 
         return items
-    }
-
-    companion object {
-        fun newInstance(ftcItems: List<CartItemFtc>): StripePriceIDs {
-            var trial: String? = null
-            val recur = mutableListOf<String>()
-
-            ftcItems.forEach {
-                if (it.isIntro) {
-                    trial = it.price.stripePriceId
-                } else {
-                    recur.add(it.price.stripePriceId)
-                }
-            }
-
-            return StripePriceIDs(
-                recurring = recur,
-                trial = trial,
-            )
-        }
     }
 }
