fe17ebfe6b7d94e93e9e0497db7e87dde018b8a0
weiguo.ni <neefrankie@gmail.com>
2022-04-26 17:47:03 +0800

    refactor: removed buildArticleSourceUrl

--- app/src/main/java/com/ft/ftchinese/repository/Config.kt
+++ app/src/main/java/com/ft/ftchinese/repository/Config.kt
@@ -2,14 +2,11 @@ package com.ft.ftchinese.repository
 
 import android.net.Uri
 import com.ft.ftchinese.BuildConfig
-import com.ft.ftchinese.model.enums.ArticleType
 import com.ft.ftchinese.model.content.ChannelSource
 import com.ft.ftchinese.model.content.HTML_TYPE_FRAGMENT
-import com.ft.ftchinese.model.content.Teaser
 import com.ft.ftchinese.model.enums.PurchaseAction
 import com.ft.ftchinese.model.enums.Tier
 import com.ft.ftchinese.model.reader.Account
-import java.util.*
 
 object Config {
 
@@ -46,7 +43,7 @@ object Config {
         }
     }
 
-    private fun appendUtm(builder: Uri.Builder): Uri.Builder {
+    fun appendUtm(builder: Uri.Builder): Uri.Builder {
         return builder
             .appendQueryParameter("utm_source", "marketing")
             .appendQueryParameter("utm_medium", "androidmarket")
@@ -71,59 +68,6 @@ object Config {
         } catch (e: Exception) {
             null
         }
-    }
-
-    // How to retrieve an article based on teaser.
-    // ArticleType.Column is ignored here since it is opened in a ChannelFragment.
-    fun buildArticleSourceUrl(account: Account?, teaser: Teaser): Uri? {
-        if (teaser.id.isBlank()) {
-            return null
-        }
-
-        val builder = Uri.parse(discoverServer(account))
-            .buildUpon()
-
-        // Otherwise use webpage.
-        builder
-            .appendPath(teaser.type.toString())
-            .appendPath(teaser.id)
-
-
-        teaser.langVariant?.aiAudioPathSuffix()?.let {
-            builder.appendPath(it)
-        }
-
-        builder.appendQueryParameter("webview", "ftcapp")
-
-        when(teaser.type) {
-            ArticleType.Interactive -> {
-
-                builder
-                    .appendQueryParameter("001", "")
-                    .appendQueryParameter("exclusive", "")
-
-                when (teaser.subType) {
-                    Teaser.SUB_TYPE_RADIO,
-                    Teaser.SUB_TYPE_SPEED_READING,
-                    Teaser.SUB_TYPE_MBAGYM -> {}
-
-                    else -> builder
-                        .appendQueryParameter("hideheader", "yes")
-                        .appendQueryParameter("ad", "no")
-                        .appendQueryParameter("inNavigation", "yes")
-                        .appendQueryParameter("for", "audio")
-                        .appendQueryParameter("enableScript", "yes")
-                        .appendQueryParameter("timestamp", "${Date().time}")
-                }
-            }
-
-            ArticleType.Video -> builder
-                .appendQueryParameter("004", "")
-
-            else -> {}
-        }
-
-        return appendUtm(builder).build()
     }
 
     // membership: premium|standard|standardmonthly
