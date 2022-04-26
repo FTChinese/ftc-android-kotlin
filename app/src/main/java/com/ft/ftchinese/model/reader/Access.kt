6cfc8d66c6a1e193a324a0bf168f010a27255321
weiguo.ni <neefrankie@gmail.com>
2022-04-26 17:42:40 +0800

    refactor: add lang field

--- app/src/main/java/com/ft/ftchinese/model/reader/Access.kt
+++ app/src/main/java/com/ft/ftchinese/model/reader/Access.kt
@@ -1,13 +1,16 @@
 package com.ft.ftchinese.model.reader
 
 import android.os.Parcelable
+import com.ft.ftchinese.model.content.Language
 import kotlinx.parcelize.Parcelize
 
+// Configuration of permission denied fragment.
 @Parcelize
 data class Access(
     val status: MemberStatus,
     val rights: Int, // User owned permissions. Sum of multiple binary.
     val content: Permission, // Content allowed permission.
+    val lang: Language,
 ) : Parcelable {
     // Check if content's permission is contained by user rights.
     val granted: Boolean
@@ -16,16 +19,27 @@ data class Access(
     val loggedIn: Boolean
         get() = status != MemberStatus.NotLoggedIn
 
+    val cancellable: Boolean
+        get() = lang != Language.CHINESE
+
+    val isBilingual: Boolean
+        get() = lang != Language.CHINESE
+
     companion object {
         // Deduce user's access permissions against
         // content's permission.
         @JvmStatic
-        fun of(contentPerm: Permission, who: Account?): Access {
+        fun of(
+            contentPerm: Permission,
+            who: Account?,
+            lang: Language = Language.CHINESE
+        ): Access {
             if (who == null) {
                 return Access(
                     status = MemberStatus.NotLoggedIn,
                     rights = Permission.FREE.id,
                     content = contentPerm,
+                    lang = lang,
                 )
             }
 
@@ -36,11 +50,12 @@ data class Access(
                 status = status,
                 rights = rights,
                 content = contentPerm,
+                lang = lang
             )
         }
 
-        fun ofEnglishArticle(who: Account?): Access {
-            return of(Permission.STANDARD, who)
+        fun ofEnglishArticle(who: Account?, lang: Language): Access {
+            return of(Permission.STANDARD, who, lang = lang)
         }
     }
 }
