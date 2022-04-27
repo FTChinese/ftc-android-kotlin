f46ae8a2f117aba837b6e8da9fc695a1e7eba1fd
weiguo.ni <neefrankie@gmail.com>
2022-04-27 14:12:06 +0800

    feat: handle clicking options

--- app/src/main/java/com/ft/ftchinese/ui/member/MemberActvityScreen.kt
+++ app/src/main/java/com/ft/ftchinese/ui/member/MemberActvityScreen.kt
@@ -1,27 +1,67 @@
 package com.ft.ftchinese.ui.member
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.ft.ftchinese.ui.SubsActivity
 import com.google.accompanist.swiperefresh.SwipeRefresh
 import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
 
 @Composable
 fun MemberActivityScreen(
     memberViewModel: MembershipViewModel,
-    showSnackBar: (String) -> Unit
+    showSnackBar: (String) -> Unit,
+    modifier: Modifier = Modifier
 ) {
+
+    val context = LocalContext.current
 
     val isRefreshing by memberViewModel.refreshingLiveData.observeAsState(false)
     val progress by memberViewModel.progressLiveData.observeAsState(false)
     val account = remember {
         memberViewModel.account
     }
+    val (showDialog, setShowDialog) = remember {
+        mutableStateOf(false)
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.StartActivityForResult(),
+    ) { result ->
+        when (result.resultCode) {
+            Activity.RESULT_OK -> {
+                memberViewModel.reloadAccount()
+            }
+            Activity.RESULT_CANCELED -> {}
+        }
+    }
 
     if (account == null) {
         showSnackBar("Not logged in")
         return
+    }
+
+    if (showDialog) {
+        CancelStripeDialog(
+            onConfirm = {
+                memberViewModel.cancelStripe(account)
+                setShowDialog(false)
+            },
+            onDismiss = {
+                setShowDialog(false)
+            }
+        )
     }
 
     SwipeRefresh(
@@ -30,11 +70,37 @@ fun MemberActivityScreen(
         ),
         onRefresh = {
             memberViewModel.refresh()
-        }
+        },
+        modifier = modifier,
     ) {
         MemberScreen(
             member = account.membership,
-            loading = progress
+            loading = progress,
+            onSubsOption = {
+                when (it) {
+                    SubsOptionRow.GoToPaywall -> {
+                        launchPaywallActivity(
+                            launcher = launcher,
+                            context = context
+                        )
+                    }
+                    SubsOptionRow.CancelStripe -> {
+                        setShowDialog(true)
+                    }
+                    SubsOptionRow.ReactivateStripe -> {
+                        memberViewModel.reactivateStripe(account)
+                    }
+                }
+            }
         )
     }
+}
+
+private fun launchPaywallActivity(
+    launcher: ManagedActivityResultLauncher<Intent, ActivityResult>,
+    context: Context
+) {
+    launcher.launch(
+        SubsActivity.intent(context)
+    )
 }
