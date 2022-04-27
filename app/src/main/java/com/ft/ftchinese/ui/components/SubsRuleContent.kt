4721719d3c44c3cb1c6eeb63056966fcb371e548
weiguo.ni <neefrankie@gmail.com>
2022-04-27 14:09:23 +0800

    refactor: add color

--- app/src/main/java/com/ft/ftchinese/ui/components/SubsRuleContent.kt
+++ app/src/main/java/com/ft/ftchinese/ui/components/SubsRuleContent.kt
@@ -1,10 +1,14 @@
-package com.ft.ftchinese.ui.product
+package com.ft.ftchinese.ui.components
 
 import androidx.compose.runtime.Composable
 import com.ft.ftchinese.model.paywall.paywallGuide
+import com.ft.ftchinese.ui.theme.OColor
 import dev.jeziellago.compose.markdowntext.MarkdownText
 
 @Composable
 fun SubsRuleContent() {
-    MarkdownText(markdown = paywallGuide)
+    MarkdownText(
+        markdown = paywallGuide,
+        color = OColor.black60,
+    )
 }
