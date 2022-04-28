26ec22ff0200dd07e5ea538d16e8d6882f03f4fe
weiguo.ni <neefrankie@gmail.com>
2022-04-28 23:53:29 +0800

    refactor: option row

--- app/src/main/java/com/ft/ftchinese/ui/member/SubsOptions.kt
+++ app/src/main/java/com/ft/ftchinese/ui/member/SubsOptions.kt
@@ -1,23 +1,53 @@
 package com.ft.ftchinese.ui.member
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ft.ftchinese.R
-import com.ft.ftchinese.ui.components.ClickableRow
 import com.ft.ftchinese.ui.components.RightArrow
 import com.ft.ftchinese.ui.theme.Dimens
-import com.ft.ftchinese.ui.theme.OColor
 
 enum class SubsOptionRow {
     GoToPaywall,
     CancelStripe,
     ReactivateStripe;
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+private fun OptionRow(
+    text: String,
+    onClick: () -> Unit,
+) {
+    Card(
+        onClick = onClick
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    horizontal = Dimens.dp16,
+                    vertical = Dimens.dp8
+                ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = text,
+                modifier = Modifier.weight(1f),
+                style = MaterialTheme.typography.body1
+            )
+
+            RightArrow()
+        }
+    }
 }
 
 @Composable
@@ -26,57 +56,35 @@ fun SubsOptions(
     reactivateStripe: Boolean,
     onClickRow: (SubsOptionRow) -> Unit
 ) {
-    val rowModifier = Modifier
-        .background(OColor.black5)
-        .padding(Dimens.dp8)
 
     Column(
         modifier = Modifier
             .fillMaxWidth()
     ) {
-        ClickableRow(
-            modifier = rowModifier,
-            endIcon = { RightArrow() },
-            onClick = {
-                onClickRow(SubsOptionRow.GoToPaywall)
-            }
+        OptionRow(
+            text = "购买订阅或更改自动续订"
         ) {
-            Text(
-                text = "购买订阅或更改自动续订",
-                color = OColor.teal
-            )
+            onClickRow(SubsOptionRow.GoToPaywall)
         }
 
         Spacer(modifier = Modifier.height(1.dp))
 
         if (reactivateStripe) {
-            ClickableRow(
-                modifier = rowModifier,
-                onClick = {
-                    onClickRow(SubsOptionRow.ReactivateStripe)
-                },
+            OptionRow(
+                text = stringResource(id = R.string.stripe_reactivate_auto_renew)
             ) {
-                Text(
-                    text = stringResource(id = R.string.stripe_reactivate_auto_renew)
-                )
+                onClickRow(SubsOptionRow.ReactivateStripe)
             }
 
-            Spacer(modifier = Modifier.height(1.dp))
         }
 
         if (cancelStripe) {
-            ClickableRow(
-                modifier = rowModifier,
-                onClick = {
-                    onClickRow(SubsOptionRow.CancelStripe)
-                }
+            OptionRow(
+                text = stringResource(id = R.string.stripe_cancel)
             ) {
-                Text(
-                    text = stringResource(id = R.string.stripe_cancel)
-                )
+                onClickRow(SubsOptionRow.CancelStripe)
             }
 
-            Spacer(modifier = Modifier.height(1.dp))
         }
     }
 }
