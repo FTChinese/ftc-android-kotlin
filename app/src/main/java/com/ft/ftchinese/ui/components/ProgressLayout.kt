a2eab7592ec3d8c36e7780874853e9014a6bea60
weiguo.ni <neefrankie@gmail.com>
2022-04-28 23:53:16 +0800

    refactor: add modifier arg

--- app/src/main/java/com/ft/ftchinese/ui/components/ProgressLayout.kt
+++ app/src/main/java/com/ft/ftchinese/ui/components/ProgressLayout.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.ft.ftchinese.ui.theme.OColor
 
 @Composable
 fun ProgressLayout(
     loading: Boolean = false,
+    modifier: Modifier = Modifier,
     content: @Composable () -> Unit,
 ) {
     Box(
