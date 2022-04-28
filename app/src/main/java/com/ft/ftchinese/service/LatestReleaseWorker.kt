a7c575f83fc47a10f33435bedfcc05ede0e9f832
weiguo.ni <neefrankie@gmail.com>
2022-04-28 18:09:58 +0800

    refactor: how to save release data

--- app/src/main/java/com/ft/ftchinese/service/LatestReleaseWorker.kt
+++ app/src/main/java/com/ft/ftchinese/service/LatestReleaseWorker.kt
@@ -6,20 +6,14 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.app.TaskStackBuilder
-import androidx.core.content.edit
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.ft.ftchinese.R
 import com.ft.ftchinese.model.AppRelease
-import com.ft.ftchinese.model.fetch.formatISODateTime
-import com.ft.ftchinese.model.fetch.parseISODateTime
 import com.ft.ftchinese.repository.ReleaseRepo
-import com.ft.ftchinese.store.FileCache
+import com.ft.ftchinese.store.ReleaseStore
 import com.ft.ftchinese.ui.settings.UpdateAppActivity
 import org.threeten.bp.ZonedDateTime
-
-private const val PREF_APK_UPDATER = "com.ft.ftchinese.apk_update_checker"
-private const val PREF_LAST_CHECKED_AT = "last_checked_at"
 
 /**
  * Background worker to check for latest release upon app launch.
@@ -32,6 +26,7 @@ class LatestReleaseWorker(appContext: Context, workerParams: WorkerParameters):
     Worker(appContext, workerParams) {
 
     private val ctx = appContext
+    private val store = ReleaseStore(appContext)
 
     override fun doWork(): Result {
         Log.i(TAG, "Start LatestReleaseWorker")
@@ -55,7 +50,7 @@ class LatestReleaseWorker(appContext: Context, workerParams: WorkerParameters):
                 return Result.success()
             }
 
-            cacheNewRelease(resp.body, resp.raw)
+            store.saveVersion(resp.body)
 
             urgeUpdate(resp.body)
         } catch (e: Exception) {
@@ -63,27 +58,20 @@ class LatestReleaseWorker(appContext: Context, workerParams: WorkerParameters):
             return Result.failure()
         }
 
-        setLastCheckTime()
-
         return Result.success()
     }
 
     private fun meetMinInterval(): Boolean {
-        val checkedAt = getLastCheckTime() ?: return true
+        val checkedAt = store.getLastCheckTime() ?: return true
 
         return checkedAt.plusHours(1).isBefore(ZonedDateTime.now())
-    }
-
-    private fun cacheNewRelease(release: AppRelease, data: String) {
-        Log.i(TAG, "Cache latest release")
-        FileCache(ctx).saveText(release.cacheFileName(), data)
     }
 
     private fun urgeUpdate(release: AppRelease) {
 
         Log.i(TAG, "Send notification for latest release")
 
-        val intent = UpdateAppActivity.newIntent(ctx, release.cacheFileName())
+        val intent = UpdateAppActivity.newIntent(ctx)
 
         val pendingIntent: PendingIntent? = TaskStackBuilder.create(ctx)
             .addNextIntentWithParentStack(intent)
@@ -99,20 +87,6 @@ class LatestReleaseWorker(appContext: Context, workerParams: WorkerParameters):
 
         with(NotificationManagerCompat.from(ctx)) {
             notify(1, builder.build())
-        }
-    }
-
-    private fun getLastCheckTime(): ZonedDateTime? {
-        val checkedAt = ctx
-            .getSharedPreferences(PREF_APK_UPDATER, Context.MODE_PRIVATE)
-            .getString(PREF_LAST_CHECKED_AT, null) ?: return null
-
-        return parseISODateTime(checkedAt)
-    }
-
-    private fun setLastCheckTime() {
-        ctx.getSharedPreferences(PREF_APK_UPDATER, Context.MODE_PRIVATE).edit {
-            putString(PREF_LAST_CHECKED_AT, formatISODateTime(ZonedDateTime.now()))
         }
     }
 
