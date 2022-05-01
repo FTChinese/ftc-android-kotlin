0baffc62d585da40724b5ab3ab263459013c206d
weiguo.ni <neefrankie@gmail.com>
2022-05-01 08:05:16 +0800

    refactor: layout size

--- app/src/main/java/com/ft/ftchinese/ui/components/ProgressLayout.kt
+++ app/src/main/java/com/ft/ftchinese/ui/components/ProgressLayout.kt
@@ -1,6 +1,7 @@
 package com.ft.ftchinese.ui.components
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -14,7 +15,8 @@ fun ProgressLayout(
     content: @Composable () -> Unit,
 ) {
     Box(
-        contentAlignment = Alignment.Center
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize().then(modifier)
     ) {
         content()
 
