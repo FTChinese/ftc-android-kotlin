f1105004b28e41465b812f5c12074c226bb9d72f
weiguo.ni <neefrankie@gmail.com>
2022-05-01 12:17:41 +0800

    refactor: removed create customer

--- app/src/main/java/com/ft/ftchinese/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/ft/ftchinese/viewmodel/UserViewModel.kt
@@ -5,17 +5,13 @@ import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.ft.ftchinese.R
 import com.ft.ftchinese.model.fetch.APIError
 import com.ft.ftchinese.model.reader.Account
 import com.ft.ftchinese.model.reader.Membership
 import com.ft.ftchinese.repository.AccountRepo
-import com.ft.ftchinese.repository.StripeClient
 import com.ft.ftchinese.store.SessionManager
-import com.ft.ftchinese.ui.base.ConnectionLiveData
 import com.ft.ftchinese.ui.components.ToastMessage
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -23,17 +19,8 @@ import kotlinx.coroutines.withContext
 
 private const val TAG = "UserViewModel"
 
-open class UserViewModel(application: Application) : AndroidViewModel(application) {
+open class UserViewModel(application: Application) : BaseAppViewModel(application) {
     protected val session = SessionManager.getInstance(application)
-
-    val connectionLiveData = ConnectionLiveData(application)
-
-    val progressLiveData = MutableLiveData(false)
-    val refreshingLiveData = MutableLiveData(false)
-
-    val toastLiveData: MutableLiveData<ToastMessage> by lazy {
-        MutableLiveData<ToastMessage>()
-    }
 
     var account by mutableStateOf<Account?>(null)
         private set
@@ -67,46 +54,10 @@ open class UserViewModel(application: Application) : AndroidViewModel(applicatio
         session.logout()
     }
 
-    fun createCustomer(account: Account) {
-        if (connectionLiveData.value != true) {
-            toastLiveData.value = ToastMessage.Resource(R.string.prompt_no_network)
-            return
-        }
-
-        progressLiveData.value = true
-
-        viewModelScope.launch {
-            try {
-                val resp = withContext(Dispatchers.IO) {
-                    StripeClient.createCustomer(account)
-                }
-
-                progressLiveData.value = false
-                if (resp.body == null) {
-                    toastLiveData.value = ToastMessage.Resource(R.string.stripe_customer_not_created)
-                    return@launch
-                }
-
-                saveAccount(account.withCustomerID(resp.body.id))
-            } catch (e: APIError) {
-                progressLiveData.value = false
-                toastLiveData.value = if (e.statusCode == 404) {
-                    ToastMessage.Resource(R.string.stripe_customer_not_found)
-                } else {
-                    ToastMessage.fromApi(e)
-                }
-            } catch (e: Exception) {
-                progressLiveData.value = false
-                toastLiveData.value = ToastMessage.fromException(e)
-            }
-        }
-    }
-
     fun refreshAccount() {
         val a = account ?: return
 
-        if (connectionLiveData.value != true) {
-            toastLiveData.value = ToastMessage.Resource(R.string.prompt_no_network)
+        if (!ensureNetwork()) {
             progressLiveData.value = false
             refreshingLiveData.value = false
             return
