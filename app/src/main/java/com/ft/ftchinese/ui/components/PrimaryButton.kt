7df12fd5a2eb849ac2086b9735fcdcd4cb0f89fa
weiguo.ni <neefrankie@gmail.com>
2022-05-06 15:32:58 +0800

    feat: SecondaryButton

--- app/src/main/java/com/ft/ftchinese/ui/components/PrimaryButton.kt
+++ app/src/main/java/com/ft/ftchinese/ui/components/PrimaryButton.kt
@@ -1,18 +1,20 @@
 package com.ft.ftchinese.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ft.ftchinese.ui.theme.OColor
 
 @Composable
 fun PrimaryButton(
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    backgroundColor: Color = OColor.teal,
     enabled: Boolean = true,
     content: @Composable RowScope.() -> Unit
 ) {
@@ -21,9 +23,33 @@ fun PrimaryButton(
         modifier = modifier,
         enabled = enabled,
         colors = ButtonDefaults.buttonColors(
-            backgroundColor = OColor.teal,
+            backgroundColor = backgroundColor,
             contentColor = OColor.white,
-            disabledBackgroundColor = OColor.teal.copy(alpha = 0.4f)
+            disabledBackgroundColor = backgroundColor.copy(alpha = 0.4f)
+        ),
+        content = content,
+    )
+}
+
+@Composable
+fun SecondaryButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    backgroundColor: Color = OColor.paper,
+    contentColor: Color = OColor.teal,
+    content: @Composable RowScope.() -> Unit
+) {
+    LocalContentColor
+    OutlinedButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        border = BorderStroke(1.dp, contentColor),
+        colors = ButtonDefaults.outlinedButtonColors(
+            backgroundColor = backgroundColor,
+            contentColor = contentColor,
+            disabledContentColor = backgroundColor.copy(alpha = 0.4f)
         ),
         content = content,
     )
