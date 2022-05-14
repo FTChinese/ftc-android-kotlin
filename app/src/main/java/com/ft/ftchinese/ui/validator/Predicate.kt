e56a574b35647d97b6c78290601b5b43a387915d
weiguo.ni <neefrankie@gmail.com>
2022-05-14 19:01:37 +0800

    feat: ValidationRule

--- app/src/main/java/com/ft/ftchinese/ui/validator/Predicate.kt
+++ app/src/main/java/com/ft/ftchinese/ui/validator/Predicate.kt
@@ -3,3 +3,8 @@ package com.ft.ftchinese.ui.validator
 // A predicate is a function that evaluates to true when its params matches the condition of the predicate.
 // When returning false, it indicates the param is not valid.
 typealias Predicate = (value: String?) -> Boolean
+
+data class ValidationRule(
+    val predicate: Predicate,
+    val message: String,
+)
