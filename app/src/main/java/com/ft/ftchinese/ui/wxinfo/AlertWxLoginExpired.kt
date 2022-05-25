e781f4e2b4eaf8f2b002dce97224cf76af8d1e9e
weiguo.ni <neefrankie@gmail.com>
2022-05-25 21:16:07 +0800

    refactor: use SimpleDialog

--- app/src/main/java/com/ft/ftchinese/ui/wxinfo/AlertWxLoginExpired.kt
+++ app/src/main/java/com/ft/ftchinese/ui/wxinfo/AlertWxLoginExpired.kt
@@ -1,38 +1,19 @@
 package com.ft.ftchinese.ui.wxinfo
 
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import com.ft.ftchinese.R
+import com.ft.ftchinese.ui.components.SimpleDialog
 
 @Composable
 fun AlertWxLoginExpired(
     onDismiss: () -> Unit,
     onConfirm: () -> Unit
 ) {
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        confirmButton = {
-            Button(
-                onClick = onConfirm
-            ) {
-                Text(text = stringResource(id = R.string.btn_ok))
-            }
-        },
-        dismissButton = {
-            Button(
-                onClick = onDismiss
-            ) {
-                Text(text = stringResource(id = R.string.btn_cancel))
-            }
-        },
-        title = {
-            Text(text = stringResource(id = R.string.wx_relogin))
-        },
-        text = {
-            Text(text = stringResource(id = R.string.wx_session_expired))
-        }
+    SimpleDialog(
+        title = stringResource(id = R.string.wx_relogin),
+        body = stringResource(id = R.string.wx_session_expired),
+        onDismiss = onDismiss,
+        onConfirm = onConfirm
     )
 }
