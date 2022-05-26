eb8ef3510474f5b321769e7a25c5cc06d1a3d6b1
weiguo.ni <neefrankie@gmail.com>
2022-05-26 18:50:41 +0800

    refactor: removed deprecation message

--- app/src/main/java/com/ft/ftchinese/store/AccountCache.kt
+++ app/src/main/java/com/ft/ftchinese/store/AccountCache.kt
@@ -3,7 +3,9 @@ package com.ft.ftchinese.store
 import com.ft.ftchinese.model.reader.Account
 import com.ft.ftchinese.model.reader.Membership
 
-@Deprecated("Removed after migration to single activity")
+/*
+ * A in-memory cached of user account.
+ */
 object AccountCache {
     private var account: Account? = null
 
