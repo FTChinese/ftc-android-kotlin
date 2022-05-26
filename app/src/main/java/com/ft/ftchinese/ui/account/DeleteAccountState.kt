414892a4e5bba82399a7b903fc8d87b19f8ccd8d
weiguo.ni <neefrankie@gmail.com>
2022-05-26 11:25:34 +0800

    feat: DeleteAccountState

---
+++ app/src/main/java/com/ft/ftchinese/ui/account/DeleteAccountState.kt
@@ -0,0 +1,73 @@
+package com.ft.ftchinese.ui.account
+
+import android.content.res.Resources
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import com.ft.ftchinese.model.fetch.FetchResult
+import com.ft.ftchinese.model.request.AccountDropped
+import com.ft.ftchinese.model.request.EmailPasswordParams
+import com.ft.ftchinese.repository.AccountRepo
+import com.ft.ftchinese.ui.base.ConnectionState
+import com.ft.ftchinese.ui.base.connectivityState
+import com.ft.ftchinese.ui.components.BaseState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+class DeleteAccountState(
+    scaffoldState: ScaffoldState,
+    scope: CoroutineScope,
+    resources: Resources,
+    connState: State<ConnectionState>,
+) : BaseState(scaffoldState, scope, resources, connState) {
+
+    var dropped by mutableStateOf<AccountDropped?>(null)
+        private set
+
+    fun resetDropped() {
+        dropped = null
+    }
+
+    fun drop(ftcId: String, params: EmailPasswordParams) {
+        if (!ensureConnected()) {
+            return
+        }
+
+        progress.value = false
+        scope.launch {
+            val result = AccountRepo.asyncDeleteAccount(
+                ftcId = ftcId,
+                params = params
+            )
+            progress.value = false
+
+            when (result) {
+                is FetchResult.LocalizedError -> {
+                    showSnackBar(result.msgId)
+                }
+                is FetchResult.TextError -> {
+                    showSnackBar(result.text)
+                }
+                is FetchResult.Success -> {
+                    dropped = result.data
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun rememberDeleteAccountState(
+    scaffoldState: ScaffoldState = rememberScaffoldState(),
+    scope: CoroutineScope = rememberCoroutineScope(),
+    resources: Resources = LocalContext.current.resources,
+    connState: State<ConnectionState> = connectivityState()
+) = remember(scaffoldState, resources, connState) {
+    DeleteAccountState(
+        scaffoldState = scaffoldState,
+        scope = scope,
+        resources = resources,
+        connState = connState
+    )
+}
