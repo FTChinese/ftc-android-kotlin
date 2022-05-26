a484054c1cd1d223b729022f8949b8fab9782084
weiguo.ni <neefrankie@gmail.com>
2022-05-26 11:25:02 +0800

    refactor: replace delete account by compose ui

---
+++ app/src/main/java/com/ft/ftchinese/ui/account/DeleteAccountActivityScreen.kt
@@ -0,0 +1,118 @@
+package com.ft.ftchinese.ui.account
+
+import android.content.Context
+import androidx.compose.material.ScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.platform.LocalContext
+import com.ft.ftchinese.R
+import com.ft.ftchinese.model.enums.Edition
+import com.ft.ftchinese.model.reader.Account
+import com.ft.ftchinese.model.request.AccountDropped
+import com.ft.ftchinese.model.request.EmailPasswordParams
+import com.ft.ftchinese.ui.base.IntentsUtil
+import com.ft.ftchinese.ui.components.ProgressLayout
+import com.ft.ftchinese.ui.formatter.FormatHelper
+import com.ft.ftchinese.viewmodel.UserViewModel
+
+@Composable
+fun DeleteAccountActivityScreen(
+    userViewModel: UserViewModel,
+    scaffoldState: ScaffoldState,
+    onDeleted: () -> Unit,
+) {
+    val context = LocalContext.current
+    val accountState = userViewModel.accountLiveData.observeAsState()
+    val account = accountState.value
+
+    val uiState = rememberDeleteAccountState(
+        scaffoldState = scaffoldState
+    )
+
+    if (account == null) {
+        return
+    }
+
+    uiState.dropped?.let {
+        when (it) {
+            AccountDropped.Success -> {
+                uiState.resetDropped()
+                onDeleted()
+            }
+            AccountDropped.SubsExists -> {
+                AlertDeleteDenied(
+                    onDismiss = uiState::resetDropped,
+                    onConfirm = {
+                        val ok = sendEmail(
+                            context = context,
+                            account = account
+                        )
+                        if (!ok) {
+                            uiState.showSnackBar(R.string.prompt_no_email_app)
+                        }
+                        uiState.resetDropped()
+                    }
+                )
+            }
+        }
+    }
+
+    ProgressLayout(
+        loading = uiState.progress.value
+    ) {
+        DeleteAccountScreen(
+            loading = uiState.progress.value,
+            onVerify = {
+                uiState.drop(
+                    ftcId = account.id,
+                    params = EmailPasswordParams(
+                        email = account.email,
+                        password = it,
+                    )
+                )
+            }
+        )
+    }
+}
+
+private fun sendEmail(
+    context: Context,
+    account: Account,
+): Boolean {
+    val intent = IntentsUtil.emailCustomerService(
+        title = context.getString(R.string.subject_delete_account_valid_subs),
+        body = composeDeletionEmail(context, account)
+    )
+
+    return if (intent.resolveActivity(context.packageManager) != null) {
+        context.startActivity(intent)
+        true
+    } else {
+        false
+    }
+}
+
+private fun composeDeletionEmail(
+    context: Context,
+    a: Account
+): String? {
+    val emailOrMobile = if (a.isMobileEmail) {
+        a.mobile
+    } else {
+        a.email
+    }
+
+    if (a.membership.tier == null || a.membership.cycle == null) {
+        return null
+    }
+
+    val edition = FormatHelper.formatEdition(
+        context,
+        Edition(
+            tier = a.membership.tier,
+            cycle = a.membership.cycle,
+        )
+    )
+
+    return "FT中文网，\n请删除我的账号 $emailOrMobile。\n我的账号已经购买了FT中文网付费订阅服务 $edition，到期时间 ${a.membership.localizeExpireDate()}。我已知悉删除账号的同时将删除我的订阅信息。"
+}
---
+++ app/src/main/java/com/ft/ftchinese/ui/account/DeleteAccountScreen.kt
@@ -0,0 +1,66 @@
+package com.ft.ftchinese.ui.account
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.ft.ftchinese.R
+import com.ft.ftchinese.ui.components.BlockButton
+import com.ft.ftchinese.ui.components.PasswordInput
+import com.ft.ftchinese.ui.components.SimpleDialog
+import com.ft.ftchinese.ui.components.rememberInputState
+import com.ft.ftchinese.ui.theme.Dimens
+import com.ft.ftchinese.ui.validator.ValidationRule
+import com.ft.ftchinese.ui.validator.Validator
+
+@Composable
+fun DeleteAccountScreen(
+    loading: Boolean,
+    onVerify: (String) -> Unit
+) {
+
+    val currentPwState = rememberInputState(
+        rules = listOf(
+            ValidationRule(
+                predicate = Validator::notEmpty,
+                message = "密码必填"
+            )
+        )
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(Dimens.dp16)
+    ) {
+
+        PasswordInput(
+            label = stringResource(id = R.string.label_old_password),
+            state = currentPwState,
+        )
+
+        Spacer(modifier = Modifier.height(Dimens.dp16))
+
+        BlockButton(
+            enabled = currentPwState.valid.value && !loading,
+            onClick = {
+                onVerify(currentPwState.field.value)
+            },
+            text = stringResource(id = R.string.btn_verify_password)
+        )
+    }
+}
+
+@Composable
+fun AlertDeleteDenied(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+) {
+    SimpleDialog(
+        title = stringResource(id = R.string.title_delete_account_denied),
+        body = stringResource(id = R.string.message_delete_account_valid_subs),
+        confirmText = stringResource(id = R.string.btn_send_email),
+        onDismiss = onDismiss,
+        onConfirm = onConfirm,
+    )
+}
