242de239e4cc988aa962e5b6d6476fedf691418d
weiguo.ni <neefrankie@gmail.com>
2022-05-26 18:38:59 +0800

    feat: compose ui version of updating mobile

---
+++ app/src/main/java/com/ft/ftchinese/ui/account/MobileActivityScreen.kt
@@ -0,0 +1,68 @@
+package com.ft.ftchinese.ui.account
+
+import android.util.Log
+import androidx.compose.material.ScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
+import com.ft.ftchinese.model.request.SMSCodeParams
+import com.ft.ftchinese.ui.components.ProgressLayout
+import com.ft.ftchinese.ui.components.rememberTimerState
+import com.ft.ftchinese.viewmodel.UserViewModel
+
+private const val TAG = "MobileScreen"
+
+@Composable
+fun MobileActivityScreen(
+    userViewModel: UserViewModel,
+    scaffoldState: ScaffoldState,
+) {
+    val accountState = userViewModel.accountLiveData.observeAsState()
+    val account = accountState.value
+
+    val uiState = rememberMobileState(
+        scaffoldState = scaffoldState
+    )
+
+    val timerState = rememberTimerState()
+
+    if (account == null) {
+        return
+    }
+
+    uiState.updated?.let {
+        Log.i(TAG, "Save updated account")
+        userViewModel.saveAccount(account.withBaseAccount(it))
+    }
+
+    LaunchedEffect(key1 = uiState.codeSent) {
+        if (uiState.codeSent) {
+            Log.i(TAG, "Start timer")
+            timerState.start()
+        }
+    }
+
+    ProgressLayout(
+        loading = uiState.progress.value
+    ) {
+        MobileScreen(
+            currentMobile = account.mobile ?: "",
+            loading = uiState.progress.value,
+            timerState = timerState,
+            onRequestCode = {
+                uiState.requestSMSCode(
+                    ftcId = account.id,
+                    params = SMSCodeParams(
+                        mobile = it
+                    )
+                )
+            },
+            onSave = {
+                uiState.changeMobile(
+                    ftcId = account.id,
+                    params = it,
+                )
+            }
+        )
+    }
+}
---
+++ app/src/main/java/com/ft/ftchinese/ui/account/MobileState.kt
@@ -0,0 +1,102 @@
+package com.ft.ftchinese.ui.account
+
+import android.content.res.Resources
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import com.ft.ftchinese.model.fetch.FetchResult
+import com.ft.ftchinese.model.reader.BaseAccount
+import com.ft.ftchinese.model.request.MobileFormParams
+import com.ft.ftchinese.model.request.SMSCodeParams
+import com.ft.ftchinese.repository.AccountRepo
+import com.ft.ftchinese.ui.base.ConnectionState
+import com.ft.ftchinese.ui.base.connectivityState
+import com.ft.ftchinese.ui.components.BaseState
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+class MobileState(
+    scaffoldState: ScaffoldState,
+    scope: CoroutineScope,
+    resources: Resources,
+    connState: State<ConnectionState>,
+) : BaseState(scaffoldState, scope, resources, connState) {
+
+    var codeSent by mutableStateOf(false)
+        private set
+
+    var updated by mutableStateOf<BaseAccount?>(null)
+        private set
+
+    fun requestSMSCode(ftcId: String, params: SMSCodeParams) {
+        if (!ensureConnected()) {
+            return
+        }
+
+        codeSent = false
+        progress.value = true
+        scope.launch {
+            val result = AccountRepo.asyncRequestSMSCode(
+                ftcId = ftcId,
+                params = params
+            )
+            progress.value = false
+            when (result) {
+                is FetchResult.LocalizedError -> {
+                    showSnackBar(result.msgId)
+                }
+                is FetchResult.TextError -> {
+                    showSnackBar(result.text)
+                }
+                is FetchResult.Success -> {
+                    showSnackBar("验证码已发送")
+                    codeSent = result.data
+                }
+            }
+        }
+    }
+
+    fun changeMobile(ftcId: String, params: MobileFormParams) {
+        if (!ensureConnected()) {
+            return
+        }
+
+        progress.value = true
+        scope.launch {
+            val result = AccountRepo.asyncUpdateMobile(
+                ftcId = ftcId,
+                params = params,
+            )
+
+            progress.value = false
+            when (result) {
+                is FetchResult.LocalizedError -> {
+                    showSnackBar(result.msgId)
+                }
+                is FetchResult.TextError -> {
+                    showSnackBar(result.text)
+                }
+                is FetchResult.Success -> {
+                    showSaved()
+                    updated = result.data
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun rememberMobileState(
+    scaffoldState: ScaffoldState = rememberScaffoldState(),
+    scope: CoroutineScope = rememberCoroutineScope(),
+    resources: Resources = LocalContext.current.resources,
+    connState: State<ConnectionState> = connectivityState()
+) = remember(scaffoldState, resources, connState) {
+    MobileState(
+        scaffoldState = scaffoldState,
+        scope = scope,
+        resources = resources,
+        connState = connState
+    )
+}
