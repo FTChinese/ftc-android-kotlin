e2f3af4cabd53b4e1ea29b561209ec9b4f2cbc8a
weiguo.ni <neefrankie@gmail.com>
2022-05-26 11:27:27 +0800

    refactor:

---
+++ app/src/main/java/com/ft/ftchinese/ui/account/address/DeleteAccountViewModel.kt
@@ -0,0 +1,34 @@
+package com.ft.ftchinese.ui.account.address
+
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import com.ft.ftchinese.ui.base.BaseViewModel
+import com.ft.ftchinese.ui.validator.LiveDataValidator
+import com.ft.ftchinese.ui.validator.Validator
+
+class DeleteAccountViewModel : BaseViewModel() {
+    val passwordLiveData = MutableLiveData("")
+    val passwordValidator = LiveDataValidator(passwordLiveData).apply {
+        addRule("密码必填", Validator::notEmpty)
+    }
+
+    private val isDirty: Boolean
+        get() = passwordValidator.isDirty()
+
+    val isFormEnabled = MediatorLiveData<Boolean>().apply {
+        addSource(passwordLiveData) {
+            value = enableForm()
+        }
+        addSource(progressLiveData) {
+            value = enableForm()
+        }
+    }
+
+    init {
+        progressLiveData.value = false
+    }
+
+    private fun enableForm(): Boolean {
+        return progressLiveData.value == false && isDirty && passwordValidator.isValid()
+    }
+}
