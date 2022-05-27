4622cce79964683f288028ff38a36f58a0b9e040
weiguo.ni <neefrankie@gmail.com>
2022-05-27 12:39:40 +0800

    feat: add suspend version

--- app/src/main/java/com/ft/ftchinese/repository/ReleaseRepo.kt
+++ app/src/main/java/com/ft/ftchinese/repository/ReleaseRepo.kt
@@ -1,8 +1,13 @@
 package com.ft.ftchinese.repository
 
+import com.ft.ftchinese.R
 import com.ft.ftchinese.model.AppRelease
+import com.ft.ftchinese.model.fetch.APIError
 import com.ft.ftchinese.model.fetch.Fetch
+import com.ft.ftchinese.model.fetch.FetchResult
 import com.ft.ftchinese.model.fetch.HttpResp
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 object ReleaseRepo {
 
@@ -14,4 +19,25 @@ object ReleaseRepo {
             .endJson(withRaw = true)
     }
 
+    suspend fun asyncGetLatest(): FetchResult<AppRelease> {
+        try {
+            val resp = withContext(Dispatchers.IO) {
+                getLatest()
+            }
+
+            return if (resp.body == null) {
+                FetchResult.LocalizedError(R.string.release_not_found)
+            } else {
+                return FetchResult.Success(resp.body)
+            }
+        } catch (e: APIError) {
+            return if (e.statusCode == 404) {
+                FetchResult.LocalizedError(R.string.release_not_found)
+            } else {
+                FetchResult.fromApi(e)
+            }
+        } catch (e: Exception) {
+            return FetchResult.fromException(e)
+        }
+    }
 }
