4b14b375aa89d14f5774a367e2e59bdc9071da4e
weiguo.ni <neefrankie@gmail.com>
2022-05-28 13:05:13 +0800

    refactor: removed unused methods

--- app/src/main/java/com/ft/ftchinese/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/ft/ftchinese/viewmodel/UserViewModel.kt
@@ -1,23 +1,12 @@
 package com.ft.ftchinese.viewmodel
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
-import com.ft.ftchinese.R
-import com.ft.ftchinese.model.fetch.APIError
 import com.ft.ftchinese.model.iapsubs.IAPSubsResult
 import com.ft.ftchinese.model.reader.Account
 import com.ft.ftchinese.model.reader.Membership
 import com.ft.ftchinese.model.stripesubs.StripeSubsResult
-import com.ft.ftchinese.repository.AccountRepo
 import com.ft.ftchinese.store.SessionManager
-import com.ft.ftchinese.ui.components.ToastMessage
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-
-private const val TAG = "UserViewModel"
 
 open class UserViewModel(application: Application) : BaseAppViewModel(application) {
     protected val session = SessionManager.getInstance(application)
@@ -66,54 +55,5 @@ open class UserViewModel(application: Application) : BaseAppViewModel(applicatio
     fun logout() {
         accountLiveData.value = null
         session.logout()
-    }
-
-    @Deprecated("")
-    fun refreshAccount() {
-        val a = accountLiveData.value ?: return
-
-        if (!ensureNetwork()) {
-            return
-        }
-
-        toastLiveData.value = ToastMessage.Resource(R.string.refreshing_account)
-
-        refreshingLiveData.value = true
-        viewModelScope.launch {
-            Log.i(TAG, "Start refreshing account")
-
-            asyncRefreshAccount(a)?.let {
-                saveAccount(it)
-            }
-
-            refreshingLiveData.value = false
-        }
-    }
-
-    protected suspend fun asyncRefreshAccount(a: Account): Account? {
-        try {
-            val refreshed = withContext(Dispatchers.IO) {
-                AccountRepo.refresh(a)
-            }
-
-            return if (refreshed == null) {
-                toastLiveData.value = ToastMessage.Resource(R.string.loading_failed)
-                null
-            } else {
-                toastLiveData.value = ToastMessage.Resource(R.string.refresh_success)
-                refreshed
-            }
-        } catch (e: APIError) {
-            toastLiveData.value = if (e.statusCode == 404) {
-                ToastMessage.Resource(R.string.account_not_found)
-            } else {
-                ToastMessage.fromApi(e)
-            }
-
-            return null
-        } catch (e: Exception) {
-            toastLiveData.value = ToastMessage.fromException(e)
-            return null
-        }
     }
 }
