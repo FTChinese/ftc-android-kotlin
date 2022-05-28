6c329219c2d932e2580b4add2b3c6c0318bc5eca
weiguo.ni <neefrankie@gmail.com>
2022-05-28 12:05:27 +0800

    refactor

---
+++ app/src/main/java/com/ft/ftchinese/ui/web/ArticleKind.kt
@@ -0,0 +1,14 @@
+package com.ft.ftchinese.ui.web
+
+object ArticleKind {
+    const val story = "story"
+    const val premium = "premium"
+    const val video = "video"
+    const val interactive = "interactive"
+    const val photoNews = "photonews"
+    const val channel = "channel"
+    const val tag = "tag"
+    const val m = "m"
+    const val archive = "archiver"
+}
+
