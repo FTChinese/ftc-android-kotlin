284df91a545fbf19108c4fdcd72b2a9e558dd25b
weiguo.ni <neefrankie@gmail.com>
2022-05-29 23:56:46 +0800

    feat: CheckButton

--- app/src/main/java/com/ft/ftchinese/ui/components/RadioIcon.kt
+++ app/src/main/java/com/ft/ftchinese/ui/components/RadioIcon.kt
@@ -1,10 +1,13 @@
 package com.ft.ftchinese.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import com.ft.ftchinese.R
+import com.ft.ftchinese.ui.theme.OColor
 
 @Composable
 fun RadioIcon(
@@ -19,7 +22,31 @@ fun RadioIcon(
                 R.drawable.ic_baseline_radio_button_unchecked_24
             }
         ),
-        contentDescription = "",
+        contentDescription = "Select",
         modifier = modifier,
     )
+}
+
+@Composable
+fun CheckButton(
+    selected: Boolean,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        onClick = onClick,
+        modifier = modifier
+    ) {
+        Icon(
+            painter = painterResource(
+                id = if (selected) {
+                    R.drawable.ic_baseline_check_circle_outline_24
+                } else {
+                    R.drawable.ic_baseline_radio_button_unchecked_24
+                }
+            ),
+            tint = OColor.teal,
+            contentDescription = "Select"
+        )
+    }
 }
