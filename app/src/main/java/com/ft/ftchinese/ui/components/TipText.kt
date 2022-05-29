22845196c5ee1630717446210f44716d94f7631c
weiguo.ni <neefrankie@gmail.com>
2022-05-29 12:41:35 +0800

    refactor: style

--- app/src/main/java/com/ft/ftchinese/ui/components/TipText.kt
+++ app/src/main/java/com/ft/ftchinese/ui/components/TipText.kt
@@ -1,17 +1,24 @@
 package com.ft.ftchinese.ui.components
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import com.ft.ftchinese.ui.theme.OColor
 
 @Composable
 fun TipText(
+    modifier: Modifier = Modifier,
+    textAlign: TextAlign = TextAlign.Start,
     text: String,
 ) {
     Text(
         text = text,
         style = MaterialTheme.typography.body2,
-        color = OColor.black60
+        color = OColor.black60,
+        modifier = Modifier.fillMaxWidth().then(modifier),
+        textAlign = textAlign,
     )
 }
