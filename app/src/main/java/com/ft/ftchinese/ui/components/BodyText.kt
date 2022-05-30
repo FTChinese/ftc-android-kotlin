774a44f951c5bb049b4958aa0432a9b296cb5d71
weiguo.ni <neefrankie@gmail.com>
2022-05-30 19:55:30 +0800

    feat: predefined text

---
+++ app/src/main/java/com/ft/ftchinese/ui/components/BodyText.kt
@@ -0,0 +1,68 @@
+package com.ft.ftchinese.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+import com.ft.ftchinese.ui.theme.OColor
+
+// Size 16
+@Composable
+fun BodyText0(
+    modifier: Modifier = Modifier,
+    textAlign: TextAlign = TextAlign.Start,
+    text: String,
+    color: Color = OColor.black
+) {
+    Text(
+        text = text,
+        style = TextStyle(
+            fontWeight = FontWeight.Normal,
+            fontSize = 18.sp,
+            letterSpacing = 0.5.sp
+        ),
+        color = color,
+        modifier = Modifier.fillMaxWidth().then(modifier),
+        textAlign = textAlign,
+    )
+}
+
+// Size 16
+@Composable
+fun BodyText1(
+    modifier: Modifier = Modifier,
+    textAlign: TextAlign = TextAlign.Start,
+    text: String,
+    color: Color = OColor.black
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.body1,
+        color = color,
+        modifier = Modifier.fillMaxWidth().then(modifier),
+        textAlign = textAlign,
+    )
+}
+
+// Size 14
+@Composable
+fun BodyText2(
+    modifier: Modifier = Modifier,
+    textAlign: TextAlign = TextAlign.Start,
+    text: String,
+    color: Color = OColor.black60
+) {
+    Text(
+        text = text,
+        style = MaterialTheme.typography.body2,
+        color = color,
+        modifier = Modifier.fillMaxWidth().then(modifier),
+        textAlign = textAlign,
+    )
+}
