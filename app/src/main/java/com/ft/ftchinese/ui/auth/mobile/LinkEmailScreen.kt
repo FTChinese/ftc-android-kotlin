e7726022c34ef392986c4cd7d44c5f9ed1eadf91
weiguo.ni <neefrankie@gmail.com>
2022-05-30 20:08:47 +0800

    refactor: add ScreenHeader

--- app/src/main/java/com/ft/ftchinese/ui/auth/mobile/LinkEmailScreen.kt
+++ app/src/main/java/com/ft/ftchinese/ui/auth/mobile/LinkEmailScreen.kt
@@ -1,13 +1,10 @@
 package com.ft.ftchinese.ui.auth.mobile
 
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
 import com.ft.ftchinese.model.request.EmailAuthFormVal
-import com.ft.ftchinese.ui.components.TipText
+import com.ft.ftchinese.ui.components.ScreenHeader
 import com.ft.ftchinese.ui.form.EmailSignInForm
 import com.ft.ftchinese.ui.theme.Dimens
 
@@ -25,14 +22,10 @@ fun LinkEmailScreen(
             .fillMaxSize()
             .padding(Dimens.dp16)
     ) {
-        Text(
-            text = "关联已有邮箱账号",
-            modifier = Modifier.fillMaxWidth(),
-            style = MaterialTheme.typography.h5,
-            textAlign = TextAlign.Center
-        )
-        TipText(
-            text = "绑定邮箱后下次可以直接使用手机号${mobile}登录该邮箱账号"
+
+        ScreenHeader(
+            title = "关联已有邮箱账号",
+            subTitle = "绑定邮箱后下次可以直接使用手机号${mobile}登录该邮箱账号"
         )
 
         Spacer(modifier = Modifier.height(Dimens.dp16))
