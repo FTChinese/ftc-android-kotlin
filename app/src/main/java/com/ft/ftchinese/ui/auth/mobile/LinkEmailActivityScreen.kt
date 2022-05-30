c84cbb19f1bc0d58e14345fe73562dcf82f4298a
weiguo.ni <neefrankie@gmail.com>
2022-05-30 19:59:39 +0800

    refactor: toast

--- app/src/main/java/com/ft/ftchinese/ui/auth/mobile/LinkEmailActivityScreen.kt
+++ app/src/main/java/com/ft/ftchinese/ui/auth/mobile/LinkEmailActivityScreen.kt
@@ -1,24 +1,20 @@
 package com.ft.ftchinese.ui.auth.mobile
 
-import android.widget.Toast
 import androidx.compose.material.ScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.ft.ftchinese.R
+import com.ft.ftchinese.model.reader.Account
 import com.ft.ftchinese.model.request.MobileLinkParams
 import com.ft.ftchinese.store.TokenManager
 import com.ft.ftchinese.ui.components.ProgressLayout
-import com.ft.ftchinese.viewmodel.UserViewModel
 
 @Composable
 fun LinkEmailActivityScreen(
-    userViewModel: UserViewModel = viewModel(),
     scaffoldState: ScaffoldState,
     mobile: String?,
-    onSuccess: () -> Unit,
+    onSuccess: (Account) -> Unit,
     onForgotPassword: (String) -> Unit,
     onSignUp: () -> Unit,
 ) {
@@ -39,13 +35,7 @@ fun LinkEmailActivityScreen(
 
     LaunchedEffect(key1 = authState.accountLoaded) {
         authState.accountLoaded?.let {
-            userViewModel.saveAccount(it)
-            Toast.makeText(
-                context,
-                R.string.login_success,
-                Toast.LENGTH_SHORT
-            ).show()
-            onSuccess()
+            onSuccess(it)
         }
     }
 
