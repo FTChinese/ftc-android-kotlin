4cce65963dd2e8b4df8260d6f6727ae1ddbb8e5e
weiguo.ni <neefrankie@gmail.com>
2022-06-03 22:04:47 +0800

    refactor: add open modifier

--- app/src/main/java/com/ft/ftchinese/ui/account/stripewallet/StripeWalletState.kt
+++ app/src/main/java/com/ft/ftchinese/ui/account/stripewallet/StripeWalletState.kt
@@ -25,7 +25,7 @@ import kotlinx.coroutines.launch
 
 private const val TAG = "StripeWallet"
 
-class StripeWalletState(
+open class StripeWalletState(
     scaffoldState: ScaffoldState,
     scope: CoroutineScope,
     resources: Resources,
