57115fd29c90aabc2e4a8e83b401616a1d0555a4
weiguo.ni <neefrankie@gmail.com>
2022-06-03 17:56:35 +0800

    feat: button to set default payment method

--- app/src/main/java/com/ft/ftchinese/ui/account/stripewallet/StripeWalletScreen.kt
+++ app/src/main/java/com/ft/ftchinese/ui/account/stripewallet/StripeWalletScreen.kt
@@ -2,12 +2,17 @@ package com.ft.ftchinese.ui.account.stripewallet
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ft.ftchinese.model.stripesubs.StripePaymentCard
 import com.ft.ftchinese.model.stripesubs.StripePaymentMethod
 import com.ft.ftchinese.ui.components.AddBankCard
 import com.ft.ftchinese.ui.components.BankCard
-import com.ft.ftchinese.ui.components.ProgressLayout
+import com.ft.ftchinese.ui.components.OTextButton
+import com.ft.ftchinese.ui.components.SimpleDialog
 import com.ft.ftchinese.ui.theme.Dimens
+import java.util.*
 
 @Composable
 fun StripeWalletScreen(
@@ -23,31 +28,37 @@ fun StripeWalletScreen(
             .fillMaxSize()
             .padding(Dimens.dp8)
     ) {
-        paymentMethod?.card?.let { card ->
+        paymentMethod?.let { pm ->
 
-//                Row(
-//                    modifier = Modifier
-//                        .fillMaxWidth(),
-//                    horizontalArrangement = Arrangement.End
-//                ) {
-//                    TextButton(
-//                        onClick = {
-//                            onSetDefault(paymentMethod)
-//                        },
-//                        enabled = (!isDefault && !loading),
-//                    ) {
-//                        Text(
-//                            text = "设为默认",
-//                        )
-//                    }
-//                }
+            val card = pm.card
 
-            BankCard(
-                brand = card.brand,
-                last4 = card.last4,
-                expYear = card.expYear,
-                expMonth = card.expMonth,
-            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(),
+            ) {
+
+                BankCard(
+                    brand = card.brand.replaceFirstChar {
+                        if (it.isLowerCase()) {
+                            it.titlecase(Locale.getDefault())
+                        } else {
+                            it.toString()
+                        }
+                    },
+                    last4 = card.last4,
+                    expYear = card.expYear,
+                    expMonth = card.expMonth,
+                )
+
+                if (!isDefault) {
+                    OTextButton(
+                        onClick = { onSetDefault(pm) },
+                        text = "设为默认",
+                        modifier = Modifier.align(Alignment.TopEnd),
+                        enabled = !loading
+                    )
+                }
+            }
 
             Spacer(modifier = Modifier.height(Dimens.dp8))
         }
@@ -56,5 +67,42 @@ fun StripeWalletScreen(
             enabled = !loading,
             onClick = onAddCard,
         )
+    }
+}
+
+@Composable
+fun AlertModifySubsPaymentMethod(
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+) {
+    SimpleDialog(
+        title = "更改订阅默认支付方式",
+        body = "是否更改当前订阅的默认支付方式？这将影响自动续订日后的支付，请确保该支付方式有效。",
+        onDismiss = onDismiss,
+        onConfirm = onConfirm,
+        confirmText = "确认更改"
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewStripeWalletScreen() {
+    StripeWalletScreen(
+        loading = false,
+        paymentMethod = StripePaymentMethod(
+            id = "test-id",
+            customerId = "test-id",
+            card = StripePaymentCard(
+                brand = "visa",
+                country = "",
+                expMonth = 10,
+                expYear = 2025,
+                last4 = "4242"
+            )
+        ),
+        isDefault = false,
+        onSetDefault = {}
+    ) {
+
     }
 }
