6bea179d25709418c2e6023eb91546703e5c21eb
weiguo.ni <neefrankie@gmail.com>
2022-06-08 00:51:33 +0800

    refactor: relocate RightArrow

--- app/src/main/java/com/ft/ftchinese/ui/components/ClickableRow.kt
+++ app/src/main/java/com/ft/ftchinese/ui/components/ClickableRow.kt
@@ -5,23 +5,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.ft.ftchinese.R
 import com.ft.ftchinese.ui.theme.Dimens
-
-@Composable
-fun RightArrow() {
-    Icon(
-        painter = painterResource(id = R.drawable.ic_keyboard_arrow_right_gray_24dp),
-        contentDescription = "Details"
-    )
-}
 
 @Composable
 fun ClickableRow(
