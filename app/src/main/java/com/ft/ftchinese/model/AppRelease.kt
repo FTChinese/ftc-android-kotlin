409cfa6742f276d4080f65f05fe61d30003f80de
weiguo.ni <neefrankie@gmail.com>
2022-06-07 22:58:31 +0800

    feat: AppDownloaded

--- app/src/main/java/com/ft/ftchinese/model/AppRelease.kt
+++ app/src/main/java/com/ft/ftchinese/model/AppRelease.kt
@@ -9,10 +9,19 @@ data class AppRelease(
     val versionCode: Int,
     val body: String? = null,
     val apkUrl: String = "",
-) {
+) : Comparable<AppRelease> {
     val isNew: Boolean
         get() = versionCode > BuildConfig.VERSION_CODE
 
     val isValid: Boolean
         get() = apkUrl.startsWith("http")
+
+    override fun compareTo(other: AppRelease): Int {
+        return versionCode - other.versionCode
+    }
 }
+
+data class AppDownloaded(
+    val release: AppRelease,
+    val downloadId: Long,
+)
