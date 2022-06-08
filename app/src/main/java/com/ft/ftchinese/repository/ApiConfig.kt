960cf60ddda5a3ecb6c8b5fac441cf5e45ddd62b
weiguo.ni <neefrankie@gmail.com>
2022-06-08 09:09:51 +0800

    feat: ApiMode

--- app/src/main/java/com/ft/ftchinese/repository/ApiConfig.kt
+++ app/src/main/java/com/ft/ftchinese/repository/ApiConfig.kt
@@ -5,9 +5,16 @@ import com.ft.ftchinese.BuildConfig
 private const val devIP = "http://192.168.0.3"
 private const val devPort = "8206"
 
+enum class ApiMode {
+    Debug,
+    Sandbox,
+    Live,
+}
+
 data class ApiConfig(
     val baseUrl: String,
     val accessToken: String,
+    val mode: ApiMode,
 ) {
     companion object {
         private const val flavorWx = "wechat"
@@ -16,19 +23,22 @@ data class ApiConfig(
         // Used for development.
         private val debugApi = ApiConfig(
             baseUrl = "$devIP:${devPort}",
-            accessToken = BuildConfig.ACCESS_TOKEN_TEST
+            accessToken = BuildConfig.ACCESS_TOKEN_TEST,
+            mode = ApiMode.Debug
         )
 
         // Production server with sandbox data
         private val releaseSandboxApi = ApiConfig(
             baseUrl = BuildConfig.API_SUBS_SANDBOX,
-            accessToken = BuildConfig.ACCESS_TOKEN_LIVE
+            accessToken = BuildConfig.ACCESS_TOKEN_LIVE,
+            mode = ApiMode.Sandbox
         )
 
         // Production server with live data
         private val releaseLiveApi = ApiConfig(
             baseUrl = BuildConfig.API_SUBS_LIVE,
-            accessToken = BuildConfig.ACCESS_TOKEN_LIVE
+            accessToken = BuildConfig.ACCESS_TOKEN_LIVE,
+            mode = ApiMode.Live
         )
 
         val ofAuth = if (BuildConfig.DEBUG || BuildConfig.FLAVOR == flavorWx) {
@@ -40,16 +50,19 @@ data class ApiConfig(
         @JvmStatic
         fun ofSubs(isTest: Boolean): ApiConfig {
             return when {
-                // For Build Variants wechatRelease
+                // For Build Variants wechatRelease,
+                // use local ip so that we could debug requests.
                 BuildConfig.FLAVOR == flavorWx  -> {
                     debugApi
                 }
                 // For other Build Variants xxxDebug
                 BuildConfig.DEBUG -> {
-                    // For Build Variants stripeDebug. Test account-only
+                    // For Build Variants stripeDebug and test account,
+                    // requests sent to sandbox api.
                     if (isTest && BuildConfig.FLAVOR == flavorStripe) {
                         releaseSandboxApi
                     } else {
+                        // otherwise local ip
                         debugApi
                     }
                 }
---
+++ app/src/main/java/com/ft/ftchinese/ui/components/Mode.kt
@@ -0,0 +1,39 @@
+package com.ft.ftchinese.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.ft.ftchinese.repository.ApiMode
+import com.ft.ftchinese.ui.theme.Dimens
+import com.ft.ftchinese.ui.theme.OColor
+
+@Composable
+fun Mode(
+    mode: ApiMode
+) {
+    Text(
+        text = mode.name,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(OColor.claret)
+            .padding(Dimens.dp8),
+        style = MaterialTheme.typography.h6,
+        color = OColor.white
+    )
+    Spacer(modifier = Modifier.height(Dimens.dp8))
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMode() {
+    Mode(mode = ApiMode.Sandbox)
+}
