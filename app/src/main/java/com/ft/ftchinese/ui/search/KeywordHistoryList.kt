1ae3c8a60a7d3d2f30d0ba6162e736e55f020155
weiguo.ni <neefrankie@gmail.com>
2022-06-10 00:16:08 +0800

    refactor: keywords list

--- app/src/main/java/com/ft/ftchinese/ui/search/KeywordHistoryList.kt
+++ app/src/main/java/com/ft/ftchinese/ui/search/KeywordHistoryList.kt
@@ -1,44 +1,50 @@
 package com.ft.ftchinese.ui.search
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Chip
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.ft.ftchinese.database.SearchEntry
 import com.ft.ftchinese.ui.components.IconDelete
+import com.ft.ftchinese.ui.components.SlimIconButton
 import com.ft.ftchinese.ui.components.SubHeading2
 import com.ft.ftchinese.ui.theme.Dimens
+import com.ft.ftchinese.ui.theme.OColor
 import com.google.accompanist.flowlayout.FlowRow
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun KeywordHistoryList(
-    entries: List<SearchEntry>,
+    modifier: Modifier = Modifier,
+    keywords: List<String>,
     onClear: () -> Unit,
     onClick: (String) -> Unit
 ) {
 
     Column(
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .background(OColor.white)
+            .padding(Dimens.dp16)
+            .fillMaxSize()
+            .then(modifier)
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
+
             SubHeading2(
                 text = "搜索历史",
                 modifier = Modifier.weight(1f),
                 textAlign = TextAlign.Start
             )
-            IconButton(
-                onClick = onClear
-            ) {
+
+            SlimIconButton(onClick = onClear) {
                 IconDelete()
             }
         }
@@ -46,17 +52,17 @@ fun KeywordHistoryList(
         FlowRow(
             modifier = Modifier.fillMaxWidth()
         ) {
-            entries.forEach { entry ->
+            keywords.forEach { kw ->
 
                 Chip(
                     onClick = {
-                        onClick(entry.keyword)
+                        onClick(kw)
                     }
                 ) {
-                    Text(text = entry.keyword)
+                    Text(text = kw)
                 }
 
-                Spacer(modifier = Modifier.width(Dimens.dp4))
+                Spacer(modifier = Modifier.width(Dimens.dp8))
             }
         }
     }
@@ -67,43 +73,18 @@ fun KeywordHistoryList(
 @Composable
 fun PreviewKeywordHistoryList() {
     KeywordHistoryList(
-        entries = listOf(
-            SearchEntry(
-                keyword = "乌克兰"
-            ),
-            SearchEntry(
-                keyword = "俄罗斯"
-            ),
-            SearchEntry(
-                keyword = "北约"
-            ),
-            SearchEntry(
-                keyword = "拜登",
-            ),
-            SearchEntry(
-                keyword = "约翰逊",
-            ),
-            SearchEntry(
-                keyword = "通胀",
-            ),
-            SearchEntry(
-                keyword = "乌克兰"
-            ),
-            SearchEntry(
-                keyword = "俄罗斯"
-            ),
-            SearchEntry(
-                keyword = "北约"
-            ),
-            SearchEntry(
-                keyword = "拜登",
-            ),
-            SearchEntry(
-                keyword = "约翰逊",
-            ),
-            SearchEntry(
-                keyword = "通胀",
-            )
+        keywords = listOf(
+            "caravan",
+            "magna cum laude",
+            "samaritan",
+            "aneurysm",
+            "gird",
+            "aphasia",
+            "saber-rattling",
+            "conviviality",
+            "walt-out",
+            "shrew",
+            "vendetta"
         ),
         onClear = {},
         onClick = {}
