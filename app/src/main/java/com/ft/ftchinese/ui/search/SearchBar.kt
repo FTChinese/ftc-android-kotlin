157ed509a8ab8dac0ff5841c1a57ad4bbdf297ce
weiguo.ni <neefrankie@gmail.com>
2022-06-10 00:14:49 +0800

    refactor: SearchTextField

--- app/src/main/java/com/ft/ftchinese/ui/search/SearchBar.kt
+++ app/src/main/java/com/ft/ftchinese/ui/search/SearchBar.kt
@@ -1,40 +1,30 @@
 package com.ft.ftchinese.ui.search
 
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.Surface
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.tooling.preview.Preview
+import com.ft.ftchinese.ui.components.SearchInputState
+import com.ft.ftchinese.ui.components.SearchTextField
+import com.ft.ftchinese.ui.components.rememberSearchInputState
 import com.ft.ftchinese.ui.theme.OColor
 
-@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun SearchBar(
-    onBack: () -> Unit
+    state: SearchInputState,
+    onSubmit: () -> Unit,
+    onBack: () -> Unit,
 ) {
-    val focusManager = LocalFocusManager.current
-    val keyboardController = LocalSoftwareKeyboardController.current
 
     TopAppBar(
         backgroundColor = OColor.wheat
     ) {
        IconButton(
            onClick = {
-               focusManager.clearFocus()
-               keyboardController?.hide()
                onBack()
            }
        ) {
@@ -45,33 +35,22 @@ fun SearchBar(
            )
        }
 
-
+        SearchTextField(
+            state = state,
+            onSubmit = {
+                onSubmit()
+            }
+        )
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview(showBackground = true)
 @Composable
-fun SearchTextField(
-    focused: Boolean,
-    modifier: Modifier = Modifier
-) {
-    val focusRequester = remember {
-        FocusRequester()
-    }
-
-    Surface(
-        modifier = modifier
-            .then(
-                Modifier
-                    .height(56.dp)
-                    .padding(
-                        top = 8.dp,
-                        bottom = 8.dp,
-                        start = if (!focused) 16.dp else 0.dp,
-                        end = 16.dp
-                    )
-            ),
-        color = Color(0xffF5F5F5),
-        shape = RoundedCornerShape(percent = 50),
+fun PreviewSearchBar() {
+    SearchBar(
+        state = rememberSearchInputState(),
+        onSubmit = {  }
     ) {
 
     }
