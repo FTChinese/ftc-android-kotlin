1e6983d60cd2a3f20a255bf345835d2ee98d940f
weiguo.ni <neefrankie@gmail.com>
2022-06-10 17:14:34 +0800

    feat: filter keywords related to bitcoins for huawei

--- app/src/main/java/com/ft/ftchinese/repository/flavors.kt
+++ app/src/main/java/com/ft/ftchinese/repository/flavors.kt
@@ -4,16 +4,29 @@ import com.ft.ftchinese.BuildConfig
 
 const val defaultFlavor = "an_ftc"
 
+const val flavorHuawei = "huawei"
+
 val flavors = mapOf(
     "play" to "play_store",
-    "xiaomi" to "an_xiaomi",
-    "huawei" to "an_huawei",
-    "baidu" to "an_baidu",
+    flavorHuawei to "an_huawei",
     "sanliuling" to "an_360shouji",
     "ftc" to defaultFlavor,
-    "tencent" to "an_tencent",
-    "samsung" to "an_samsung",
     "oppo" to "an_oppo",
+    "vivo" to "an_vivo",
 )
 
 val currentFlavor = flavors[BuildConfig.FLAVOR] ?: defaultFlavor
+
+val forbiddenKeywords = mapOf(
+    "比特币" to 1,
+    "虚拟币" to 1,
+    "加密货币" to 1,
+)
+
+fun isKeywordForbidden(kw: String): Boolean {
+    if (BuildConfig.FLAVOR != flavorHuawei) {
+        return false
+    }
+
+    return forbiddenKeywords.containsKey(kw)
+}
