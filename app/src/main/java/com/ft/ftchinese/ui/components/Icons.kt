4a6b030dafa0575d02445a71d2d78d1346b52fbf
weiguo.ni <neefrankie@gmail.com>
2022-06-13 14:44:33 +0800

    refactor: removed IconCheck

--- app/src/main/java/com/ft/ftchinese/ui/components/Icons.kt
+++ app/src/main/java/com/ft/ftchinese/ui/components/Icons.kt
@@ -1,17 +1,11 @@
 package com.ft.ftchinese.ui.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Icon
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.LocalContentColor
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import com.ft.ftchinese.R
 
 @Composable
@@ -88,26 +82,3 @@ fun IconSearch(
     )
 }
 
-@Composable
-fun IconCheck(
-    checked: Boolean,
-    tint: Color = LocalContentColor.current.copy(alpha = LocalContentAlpha.current)
-) {
-    Box(
-        modifier = Modifier
-            .width(24.dp)
-            .height(24.dp)
-    ) {
-
-        Icon(
-            painter = painterResource(id = if (checked) {
-                R.drawable.ic_baseline_check_circle_outline_24
-            } else {
-                R.drawable.ic_baseline_radio_button_unchecked_24
-            }),
-            contentDescription = "Checked",
-            modifier = Modifier.align(Alignment.Center),
-            tint = tint
-        )
-    }
-}
