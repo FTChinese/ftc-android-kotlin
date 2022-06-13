b92857f30252437b75073a3ad3e2bf9120faeab5
weiguo.ni <neefrankie@gmail.com>
2022-06-13 16:47:51 +0800

    feat: compose ui version of BottomTool

---
+++ app/src/main/java/com/ft/ftchinese/ui/article/BottomTool.kt
@@ -0,0 +1,71 @@
+package com.ft.ftchinese.ui.article
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.ft.ftchinese.R
+import com.ft.ftchinese.ui.components.SlimIconButton
+import com.ft.ftchinese.ui.components.SlimIconToggleButton
+import com.ft.ftchinese.ui.theme.Dimens
+import com.ft.ftchinese.ui.theme.OColor
+
+@Composable
+fun BottomTool(
+    bookmarked: Boolean,
+    onBookmark: (Boolean) -> Unit,
+    onShare: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .background(OColor.wheat)
+            .padding(
+                horizontal = Dimens.dp16,
+                vertical = Dimens.dp8,
+            )
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        SlimIconButton(
+            onClick = onShare
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_share_black_24dp),
+                contentDescription = "Share",
+                tint = OColor.teal
+            )
+        }
+
+        SlimIconToggleButton(
+            checked = bookmarked,
+            onCheckedChange = onBookmark,
+        ) {
+            Icon(
+                painter = painterResource(id = if (bookmarked) {
+                    R.drawable.ic_bookmark_black_24dp
+                } else {
+                    R.drawable.ic_bookmark_border_black_24dp
+                }),
+                contentDescription = "Bookmark",
+                tint = OColor.teal
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewBottomTool() {
+    BottomTool(
+        bookmarked = true,
+        onBookmark = {}
+    ) {
+
+    }
+}
