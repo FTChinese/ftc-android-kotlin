1ecadf321e1a53f7e66f95435e1c36bca7c12a80
weiguo.ni <neefrankie@gmail.com>
2022-06-13 14:53:31 +0800

    refactor: button styles

--- app/src/main/java/com/ft/ftchinese/ui/theme/Theme.kt
+++ app/src/main/java/com/ft/ftchinese/ui/theme/Theme.kt
@@ -1,9 +1,10 @@
 package com.ft.ftchinese.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 
 private val LightColors = lightColors(
     background = OColor.paper,
@@ -24,34 +25,3 @@ fun OTheme(
     )
 }
 
-object OButton {
-    @Composable
-    fun primaryButtonColors(
-        backgroundColor: Color = OColor.teal,
-        contentColor: Color = OColor.white,
-    ): ButtonColors = ButtonDefaults.buttonColors(
-        backgroundColor = backgroundColor,
-        contentColor = contentColor,
-        disabledBackgroundColor = backgroundColor.copy(alpha = 0.4F)
-    )
-
-    @Composable
-    fun outlinedColors(
-        backgroundColor: Color = Color.Transparent,
-        contentColor: Color = OColor.teal,
-    ): ButtonColors = ButtonDefaults.outlinedButtonColors(
-        backgroundColor = backgroundColor,
-        contentColor = contentColor,
-        disabledContentColor = contentColor.copy(alpha = 0.4F)
-    )
-
-    @Composable
-    fun textColors(
-        backgroundColor: Color = Color.Transparent,
-        contentColor: Color = OColor.teal,
-    ): ButtonColors = ButtonDefaults.textButtonColors(
-        backgroundColor = backgroundColor,
-        contentColor = contentColor,
-        disabledContentColor = contentColor.copy(alpha = 0.4F),
-    )
-}
