ca0e2cb158ad596c5c132e8853512e2c3d63b35e
weiguo.ni <neefrankie@gmail.com>
2022-06-13 16:47:01 +0800

    refactor: add namedId field

--- app/src/main/java/com/ft/ftchinese/model/content/Language.kt
+++ app/src/main/java/com/ft/ftchinese/model/content/Language.kt
@@ -1,9 +1,15 @@
 package com.ft.ftchinese.model.content
 
-enum class Language(val symbol: String) {
-    CHINESE("cn"),
-    ENGLISH("en"),
-    BILINGUAL("bi");
+import androidx.annotation.StringRes
+import com.ft.ftchinese.R
+
+enum class Language(
+    val symbol: String,
+    @StringRes val nameId: Int
+) {
+    CHINESE("cn", R.string.titlebar_cn),
+    ENGLISH("en", R.string.titlebar_en),
+    BILINGUAL("bi", R.string.titlebar_bilingual);
 
     fun aiAudioPathSuffix(): String {
         return when (this) {
