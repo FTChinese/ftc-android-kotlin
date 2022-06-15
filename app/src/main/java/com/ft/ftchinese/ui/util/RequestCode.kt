0591a31ef5e8949cb60cfec6d6089031b7e97c2c
weiguo.ni <neefrankie@gmail.com>
2022-06-15 16:18:08 +0800

    refactor: cleanup

--- app/src/main/java/com/ft/ftchinese/ui/util/RequestCode.kt
+++ app/src/main/java/com/ft/ftchinese/ui/util/RequestCode.kt
@@ -2,12 +2,6 @@ package com.ft.ftchinese.ui.util
 
 object RequestCode {
     const val SIGN_IN = 1
-    const val SIGN_UP = 2
-    const val PAYMENT = 3
-    const val LINK = 4
-    const val UNLINK = 5
-    const val PASSWORD_RESET = 6
     const val MEMBER_REFRESHED = 7
     const val SELECT_SOURCE = 55
-    const val PERMISSIONS = 1002
 }
