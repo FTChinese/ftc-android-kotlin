a10a50fd12365155da2cc45bf7fe619e35c713cc
weiguo.ni <neefrankie@gmail.com>
2022-06-15 16:25:09 +0800

    refactor: delete files

--- app/src/main/java/com/ft/ftchinese/ui/article/BookmarkState.kt
+++
@@ -1,9 +0,0 @@
-package com.ft.ftchinese.ui.article
-
-data class BookmarkState(
-    val isStarring: Boolean,
-    // Used in snackbar when user clicked star icon.
-    // Upon initial loading, the state is retrieved from DB
-    // and you should not show any message.
-    val message: Int? = null,
-)
