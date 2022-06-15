b867f71ae34c52f22555c4dbf36cad8ca04904a8
weiguo.ni <neefrankie@gmail.com>
2022-06-15 16:18:28 +0800

    feat: ComposeChromeClient

--- app/src/main/java/com/ft/ftchinese/ui/web/ChromeClient.kt
+++ app/src/main/java/com/ft/ftchinese/ui/web/ChromeClient.kt
@@ -3,8 +3,17 @@ package com.ft.ftchinese.ui.web
 import android.util.Log
 import android.webkit.ConsoleMessage
 import android.webkit.WebChromeClient
+import com.google.accompanist.web.AccompanistWebChromeClient
 
 class ChromeClient : WebChromeClient() {
+
+    override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
+        Log.i("ChromeClient", "${consoleMessage?.lineNumber()} : ${consoleMessage?.message()}")
+        return true
+    }
+}
+
+class ComposeChromeClient : AccompanistWebChromeClient() {
 
     override fun onConsoleMessage(consoleMessage: ConsoleMessage?): Boolean {
         Log.i("ChromeClient", "${consoleMessage?.lineNumber()} : ${consoleMessage?.message()}")
