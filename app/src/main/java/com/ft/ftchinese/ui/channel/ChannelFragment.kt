1988ebf991fa5b0d8b90d2471c7abb71156fa05b
weiguo.ni <neefrankie@gmail.com>
2022-06-15 16:30:32 +0800

    refactor: switch to compose ui

--- app/src/main/java/com/ft/ftchinese/ui/channel/ChannelFragment.kt
+++ app/src/main/java/com/ft/ftchinese/ui/channel/ChannelFragment.kt
@@ -1,77 +1,23 @@
 package com.ft.ftchinese.ui.channel
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
 import androidx.core.os.bundleOf
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import androidx.work.Data
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkManager
-import androidx.work.workDataOf
-import com.ft.ftchinese.BuildConfig
-import com.ft.ftchinese.R
-import com.ft.ftchinese.databinding.FragmentChannelBinding
+import androidx.fragment.app.Fragment
 import com.ft.ftchinese.model.content.ChannelSource
-import com.ft.ftchinese.model.content.OpenGraphMeta
-import com.ft.ftchinese.model.reader.Account
-import com.ft.ftchinese.repository.Config
-import com.ft.ftchinese.service.*
-import com.ft.ftchinese.store.SessionManager
-import com.ft.ftchinese.ui.web.Paging
-import com.ft.ftchinese.ui.base.ScopedFragment
-import com.ft.ftchinese.ui.base.ToastMessage
-import com.ft.ftchinese.ui.base.toast
-import com.ft.ftchinese.ui.web.*
-import com.ft.ftchinese.ui.webpage.WVClient
-import com.ft.ftchinese.ui.webpage.configWebView
-import kotlinx.coroutines.cancel
-import java.util.*
-import kotlin.properties.Delegates
+import com.ft.ftchinese.ui.theme.OTheme
 
 /**
  * Hosted inside [TabPagerAdapter] or [ChannelActivity]
  * when used to handle pagination.
  */
-class ChannelFragment : ScopedFragment(),
-    SwipeRefreshLayout.OnRefreshListener {
-
-    /**
-     * Meta data about current page: the tab's title, where to load data, etc.
-     * Passed in when the fragment is created.
-     */
-    private var channelSource: ChannelSource? = null
-
-    private lateinit var sessionManager: SessionManager
-    private lateinit var binding: FragmentChannelBinding
-
-    private lateinit var channelViewModel: ChannelViewModel
-
-    // Record when this page starts to load.
-    private var start by Delegates.notNull<Long>()
-
-    /**
-     * Bind listeners here.
-     */
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-
-        sessionManager = SessionManager.getInstance(context)
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // Get metadata about current tab
-        channelSource = arguments?.getParcelable(ARG_CHANNEL_SOURCE) ?: return
-
-        start = Date().time / 1000
-    }
+class ChannelFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -79,159 +25,19 @@ class ChannelFragment : ScopedFragment(),
         savedInstanceState: Bundle?
     ): View {
 
-        super.onCreateView(inflater, container, savedInstanceState)
-        binding = DataBindingUtil.inflate(
-                inflater,
-                R.layout.fragment_channel,
-                container,
-                false)
+        val channelSource = arguments?.getParcelable<ChannelSource>(ARG_CHANNEL_SOURCE)
 
-        // Setup swipe refresh listener
-        binding.swipeRefresh.setOnRefreshListener(this)
+        return ComposeView(requireContext()).apply {
 
-        return binding.root
-    }
+            setContent {
+                channelSource?.let {
+                    ChannelFragScreen(
+                        source = it
+                    )
+                }
 
-    private val clientListener = object : WebViewListener {
-        override fun onOpenGraph(openGraph: OpenGraphMeta) {
-
-        }
-
-        override fun onChannelSelected(source: ChannelSource) {
-            ChannelActivity.start(
-                context,
-                source.withParentPerm(
-                    channelSource?.permission
-                )
-            )
-        }
-
-        override fun onPagination(paging: Paging) {
-            val currentChannel = channelSource ?: return
-
-            val pagedSource = currentChannel
-                .withPagination(paging.key, paging.page)
-
-            if (currentChannel.isSamePage(pagedSource)) {
-                return
-            }
-
-            ChannelActivity.start(context, pagedSource)
-        }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        channelViewModel = ViewModelProvider(this)[ChannelViewModel::class.java]
-
-        // Network status.
-        connectionLiveData.observe(viewLifecycleOwner) {
-            channelViewModel.isNetworkAvailable.value = it
-        }
-
-        setupViewModel()
-
-        configWebView(
-            webView = binding.webView,
-            jsInterface = JsInterface(
-                BaseJsEventListener(
-                    context = requireContext(),
-                    channelSource = channelSource,
-                )
-            ),
-            client = WVClient(
-                context = requireContext(),
-                listener = clientListener
-            )
-        )
-        channelSource?.let {
-            channelViewModel.load(it, sessionManager.loadAccount())
-        }
-    }
-
-    private fun setupViewModel() {
-
-        channelViewModel.progressLiveData.observe(viewLifecycleOwner) {
-            binding.inProgress = it
-        }
-
-        channelViewModel.refreshingLiveData.observe(viewLifecycleOwner) {
-            binding.swipeRefresh.isRefreshing = it
-            context?.toast(R.string.refresh_success)
-        }
-
-        channelViewModel.errorLiveData.observe(viewLifecycleOwner) {
-            when (it) {
-                is ToastMessage.Resource -> context?.toast(it.id)
-                is ToastMessage.Text -> context?.toast(it.text)
             }
         }
-
-        channelViewModel.htmlLiveData.observe(viewLifecycleOwner) {
-            load(it)
-        }
-    }
-
-    override fun onRefresh() {
-        context?.toast(R.string.refreshing_data)
-        channelSource?.let {
-            channelViewModel.refresh(it, sessionManager.loadAccount())
-        }
-    }
-
-    private fun load(html: String) {
-        if (BuildConfig.DEBUG) {
-            Log.i(TAG, "Loading web page to web view")
-        }
-
-        binding.webView.loadDataWithBaseURL(
-            Config.discoverServer(sessionManager.loadAccount()),
-            html,
-            "text/html",
-            null,
-            null)
-    }
-
-    override fun onPause() {
-        super.onPause()
-        cancel()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        cancel()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-
-        val account = sessionManager.loadAccount() ?: return
-
-        if (account.id == "") {
-            return
-        }
-
-        sendReadLen(account)
-    }
-
-    private fun sendReadLen(account: Account) {
-        val data: Data = workDataOf(
-            KEY_DUR_URL to "/android/channel/${channelSource?.title}",
-            KEY_DUR_REFER to "http://www.ftchinese.com/",
-            KEY_DUR_START to start,
-            KEY_DUR_END to Date().time / 1000,
-            KEY_DUR_USER_ID to account.id
-        )
-
-        val lenWorker = OneTimeWorkRequestBuilder<ReadingDurationWorker>()
-            .setInputData(data)
-            .build()
-
-        context?.run {
-            WorkManager.getInstance(this).enqueue(lenWorker)
-        }
-
     }
 
     companion object {
@@ -240,7 +46,7 @@ class ChannelFragment : ScopedFragment(),
          * fragment.
          */
         private const val ARG_CHANNEL_SOURCE = "arg_channel_source"
-        private const val TAG = "ChannelFragment"
+//        private const val TAG = "ChannelFragment"
 
         /**
          * Returns a new instance of this fragment for the given section
@@ -253,4 +59,21 @@ class ChannelFragment : ScopedFragment(),
     }
 }
 
+@Composable
+private fun ChannelFragScreen(
+    source: ChannelSource,
+) {
+    val scaffoldState = rememberScaffoldState()
+
+    OTheme {
+        Scaffold(
+            scaffoldState = scaffoldState
+        ) {
+            ChannelActivityScreen(
+                scaffoldState = scaffoldState,
+                channelSource = source
+            )
+        }
+    }
+}
 
