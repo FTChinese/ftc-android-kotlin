8cce2807c575e128e36228167db774e11b5042f3
weiguo.ni <neefrankie@gmail.com>
2022-06-15 16:19:54 +0800

    refactor: WebPageActivityScreen

--- app/src/main/java/com/ft/ftchinese/ui/webpage/WebpageActivity.kt
+++ app/src/main/java/com/ft/ftchinese/ui/webpage/WebpageActivity.kt
@@ -2,25 +2,10 @@ package com.ft.ftchinese.ui.webpage
 
 import android.content.Context
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.browser.customtabs.CustomTabsIntent
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.rememberScaffoldState
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import com.ft.ftchinese.model.legal.WebpageMeta
-import com.ft.ftchinese.ui.components.MenuOpenInBrowser
-import com.ft.ftchinese.ui.components.ProgressLayout
-import com.ft.ftchinese.ui.components.Toolbar
-import com.ft.ftchinese.ui.theme.OTheme
-import com.ft.ftchinese.ui.web.ComposeWebView
-import com.ft.ftchinese.ui.web.rememberWebViewClient
-import com.google.accompanist.web.rememberWebViewState
 
 class WebpageActivity : ComponentActivity() {
 
@@ -49,52 +34,6 @@ class WebpageActivity : ComponentActivity() {
 
             }
             context.startActivity(intent)
-        }
-    }
-}
-
-@Composable
-fun WebpageActivityScreen(
-    pageMeta: WebpageMeta,
-    onExit: () -> Unit
-) {
-    val context = LocalContext.current
-    val scaffoldState = rememberScaffoldState()
-    val webViewState = rememberWebViewState(url = pageMeta.url)
-
-    OTheme {
-        Scaffold(
-            topBar = {
-                Toolbar(
-                    heading = pageMeta.title,
-                    onBack = onExit,
-                    actions = {
-                        if (pageMeta.showMenu) {
-                            MenuOpenInBrowser {
-                                CustomTabsIntent
-                                    .Builder()
-                                    .build()
-                                    .launchUrl(
-                                        context,
-                                        Uri.parse(pageMeta.url)
-                                    )
-                            }
-                        }
-                    }
-                )
-            },
-            scaffoldState = scaffoldState
-        ) { innerPadding ->
-
-            ProgressLayout(
-                loading = webViewState.isLoading,
-                modifier = Modifier.padding(innerPadding)
-            ) {
-                ComposeWebView(
-                    wvState = webViewState,
-                    webClient = rememberWebViewClient()
-                )
-            }
         }
     }
 }
