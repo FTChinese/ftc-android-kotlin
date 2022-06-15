060869c483264d5c2458c13bd188e2c3bb08ce17
weiguo.ni <neefrankie@gmail.com>
2022-06-15 16:15:33 +0800

    feat: remembered tracker

---
+++ app/src/main/java/com/ft/ftchinese/ui/components/Tracker.kt
@@ -0,0 +1,71 @@
+package com.ft.ftchinese.ui.components
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
+import com.ft.ftchinese.model.content.ChannelSource
+import com.ft.ftchinese.model.content.Teaser
+import com.ft.ftchinese.model.reader.Account
+import com.ft.ftchinese.repository.Config
+import com.ft.ftchinese.service.*
+import com.ft.ftchinese.tracking.StatsTracker
+import java.util.*
+
+@Composable
+fun rememberTracker(
+    context: Context = LocalContext.current
+) = remember {
+    StatsTracker.getInstance(context)
+}
+
+@Composable
+fun rememberStartTime() = remember {
+    Date().time / 1000
+}
+
+fun sendChannelReadLen(
+    context: Context,
+    userId: String,
+    startTime: Long,
+    source: ChannelSource,
+) {
+    val data: Data = workDataOf(
+        KEY_DUR_URL to "/android/channel/${source.title}",
+        KEY_DUR_REFER to "http://www.ftchinese.com/",
+        KEY_DUR_START to startTime,
+        KEY_DUR_END to Date().time / 1000,
+        KEY_DUR_USER_ID to userId
+    )
+
+    val lenWorker = OneTimeWorkRequestBuilder<ReadingDurationWorker>()
+        .setInputData(data)
+        .build()
+
+    WorkManager.getInstance(context).enqueue(lenWorker)
+}
+
+fun sendArticleReadLen(
+    context: Context,
+    account: Account,
+    teaser: Teaser,
+    startAt: Long
+) {
+    val data: Data = workDataOf(
+        KEY_DUR_URL to "/android/${teaser.type}/${teaser.id}/${teaser.title}",
+        KEY_DUR_REFER to Config.discoverServer(account),
+        KEY_DUR_START to startAt,
+        KEY_DUR_END to Date().time / 1000,
+        KEY_DUR_USER_ID to account.id
+    )
+
+    val lenWorker = OneTimeWorkRequestBuilder<ReadingDurationWorker>()
+        .setInputData(data)
+        .build()
+
+    WorkManager.getInstance(context).enqueue(lenWorker)
+}
