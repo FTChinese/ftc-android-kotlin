4f15df05b0c5920fb334c42b39825706192da208
weiguo.ni <neefrankie@gmail.com>
2022-06-15 16:15:51 +0800

    feat: remembered WxApi

---
+++ app/src/main/java/com/ft/ftchinese/ui/components/WxApi.kt
@@ -0,0 +1,47 @@
+package com.ft.ftchinese.ui.components
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import com.ft.ftchinese.BuildConfig
+import com.ft.ftchinese.model.reader.WxOAuth
+import com.ft.ftchinese.model.reader.WxOAuthKind
+import com.tencent.mm.opensdk.modelmsg.SendAuth
+import com.tencent.mm.opensdk.openapi.IWXAPI
+import com.tencent.mm.opensdk.openapi.WXAPIFactory
+
+@Composable
+fun rememberWxApi(
+    context: Context = LocalContext.current
+) = remember {
+    WXAPIFactory.createWXAPI(
+        context,
+        BuildConfig.WX_SUBS_APPID,
+        false
+    )
+}
+
+fun launchWxLogin(
+    wxApi: IWXAPI
+) {
+    val stateCode = WxOAuth.generateStateCode(WxOAuthKind.LOGIN)
+
+    val req = SendAuth.Req()
+    req.scope = WxOAuth.SCOPE
+    req.state = stateCode
+
+    wxApi.sendReq(req)
+}
+
+fun launchWxForLink(
+    wxApi: IWXAPI
+) {
+    val stateCode = WxOAuth.generateStateCode(WxOAuthKind.LINK)
+
+    val req = SendAuth.Req()
+    req.scope = WxOAuth.SCOPE
+    req.state = stateCode
+
+    wxApi.sendReq(req)
+}
