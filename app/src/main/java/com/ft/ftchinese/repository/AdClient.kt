3f82812a500b34114157499f51585cc8621dc809
weiguo.ni <neefrankie@gmail.com>
2022-06-16 21:42:52 +0800

    feat: asyncSendImpression

--- app/src/main/java/com/ft/ftchinese/repository/AdClient.kt
+++ app/src/main/java/com/ft/ftchinese/repository/AdClient.kt
@@ -4,9 +4,12 @@ import android.net.Uri
 import com.ft.ftchinese.model.fetch.Fetch
 import com.ft.ftchinese.model.fetch.HttpResp
 import com.ft.ftchinese.model.splash.Schedule
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.util.*
 
 object AdClient {
+
     fun sendImpression(url: String) {
         val timestamp = Date().time / 1000
 
@@ -17,6 +20,18 @@ object AdClient {
             .toString()
 
         Fetch().get(bustedUrl).endText()
+    }
+
+    suspend fun asyncSendImpression(url: String): Result<Boolean> {
+        return try {
+            withContext(Dispatchers.IO) {
+                sendImpression(url)
+            }
+
+            Result.success(true)
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
     }
 
     fun fetchSchedule(url: String): HttpResp<Schedule> {
