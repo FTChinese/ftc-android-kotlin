1a78f346565d5b9e3e52cf0cac045922c3eee0aa
weiguo.ni <neefrankie@gmail.com>
2022-06-16 09:03:02 +0800

    refactor: removed ScreenshotParams

--- app/src/main/java/com/ft/ftchinese/ui/article/screenshot/ScreenshotMeta.kt
+++ app/src/main/java/com/ft/ftchinese/ui/article/screenshot/ScreenshotMeta.kt
@@ -1,18 +1,10 @@
 package com.ft.ftchinese.ui.article.screenshot
 
 import android.net.Uri
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class ScreenshotMeta(
     val imageUri: Uri,
     val title: String,
     val description: String,
-) : Parcelable
-
-data class ScreenshotParams(
-    val imageUrl: String,
-    val articleId: String,
-    val articleType: String,
 )
+
