120583fa14766cdebf024f29f5330892aa63ad6f
weiguo.ni <neefrankie@gmail.com>
2022-06-17 20:05:54 +0800

    feat: move feedback

--- app/src/main/java/com/ft/ftchinese/ui/settings/overview/PreferenceActivityScreen.kt
+++ app/src/main/java/com/ft/ftchinese/ui/settings/overview/PreferenceActivityScreen.kt
@@ -1,17 +1,33 @@
 package com.ft.ftchinese.ui.settings.overview
 
-import androidx.compose.material.ScaffoldState
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import com.ft.ftchinese.ui.settings.SettingScreen
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ft.ftchinese.R
+import com.ft.ftchinese.ui.base.toast
+import com.ft.ftchinese.ui.util.IntentsUtil
+import com.ft.ftchinese.viewmodel.UserViewModel
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun PreferenceActivityScreen(
+    userViewModel: UserViewModel = viewModel(),
     scaffoldState: ScaffoldState,
+    onLoggedOut: () -> Unit,
     onNavigateTo: (SettingScreen) -> Unit
 ) {
+    val context = LocalContext.current
+
+    val scope = rememberCoroutineScope()
+
     val prefState = rememberPrefState(
-        scaffoldState = scaffoldState
+        scaffoldState = scaffoldState,
+        scope = scope,
     )
 
     LaunchedEffect(key1 = Unit) {
@@ -19,21 +35,62 @@ fun PreferenceActivityScreen(
         prefState.countReadArticles()
     }
 
-    PreferenceScreen(
-        cacheSize = prefState.cacheSize,
-        readCount = prefState.readCount,
-        onClickRow = { rowId ->
-            when (rowId) {
-                SettingScreen.ClearCache -> {
-                    prefState.clearCache()
+    val rows = remember(
+        context.resources,
+        prefState.cacheSize,
+        prefState.readEntries
+    ) {
+        SettingRow.build(
+            context.resources,
+            cacheSize = prefState.cacheSize,
+            readCount = prefState.readEntries
+        )
+    }
+
+    val bottomSheetState = rememberModalBottomSheetState(
+        initialValue = ModalBottomSheetValue.Hidden,
+        skipHalfExpanded = false,
+    )
+
+    ModalBottomSheetLayout(
+        sheetContent = {
+            LogoutSheet {
+                userViewModel.logout()
+                scope.launch {
+                    bottomSheetState.hide()
                 }
-                SettingScreen.ClearHistory -> {
-                    prefState.truncateReadArticles()
-                }
-                else -> {
-                    onNavigateTo(rowId)
-                }
+                onLoggedOut()
             }
         }
-    )
+    ) {
+        PreferenceScreen(
+            rows = rows,
+            onClickRow = { rowId ->
+                when (rowId) {
+                    SettingScreen.ClearCache -> {
+                        prefState.clearCache()
+                    }
+                    SettingScreen.ClearHistory -> {
+                        prefState.truncateReadArticles()
+                    }
+                    SettingScreen.Feedback -> {
+                        val ok = IntentsUtil.sendFeedbackEmail(context = context)
+                        if (!ok) {
+                            context.toast(R.string.prompt_no_email_app)
+                        }
+                    }
+                    else -> {
+                        onNavigateTo(rowId)
+                    }
+                }
+            },
+            isLoggedIn = userViewModel.isLoggedIn,
+            onLogout = {
+                scope.launch {
+                    bottomSheetState.show()
+                }
+            }
+        )
+    }
+
 }
