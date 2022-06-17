9d56f2062671c6862fd67f6f6cb3e3baad1cc185
weiguo.ni <neefrankie@gmail.com>
2022-06-17 19:58:04 +0800

    refactor

--- app/src/main/java/com/ft/ftchinese/ui/components/ListItemTwoLine.kt
+++ app/src/main/java/com/ft/ftchinese/ui/components/ListItemTwoLine.kt
@@ -1,15 +1,13 @@
 package com.ft.ftchinese.ui.components
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ft.ftchinese.ui.theme.Dimens
-import com.ft.ftchinese.ui.theme.OColor
 
 @Composable
 fun ListItemTwoLine(
@@ -18,22 +16,13 @@ fun ListItemTwoLine(
     modifier: Modifier = Modifier
 ) {
     Column(
-        modifier = modifier
+        modifier = Modifier
+            .fillMaxWidth()
+            .then(modifier)
     ) {
-        Text(
-            text = primary,
-            style = MaterialTheme.typography.body1,
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Text(
-            text = secondary,
-            style = MaterialTheme.typography.body2,
-            color = OColor.black60,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = Dimens.dp4)
-        )
+        BodyText1(text = primary)
+        Spacer(modifier = Modifier.height(Dimens.dp4))
+        BodyText2(text = secondary)
     }
 }
 
