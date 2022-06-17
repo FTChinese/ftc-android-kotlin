8d12d06377467a0138dd30c4da02ef5e86c0b653
weiguo.ni <neefrankie@gmail.com>
2022-06-17 19:57:29 +0800

    feat: Figure

---
+++ app/src/main/java/com/ft/ftchinese/ui/components/Figure.kt
@@ -0,0 +1,45 @@
+package com.ft.ftchinese.ui.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.ft.ftchinese.R
+
+@Composable
+fun Figure(
+    imageUrl: String?,
+    modifier: Modifier = Modifier,
+    imageSize: Dp = 128.dp,
+    caption: String = "",
+) {
+    Column(
+        modifier = Modifier.fillMaxWidth().then(modifier)
+    ) {
+        AsyncImage(
+            model = imageUrl,
+            contentDescription = "",
+            placeholder = painterResource(id = R.drawable.ic_account_circle_black_24dp),
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .size(imageSize)
+                .clip(RoundedCornerShape(10.dp)),
+            contentScale = ContentScale.Fit,
+        )
+        Text(
+            text = caption,
+            style = MaterialTheme.typography.subtitle2,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+    }
+}
