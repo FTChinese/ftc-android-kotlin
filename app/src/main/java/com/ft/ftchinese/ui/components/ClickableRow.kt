dd6e4ec8a09c4a9973e2aaf099e76c83e4e8d1b1
weiguo.ni <neefrankie@gmail.com>
2022-06-17 19:57:16 +0800

    feat: add margin and padding

--- app/src/main/java/com/ft/ftchinese/ui/components/ClickableRow.kt
+++ app/src/main/java/com/ft/ftchinese/ui/components/ClickableRow.kt
@@ -1,33 +1,44 @@
 package com.ft.ftchinese.ui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ft.ftchinese.ui.theme.Dimens
+
+data class TableGroup<T>(
+    val header: String,
+    val rows: List<T>
+)
 
 @Composable
 fun ClickableRow(
     modifier: Modifier = Modifier,
     enabled: Boolean = true,
     onClick: () -> Unit,
-    startIcon: @Composable () -> Unit = {},
-    endIcon: @Composable () -> Unit = {},
-    content: @Composable () -> Unit,
+    margin: PaddingValues = PaddingValues(0.dp),
+    contentPadding: PaddingValues = PaddingValues(Dimens.dp4),
+    background: Color = Color.Transparent,
+    startIcon: @Composable RowScope.() -> Unit = {},
+    endIcon: @Composable RowScope.() -> Unit = {},
+    content: @Composable BoxScope.() -> Unit,
 ) {
     Row(
         modifier = Modifier
+            .fillMaxWidth()
+            .padding(margin)
             .clickable(
                 enabled = enabled,
                 onClick = onClick,
             )
-            .fillMaxWidth()
+            .background(background)
+            .padding(contentPadding)
             .then(modifier),
         verticalAlignment = Alignment.CenterVertically,
     ) {
@@ -53,7 +64,7 @@ fun PreviewClickableRow() {
                 vertical = Dimens.dp8
             ),
         endIcon = {
-            RightArrow()
+            IconRightArrow()
         },
         onClick = {  }
     ) {
