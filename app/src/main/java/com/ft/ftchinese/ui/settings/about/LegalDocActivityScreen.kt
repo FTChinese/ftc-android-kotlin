e27f78882532f4ea8951fdd737e251e6155bb970
weiguo.ni <neefrankie@gmail.com>
2022-06-17 21:41:10 +0800

    refactor: use WebpageScreen

---
+++ app/src/main/java/com/ft/ftchinese/ui/settings/about/LegalDocActivityScreen.kt
@@ -0,0 +1,29 @@
+package com.ft.ftchinese.ui.settings.about
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import com.ft.ftchinese.model.legal.legalPages
+import com.ft.ftchinese.ui.base.toast
+import com.ft.ftchinese.ui.webpage.WebpageScreen
+
+@Composable
+fun LegalDocActivityScreen(
+    index: Int,
+    onClose: () -> Unit,
+) {
+    val context = LocalContext.current
+    val page = remember(index) {
+        legalPages.getOrNull(index = index)
+    }
+
+    if (page == null) {
+        context.toast("Data not found!")
+        return
+    }
+
+    WebpageScreen(
+        pageMeta = page,
+        onClose = onClose,
+    )
+}
