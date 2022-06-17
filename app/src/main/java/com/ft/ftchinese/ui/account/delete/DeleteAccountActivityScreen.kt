6281dad5277173e35676e8a5ca265910260039ca
weiguo.ni <neefrankie@gmail.com>
2022-06-17 20:00:52 +0800

    refactor: sending email

--- app/src/main/java/com/ft/ftchinese/ui/account/delete/DeleteAccountActivityScreen.kt
+++ app/src/main/java/com/ft/ftchinese/ui/account/delete/DeleteAccountActivityScreen.kt
@@ -1,6 +1,5 @@
 package com.ft.ftchinese.ui.account.delete
 
-import android.content.Context
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ScaffoldState
 import androidx.compose.runtime.Composable
@@ -8,13 +7,10 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.ft.ftchinese.R
-import com.ft.ftchinese.model.enums.Edition
-import com.ft.ftchinese.model.reader.Account
 import com.ft.ftchinese.model.request.AccountDropped
 import com.ft.ftchinese.model.request.EmailPasswordParams
-import com.ft.ftchinese.ui.util.IntentsUtil
 import com.ft.ftchinese.ui.components.ProgressLayout
-import com.ft.ftchinese.ui.formatter.FormatHelper
+import com.ft.ftchinese.ui.util.IntentsUtil
 import com.ft.ftchinese.viewmodel.UserViewModel
 
 @Composable
@@ -45,7 +41,7 @@ fun DeleteAccountActivityScreen(
                 AlertDeleteDenied(
                     onDismiss = uiState::resetDropped,
                     onConfirm = {
-                        val ok = sendEmail(
+                        val ok = IntentsUtil.sendDeleteAccountEmail(
                             context = context,
                             account = account
                         )
@@ -78,44 +74,3 @@ fun DeleteAccountActivityScreen(
     }
 }
 
-private fun sendEmail(
-    context: Context,
-    account: Account,
-): Boolean {
-    val intent = IntentsUtil.emailCustomerService(
-        title = context.getString(R.string.subject_delete_account_valid_subs),
-        body = composeDeletionEmail(context, account)
-    )
-
-    return if (intent.resolveActivity(context.packageManager) != null) {
-        context.startActivity(intent)
-        true
-    } else {
-        false
-    }
-}
-
-private fun composeDeletionEmail(
-    context: Context,
-    a: Account
-): String? {
-    val emailOrMobile = if (a.isMobileEmail) {
-        a.mobile
-    } else {
-        a.email
-    }
-
-    if (a.membership.tier == null || a.membership.cycle == null) {
-        return null
-    }
-
-    val edition = FormatHelper.formatEdition(
-        context,
-        Edition(
-            tier = a.membership.tier,
-            cycle = a.membership.cycle,
-        )
-    )
-
-    return "FT中文网，\n请删除我的账号 $emailOrMobile。\n我的账号已经购买了FT中文网付费订阅服务 $edition，到期时间 ${a.membership.localizeExpireDate()}。我已知悉删除账号的同时将删除我的订阅信息。"
-}
