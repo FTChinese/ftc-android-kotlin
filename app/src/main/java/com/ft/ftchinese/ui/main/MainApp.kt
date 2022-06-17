53f2fae1c9b000558c7be2aced1cba196e550fe8
weiguo.ni <neefrankie@gmail.com>
2022-06-17 20:08:22 +0800

    refactor: removed drawer

--- app/src/main/java/com/ft/ftchinese/ui/main/MainApp.kt
+++ app/src/main/java/com/ft/ftchinese/ui/main/MainApp.kt
@@ -3,81 +3,76 @@ package com.ft.ftchinese.ui.main
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.ft.ftchinese.R
+import com.ft.ftchinese.ui.components.IconSearch
+import com.ft.ftchinese.ui.main.home.MainBottomBar
+import com.ft.ftchinese.ui.main.home.MainNavScreen
+import com.ft.ftchinese.ui.search.SearchableActivity
 import com.ft.ftchinese.ui.theme.Dimens
 import com.ft.ftchinese.ui.theme.OColor
-import kotlinx.coroutines.launch
 
 @Composable
 fun MainApp() {
-    val scope = rememberCoroutineScope()
-    val drawerState = rememberDrawerState(DrawerValue.Closed)
-    val scaffoldState = rememberScaffoldState(
-        drawerState = drawerState
-    )
+
+    val context = LocalContext.current
+    val scaffoldState = rememberScaffoldState()
     val navController = rememberNavController()
     val backstackEntry = navController.currentBackStackEntryAsState()
     
-    val currentScreen = BottomNavScreen.fromRoute(
+    val currentScreen = MainNavScreen.fromRoute(
         backstackEntry.value?.destination?.route
     )
 
     Scaffold(
-        drawerContent = {
-            Button(
-                modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(top = 16.dp),
-                onClick = { scope.launch { drawerState.close() } },
-                content = { Text("Close Drawer") }
-            )
-        },
         topBar = {
-            TopAppBar(
-                title = {
-                    if (currentScreen == BottomNavScreen.News) {
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_menu_masthead), 
-                            contentDescription = "",
-                            contentScale = ContentScale.Fit
-                        )
-                    } else {
-                        Text(text = stringResource(id = currentScreen.titleId))
+            if (currentScreen.showTopBar) {
+                TopAppBar(
+                    title = {
+                        if (currentScreen.showTopBar) {
+                            when (currentScreen) {
+                                MainNavScreen.News -> {
+                                    Image(
+                                        painter = painterResource(id = R.drawable.ic_menu_masthead),
+                                        contentDescription = "",
+                                        contentScale = ContentScale.Fit
+                                    )
+                                }
+                                else -> {
+                                    Text(text = stringResource(id = currentScreen.titleId))
+                                }
+                            }
+                        }
+                    },
+                    elevation = Dimens.dp4,
+                    backgroundColor = OColor.wheat,
+                    actions = {
+                        IconButton(
+                            onClick = {
+                                SearchableActivity.start(context)
+                            }
+                        ) {
+                            IconSearch()
+                        }
                     }
-                },
-                navigationIcon = {
-                    MenuIcon {
-                        scope.launch { drawerState.open() }
-                    }
-                },
-                elevation = Dimens.dp4,
-                backgroundColor = OColor.wheat,
-                actions = {
-                    SearchIcon {
+                )
+            }
 
-                    }
-                }
-            )
         },
         // See https://developer.android.com/jetpack/compose/navigation#bottom-nav
         bottomBar = {
-            BottomNavView(
+            MainBottomBar(
                 onClick = { screen ->
                     navController.navigate(screen.route) {
                         popUpTo(navController.graph.findStartDestination().id) {
@@ -94,54 +89,39 @@ fun MainApp() {
     ) { innerPadding ->
         NavHost(
             navController = navController,
-            startDestination = BottomNavScreen.News.route,
+            startDestination = MainNavScreen.News.route,
             modifier = Modifier.padding(innerPadding)
         ) {
             composable(
-                route = BottomNavScreen.News.route
+                route = MainNavScreen.News.route
             ) {
                 Text(text = "News")
             }
             
             composable(
-                route = BottomNavScreen.English.route
+                route = MainNavScreen.English.route
             ) {
                 Text(text = "English")
             }
             
             composable(
-                route = BottomNavScreen.FtAcademy.route
+                route = MainNavScreen.FtAcademy.route
             ) {
                 Text(text = "FT Academy")
             }
             
             composable(
-                route = BottomNavScreen.Video.route
+                route = MainNavScreen.Video.route
             ) {
                 Text(text = "Video")
             }
             
             composable(
-                route = BottomNavScreen.MyFt.route
+                route = MainNavScreen.MyFt.route
             ) {
                 Text(text = "My Ft")
             }
         }
-    }
-}
-
-@Composable
-private fun MenuIcon(
-    onClick: () -> Unit
-) {
-    IconButton(
-        onClick = onClick
-    ) {
-        Icon(
-            imageVector = Icons.Filled.Menu,
-            contentDescription = "Menu",
-            tint = OColor.black90,
-        )
     }
 }
 
