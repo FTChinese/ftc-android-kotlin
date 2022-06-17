24a75920d3168a74b88cc7a1cec0170162b8e116
weiguo.ni <neefrankie@gmail.com>
2022-06-17 20:08:43 +0800

    feat: MyFtScreen

---
+++ app/src/main/java/com/ft/ftchinese/ui/myft/MyFtScreen.kt
@@ -0,0 +1,152 @@
+package com.ft.ftchinese.ui.myft
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ft.ftchinese.BuildConfig
+import com.ft.ftchinese.R
+import com.ft.ftchinese.ui.components.*
+import com.ft.ftchinese.ui.test.TestActivity
+import com.ft.ftchinese.ui.theme.Dimens
+
+enum class MyFtRow(
+    @StringRes val textId: Int,
+) {
+    Account(R.string.title_account),
+    Paywall(R.string.title_subscription),
+    MySubs(R.string.title_my_subs),
+    ReadHistory(R.string.myft_reading_history),
+    Bookmarked(R.string.myft_starred_articles),
+    Topics(R.string.myft_following);
+}
+
+val loggedInRows: List<MyFtRow> = listOf(
+    MyFtRow.Account,
+    MyFtRow.Paywall,
+    MyFtRow.MySubs,
+)
+
+val articleRows: List<MyFtRow> = listOf(
+    MyFtRow.ReadHistory,
+    MyFtRow.Bookmarked,
+    MyFtRow.Topics
+)
+
+fun buildMyFtRows(loggedIn: Boolean): List<TableGroup<MyFtRow>> {
+    if (loggedIn) {
+        return listOf(
+            TableGroup(
+                header = "",
+                rows = loggedInRows
+            ),
+            TableGroup(
+                header = "",
+                rows = articleRows
+            )
+        )
+    }
+
+    return listOf(
+        TableGroup(
+            header = "",
+            rows = articleRows,
+        )
+    )
+}
+
+@Composable
+fun MyFtScreen(
+    loggedIn: Boolean,
+    avatarUrl: String?,
+    displayName: String?,
+    onLogin: () -> Unit,
+    onClick: (MyFtRow) -> Unit
+) {
+
+    val context = LocalContext.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(176.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.Bottom
+        ) {
+            Figure(
+                imageUrl = avatarUrl,
+                caption = displayName ?: ""
+            )
+        }
+
+        if (!loggedIn) {
+            PrimaryButton(
+                onClick = onLogin,
+                text = stringResource(id = R.string.title_login_signup),
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(Dimens.dp8))
+        Divider()
+
+        buildMyFtRows(loggedIn).forEachIndexed { index, tableGroup ->
+
+            if (index != 0) {
+                Divider(startIndent = Dimens.dp8)
+            }
+
+            tableGroup.rows.forEach { row ->
+                ClickableRow(
+                    onClick = { onClick(row) },
+                    endIcon = {
+                        IconRightArrow()
+                    },
+                    contentPadding = PaddingValues(Dimens.dp8)
+                ) {
+                    SubHeading2(text = stringResource(id = row.textId))
+                }
+            }
+        }
+
+        if (BuildConfig.DEBUG) {
+            Divider()
+            
+            ClickableRow(
+                onClick = { TestActivity.start(context) },
+                contentPadding = PaddingValues(Dimens.dp8),
+                endIcon = {
+                    IconRightArrow()
+                }
+            ) {
+                SubHeading2(text = "Test")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewMyFtScreen() {
+    MyFtScreen(
+        loggedIn = true,
+        avatarUrl = null,
+        displayName = "Hello",
+        onLogin = {},
+        onClick = {}
+    )
+}
