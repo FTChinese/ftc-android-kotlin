87a51f2ff8cf943a75eaac045802945d43e055c0
weiguo.ni <neefrankie@gmail.com>
2022-06-18 21:25:14 +0800

    refactor: cleanup

--- app/src/main/java/com/ft/ftchinese/ui/base/BaseViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.ft.ftchinese.ui.base
-
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-
-abstract class BaseViewModel: ViewModel() {
-    val progressLiveData = MutableLiveData<Boolean>()
-    val isNetworkAvailable = MutableLiveData<Boolean>()
-}
