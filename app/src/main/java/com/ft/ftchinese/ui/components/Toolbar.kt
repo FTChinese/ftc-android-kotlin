ee267d11e0bcfe374e97af58534715f56133d89d
weiguo.ni <neefrankie@gmail.com>
2022-06-18 09:51:55 +0800

    refactor: back button

--- app/src/main/java/com/ft/ftchinese/ui/components/Toolbar.kt
+++ app/src/main/java/com/ft/ftchinese/ui/components/Toolbar.kt
@@ -1,15 +1,11 @@
 package com.ft.ftchinese.ui.components
 
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ft.ftchinese.ui.theme.Dimens
@@ -18,9 +14,8 @@ import com.ft.ftchinese.ui.theme.OColor
 @Composable
 fun Toolbar(
     heading: String,
-    icon: ImageVector = Icons.Filled.ArrowBack,
     onBack: () -> Unit,
-    actions: @Composable RowScope.() -> Unit = {}
+    actions: @Composable (RowScope.() -> Unit) = {}
 ) {
     TopAppBar(
         title = {
@@ -30,11 +25,7 @@ fun Toolbar(
         },
         navigationIcon = {
             IconButton(onClick = onBack) {
-                Icon(
-                    imageVector = icon,
-                    "",
-                    tint = OColor.black90
-                )
+                IconArrowBackIOS()
             }
         },
         actions = actions,
