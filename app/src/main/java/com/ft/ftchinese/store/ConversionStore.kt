1c7e0a3dc6b7f520a49761872a238ed73ac4fa16
weiguo.ni <neefrankie@gmail.com>
2022-06-18 21:25:54 +0800

    refactor:

--- app/src/main/java/com/ft/ftchinese/store/ConversionStore.kt
+++ app/src/main/java/com/ft/ftchinese/store/ConversionStore.kt
@@ -4,23 +4,38 @@ import android.content.Context
 import androidx.core.content.edit
 import com.ft.ftchinese.service.KEY_DUR_END
 
-object ConversionStore {
-    private const val FILE_NAME = "com.ft.ftchinese.conversion"
-    private const val KEY_CAMPAIGN_ID = "campaign_id"
+private const val FILE_NAME = "com.ft.ftchinese.conversion"
+private const val KEY_CAMPAIGN_ID = "campaign_id"
 
-    fun save(ctx: Context, id: Long) {
-        ctx.getSharedPreferences(FILE_NAME, Context.MODE_PRIVATE).edit(commit = true) {
+class ConversionStore private constructor(context: Context) {
+    private val sharedPreferences = context.getSharedPreferences(
+        FILE_NAME,
+        Context.MODE_PRIVATE
+    )
+
+    fun save(id: Long) {
+        sharedPreferences.edit(commit = true) {
             putLong(KEY_CAMPAIGN_ID, id)
         }
     }
 
-    fun exists(ctx: Context): Boolean {
-        val id = ctx.getSharedPreferences(
-            FILE_NAME,
-            Context.MODE_PRIVATE
-        )
-            .getLong(KEY_DUR_END, 0)
+    fun exists(): Boolean {
+        val id = sharedPreferences.getLong(KEY_DUR_END, 0)
 
         return id > 0
     }
+
+    companion object {
+        private var instance: ConversionStore? = null
+
+        @Synchronized
+        fun getInstance(context: Context): ConversionStore {
+            if (instance == null) {
+                instance = ConversionStore(context.applicationContext)
+            }
+
+            return instance!!
+        }
+    }
 }
+
