95ab0e8fa5e3c3bf7cc00be6c5b49465390b2022
weiguo.ni <neefrankie@gmail.com>
2022-06-18 09:59:03 +0800

    feat: scollable tab for main screen

---
+++ app/src/main/java/com/ft/ftchinese/ui/main/home/ChannelPagerScreen.kt
@@ -0,0 +1,20 @@
+package com.ft.ftchinese.ui.main.home
+
+import androidx.compose.material.ScaffoldState
+import androidx.compose.runtime.Composable
+import com.ft.ftchinese.model.content.ChannelSource
+
+@Composable
+fun ChannelPagerScreen(
+    scaffoldState: ScaffoldState,
+    channelSource: List<ChannelSource>
+) {
+    ChannelPagerLayout(
+        pages = channelSource
+    ) {
+        ChannelTabScreen(
+            scaffoldState = scaffoldState,
+            channelSource = it
+        )
+    }
+}
