3225ffc1741b40ea3cdea8a326ad73c361cff104
weiguo.ni <neefrankie@gmail.com>
2022-06-18 11:47:49 +0800

    feat: optional navigation icon

--- app/src/main/java/com/ft/ftchinese/ui/main/home/MainToolBar.kt
+++ app/src/main/java/com/ft/ftchinese/ui/main/home/MainToolBar.kt
@@ -1,14 +1,19 @@
 package com.ft.ftchinese.ui.main.home
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.ft.ftchinese.R
+import com.ft.ftchinese.ui.components.IconArrowBackIOS
 import com.ft.ftchinese.ui.components.IconSearch
 import com.ft.ftchinese.ui.theme.Dimens
 import com.ft.ftchinese.ui.theme.OColor
@@ -16,7 +21,8 @@ import com.ft.ftchinese.ui.theme.OColor
 @Composable
 fun MainToolBar(
     screen: MainNavScreen,
-    onSearch: () -> Unit
+    onSearch: () -> Unit,
+    onBack: () -> Unit,
 ) {
     TopAppBar(
         title = {
@@ -25,7 +31,12 @@ fun MainToolBar(
                     Image(
                         painter = painterResource(id = R.drawable.ic_menu_masthead),
                         contentDescription = "",
-                        contentScale = ContentScale.Fit
+                        contentScale = ContentScale.FillHeight,
+                        modifier = Modifier
+                            .padding(
+                                vertical = Dimens.dp16
+                            )
+                            .fillMaxHeight()
                     )
                 }
                 else -> {
@@ -33,7 +44,18 @@ fun MainToolBar(
                 }
             }
         },
-        elevation = Dimens.dp4,
+        navigationIcon = if (!screen.showBottomBar && screen.showTopBar) {
+            {
+                IconButton(
+                    onClick = onBack
+                ) {
+                    IconArrowBackIOS()
+                }
+            }
+        } else {
+            null
+        },
+        elevation = Dimens.zero,
         backgroundColor = OColor.wheat,
         actions = {
             IconButton(
@@ -43,4 +65,16 @@ fun MainToolBar(
             }
         }
     )
+}
+
+
+@Preview
+@Composable
+fun PreviewMainToolbar() {
+    MainToolBar(
+        screen = MainNavScreen.News,
+        onSearch = { /*TODO*/ }
+    ) {
+
+    }
 }
