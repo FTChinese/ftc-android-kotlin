15d8b9e9ea2a3e18fbda51bde1f1f889d24a6834
weiguo.ni <neefrankie@gmail.com>
2022-06-19 13:07:22 +0800

    refactor: properly handle activity result after account changes

--- app/src/main/java/com/ft/ftchinese/ui/main/myft/MyFtActivityScreen.kt
+++ app/src/main/java/com/ft/ftchinese/ui/main/myft/MyFtActivityScreen.kt
@@ -4,15 +4,20 @@ import android.app.Activity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.ft.ftchinese.R
 import com.ft.ftchinese.ui.account.AccountActivity
 import com.ft.ftchinese.ui.auth.AuthActivity
 import com.ft.ftchinese.ui.main.home.MainNavScreen
 import com.ft.ftchinese.ui.settings.SettingsActivity
 import com.ft.ftchinese.ui.subs.MemberActivity
 import com.ft.ftchinese.ui.subs.SubsActivity
+import com.ft.ftchinese.ui.util.AccountAction
+import com.ft.ftchinese.ui.util.IntentsUtil
+import com.ft.ftchinese.ui.util.toast
 import com.ft.ftchinese.viewmodel.UserViewModel
 
 @Composable
@@ -21,16 +26,30 @@ fun MyFtActivityScreen(
     onNavigate: (MainNavScreen) -> Unit
 ) {
     val context = LocalContext.current
-    val accountState = userViewModel.accountLiveData.observeAsState()
-    val account = accountState.value
+    val account by userViewModel.accountLiveData.observeAsState()
+    val isLoggedIn = userViewModel.loggedInLiveData.observeAsState(false)
 
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult(),
     ) { result ->
         when (result.resultCode) {
             Activity.RESULT_OK -> {
+                // Note the account state enclosed here won't be updated.
                 val a = userViewModel.reloadAccount()
-                // TODO: we need to distinguish login/logou action.
+                result.data?.let {
+                    when (IntentsUtil.getAccountAction(it)) {
+                        AccountAction.LoggedOut -> {
+                            context.toast(R.string.action_logout)
+                        }
+                        AccountAction.SignedIn -> {
+                            context.toast(R.string.login_success)
+                        }
+                        AccountAction.Deleted -> {
+                            context.toast(R.string.message_account_deleted)
+                        }
+                        else -> {}
+                    }
+                }
             }
             Activity.RESULT_CANCELED -> {
 
@@ -39,7 +58,7 @@ fun MyFtActivityScreen(
     }
 
     MyFtScreen(
-        loggedIn = userViewModel.isLoggedIn,
+        loggedIn = isLoggedIn.value,
         avatarUrl = account?.wechat?.avatarUrl,
         displayName = account?.displayName,
         onLogin = {
@@ -63,6 +82,7 @@ fun MyFtActivityScreen(
                     )
                 }
                 MyFtRow.Settings -> {
+                    // Launch setting for result in case user clicked logout button.
                     SettingsActivity.launch(
                         launcher = launcher,
                         context = context
