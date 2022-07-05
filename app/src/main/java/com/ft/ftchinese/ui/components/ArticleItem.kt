3f578b6c89a960bdedb8eef7db852bf56083e571
weiguo.ni <neefrankie@gmail.com>
2022-07-05 16:23:36 +0800

    refactor: adapt for dark theme

--- app/src/main/java/com/ft/ftchinese/ui/components/ArticleItem.kt
+++ app/src/main/java/com/ft/ftchinese/ui/components/ArticleItem.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.em
 import com.ft.ftchinese.ui.theme.Dimens
-import com.ft.ftchinese.ui.theme.OColor
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -44,7 +43,6 @@ fun ArticleItem(
                 style = MaterialTheme
                     .typography
                     .body2,
-                color = OColor.black70,
                 lineHeight = 2.em
             )
         }
