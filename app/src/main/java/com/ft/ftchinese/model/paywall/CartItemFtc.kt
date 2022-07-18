abdf79308d72b6eae31e20e0db9834ded85def66
weiguo.ni <neefrankie@gmail.com>
2022-07-18 17:39:52 +0800

    feat: payablePrice and overriddenPrice

--- app/src/main/java/com/ft/ftchinese/model/paywall/CartItemFtc.kt
+++ app/src/main/java/com/ft/ftchinese/model/paywall/CartItemFtc.kt
@@ -3,12 +3,13 @@ package com.ft.ftchinese.model.paywall
 import com.ft.ftchinese.model.ftcsubs.Discount
 import com.ft.ftchinese.model.ftcsubs.Price
 import com.ft.ftchinese.model.ftcsubs.YearMonthDay
+import com.ft.ftchinese.model.reader.Membership
 
 data class CartItemFtc(
     val intent: CheckoutIntent,
     val price: Price,
     val discount:  Discount? = null,
-    val isIntro: Boolean
+    val isIntro: Boolean, // Indicating trial price.
 ) {
     fun normalizePeriod(): YearMonthDay {
         if (discount != null && !discount.overridePeriod.isZero()) {
@@ -20,5 +21,63 @@ data class CartItemFtc(
 
     fun payableAmount(): Double {
         return price.unitAmount - (discount?.priceOff ?: 0.0)
+    }
+
+    fun payablePrice(): PriceParts {
+        return PriceParts(
+            symbol = price.currency,
+            amount = if (discount != null) {
+                price.unitAmount - (discount.priceOff ?: 0.0)
+            } else {
+                  price.unitAmount
+            },
+            period = normalizePeriod(),
+            isRecurring = false,
+            highlighted = true,
+        )
+    }
+
+    fun overriddenPrice(): PriceParts? {
+        if (discount == null) {
+            return null
+        }
+
+        return PriceParts(
+            symbol = price.currency,
+            amount = price.unitAmount,
+            period = price.periodCount,
+            isRecurring = false,
+            highlighted = false,
+            crossed = true,
+        )
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun ofIntro(introPrice: Price?): CartItemFtc? {
+            return if (introPrice == null || !introPrice.isValid()) {
+                null
+            } else {
+                CartItemFtc(
+                    intent = CheckoutIntent.newMember,
+                    price = introPrice,
+                    discount = null,
+                    isIntro = true,
+                )
+            }
+        }
+
+        @JvmStatic
+        fun newInstance(price: Price, m: Membership): CartItemFtc {
+            val offerFilter = m.offerKinds
+
+            return CartItemFtc(
+                intent = CheckoutIntent.ofFtc(m, price),
+                price = price,
+                discount = price.filterOffer(offerFilter),
+                isIntro = price.isIntro && price.isValid()
+            )
+        }
     }
 }
--- app/src/main/java/com/ft/ftchinese/model/paywall/CartItemStripe.kt
+++ app/src/main/java/com/ft/ftchinese/model/paywall/CartItemStripe.kt
@@ -1,14 +1,54 @@
 package com.ft.ftchinese.model.paywall
 
-import android.os.Parcelable
 import com.ft.ftchinese.model.stripesubs.StripeCoupon
 import com.ft.ftchinese.model.stripesubs.StripePrice
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class CartItemStripe(
     val intent: CheckoutIntent,
     val recurring: StripePrice,
     val trial: StripePrice?,
     val coupon: StripeCoupon?,
-) : Parcelable
+) {
+    fun payablePrice(): PriceParts {
+        if (trial != null) {
+            PriceParts(
+                symbol = getCurrencySymbol(trial.currency),
+                amount = convertCent(trial.unitAmount),
+                period = trial.periodCount,
+                isRecurring = false,
+                highlighted = true
+            )
+        }
+
+        if (coupon != null) {
+            return PriceParts(
+                symbol = getCurrencySymbol(coupon.currency),
+                amount = convertCent(recurring.unitAmount - coupon.amountOff),
+                period = recurring.periodCount,
+                isRecurring = false,
+                highlighted = true,
+            )
+        }
+
+        return PriceParts(
+            symbol = getCurrencySymbol(recurring.currency),
+            amount = convertCent(recurring.unitAmount),
+            period = recurring.periodCount,
+            isRecurring = true,
+            highlighted = true,
+        )
+    }
+
+    fun overriddenPrice(): PriceParts? {
+        if (trial == null && coupon == null) {
+            return null
+        }
+
+        return PriceParts(
+            symbol = getCurrencySymbol(recurring.currency),
+            amount = convertCent(recurring.unitAmount),
+            period = recurring.periodCount,
+            isRecurring = true
+        )
+    }
+}
