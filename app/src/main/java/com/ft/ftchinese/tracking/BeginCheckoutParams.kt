5036c642e0b565056a8d0bafc531100a16f3f9b3
weiguo.ni <neefrankie@gmail.com>
2022-07-18 17:40:46 +0800

    refactor

--- app/src/main/java/com/ft/ftchinese/tracking/BeginCheckoutParams.kt
+++ app/src/main/java/com/ft/ftchinese/tracking/BeginCheckoutParams.kt
@@ -3,6 +3,7 @@ package com.ft.ftchinese.tracking
 import com.ft.ftchinese.model.enums.PayMethod
 import com.ft.ftchinese.model.paywall.CartItemFtc
 import com.ft.ftchinese.model.paywall.CartItemStripe
+import com.ft.ftchinese.model.paywall.convertCent
 
 data class BeginCheckoutParams(
     val value: Double,
@@ -23,9 +24,9 @@ data class BeginCheckoutParams(
         fun ofStripe(item: CartItemStripe): BeginCheckoutParams {
             return BeginCheckoutParams(
                 value = if (item.trial != null) {
-                    item.trial.moneyAmount
+                    convertCent(item.trial.unitAmount)
                 } else {
-                    item.recurring.moneyAmount
+                    convertCent((item.recurring.unitAmount))
                 },
                 currency = item.recurring.currency,
                 payMethod = PayMethod.STRIPE,
--- app/src/main/java/com/ft/ftchinese/tracking/PaySuccessParams.kt
+++ app/src/main/java/com/ft/ftchinese/tracking/PaySuccessParams.kt
@@ -4,6 +4,7 @@ import com.ft.ftchinese.model.enums.Edition
 import com.ft.ftchinese.model.enums.PayMethod
 import com.ft.ftchinese.model.ftcsubs.FtcPayIntent
 import com.ft.ftchinese.model.paywall.CartItemStripe
+import com.ft.ftchinese.model.paywall.convertCent
 
 data class PaySuccessParams(
     val edition: Edition,
@@ -28,9 +29,9 @@ data class PaySuccessParams(
                 edition = item.recurring.edition,
                 currency = item.recurring.currency,
                 amountPaid = if (item.trial != null) {
-                    item.trial.moneyAmount
+                    convertCent(item.trial.unitAmount)
                 } else {
-                    item.recurring.moneyAmount
+                    convertCent(item.recurring.unitAmount)
                 },
                 payMethod = PayMethod.STRIPE,
             )
