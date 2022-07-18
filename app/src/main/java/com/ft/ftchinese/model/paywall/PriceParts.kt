b2eb35e4cf454cb1ae24f52a992a025a1ab2bbab
weiguo.ni <neefrankie@gmail.com>
2022-07-18 17:37:12 +0800

    feat: convertCent; MoneyParts; PriceParts

--- app/src/main/java/com/ft/ftchinese/model/paywall/PriceParts.kt
+++ app/src/main/java/com/ft/ftchinese/model/paywall/PriceParts.kt
@@ -1,30 +1,40 @@
 package com.ft.ftchinese.model.paywall
 
+import com.ft.ftchinese.model.ftcsubs.YearMonthDay
 import java.util.*
+
+private val symbols = mapOf(
+    "cny" to "¥",
+    "usd" to "$",
+    "gbp" to "£",
+)
+
+fun getCurrencySymbol(currency: String): String {
+    return if (currency.isBlank()) {
+        ""
+    } else {
+        symbols[currency] ?: currency.uppercase(Locale.ROOT)
+    }
+}
+
+fun convertCent(amount: Int): Double {
+    return amount
+        .toBigDecimal()
+        .divide(100.toBigDecimal())
+        .toDouble()
+}
+
+data class MoneyParts(
+    val symbol: String,
+    val amount: Double,
+)
 
 data class PriceParts(
     val symbol: String,
-    val amount: String,
-    val cycle: String,
-    val notes: String = "",
-) {
-    fun string(): String {
-        return "${symbol}${amount}${cycle}"
-    }
-
-    companion object {
-        val symbols = mapOf(
-            "cny" to "¥",
-            "usd" to "$",
-            "gbp" to "£",
-        )
-
-        fun findSymbol(currency: String): String {
-            return if (currency.isBlank()) {
-                ""
-            } else {
-                symbols[currency] ?: currency.uppercase(Locale.ROOT)
-            }
-        }
-    }
-}
+    val amount: Double,
+    val separator: String = "/",
+    val period: YearMonthDay,
+    val isRecurring: Boolean,
+    val highlighted: Boolean = false,
+    val crossed: Boolean = false,
+)
