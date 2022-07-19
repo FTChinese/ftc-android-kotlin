043a58c46c39400b94fa22a41baa15c57270aa80
weiguo.ni <neefrankie@gmail.com>
2022-07-19 15:03:06 +0800

    feat: foramt date

---
+++ app/src/main/java/com/ft/ftchinese/ui/formatter/FormatDate.kt
@@ -0,0 +1,50 @@
+package com.ft.ftchinese.ui.formatter
+
+import android.content.Context
+import com.ft.ftchinese.R
+import com.ft.ftchinese.model.reader.AutoRenewMoment
+import org.threeten.bp.ZonedDateTime
+
+fun formatYearMonthDate(ctx: Context, date: ZonedDateTime): String {
+    return ctx.getString(
+        R.string.formatter_year_month_date,
+        date.year,
+        date.monthValue,
+        date.dayOfMonth,
+    )
+}
+
+fun formatMoment(ctx: Context, moment: AutoRenewMoment): String {
+    return if (moment.month != null) {
+        ctx.getString(
+            R.string.formatter_month_date,
+            moment.month,
+            moment.date
+        )
+    } else {
+        ctx.getString(
+            R.string.formatter_date,
+            moment.date
+        )
+    }
+}
+
+fun formatRedeemPeriod(ctx: Context, start: ZonedDateTime?, end: ZonedDateTime?): String {
+
+    return ctx.getString(
+        R.string.redeem_period,
+        start?.let {
+            formatYearMonthDate(ctx, it)
+        } ?: "",
+        end?.let {
+            formatYearMonthDate(ctx, it)
+        } ?: "",
+    )
+}
+
+fun formatCouponEnjoyed(ctx: Context, date: ZonedDateTime): String {
+    return ctx.getString(
+        R.string.coupon_enjoyed,
+        formatYearMonthDate(ctx, date)
+    )
+}
