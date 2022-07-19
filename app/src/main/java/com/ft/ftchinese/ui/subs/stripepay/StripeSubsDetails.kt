407d5090a6ea02d765d69592ced105865ac0d68b
weiguo.ni <neefrankie@gmail.com>
2022-07-19 15:05:41 +0800

    feat: show coupon for apply coupon intent after success

--- app/src/main/java/com/ft/ftchinese/ui/subs/stripepay/StripeSubsDetails.kt
+++ app/src/main/java/com/ft/ftchinese/ui/subs/stripepay/StripeSubsDetails.kt
@@ -1,27 +1,36 @@
 package com.ft.ftchinese.ui.subs.stripepay
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import com.ft.ftchinese.R
 import com.ft.ftchinese.model.enums.Cycle
 import com.ft.ftchinese.model.enums.StripeSubStatus
 import com.ft.ftchinese.model.enums.Tier
+import com.ft.ftchinese.model.fetch.formatLocalDate
+import com.ft.ftchinese.model.stripesubs.StripeCoupon
 import com.ft.ftchinese.model.stripesubs.StripeSubs
+import com.ft.ftchinese.ui.components.Heading3
 import com.ft.ftchinese.ui.components.ListItemTwoCol
+import com.ft.ftchinese.ui.components.SubHeading1
 import com.ft.ftchinese.ui.formatter.FormatHelper
+import com.ft.ftchinese.ui.formatter.formatAmountOff
 import com.ft.ftchinese.ui.theme.Dimens
 import com.ft.ftchinese.ui.theme.OColor
 import org.threeten.bp.ZonedDateTime
-import org.threeten.bp.format.DateTimeFormatter
 
 @Composable
 fun StripeSubsDetails(
-    subs: StripeSubs
+    subs: StripeSubs,
+    coupon: StripeCoupon?
 ) {
     val context = LocalContext.current
 
@@ -29,12 +38,20 @@ fun StripeSubsDetails(
         modifier = Modifier
             .padding(top = Dimens.dp16)
     ) {
-        Text(
+        Heading3(
             text = "订阅成功",
-            modifier = Modifier.fillMaxWidth(),
             textAlign = TextAlign.Center,
-            style = MaterialTheme.typography.h6
         )
+
+        if (coupon != null) {
+            SubHeading1(
+                text = stringResource(
+                    R.string.coupon_redeemed,
+                    formatAmountOff(context, coupon.moneyParts)
+                )
+            )
+        }
+
         ListItemTwoCol(
             lead = "订阅方案",
             tail = FormatHelper.getTier(context, subs.tier)
@@ -47,11 +64,11 @@ fun StripeSubsDetails(
         )
         ListItemTwoCol(
             lead = "开始时间",
-            tail = subs.currentPeriodStart.format(DateTimeFormatter.ISO_LOCAL_DATE)
+            tail = formatLocalDate(subs.currentPeriodStart)
         )
         ListItemTwoCol(
             lead = "结束时间",
-            tail = subs.currentPeriodEnd.format(DateTimeFormatter.ISO_LOCAL_DATE)
+            tail = formatLocalDate(subs.currentPeriodEnd)
         )
 
         Text(
@@ -68,16 +85,27 @@ fun StripeSubsDetails(
 @Preview(showBackground = true)
 @Composable
 fun PreviewSubsDetails() {
-    StripeSubsDetails(subs = StripeSubs(
-        id = "",
-        tier = Tier.STANDARD,
-        cycle = Cycle.YEAR,
-        cancelAtPeriodEnd = false,
-        currentPeriodStart = ZonedDateTime.now(),
-        currentPeriodEnd = ZonedDateTime.now().plusYears(1),
-        customerId = "",
-        latestInvoiceId = "",
-        liveMode = false,
-        status = StripeSubStatus.Active,
-    ))
+    StripeSubsDetails(
+        subs = StripeSubs(
+            id = "",
+            tier = Tier.STANDARD,
+            cycle = Cycle.YEAR,
+            cancelAtPeriodEnd = false,
+            currentPeriodStart = ZonedDateTime.now(),
+            currentPeriodEnd = ZonedDateTime.now().plusYears(1),
+            customerId = "",
+            latestInvoiceId = "",
+            liveMode = false,
+            status = StripeSubStatus.Active,
+        ),
+        coupon = StripeCoupon(
+            id = "coupon-id",
+            amountOff = 10,
+            currency = "gbp",
+            redeemBy = 0,
+            priceId = "attached-price-id",
+            startUtc = ZonedDateTime.now(),
+            endUtc = ZonedDateTime.now().plusDays(7)
+        )
+    )
 }
