4d474b1c4f8e995e0953673b94a8d32f069c15dc
weiguo.ni <neefrankie@gmail.com>
2022-07-20 14:07:52 +0800

    refactor: formatting

--- app/src/main/java/com/ft/ftchinese/ui/components/ListItemIconText.kt
+++ app/src/main/java/com/ft/ftchinese/ui/components/ListItemIconText.kt
@@ -1,8 +1,6 @@
 package com.ft.ftchinese.ui.components
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -24,7 +22,14 @@ fun ListItemIconText(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier.fillMaxWidth(),
     ) {
-        Icon(painter = icon, contentDescription = "", tint = iconTint)
+        Icon(
+            painter = icon,
+            contentDescription = "",
+            tint = iconTint
+        )
+
+        Spacer(modifier = Modifier.width(Dimens.dp4))
+
         Text(
             text = text,
             modifier = Modifier.padding(start = Dimens.dp16),
