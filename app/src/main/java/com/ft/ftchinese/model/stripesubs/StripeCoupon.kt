78ae0a6005e76e90391e86d26b05119b18739f57
weiguo.ni <neefrankie@gmail.com>
2022-07-20 16:40:09 +0800

    refactor: check is coupon status is active

--- app/src/main/java/com/ft/ftchinese/model/stripesubs/StripeCoupon.kt
+++ app/src/main/java/com/ft/ftchinese/model/stripesubs/StripeCoupon.kt
@@ -1,5 +1,6 @@
 package com.ft.ftchinese.model.stripesubs
 
+import com.ft.ftchinese.model.enums.DiscountStatus
 import com.ft.ftchinese.model.paywall.MoneyParts
 import com.ft.ftchinese.model.paywall.convertCent
 import com.ft.ftchinese.model.paywall.getCurrencySymbol
@@ -18,10 +19,15 @@ data class StripeCoupon(
     val startUtc: ZonedDateTime? = null,
     @Serializable(with = DateTimeAsStringSerializer::class)
     val endUtc: ZonedDateTime? = null,
+    val status: DiscountStatus? = null,
 ) {
     fun isValid(): Boolean {
         if (startUtc == null || endUtc == null) {
             return amountOff > 0
+        }
+
+        if (status != DiscountStatus.Active) {
+            return false
         }
 
         val now = ZonedDateTime.now()
