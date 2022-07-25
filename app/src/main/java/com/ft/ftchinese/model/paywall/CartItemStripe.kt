208b164492bf673824b2b5adc720debfaaf34bb0
weiguo.ni <neefrankie@gmail.com>
2022-07-25 16:19:33 +0800

    refactor: add recurPrice

--- app/src/main/java/com/ft/ftchinese/model/paywall/CartItemStripe.kt
+++ app/src/main/java/com/ft/ftchinese/model/paywall/CartItemStripe.kt
@@ -18,46 +18,13 @@ data class CartItemStripe(
     val isForbidden: Boolean
         get() = intent.kind == IntentKind.Forbidden
 
-    fun payablePrice(): PriceParts {
-        if (trial != null) {
-            PriceParts(
-                symbol = getCurrencySymbol(trial.currency),
-                amount = convertCent(trial.unitAmount),
-                period = trial.periodCount,
-                isRecurring = false,
-                highlighted = true
-            )
-        }
-
-        if (coupon != null) {
-            return PriceParts(
-                symbol = getCurrencySymbol(coupon.currency),
-                amount = convertCent(recurring.unitAmount - coupon.amountOff),
-                period = recurring.periodCount,
-                isRecurring = false,
-                highlighted = true,
-            )
-        }
-
+    fun recurPrice(): PriceParts {
         return PriceParts(
             symbol = getCurrencySymbol(recurring.currency),
             amount = convertCent(recurring.unitAmount),
             period = recurring.periodCount,
             isRecurring = true,
             highlighted = true,
-        )
-    }
-
-    fun overriddenPrice(): PriceParts? {
-        if (trial == null && coupon == null) {
-            return null
-        }
-
-        return PriceParts(
-            symbol = getCurrencySymbol(recurring.currency),
-            amount = convertCent(recurring.unitAmount),
-            period = recurring.periodCount,
-            isRecurring = true
         )
     }
 
