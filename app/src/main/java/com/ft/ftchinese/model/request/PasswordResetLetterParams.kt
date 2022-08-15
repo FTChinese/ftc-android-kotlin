fbaff0eea66b93b107c9a82ab7e7ed982faeeeda
weiguo.ni <neefrankie@gmail.com>
2022-08-15 14:49:44 +0800

    fix: change useCode to non-default since kotlin refuses to serialize default values

--- app/src/main/java/com/ft/ftchinese/model/request/PasswordResetLetterParams.kt
+++ app/src/main/java/com/ft/ftchinese/model/request/PasswordResetLetterParams.kt
@@ -5,5 +5,5 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class PasswordResetLetterParams(
     val email: String,
-    val useCode: Boolean = true, // On mobile apps use a code rather than link.
+    val useCode: Boolean, // On mobile apps use a code rather than link.
 )
