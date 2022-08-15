de732c7138ca5504e1cf25e78dcdf283a5f2a40c
weiguo.ni <neefrankie@gmail.com>
2022-08-15 14:47:54 +0800

    fix: add encodeDefaults = true to kotlin serialization's setting

--- app/src/main/java/com/ft/ftchinese/model/fetch/json.kt
+++ app/src/main/java/com/ft/ftchinese/model/fetch/json.kt
@@ -4,4 +4,10 @@ import kotlinx.serialization.json.Json
 
 val marshaller = Json {
     ignoreUnknownKeys = true
+    // This is important.
+    // The default false value is not compatible with Golang's unmarshal mechanism.
+    // For instance, if you have a default value `true`,
+    // Kotlin will ignore it. When request sent to Go,
+    // the zero value is false.
+    encodeDefaults = true
 }
