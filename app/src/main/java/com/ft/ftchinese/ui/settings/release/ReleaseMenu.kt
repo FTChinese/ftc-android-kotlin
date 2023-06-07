d258c02dacc325cd8d2394bc1d8841138a56ebb7
weiguo.ni <neefrankie@gmail.com>
2023-06-07 16:09:12 +0800

    refactor: add refresh row and switch

--- app/src/main/java/com/ft/ftchinese/ui/settings/release/ReleaseMenu.kt
+++ app/src/main/java/com/ft/ftchinese/ui/settings/release/ReleaseMenu.kt
@@ -3,6 +3,7 @@ package com.ft.ftchinese.ui.settings.release
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,12 +11,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.ft.ftchinese.ui.components.ClickableRow
 import com.ft.ftchinese.ui.components.IconDelete
 import com.ft.ftchinese.ui.components.IconFolderOpen
+import com.ft.ftchinese.ui.components.IconRefresh
 import com.ft.ftchinese.ui.theme.Dimens
 
 @Composable
 fun ReleaseMenu(
-    onClickDelete: () -> Unit,
-    onViewDownloads: () -> Unit
+    checkOnLaunch: Boolean,
+    onToggleCheck: (Boolean) -> Unit,
+    onDeleteDownload: () -> Unit,
+    onOpenDownloadsFolder: () -> Unit,
+    onRefreshRelease: () -> Unit,
+    progress: Boolean
 ) {
     
     Column(
@@ -23,28 +29,56 @@ fun ReleaseMenu(
     ) {
 
         ClickableRow(
-            onClick = onClickDelete,
-            startIcon = {
+            onClick = {  },
+            contentPadding = PaddingValues(Dimens.dp16),
+            endIcon = {
+                Switch(
+                    checked = checkOnLaunch,
+                    onCheckedChange = onToggleCheck,
+                )
+            }
+        ) {
+            Text(
+                text = "App启动时检查新版本",
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+
+        ClickableRow(
+            onClick = onOpenDownloadsFolder,
+            contentPadding = PaddingValues(Dimens.dp16),
+            endIcon = {
+                IconFolderOpen()
+            }
+        ) {
+            Text(
+                text = "打开下载文件夹",
+            )
+        }
+
+        ClickableRow(
+            onClick = onDeleteDownload,
+            endIcon = {
                 IconDelete()
             },
             contentPadding = PaddingValues(Dimens.dp16)
         ) {
             Text(
                 text = "删除下载的文件",
-
             )
         }
 
         ClickableRow(
-            onClick = onViewDownloads,
-            startIcon = {
-                IconFolderOpen()
+            onClick = onRefreshRelease,
+            endIcon = {
+                IconRefresh()
             },
-            contentPadding = PaddingValues(Dimens.dp16)
+            contentPadding = PaddingValues(Dimens.dp16),
+            enabled = !progress
         ) {
             Text(
-                text = "打开下载文件夹",
-
+                text = "重新检查",
             )
         }
     }
@@ -54,8 +88,11 @@ fun ReleaseMenu(
 @Composable
 fun PreviewReleaseMenu() {
     ReleaseMenu(
-        onClickDelete = {}
-    ) {
-
-    }
+        onDeleteDownload = {},
+        onOpenDownloadsFolder = {},
+        onToggleCheck = {},
+        onRefreshRelease = {},
+        checkOnLaunch = true,
+        progress = false,
+    )
 }
